26181f9e66d8984b7bba80b30d05390f3d10280f
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-06-03 12:30:49 +0900

    update

---
+++ baekjoonn.py
@@ -0,0 +1,16 @@
+A = [list(input()) for _ in range(5)]
+len1 = 0
+
+for i in range(5):
+    if len(A[0:][i]) > len1:
+         len1 = len(A[0:][i])
+
+print(A)
+print(len1)
+answer = []
+
+for k in range(len1):
+    for j in range(5):
+        answer.append(A[j][k])
+
+print(answer)
---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = map(int, input())
+b= a + int(543)
+print(b)
