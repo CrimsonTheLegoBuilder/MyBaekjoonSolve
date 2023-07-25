b2b3bf8d0555c25beca95f6d6c58a599314345f2
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-07-26 00:16:47 +0900

    2307260016

---
+++ bj4101.py
@@ -0,0 +1,10 @@
+import sys
+
+while True:
+    a, b = map(int, sys.stdin.readline().strip().split())
+    if not a and not b:
+        exit(0)
+    if a > b:
+        print('YES')
+    else:
+        print('NO')
