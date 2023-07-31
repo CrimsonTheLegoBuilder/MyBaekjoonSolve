9322219cd7684438859ef740ff7436c645be4c7c
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-07-31 23:43:17 +0900

    2307312343

---
+++ bj1147_picnic.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def rhombus(x, y):
+    global N, M, ans
+    if x == y:
+        # print(x, y)
+        ans += (N - y + 1) * (M - x + 1) * (x + (x - 1)//2 * 2)
+        # print((N - y + 1) * (M - x + 1) * x)
+    elif x > y:
+        for i in range(1, x):
+            if y**2 + i**2 == (x - i)**2:
+                # print(x, y)
+                ans += (N - y + 1) * (M - x + 1) * 2
+                # print((N - y + 1) * (M - x + 1))
+    elif x < y:
+        for j in range(1, y):
+            if x**2 + j**2 == (y - j)**2:
+                ans += (N - y + 1) * (M - x + 1) * 2
+    if x != y and x % 2 == 0 and y % 2 == 0:
+        # print(x, y)
+        ans += (N - y + 1) * (M - x + 1)
+        # print((N - y + 1) * (M - x + 1))
+    return 0
+
+
+N, M = map(int, sys.stdin.readline().strip().split())
+ans = 0
+
+for X in range(1, M + 1):
+    for Y in range(1, N + 1):
+        rhombus(X, Y)
+
+print(ans)
--- bj1438.py
+++ bj1438.py
@@ -1,15 +1,38 @@
 import sys
-from itertools import combinations_with_replacement
+
+
+def dfs(o):
+    global ans
+    if len(combi) == 2:
+        y_s = sorted([d[1] for d in dots if combi[0] <= d[0] <= combi[1]])
+        if len(y_s) < n//2:
+            return
+        for k in range(len(y_s) - n//2 + 1):
+            ans = min(ans, (combi[1] - combi[0] + 2) * (y_s[k + n//2 - 1] - y_s[k] + 2))
+            # print(ans)
+        return
+    for o1 in range(o, len(X)):
+        combi.append(X[o1])
+        dfs(o1)
+        combi.pop()
+
 
 n = int(sys.stdin.readline().strip())
-dots = [tuple(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+X_ = []
+X = []
+combi = []
+dots = []
 ans = int(9e12)
+for _ in range(n):
+    x, y = map(int, sys.stdin.readline().strip().split())
+    dots.append((x, y))
+    X_.append(x)
 
-Y = sorted(list(set(d[1] for d in dots)))
-# for a, b in combinations(tuple(range(n)), 3):
-for y1, y2 in combinations_with_replacement(Y, 2):
-    X = sorted([d[0] for d in dots if y1 <= d[1] <= y2])
-    for i in range(len(X) - n//2 + 1):
-        ans = min(ans, (y2 - y1 + 2) * (X[i + n//2 - 1] - X[i] + 2))
+X_.sort()
+X.append(X_[0])
+for i in range(1, n):
+    if X_[i] != X_[i - 1]:
+        X.append(X_[i])
 
+dfs(0)
 print(ans)
