a80f10c1ac2cc314ba6dddd30e8f72c938e75a33
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-07-31 20:34:47 +0900

    2307312034

--- __test_1.py
+++ __test_1.py
@@ -1,16 +1,51 @@
-def facto(n):
-    if n <= 1:
-        return 1
-    return n * facto(n - 1)
+# def summation(a):
+#     if a < 10:
+#         return a
+#     s = a % 10
+#     d = a // 10
+#     return s + summation(d)
+#
+#
+# target = int(input())
+# print(summation(target))
 
 
-def fibo(n):
-    if n <= 1:
-        return n
-    return fibo(n - 1) + fibo(n - 2)
+a = int(input())
+i = 2
+flag = 0
 
+while i < a:
+    if not a % i:
+        print(a % i)
+        print(-1)
+        flag = 1
+        # exit(0)
+        break
+    i += 1
 
-print(facto(10))
-print(fibo(10))
+if not flag:
+    print(1)
 
+# b = [[1, 2, 3], [3, 4, 5], [4, 5, 6]]
+# a = [sum(b[i]) for i in range(3)]
+# print(*a)
 
+# n = int(input())
+# arr = [1] * (n + 1)
+# for i in range(2, int(n**.5) + 2):
+#     if arr[i] == 1:
+#         for j in range(i * 2, n + 1, i):
+#             arr[j] = 0
+#
+# print(arr[n])
+
+arr = [[1, 2, 3], [2, 3, 4], [4, 5, 6]]
+ans = []
+n = int(input())
+
+for i in range(3):
+    for j in range(3):
+        if arr[i][j] == n:
+            ans.append((i, j))
+
+print(ans)
