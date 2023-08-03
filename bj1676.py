cf147874eddadcdcc2acafab349f930fb10ea9ba
Crimson_Jin <jjhk192@naver.com>
2023-08-03 10:24:50 +0900

    bj1676

---
+++ bj1676.py
@@ -0,0 +1,17 @@
+import sys
+n = int(sys.stdin.readline().strip())
+c = n // 125
+rs = n % 125
+s = rs // 25
+r5 = rs % 25
+a = r5 // 5
+print(31 * c + 6*s + a)
+
+
+def facto(x):
+    if x <= 1:
+        return 1
+    return x * facto(x-1)
+
+
+print(str(facto(n))[-(31 * c + 6*s + a) - 1:])
