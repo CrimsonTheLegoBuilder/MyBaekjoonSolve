6de7b3f36f68762299e2a556f31f6180bf99f201
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-08-12 01:03:58 +0900

    2308120103

--- bj1433.py
+++ bj1433.py
@@ -1,14 +1,14 @@
 import sys
 
 N, target = map(int, sys.stdin.readline().strip().split())
-high_sl = 0.0
-high_sv = 0.0
-low_sl = 0.0
-low_sv = 0.0
+high_sl = 0.0  # high concentration solute
+high_sv = 0.0  # high concentration solution (solvent)
+low_sl = 0.0  # low concentration solute
+low_sv = 0.0  # low concentration solution
 ans = 0.0
 for _ in range(N):
-    c, sv = map(int, sys.stdin.readline().strip().split())
-    sl = sv * c / 100.0
+    c, sv = map(int, sys.stdin.readline().strip().split())  # concentration, solution
+    sl = sv * c / 100.0  # amount ot solute
     if c == target:
         ans += sv
     elif c > target:
@@ -17,9 +17,9 @@ for _ in range(N):
     else:
         low_sl += sl
         low_sv += sv
-print(high_sv, high_sl, low_sv, low_sl)
+# print(high_sv, high_sl, low_sv, low_sl)
 
-if low_sv < 0.000001:
+if low_sv < 0.000001:  # if low concentration solution doesn't exist:
     print(ans)
     exit(0)
 x = (100 * high_sl - target * high_sv) / (target * low_sv - 100 * low_sl)
