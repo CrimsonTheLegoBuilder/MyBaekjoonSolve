7fb7b557523a5a93f3fdc283181d77b81b5bd034
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-08-14 07:48:27 +0900

    2308140748

--- __dictionary_bj11975.py
+++ __dictionary_bj11975.py
@@ -32,3 +32,74 @@ E = len(dic_e)
 ans = 1 + E - V
 # print(E, V)
 print(ans)
+
+# x,y,v,e=0,0,{},{}
+# N=int(input())
+# d=input()
+# for i in range(N):
+#  a=v.setdefault((x,y),i)
+#  o=d[i]
+#  if o=='N':y+=1
+#  if o=='E':x+=1
+#  if o=='W':x-=1
+#  if o=='S':y-=1
+#  b=v.setdefault((x,y),i+1)
+#  e[(min(a,b),max(a,b))]=0
+# print(1+len(e)-len(v))
+#
+# x,y,v,e=0,0,{},{}
+# N=int(input())
+# O=input()
+# for o in O:
+#  a=v.setdefault((x,y),N)
+#  if o=='N':y+=1
+#  if o=='E':x+=1
+#  if o=='W':x-=1
+#  if o=='S':y-=1
+#  N-=1
+#  b=v.setdefault((x,y),N)
+#  e[(min(a,b),max(a,b))]=0
+# print(1+len(e)-len(v))
+#
+# x,y,v,e=0,0,{(0,0):0},{}
+# N=int(input())
+# O=input()
+# for o in O:
+#  a=v[(x,y)]
+#  if o=='N':y+=1
+#  if o=='E':x+=1
+#  if o=='W':x-=1
+#  if o=='S':y-=1
+#  N+=1
+#  b=v.setdefault((x,y),N)
+#  e[(min(a,b),max(a,b))]=0
+# print(1+len(e)-len(v))
+#
+# x,y,v,e=0,0,{(0,0):0},{}
+# N=int(input())
+# O=input()
+# for o in O:
+#  a=v[(x,y)]
+#  if o=='N':y+=1
+#  if o=='E':x+=1
+#  if o=='W':x-=1
+#  if o=='S':y-=1
+#  N+=1
+#  b=v.setdefault((x,y),N)
+#  if a>b:a,b=b,a
+#  e[(a,b)]=0
+# print(1+len(e)-len(v))
+#
+# v,e={(0,0):0},{}
+# x=y=0
+# N=int(input())
+# for o in input():
+#  a=v[(x,y)]
+#  if o=='N':y+=1
+#  elif o=='E':x+=1
+#  elif o=='W':x-=1
+#  else:y-=1
+#  N+=1
+#  b=v.setdefault((x,y),N)
+#  e[(min(a,b),max(a,b))]=0
+# print(1+len(e)-len(v))
--- bj11975.py
+++ bj11975.py
@@ -1,3 +1,5 @@
+# WRONG CODE
+
 import sys
 
 
