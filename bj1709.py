ea26291c636a991ff1664ab4c98e388832c4811f
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-08-16 01:32:41 +0900

    2308160132

--- bj1709.py
+++ bj1709.py
@@ -3,10 +3,12 @@ import sys
 N = int(sys.stdin.readline().strip())
 ans = 0
 y = 0
-x = N
-while x >= 0:
+x = N-1
+while x > y:
     while x**2 + y**2 < N**2:
         ans += 1
+        print(x, y, ans)
         y += 1
     x -= 1
 print(ans)
+print(ans*8)
