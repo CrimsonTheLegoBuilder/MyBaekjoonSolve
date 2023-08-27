5cd20f9aa509f088fd25bf00ccb5fb1865ac3afc
CrimconTheLegoBuilder <redcube231@gmail.com>
2023-08-28 08:55:33 +0900

    2308280855

--- bj1660.py
+++ bj1660.py
@@ -1,18 +1,16 @@
 import sys
 
 limit = 121
-dp = [0]*limit
+qh = [0]*limit
 for i in range(1, limit+1):
-    dp[i-1] = (i**3+3*i**2+2*i)//6
+    qh[i-1] = (i**3+3*i**2+2*i)//6
 N = int(sys.stdin.readline().strip())
-ans = 300_001
-for x in range(limit):
-    N_ = N
-    temp = 0
-    for y in range(x, -1, -1):
-        temp += N_ // dp[y]
-        N_ %= dp[y]
-    print(temp)
-    ans = min(ans, temp)
-print(ans)
-print(dp)
+dp = [300_001]*(N+1)
+for x in range(1, N+1):
+    for q in qh:
+        if x == q:
+            dp[x] = 1
+        if x < q:
+            break
+        dp[x] = min(dp[x], dp[x-q] + 1)
+print(dp[N])
