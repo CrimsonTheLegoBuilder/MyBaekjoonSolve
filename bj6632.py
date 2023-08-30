d7acbb6ff6f7197aaaf70df1ec88308057a2f221
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-08-31 00:16:29 +0900

    2308310016

--- bj6632.py
+++ bj6632.py
@@ -1,8 +1,13 @@
 import sys
+# sys.stdin = open('input_arable_area.txt', 'r')
 
 
 def cross(d1, d2, d3, d4):  # cross product / get CCW / get Torque / get area
     return (d2[0] - d1[0]) * (d4[1] - d3[1]) - (d2[1] - d1[1]) * (d4[0] - d3[0])
+
+
+def dot(d1, d2, d3, d4):  # dot product / get projection / get Force
+    return (d2[0] - d1[0]) * (d4[0] - d3[0]) + (d2[1] - d1[1]) * (d4[1] - d3[1])
 
 
 def get_polygon_area(hull):  # get area
@@ -14,43 +19,95 @@ def get_polygon_area(hull):  # get area
     return p_area
 
 
+def non_convex_inner_check(hull, d_):
+    x_, y_ = d_
+    cnt = 0
+    for i in range(N):
+        d_1, d_2 = hull[i-1], hull[i]
+        if d_1[1] > d_2[1]:
+            d_1, d_2 = d_2, d_1
+        x1, y1 = d_1
+        x2, y2 = d_2
+        if cross(d_1, d_, d_, d_2) == 0 and dot(d_1, d_, d_, d_2) >= 0:
+            return 1
+        if x_ > max(x1, x2):
+            continue
+        if y_ <= min(y1, y2):
+            continue
+        if y_ > max(y1, y2):
+            continue
+        if cross(d_1, d_2, d_2, d_) < 0:
+            continue
+        if y_ == y1 == y2:
+            continue
+        if y1 < y_ <= y2:
+            cnt += 1
+        elif cross(d_1, d_2, d_2, d_) > 0:
+            cnt += 1
+    if cnt % 2:
+        return 1
+    else:
+        return 0
+
+
+def cross_s(hull, r, c):
+    d1 = r, c
+    d2 = r+1, c+1
+    d3 = r+1, c
+    d4 = r, c+1
+    n = len(hull)
+    for i in range(n):
+        d_1, d_2 = hull[i-1], hull[i]
+        if cross(d_1, d_2, d_2, d1) * cross(d_2, d_1, d_1, d2) > 0 and cross(d1, d2, d2, d_1) * cross(d2, d1, d1, d_2) > 0:
+            return 1
+        if cross(d_1, d_2, d_2, d3) * cross(d_2, d_1, d_1, d4) > 0 and cross(d3, d4, d4, d_1) * cross(d4, d3, d3, d_2) > 0:
+            return 1
+    return 0
+
+
 while True:
     N = int(sys.stdin.readline().strip())
     if not N:
         break
     hull = [tuple(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
-    a = min(hull)
-    b = max(hull)
-    print(a, b)
-    grid = [[1]*201 for _ in range(201)]
-    for i in range(a[0]+100, b[0]+101):
-        for j in range(201):
-            grid[i][j] = 0
+    # grid = [[0]*201 for _ in range(201)]
     ccw = get_polygon_area(hull)
-    print(ccw)
     if ccw < 0:
         hull.reverse()
-    ccw = get_polygon_area(hull)
-    print(ccw)
-    for i in range(N):
-        d_1 = hull[i]
-        x1, y1 = d_1
-        d_2 = hull[(i+1) % N]
-        x2, y2 = d_2
-        if x1 == x2:
-            continue
-        if x1 < x2:
-            for x in range(x1, x2+1):
-                for y in range(-100, 100):
-                    if cross(d_1, d_2, d_2, (x, y)) >= 0:
-                        break
-                    grid[x+100][y+100] = 1
-        if x1 > x2:
-            for x in range(x2, x1+1):
-                for y in range(100, -100, -1):
-                    if cross(d_1, d_2, d_2, (x, y)) >= 0:
-                        break
-                    grid[x+100][y+100] = 1
+    dots = {}
+    for i in range(-100, 100):
+        for j in range(-100, 100):
+            if non_convex_inner_check(hull, (i, j)):
+                dots.setdefault((i, j), 1)
+    # print(dots)
+    ans = 0
+    for i in range(-100, 100):
+        for j in range(-100, 100):
+            if dots.get((i, j)) and dots.get((i+1, j)) and dots.get((i, j+1)) and dots.get((i+1, j+1)):
+                if not cross_s(hull, i, j) and non_convex_inner_check(hull, (i+0.5, j+0.5)):
+                    ans += 1
+    print(ans)
+    # grid[i][j] = 0
 
-    for row in grid[a[0]+100:b[0]+101]:
-        print(*row)
+    # for row in grid:
+    #     print(*row)
+
+
+'''
+13
+-100 100
+91 -100
+-99 99
+92 -99
+-98 98
+93 -98
+-97 97
+94 -97
+-96 96
+95 -96
+-95 95
+96 -95
+-97 100
+
+# 6
+'''
