678f16f9d331425d200743aad67a9b70eaebbced
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-08-31 07:52:50 +0900

    2308310752

--- __test.py
+++ __test.py
@@ -1,3 +1,11 @@
 import sys
 from math import acos, sin, cos, isclose
-print(acos(4))
+# print(acos(4))
+
+
+def cross(d1, d2, d3, d4):  # cross product / get CCW / get Torque / get area
+    return (d2[0] - d1[0]) * (d4[1] - d3[1]) - (d2[1] - d1[1]) * (d4[0] - d3[0])
+
+
+a = cross((96, -95), (-97, 100), (-97, 100), (-95, 98))
+print(a)
--- bj6632_2.py
+++ bj6632_2.py
@@ -10,20 +10,11 @@ def dot(d1, d2, d3, d4):  # dot product / get projection / get Force
     return (d2[0] - d1[0]) * (d4[0] - d3[0]) + (d2[1] - d1[1]) * (d4[1] - d3[1])
 
 
-def get_polygon_area(hull):  # get area
-    pivot = (0, 0)
-    len_hull = len(hull)
-    p_area = 0
-    for dh in range(len_hull):
-        p_area += cross(pivot, hull[dh - 1], hull[dh - 1], hull[dh])
-    return p_area
-
-
-def non_convex_inner_check(hull, d_):
+def non_convex_inner_check(hull_, d_):
     x_, y_ = d_
     cnt = 0
-    for i in range(N):
-        d_1, d_2 = hull[i-1], hull[i]
+    for i_ in range(N):
+        d_1, d_2 = hull_[i_-1], hull_[i_]
         if d_1[1] > d_2[1]:
             d_1, d_2 = d_2, d_1
         x1, y1 = d_1
@@ -50,17 +41,19 @@ def non_convex_inner_check(hull, d_):
         return 0
 
 
-def cross_s(hull, r, c):
+def cross_s(hull_, r, c):
     d1 = r, c
     d2 = r+1, c+1
     d3 = r+1, c
     d4 = r, c+1
-    n = len(hull)
-    for i in range(n):
-        d_1, d_2 = hull[i-1], hull[i]
-        if cross(d_1, d_2, d_2, d1) * cross(d_2, d_1, d_1, d2) > 0 and cross(d1, d2, d2, d_1) * cross(d2, d1, d1, d_2) > 0:
+    n = len(hull_)
+    for i_ in range(n):
+        d_1, d_2 = hull_[i_-1], hull_[i_]
+        if cross(d_1, d_2, d_2, d1) * cross(d_2, d_1, d_1, d2) > 0 and \
+                cross(d1, d2, d2, d_1) * cross(d2, d1, d1, d_2) > 0:
             return 1
-        if cross(d_1, d_2, d_2, d3) * cross(d_2, d_1, d_1, d4) > 0 and cross(d3, d4, d4, d_1) * cross(d4, d3, d3, d_2) > 0:
+        if cross(d_1, d_2, d_2, d3) * cross(d_2, d_1, d_1, d4) > 0 and \
+                cross(d3, d4, d4, d_1) * cross(d4, d3, d3, d_2) > 0:
             return 1
     return 0
 
@@ -70,31 +63,26 @@ while True:
     if not N:
         break
     hull = [tuple(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
-    # grid = [[0]*201 for _ in range(201)]
-    ccw = get_polygon_area(hull)
-    if ccw < 0:
-        hull.reverse()
+    xM = max(hull)[0]
+    xm = min(hull)[0]
+    yM = -200
+    ym = 200
+    for _, y in hull:
+        yM = max(yM, y)
+        ym = min(ym, y)
+
     dots = {}
-    for i in range(-100, 101):
-        for j in range(-100, 101):
+    for i in range(xm, xM+1):
+        for j in range(ym, yM+1):
             if non_convex_inner_check(hull, (i, j)):
                 dots.setdefault((i, j), 1)
-    # print(dots)
     ans = 0
-    square = []
-    for i in range(-100, 101):
-        for j in range(-100, 101):
-            if dots.get((i, j)) and dots.get((i+1, j)) and dots.get((i, j+1)) and dots.get((i+1, j+1)):
-                # print(i, j)
-                if not cross_s(hull, i, j) and non_convex_inner_check(hull, (i+0.5, j+0.5)):
-                    ans += 1
-                    square.append((i, j))
-    print(ans)
-    # print(square)
-    # grid[i][j] = 0
-
-    # for row in grid:
-    #     print(*row)
+    for i, j in dots:
+        if dots.get((i, j)) and dots.get((i+1, j)) and dots.get((i, j+1)) and dots.get((i+1, j+1)):
+            if not cross_s(hull, i, j) and non_convex_inner_check(hull, (i+0.5, j+0.5)):
+                ans += 1
+    # print(ans)
+    # sys.stdout.write(f'{ans}\n')
 
 
 '''
