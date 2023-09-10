ec5dc05cde57c4aadfe6dfd265d86c1a48750029
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-09-11 07:35:21 +0900

    2309110735

--- bj4167.py
+++ bj4167.py
@@ -4,7 +4,7 @@ from math import cos, sin, asin, radians
 
 def arc_hav(p1, p2, l1, l2):
     h = hav(p2 - p1) + cos(radians(p1)) * cos(radians(p2)) * hav(l2 - l1)
-    return 2 * asin(h**.5)
+    return asin(h**.5)
 
 
 def hav(t1):
@@ -15,7 +15,7 @@ r = 6371009
 N = int(sys.stdin.readline().strip())
 for _ in range(N):
     lat1, lon1, lat2, lon2 = map(float, sys.stdin.readline().strip().split())
-    theta = arc_hav(lat1, lat2, lon1, lon2)
+    theta = 2 * arc_hav(lat1, lat2, lon1, lon2)
     arc = r * theta
     string = (2*r**2 - 2*r*r*cos(theta))**.5
     # print(arc, string)
