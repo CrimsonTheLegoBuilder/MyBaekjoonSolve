01f0a18c354866eb154f3dadc3497020ed3ed0a9
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-09-19 09:53:25 +0900

    2309190953

---
+++ bj3987.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def dfs(d, r, c):
+    cnt = 0
+    i, j = r, c
+    i_, j_ = drc[d][0], drc[d][1]
+    while 1:
+        i += i_
+        j += j_
+        cnt += 1
+        print(d, cnt)
+        if i < 0 or i >= N or j < 0 or j >= M:
+            return 0, cnt
+        if grid[i][j] == 'C':
+            return 0, cnt
+        if i == r and j == c:
+            return 1, cnt
+        if grid[i][j] == '/':
+            i_, j_ = -j_, -i_
+        if grid[i][j] == '\\':
+            i_, j_ = j_, i_
+
+
+drc = ((-1, 0), (0, 1), (1, 0), (0, -1))
+D = ['U', 'R', 'D', 'L']
+INF = int(12e12)
+N, M = map(int, sys.stdin.readline().strip().split())
+grid = [sys.stdin.readline().strip() for _ in range(N)]
+ir, ic = map(int, sys.stdin.readline().strip().split())
+ans = ['S', 0]
+for x in range(4):
+    a, t = dfs(x, ir-1, ic-1)
+    if a and ans[1] < t:
+        ans[0] = D[x]
+        ans[1] = INF
+        # print(*ans)
+    elif ans[1] < t:
+        ans[0] = D[x]
+        ans[1] = t
+print(f'{ans[0]}\n{ans[1] if ans[1] < INF else "Voyager"}')
--- bj9663_bit2.py
+++ bj9663_bit2.py
@@ -1,20 +1,31 @@
+# import sys
+#
+#
+# def dfs(x, r, d1, d2):
+#     global n_q
+#     if x == N:
+#         n_q += 1
+#         return
+#     d1 <<= 1
+#     d2 >>= 1
+#     q_r = r | d1 | d2
+#     for i in range(N):
+#         if not q_r & 1 << i:
+#             dfs(x+1, r | 1 << i, d1 | 1 << i, d2 | 1 << i)
+#
+#
+# # N = int(sys.stdin.readline().strip())
+# n_q = 0
+# N_Q = [0]*15
+# for i in range(1, 16):
+#     N = i
+#     dfs(0, 0, 0, 0)
+#     print(n_q)
+#     N_Q[i-1] = n_q
+#     n_q = 0
+# print(*N_Q)
+
 import sys
-
-
-def dfs(x, r, d1, d2):
-    global n_q
-    if x == N:
-        n_q += 1
-        return
-    d1 <<= 1
-    d2 >>= 1
-    q_r = r | d1 | d2
-    for i in range(N):
-        if not q_r & 1 << i:
-            dfs(x+1, r | 1 << i, d1 | 1 << i, d2 | 1 << i)
-
-
 N = int(sys.stdin.readline().strip())
-n_q = 0
-dfs(0, 0, 0, 0)
-print(n_q)
+N_Q = [1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184]
+print(N_Q[N-1])
