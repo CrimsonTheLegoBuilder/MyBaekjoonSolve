cab22f7bcb51a529757ef579e749777235b92476
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-11-08 03:06:30 +0900

    2311080306

--- C/boj_solve/boj22626_point_brute.cpp
+++ C/boj_solve/boj22626_point_brute.cpp
@@ -33,6 +33,8 @@ ld bisect(const Line& l1, const Line& l2) {
 Line L(const Pos& d, ld theta) {
 	if (z(PI * .5 - std::abs(theta))) return { 1, 0, d.x };
 	ld vy = tan(theta);
+	//std::cout << theta << " " << PI * .5 << " Line consist DEBUG\n";
+	if (std::abs(theta) > (PI * .5)) vy *= -1;
 	ld vx = (std::abs(theta) > (PI * .5)) ? 1 : -1;
 	return { vy, vx, vy * d.x + vx * d.y };
 }
@@ -61,6 +63,8 @@ ld norm(ld a) {
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(7);
 	std::cin >> N;
 	if (N == 1) { std::cout << "None\n"; return 0; }
 	if (N == 2) { std::cout << "Many\n"; return 0; }
@@ -94,7 +98,7 @@ int main() {
 				if (D != dist(P[i][0], P[i][1], p)) f = 0; break;
 			}
 			if (f) {
-				std::cout << p.x << " " << p.y << " NP\n";
+				std::cout << p.x << " " << p.y << "\n";
 				return 0;
 			}
 		}
@@ -102,18 +106,20 @@ int main() {
 		return 0;
 	}
 	Line lp1, lp2, la;
-	std::cout << l1 / l2 << " " << l1 / l3 << " " << l2 / l3 << "\n";
+	//std::cout << l1 / l2 << " " << l1 / l3 << " " << l2 / l3 << " cross value DEBUG\n";
 	if (z(l1 / l2)) lp1 = l1, lp2 = l2, la = l3;
 	else if (z(l2 / l3)) lp1 = l2, lp2 = l3, la = l1;
 	else if (z(l1 / l3)) lp1 = l1, lp2 = l3, la = l2;
 	ld a1 = norm(bisect(lp1, la)), a2 = norm(bisect(lp2, la));
 	ld a3 = norm(a1 + PI * 0.5), a4 = norm(a2 + PI * 0.5);
-	std::cout << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
-	Pos ax = IP(lp2, la); std::cout << ax.x << " " << ax.y << "\n";
-	Line lx = L(IP(lp2, la), a4); std::cout << lx.vy << " " << lx.vx << " " << lx.c << "\n";
+	//std::cout << a1 << " " << a2 << " " << a3 << " " << a4 << " DEBUG\n";
+	//Pos ax = IP(lp1, la); std::cout << ax.x << " " << ax.y << " DEBUG\n";
+	//ax = IP(lp2, la); std::cout << ax.x << " " << ax.y << " DEBUG\n";
+	//Line lx = L(IP(lp2, la), a2); std::cout << lx.vy << " " << lx.vx << " " << lx.c << " DEBUG\n";
+	//Line ly = L(IP(lp2, la), a4); std::cout << ly.vy << " " << ly.vx << " " << ly.c << " DEBUG\n";
 	p1 = z(a1 - a2) ? IP(L(IP(lp1, la), a1), L(IP(lp2, la), a4)) : IP(L(IP(lp1, la), a1), L(IP(lp2, la), a2));
 	p2 = z(a3 - a2) ? IP(L(IP(lp1, la), a3), L(IP(lp2, la), a4)) : IP(L(IP(lp1, la), a3), L(IP(lp2, la), a2));
-	std::cout << p1.x << " " << p1.y << " " << p2.x << " " << p2.y << "\n";
+	//std::cout << p1.x << " " << p1.y << " " << p2.x << " " << p2.y << " DEBUG\n";
 	std::vector<Pos> tmp = { p1, p2 };
 	for (const Pos& p : tmp) {
 		ld D = dist(P[0][0], P[0][1], p);
@@ -122,7 +128,7 @@ int main() {
 			if (D != dist(P[i][0], P[i][1], p)) f = 0; break;
 		}
 		if (f) {
-			std::cout << p.x << " " << p.y << " P\n";
+			std::cout << p.x << " " << p.y << "\n";
 			return 0;
 		}
 	}
