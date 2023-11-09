d2d2bec5c64ee381c25a9aefca9a45a174154919
CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-11-09 13:30:10 +0900

    2311091330

--- C/boj_solve/boj20149_intersection.cpp
+++ C/boj_solve/boj20149_intersection.cpp
@@ -13,46 +13,40 @@ struct Pdd { ld x, y; }x;
 struct Line {  // vy * x + vx * y - c == 0;
 	ld vy, vx, c;
 	ld operator / (const Line& l) const { return vy * l.vx - vx * l.vy; }
-}L1, L2;
+};
 
 Line L(const Pii& d1, const Pii& d2) {
 	ld vy = d2.y - d1.y, vx = d1.x - d2.x;
 	ld c = vy * d1.x + vx * d1.y;
 	return { vy, vx, c };
 }
-ll C(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) {
-	ll tmp = (d2.x - d1.x) * (d4.y - d3.y) - (d2.y - d1.y) * (d4.x - d3.x);
-	if (!tmp) return 0;
-	if (tmp > 0) return 1;
-	if (tmp < 0) return -1;
-}
-ll C(const Pii& d1, const Pii& d2, const Pii& d3) {
-	ll tmp = (d2.x - d1.x) * (d3.y - d2.y) - (d2.y - d1.y) * (d3.x - d2.x);
-	if (!tmp) return 0;
-	if (tmp > 0) return 1;
-	if (tmp < 0) return -1;
-}
-ll D(const Pii& d1, const Pii& d2, const Pii& d3) {
-	ll tmp = (d2.x - d1.x) * (d3.x - d2.x) + (d2.y - d1.y) * (d3.y - d2.y);
-	if (!tmp) return 0;
-	if (tmp > 0) return 1;
-	if (tmp < 0) return -1;
-}
 Pdd IP(const Line& L1, const Line& L2) {
 	ld det = L1 / L2;
 	return { (L1.c * L2.vx - L1.vx * L2.c) / det, (L1.vy * L2.c - L1.c * L2.vy) / det };
 }
+int C(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) {
+	ll ccw = (d2.x - d1.x) * (d4.y - d3.y) - (d2.y - d1.y) * (d4.x - d3.x);
+	return ccw > 0 ? 1 : ccw < 0 ? -1 : 0;
+}
+int C(const Pii& d1, const Pii& d2, const Pii& d3) {
+	ll ccw = (d2.x - d1.x) * (d3.y - d2.y) - (d2.y - d1.y) * (d3.x - d2.x);
+	return ccw > 0 ? 1 : ccw < 0 ? -1 : 0;
+}
+int D(const Pii& d1, const Pii& d2, const Pii& d3) {
+	ll dot = (d2.x - d1.x) * (d3.x - d2.x) + (d2.y - d1.y) * (d3.y - d2.y);
+	return dot > 0 ? 1 : dot < 0 ? -1 : 0;
+}
 bool X(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) {
 	bool F1 = C(d1, d2, d3) * C(d2, d1, d4) > 0 && C(d3, d4, d1) * C(d4, d3, d2) > 0;
-	bool F2 = !C(d1, d2, d3) && D(d1, d3, d2) >= 0 ||
-		!C(d1, d2, d4) && D(d1, d4, d2) >= 0 ||
-		!C(d3, d4, d1) && D(d3, d1, d4) >= 0 ||
-		!C(d3, d4, d2) && D(d3, d2, d4) >= 0;
+	bool F2 = (!C(d1, d2, d3) && D(d1, d3, d2) >= 0) ||
+		(!C(d1, d2, d4) && D(d1, d4, d2) >= 0) ||
+		(!C(d3, d4, d1) && D(d3, d1, d4) >= 0) ||
+		(!C(d3, d4, d2) && D(d3, d2, d4) >= 0);
 	return F1 || F2;
 }
 bool P(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) {
 	bool F0 = !C(d1, d2, d3, d4);
-	bool F1 = d1 == d3 && d2 == d4 || d1 == d4 && d2 == d3;
+	bool F1 = (d1 == d3 && d2 == d4) || (d1 == d4 && d2 == d3);
 	bool F2 = F0 && (D(d1, d3, d2) > 0 || D(d1, d4, d2) > 0 || D(d3, d1, d4) > 0 || D(d3, d2, d4) > 0);
 	if (F0 && !F1 && !F2) {
 		if (d1 == d3 || d1 == d4) c = d1;
