ae756e84ebd397ccb2ded074cc06ea34e58bba39
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2023-11-09 19:14:08 +0900

    2311091914

--- C/boj_solve/boj1288_voronoi_ON_2.cpp
+++ C/boj_solve/boj1288_voronoi_ON_2.cpp
@@ -4,9 +4,8 @@
 #include <deque>
 #include <cmath>
 #include <cstring>
-#include <cassert>
 typedef long long ll;
-typedef double ld;
+typedef long double ld;
 const int LEN = 100;
 const ld TOL = 1e-9;
 int N;
@@ -19,12 +18,12 @@ struct Pos {
 	bool c;
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator * (const ld& s) const { return { x * s, y * s }; }
-}pos[LEN], pu = { X, X }, pr = { X, 0 }, pl = { 0, X }, pd = { 0, 0 };
-struct Vec {
-	ld vy, vx;
-	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }  //cross
-	Vec operator ~ () const { return { -vx, vy }; }
-};
+}pos[LEN], pu, pr, pl, pd;
+//struct Vec {
+//	ld vy, vx;
+//	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }  //cross
+//	Vec operator ~ () const { return { -vx, vy }; }  //rotate PI/2
+//};
 struct Line {
 	ld vy, vx, c;  // a(vy) * x + b(vx) * y - c == 0;
 	bool operator < (const Line& l) const {
@@ -35,7 +34,8 @@ struct Line {
 		return z(ccw) ? c * hypot(l.vx, l.vy) < l.c * hypot(vx, vy) : ccw > 0;  // sort by distance
 	}
 	ld operator / (const Line& l) const { return vy * l.vx - vx * l.vy; }  //cross
-	Vec V() const { return { vy, vx }; }
+	Line operator ~ () const { return { -vx, vy, c }; }  //rotate PI/2
+	//Vec V() const { return { vy, vx }; }
 };
 Line L(const Pos& s, const Pos& e) {  //line consist with two point
 	ld dy = e.y - s.y;
@@ -43,15 +43,18 @@ Line L(const Pos& s, const Pos& e) {  //line consist with two point
 	ld c = dy * s.x + dx * s.y;// -hypot(dy, dx) * TOL;
 	return { dy, dx, c };
 }
-Line L(const Vec& v, const Pos& p) {  //line consist with vector && pos
-	ld c = v.vy * p.x + v.vx * p.y;
-	return { v.vy, v.vx, c };
+//Line L(const Vec& v, const Pos& p) {  //line consist with vector && pos
+//	ld c = v.vy * p.x + v.vx * p.y;
+//	return { v.vy, v.vx, c };
+//}
+Line L(const Line& l, const Pos& p) {  //line consist with vector && pos
+	ld c = l.vy * p.x + l.vx * p.y;
+	return { l.vy, l.vx, c };
 }
 Line LBX(const Pos& s, const Pos& e) {  //line bisector
 	Pos m = (s + e) * .5; //middle point
-	Line l = L(s, e);     //s->e line
-	Vec v = ~l.V();       //rotate PI/2
-	return L(v, m);
+	Line l = ~L(s, e);     //s=>e line => rotate PI/2
+	return L(l, m);
 }
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3) {
 	return (d2.x - d1.x) * (d3.y - d2.y) - (d2.y - d1.y) * (d3.x - d2.x);
@@ -100,25 +103,28 @@ std::vector<Line> HP;
 std::vector<Pos> inx;
 void solve() {
 	std::cin >> N >> X;
+	pu = { X, X }, pr = { X, 0 }, pl = { 0, X }, pd = { 0, 0 };
 	ld fld = X * X, occ = 0, enm = 0;  //total field, occupied, enemy's
 	for (int i = 0; i < N; i++) { std::cin >> pos[i].x >> pos[i].y >> pos[i].c; }
 	for (int i = 0; i < N; i++) {
 		HP.clear(); inx.clear();
 		HP.push_back(L(pr, pu)); HP.push_back(L(pu, pl));
 		HP.push_back(L(pl, pd)); HP.push_back(L(pd, pr));
+		//std::cout << pu.x << "\n";
 		for (int j = 0; j < N; j++) {
 			if (i == j) continue;
-			Line bxl = LBX(pos[i], pos[j]);
-			std::cout << bxl.vy << " " << bxl.vy << " " << bxl.c << "\n";
-			HP.push_back(bxl);
+			Line lbx = LBX(pos[i], pos[j]);
+			//std::cout << lbx.vy << " " << lbx.vy << " " << lbx.c << " DEBUG\n";
+			HP.push_back(lbx);
 		}
 		f = half_plane_inx(HP, inx);
-		std::cout << f << "\n";
-		if (pos[i].c) occ += A(inx);
-		else enm += A(inx);
+		//std::cout << f << " DEBUG\n";
+		if (pos[i].c) enm += A(inx);
+		else occ += A(inx);
 	}
-	std::cout << fld << "\n";
+	//std::cout << fld << " DEBUG\n";
 	std::cout << occ << " " << enm << "\n";
+	//std::cout << occ << " " << (fld - occ) << "\n";
 }
 
 
