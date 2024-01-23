fcf60d1122145fb85d49484bc5c0eef70642f91c
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-01-23 23:42:51 +0900

    2401232342

--- C/boj_solve/boj18552_bi_search.cpp
+++ C/boj_solve/boj18552_bi_search.cpp
@@ -7,7 +7,7 @@ typedef long double ld;
 const ld TOL = 1e-6;
 const ld MAX = 2e8;
 const int LEN = 50;
-int K, N;
+int K, N, M, Z;
 
 bool z(const ld& x) { return std::abs(x) < TOL; }
 struct Pos {
@@ -17,9 +17,38 @@ struct Pos {
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
 	Pos operator * (ld n) const { return { x * n, y * n }; }
 	Pos operator ~ () const { return { y, -x }; }
-} FR, FL, RR, RL;
+} p, FR, FL, RR, RL;
 std::vector<Pos> potholes[LEN];
-
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return (d2.x - d1.x) * (d3.y - d2.y) - (d2.y - d1.y) * (d3.x - d2.x);
+}
+std::vector<Pos> monotone_chain(std::vector<Pos>& C) {
+	std::vector<Pos> H;
+	std::sort(C.begin(), C.end());
+	if (C.size() <= 2) {
+		for (const Pos& p : C) H.push_back(p);
+		return H;
+	}
+	for (int i = 0; i < C.size(); i++) {
+		while (H.size() > 1 && (cross(H[H.size() - 2], H[H.size() - 1], C[i]) < 0 || z(cross(H[H.size() - 2], H[H.size() - 1], C[i])))) {
+			H.pop_back();
+		}
+		H.push_back(C[i]);
+	}
+	H.pop_back();
+	int s = H.size() + 1;
+	for (int i = C.size() - 1; i >= 0; i--) {
+		while (H.size() > s && (cross(H[H.size() - 2], H[H.size() - 1], C[i]) < 0 || z(cross(H[H.size() - 2], H[H.size() - 1], C[i])))) {
+			H.pop_back();
+		}
+		H.push_back(C[i]);
+	}
+	H.pop_back();
+	return H;
+}
+bool cross() {
+	return 1;
+}
 ld bi_search(int K) {
 	ld s = 0, e = MAX, m = 0;
 	int cnt = 100;
@@ -39,8 +68,15 @@ void solve() {
 	std::cin >> K;
 	std::cin >> FR.x >> FR.y >> FL.x >> FL.y >> RL.x >> RL.y >> RR.x >> RR.y;
 	std::cin >> N;
-
-
+	for (int i = 0; i < N; i++) {
+		std::cin >> M;
+		for (int j = 0; j < M; j++) {
+			std::cin >> p.x >> p.y >> Z;
+			if (!Z) potholes[i].push_back(p);
+		}
+		potholes[i] = monotone_chain(potholes[i]);
+	}
+	std::cout << bi_search(K);
 	return;
 }
 int main() { solve(); return 0; }//boj18552
