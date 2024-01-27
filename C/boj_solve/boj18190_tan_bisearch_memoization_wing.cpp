1babe202ca909349f5e98e84c2bbd6825120bdfb
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-01-27 14:55:33 +0900

    2401271455

--- C/boj_solve/boj18190_tan_bisearch_memoization_wing.cpp
+++ C/boj_solve/boj18190_tan_bisearch_memoization_wing.cpp
@@ -27,7 +27,7 @@ struct Pos {
 		x /= scale; y /= scale;
 		return *this;
 	}
-} NH[LEN], MH[LEN];
+} NH[LEN], MH[LEN], q[LEN];
 const Pos O = { 0, 0 };
 struct Info { ll area, l, r; };
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) {
@@ -202,10 +202,12 @@ ld find_inx_get_area_bi_search(Pos H_in[], ll memo_in[], const int& sz_in, Pos H
 	}
 	return area * .5;
 }
-void query() {
-	Pos candle;
-	std::cin >> candle.x >> candle.y;
-	candle *= SCALE;
+//void query() {
+void query(const int& i) {
+	Pos candle = q[i];
+	//std::cin >> candle.x >> candle.y;
+	//candle.xf = (ld)candle.x;
+	//candle.yf = (ld)candle.y;
 	int f1, f2;
 	f1 = inner_check_bi_search(MH, M, candle) > -1;
 	f2 = inner_check_bi_search(NH, N, candle) < 1;
@@ -222,13 +224,15 @@ void init() {
 	std::cin >> N >> M >> Q;
 	for (int i = 0; i < N; i++) std::cin >> NH[i].x >> NH[i].y, NH[i] *= SCALE;
 	for (int j = 0; j < M; j++) std::cin >> MH[j].x >> MH[j].y, MH[j] *= SCALE;
+	for (int k = 0; k < Q; k++) std::cin >> q[k].x >> q[k].y, q[k] *= SCALE;
 	get_area_memo(NH, memo_n, N);
 	get_area_memo(MH, memo_m, M);
 	return;
 }
-void solve() { init(); while (Q--) query(); return; }
+//void solve() { init(); while (Q--) query(); return; }
+void solve() { init(); for (int i = 0; i < Q; i++) query(i); return; }
 int main() { solve(); return 0; }//boj18190
-//std::cout << wing_r << " " << wing_l << "\n";
+	//std::cout << wing_r << " " << wing_l << "\n";
 
 /*
 
