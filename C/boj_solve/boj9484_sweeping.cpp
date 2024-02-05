57ee2fe91a5388506c814c4763c5608219566d7a
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-05 21:38:24 +0900

    2402051238

--- C/boj_solve/boj9484_sweeping.cpp
+++ C/boj_solve/boj9484_sweeping.cpp
@@ -1,12 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <cstring>
 typedef long long ll;
-const ll INF = 1e18;
+const ll INF = 1e17;
 const int LEN = 2'001;
-
-int N, M, pos[LEN];
-ll ret;
-char c;
+int N, M, order[LEN], idx[LEN];
 
 struct Pos {
 	ll x, y;
@@ -24,224 +24,209 @@ struct Pos {
 	ll operator ! () const { return x * y; }
 	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
 	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
-	//ld mag() const { return hypot(x, y); }
 } P[LEN];
 const Pos O = { 0, 0 }, MAXL = { 0, INF }, MAXR = { INF, 0 }, pivot = { -1, -1 };
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-struct Slope {  //segment's two point and slope
-	int u, v;
+struct Slope {//segment's two point and slope
+	int u, v;//idx
 	ll dx, dy;
+	Pos p() const { return { u, v }; }
 	bool operator < (const Slope& s) const {
-		if (dy * s.dx == dx * s.dy) {
-			if (u == s.u) return v < s.v;
-			return u < s.u;
-		}
+		if (dy * s.dx == dx * s.dy) return p() < s.p();
 		return dy * s.dx < dx * s.dy;
 	}
 	bool operator == (const Slope& s) const { return dy * s.dx == dx * s.dy; }
-}slopes[LEN * LEN];
-//struct LenNode {  //segment tree for cal subsequence max len
-//	ll max, llen, rlen, all;
-//	LenNode operator + (const LenNode& r) const {
-//		return {
-//			std::max({max, r.max, rlen + r.llen}),
-//			std::max({llen, all + r.llen}),
-//			std::max({rlen + r.all, r.rlen}),
-//			all + r.all
-//		};
-//	}
-//}seg_len[LEN * 4];
-//void update(int idx, int v, int s = 0, int e = N - 1, int n = 1) {
-//	if (e < idx || idx < s) return;
-//	if (s == e) {
-//		seg_len[n] = { v, v, v, v };
-//		return;
-//	}
-//	int m = s + e >> 1;
-//	update(idx, v, s, m, n << 1);
-//	update(idx, v, m + 1, e, n << 1 | 1);
-//	seg_len[n] = seg_len[n << 1] + seg_len[n << 1 | 1];
-//}
-//LenNode get_len(int l, int r, int s = 0, int e = N - 1, int n = 1) {
-//	if (e < l || r < s) return { 0, 0, 0, 0 };
-//	if (l <= s && e <= r) return seg_len[n];
-//	int m = s + e >> 1;
-//	return get_len(l, r, s, m, n << 1) + get_len(l, r, m + 1, e, n << 1 | 1);
-//}
+} slopes[LEN * LEN];
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> N;
-	for (int i = 0; i < N; i++) std::cin >> P[i].x >> P[i].y;
-	std::sort(P, P + N);
-	ll MIN = INF, MAX = -INF;
+	freopen("boj9484_triangle_in.txt", "r", stdin);
+	freopen("boj9484_triangle_out.txt", "w", stdout);
+	while (1) {
+		memset(order, 0, LEN);
+		memset(idx, 0, LEN);
+		memset(P, 0, LEN);
+		std::cin >> N;
+		if (!N) return;
 
-	for (int i = 0; i < N; i++) {
-		Pos p1 = P[i];
-		for (int j = i + 1; j < N; j++) {
-			ll dx = P[j].x - P[i].x;
-			ll dy = P[j].y - P[i].y;
-			if (P[j].x == P[i].x) {
-				if (i > 0) {
-					MIN = std::min({ MIN, std::abs(cross(p1, P[j], P[i - 1])) });
-					MAX = std::min({ MAX, std::abs(cross(p1, P[j], P[0])) });
-				}
-				else if (j < N - 1) {
-					MIN = std::min({ MIN, std::abs(cross(p1, P[j], P[j + 1])) });
-					MAX = std::min({ MAX, std::abs(cross(p1, P[j], P[N - 1])) });
-				}
-			}
-			if (!dx) continue;
-			if (dx * dy >= 0) {
-				dx = std::abs(dx); dy = std::abs(dy);
-			}
-			else {
-				dx = std::abs(dx); dy = -std::abs(dy);
-			}
-			slopes[M++] = { i, j, dx, dy };
-		}
-	}
-	std::sort(slopes, slopes + M);
+		for (int i = 0; i < N; i++) std::cin >> P[i].x >> P[i].y;
+		std::sort(P, P + N);
 
-	for (int i = 0; i < N; i++) pos[i] = i;  //cur. idx
-	//ret = std::max(ret, seg_len[1].max);
-	for (int i = 0, j; i < M; i = j) {
-		j = i;
-		Pos S = P[slopes[i].u], E = P[slopes[i].u];
-		int s = slopes[i].u, e = slopes[i].u;
-		while (j < M && slopes[i] == slopes[j]) {
-			int u = slopes[j].u, v = slopes[j].v;
-			if (e < v) e = v;
-			if (E < P[pos[v]]) { E = P[pos[v]];}
-			if (u > 0) {
-				MIN = std::min({ MIN, std::abs(cross(P[pos[u]], P[pos[v]], P[pos[u - 1]]))});
+		for (int i = 0; i < N; i++) order[i] = i, idx[i] = i;
+		M = 0;
+		ll MIN = INF, MAX = -INF;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (P[i].x == P[j].x) {
+					if (i > 0) {
+						MIN = std::min({ MIN, std::abs(cross(P[i], P[j], P[i - 1])) });
+						MAX = std::max({ MAX, std::abs(cross(P[i], P[j], P[0])) });
+					}
+					else if (j < N - 1) {
+						MIN = std::min({ MIN, std::abs(cross(P[i], P[j], P[j + 1])) });
+						MAX = std::max({ MAX, std::abs(cross(P[i], P[j], P[N - 1])) });
+					}
+				}
+				ll dx = P[j].x - P[i].x;
+				ll dy = P[j].y - P[i].y;
+				if (!dx) continue;
+				if (dx * dy >= 0) dx = std::abs(dx), dy = std::abs(dy);
+				else dx = std::abs(dx), dy = -std::abs(dy);
+				slopes[M++] = { i, j, dx, dy };
 			}
-			if (v < N - 1) {
-				MIN = std::min({ MIN, std::abs(cross(P[pos[u]], P[pos[v]], P[pos[v + 1]]))});
-			}
-			//int vx = spot[u].v, vy = spot[v].v;
-			//update(x, vy); update(y, vx);
-			int x = pos[u], y = pos[v];
-			pos[u] = y; pos[v] = x;
-			j++;
 		}
-		if (s > 0) MAX = std::max({ MAX, std::abs(cross(S, E, P[pos[0]])) });
-		if (e < N - 1) MAX = std::max({ MAX, std::abs(cross(S, E, P[pos[N - 1]])) });
-		
-		//ret = std::max(ret, seg_len[1].max);
+		std::sort(slopes, slopes + M);
+
+		for (int i = 0, j; i < M; i = j) {
+			j = i;
+			int s = order[slopes[i].u], e = order[slopes[i].u];
+			while (j < M && slopes[i] == slopes[j]) {
+				int u = slopes[j].u, v = slopes[j].v;
+				if (e < order[v]) e = order[v];
+				int ou = order[u], ov = order[v];
+				if (ou > 0)
+					MIN = std::min({ MIN, std::abs(cross(P[u], P[v], P[idx[ou - 1]])) });
+				if (ov < N - 1)
+					MIN = std::min({ MIN, std::abs(cross(P[u], P[v], P[idx[ov + 1]])) });
+				order[u] = ov; order[v] = ou;
+				idx[ou] = v; idx[ov] = u;
+				j++;
+			}
+			if (s == 0 && e == N - 1) MAX = 0;
+			if (s > 0) MAX = std::max({ MAX, std::abs(cross(P[idx[s]], P[idx[e]], P[idx[0]]))});
+			if (e < N - 1) MAX = std::max({ MAX, std::abs(cross(P[idx[s]], P[idx[e]], P[idx[N - 1]])) });
+			
+		}
+		std::cout << (MIN >> 1) << '.' << (MIN & 1 ? "5 " : "0 ");
+		std::cout << (MAX >> 1) << '.' << (MAX & 1 ? "5\n" : "0\n");
 	}
-	std::cout << MIN << "\n" << MAX << "\n";
-	std::cout << (MIN >> 1) << '.' << (MIN & 1 ? "5\n" : "0\n");
-	std::cout << (MAX >> 1) << '.' << (MAX & 1 ? "5\n" : "0\n");
 	return;
 }
 int main() { solve(); return 0; }//boj9484
 
+/*
+3
+-1 -1
+-1 0
+-1 1
+0
+*/
 
-//ll V[LEN];
-	//for (int i = 0; i < N; i++) {
-	//	V[i] = dots[i].v;
-	//}
 
-
-//#include <iostream>
+//#define _CRT_SECURE_NO_WARNINGS
+////#include <iostream>
 //#include <algorithm>
+//#include <cmath>
+//#include <cstring>
 //typedef long long ll;
+//const ll INF = 1e17;
 //const int LEN = 2'001;
-//
-//int N, M, pos[LEN], ret;
-//ll V[LEN];
+//int N, M, order[LEN], idx[LEN];
+//ll ret;
 //char c;
 //
-//struct Pos {  //dots
-//	ll x, y, v;
-//	bool operator < (const Pos& p) const { return (x == p.x ? y < p.y : x < p.x); }
-//}dots[LEN];
-//struct Slope {  //segment's two point and slope
-//	int u, v;
+//struct Pos {
+//	ll x, y;
+//	Pos(ll X, ll Y) : x(X), y(Y) {}
+//	Pos() : x(0), y(0) {}
+//	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+//	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+//	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+//	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+//	Pos operator * (const ll& n) const { return { x * n, y * n }; }
+//	Pos operator / (const ll& n) const { return { x / n, y / n }; }
+//	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+//	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+//	Pos operator ~ () const { return { -y, x }; }
+//	ll operator ! () const { return x * y; }
+//	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+//	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+//	//ld mag() const { return hypot(x, y); }
+//} P[LEN];
+//const Pos O = { 0, 0 }, MAXL = { 0, INF }, MAXR = { INF, 0 }, pivot = { -1, -1 };
+//ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+//struct Slope {//segment's two point and slope
+//	int u, v;//idx
 //	ll dx, dy;
-//	bool operator < (const Slope& s) const { return dy * s.dx < dx * s.dy; }
+//	Pos p() const { return { u, v }; }
+//	bool operator < (const Slope& s) const {
+//		//if (dy * s.dx == dx * s.dy) return u == s.u ? v < s.v : u < s.u;
+//		if (dy * s.dx == dx * s.dy) return p() < s.p();
+//		return dy * s.dx < dx * s.dy;
+//	}
 //	bool operator == (const Slope& s) const { return dy * s.dx == dx * s.dy; }
-//}slopes[LEN * LEN];
-//
-//struct LenNode {  //segment tree for cal subsequence max len
-//	int max, llen, rlen, all;
-//	LenNode operator + (const LenNode& r) const {
-//		return {
-//			std::max({max, r.max, rlen + r.llen}),
-//			std::max({llen, all + r.llen}),
-//			std::max({rlen + r.all, r.rlen}),
-//			all + r.all
-//		};
-//	}
-//}seg_len[LEN * 4];
-//
-//void update(int idx, int v, int s = 0, int e = N - 1, int n = 1) {
-//	if (e < idx || idx < s) return;
-//	if (s == e) {
-//		seg_len[n] = { v, v, v, v };
-//		return;
-//	}
-//	int m = s + e >> 1;
-//	update(idx, v, s, m, n << 1);
-//	update(idx, v, m + 1, e, n << 1 | 1);
-//	seg_len[n] = seg_len[n << 1] + seg_len[n << 1 | 1];
-//}
-//LenNode get_len(int l, int r, int s = 0, int e = N - 1, int n = 1) {
-//	if (e < l || r < s) return { 0, 0, 0, 0 };
-//	if (l <= s && e <= r) return seg_len[n];
-//	int m = s + e >> 1;
-//	return get_len(l, r, s, m, n << 1) + get_len(l, r, m + 1, e, n << 1 | 1);
-//}
-//
-//
-//
-//int main() {
+//} slopes[LEN * LEN];
+//void solve() {
 //	std::cin.tie(0)->sync_with_stdio(0);
 //	std::cout.tie(0);
-//	std::cin >> N;
-//	for (int i = 0; i < N; i++) {
-//		std::cin >> dots[i].x >> dots[i].y >> dots[i].v;
-//	}
-//	std::sort(dots, dots + N);
-//	for (int i = 0; i < N; i++) {
-//		V[i] = dots[i].v;
-//	}
+//	freopen("boj9484_triangle_in.txt", "r", stdin);
+//	freopen("boj9484_triangle_out.txt", "w", stdout);
+//	while (1) {
+//		memset(order, 0, LEN);
+//		memset(idx, 0, LEN);
+//		memset(P, 0, LEN);
+//		std::cin >> N;
+//		if (!N) return;
+//		for (int i = 0; i < N; i++) std::cin >> P[i].x >> P[i].y;
+//		std::sort(P, P + N);
+//		for (int i = 0; i < N; i++) order[i] = i, idx[i] = i;
 //
-//	for (int i = 0; i < N; i++) {
-//		for (int j = i + 1; j < N; j++) {
-//			ll dx = dots[j].x - dots[i].x;
-//			ll dy = dots[j].y - dots[i].y;
-//			if (!dx) continue;
-//			if (dx * dy >= 0) {
-//				dx = std::abs(dx); dy = std::abs(dy);
+//		M = 0;
+//		ll MIN = INF, MAX = -INF;
+//		for (int i = 0; i < N; i++) {
+//			for (int j = i + 1; j < N; j++) {
+//				if (P[i].x == P[j].x) {
+//					if (i > 0) {
+//						MIN = std::min({ MIN, std::abs(cross(P[i], P[j], P[i - 1])) });
+//						MAX = std::max({ MAX, std::abs(cross(P[i], P[j], P[0])) });
+//						//std::cout << "i > 0 " << MIN << " " << MAX << "\n";
+//					}
+//					else if (j < N - 1) {
+//						MIN = std::min({ MIN, std::abs(cross(P[i], P[j], P[j + 1])) });
+//						MAX = std::max({ MAX, std::abs(cross(P[i], P[j], P[N - 1])) });
+//						//std::cout << "j < N - 1 " << MIN << " " << MAX << "\n";
+//					}
+//				}
+//				ll dx = P[j].x - P[i].x;
+//				ll dy = P[j].y - P[i].y;
+//				if (!dx) continue;
+//				if (dx * dy >= 0) dx = std::abs(dx), dy = std::abs(dy);
+//				else dx = std::abs(dx), dy = -std::abs(dy);
+//				slopes[M++] = { i, j, dx, dy };
 //			}
-//			else {
-//				dx = std::abs(dx); dy = -std::abs(dy);
+//		}
+//		std::sort(slopes, slopes + M);
+//		//std::cout << MIN << " " << MAX << "\n";
+//
+//		for (int i = 0, j; i < M; i = j) {
+//			j = i;
+//			int s = order[slopes[i].u], e = order[slopes[i].u];
+//			//Pos S = P[slopes[i].u], E = P[slopes[i].u];
+//			while (j < M && slopes[i] == slopes[j]) {
+//				int u = slopes[j].u, v = slopes[j].v;
+//				if (e < order[v]) e = order[v];
+//				//if (E < P[v]) E = P[v];
+//				int ou = order[u], ov = order[v];
+//				if (ou > 0)
+//					MIN = std::min({ MIN, std::abs(cross(P[u], P[v], P[idx[ou - 1]])) });
+//				if (ov < N - 1)
+//					MIN = std::min({ MIN, std::abs(cross(P[u], P[v], P[idx[ov + 1]])) });
+//				order[u] = ov; order[v] = ou;
+//				idx[ou] = v; idx[ov] = u;
+//				j++;
 //			}
-//			slopes[M++] = { i, j, dx, dy };
-//		}
-//	}
-//	std::sort(slopes, slopes + M);
+//			//if (s > 0) MAX = std::max({ MAX, std::abs(cross(S, E, P[idx[0]]))});
+//			if (s == 0 && e == N - 1) MAX = 0;
+//			if (s > 0) MAX = std::max({ MAX, std::abs(cross(P[idx[s]], P[idx[e]], P[idx[0]]))});
+//			//if (e < N - 1) MAX = std::max({ MAX, std::abs(cross(S, E, P[idx[N - 1]])) });
+//			if (e < N - 1) MAX = std::max({ MAX, std::abs(cross(P[idx[s]], P[idx[e]], P[idx[N - 1]])) });
+//			//std::cout << "s e " << s << " " << e << " MIN " << MIN << " MAX " << MAX << "\n";
 //
-//	for (int i = 0; i < N; i++) {
-//		pos[i] = i;  //cur. idx
-//		update(i, V[i]);
-//	}
-//	ret = std::max(ret, seg_len[1].max);
-//
-//	for (int i = 0, j; i < M; i = j) {
-//		j = i;
-//		while (j < M && slopes[i] == slopes[j]) {
-//			int u = slopes[j].u, v = slopes[j].v;
-//			int x = pos[u], y = pos[v];
-//			int vx = V[u], vy = V[v];
-//			update(x, vy); update(y, vx);
-//			pos[u] = y; pos[v] = x;
-//			j++;
 //		}
-//		ret = std::max(ret, seg_len[1].max);
+//		//std::cout << MIN << "\n" << MAX << "\n";
+//		std::cout << (MIN >> 1) << '.' << (MIN & 1 ? "5 " : "0 ");
+//		std::cout << (MAX >> 1) << '.' << (MAX & 1 ? "5\n" : "0\n");
 //	}
-//	std::cout << ret;
-//	return 0;
+//	return;
 //}
+//int main() { solve(); return 0; }//boj9484
