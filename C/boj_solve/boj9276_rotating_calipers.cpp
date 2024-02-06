d47e2fdaf2345f90a9c4a0ec08b0ed692435ec98
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-06 23:05:31 +0900

    2402062305

--- C/boj_solve/boj9276_rotating_calipers.cpp
+++ C/boj_solve/boj9276_rotating_calipers.cpp
@@ -49,14 +49,14 @@ std::vector<Pos> monotone_chain(std::vector<Pos>& C) {
 		return H;
 	}
 	for (int i = 0; i < C.size(); i++) {
-		while (H.size() > 1 && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0) 
+		while (H.size() > 1 && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
 			H.pop_back();
 		H.push_back(C[i]);
 	}
 	H.pop_back();
 	int sz = H.size() + 1;
 	for (int i = C.size() - 1; i >= 0; i--) {
-		while (H.size() > sz && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0) 
+		while (H.size() > sz && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
 			H.pop_back();
 		H.push_back(C[i]);
 	}
@@ -67,37 +67,56 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(8);
-	std::cin >> N;
-	C.resize(N);
-	for (int i = 0; i < N; i++) std::cin >> C[i].x >> C[i].y, C[i] *= 2;
-	H = monotone_chain(C);
-	N = H.size();
-	if (N == 2) { std::cout << (H[0] - H[1]).mag() << "\n"; return; }
-	//for (int i = 0; i < N; i++) std::cout << H[i].x << " " << H[i].y << "\n";
+	std::cout.precision(6);
+	//freopen("boj9276_fo.in", "r", stdin);
+	//freopen("boj9276_ans_txt", "w", stdout);
+	while (std::cin >> N) {
+		C.resize(N);
+		for (int i = 0; i < N; i++) std::cin >> C[i].x >> C[i].y, C[i] *= 2;
+		H = monotone_chain(C);
+		N = H.size();
+		if (N == 2) { std::cout << (H[0] - H[1]).mag() << "\n"; return; }
+		//for (int i = 0; i < N; i++) std::cout << H[i].x << " " << H[i].y << "\n";
 
-	int U{ 0 }, R{ 0 }, L{ 0 };
-	for (int j = 1; j < N; j++) if (cross(H[0], H[1], H[j % N], H[(j + 1) % N]) <= 0) { U = j; break; }
-	//std::cout << "DEBUG U: " << U << " \n";
-	for (int k = 0; k < N; k++) if (dot(H[0], H[1], H[k % N], H[(k + 1) % N]) <= 0) { R = k; break; }
-	//std::cout << "DEBUG R: " << R << " \n";
-	for (int l = U; l <= N; l++) if (dot(H[0], H[1], H[l % N], H[(l + 1) % N]) >= 0) { L = l; break; }
-	//std::cout << "DEBUG L: " << L << " \n";
-	U %= N, R %= N, L %= N;
-
-	ld MIN{ INF };
-	for (int i = 0; i <= N; i++) {
-		while (cross(H[i % N], H[(i + 1) % N], H[U % N], H[(U + 1) % N]) > 0) U++;
-		while (dot(H[i % N], H[(i + 1) % N], H[R % N], H[(R + 1) % N]) > 0) R++;
-		while (dot(H[i % N], H[(i + 1) % N], H[L % N], H[(L + 1) % N]) < 0) L++;
+		int U{ 0 }, R{ 0 }, L{ 0 };
+		for (int j = 1; j < N; j++) if (cross(H[0], H[1], H[j % N], H[(j + 1) % N]) <= 0) { U = j; break; }
+		//std::cout << "DEBUG U: " << U << " \n";
+		for (int k = 0; k < N; k++) if (dot(H[0], H[1], H[k % N], H[(k + 1) % N]) <= 0) { R = k; break; }
+		//std::cout << "DEBUG R: " << R << " \n";
+		for (int l = U; l <= N; l++) if (dot(H[0], H[1], H[l % N], H[(l + 1) % N]) >= 0) { L = l; break; }
+		//std::cout << "DEBUG L: " << L << " \n";
 		U %= N, R %= N, L %= N;
-		ld h = cross(H[i % N], H[(i + 1) % N], H[U % N]) / (H[i % N] - H[(i + 1) % N]).mag();
-		ld w = dot(H[i % N], H[(i + 1) % N], H[L % N], H[R % N]) / (H[i % N] - H[(i + 1) % N]).mag();
-		ld box = h + w;
-		MIN = std::min(MIN, box);
-		//std::cout << "DEBUG i: " << i << " U: " << U << " R: " << R << " L: " << L << " box: " << box << " area: " << MIN << "\n";
+
+		ld MIN{ INF };
+		for (int i = 0; i <= N; i++) {
+			while (cross(H[i % N], H[(i + 1) % N], H[U % N], H[(U + 1) % N]) > 0) U++;
+			while (dot(H[i % N], H[(i + 1) % N], H[R % N], H[(R + 1) % N]) > 0) R++;
+			while (dot(H[i % N], H[(i + 1) % N], H[L % N], H[(L + 1) % N]) < 0) L++;
+			U %= N, R %= N, L %= N;
+			ld h = cross(H[i % N], H[(i + 1) % N], H[U % N]) / (H[i % N] - H[(i + 1) % N]).mag();
+			ld w = dot(H[i % N], H[(i + 1) % N], H[L % N], H[R % N]) / (H[i % N] - H[(i + 1) % N]).mag();
+			ld box = h + w;
+			MIN = std::min(MIN, box);
+			//std::cout << "DEBUG i: " << i << " U: " << U << " R: " << R << " L: " << L << " box: " << box << " area: " << MIN << "\n";
+		}
+		std::cout << MIN << "\n";
 	}
-	std::cout << MIN << "\n";
 	return;
 }
-int main() { solve(); return 0; }//boj19586
+int main() { solve(); return 0; }//boj9276
+
+/*
+
+3
+0 0
+2 0
+1 2
+
+3
+0 0
+2 0
+-2 2
+
+4.4721359 0.893327
+
+*/
