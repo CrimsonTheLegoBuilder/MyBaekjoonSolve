5aeb211323841ed5f8e6d1332846d1fc8bf1e00e
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-07 22:03:27 +0900

    2402071003

--- C/boj_solve/bojBRONZE.cpp
+++ C/boj_solve/bojBRONZE.cpp
@@ -3,12 +3,12 @@
 #include <cstring>
 #include <vector>
 #include <cmath>
-int a, b, N;
+typedef long long ll;
+ll a, b, N;
 
 int main() {
-	std::cin >> a >> b >> N;
+	//std::cin >> a >> b >> N;
 	if (a + b >= N << 1) std::cout << a + b - (N << 1) << "\n";
 	else std::cout << a + b << "\n";
 	return 0;
 }
-
