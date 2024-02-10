d4745f1c3bfaa70b73c90765dd94028dc1a72b46
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-11 00:23:59 +0900

    2402110023

--- Python/boj2650_cross_point.py
+++ Python/boj2650_cross_point.py
@@ -30,6 +30,10 @@ def cross(d1: Pos, d2: Pos, d3: Pos) -> int or float:
     return (d2 - d1) // (d3 - d2)
 
 
+def parallel(d1: Pos, d2: Pos, d3: Pos, d4: Pos) -> bool:
+    return (d2 - d1) // (d4 - d3) == 0
+
+
 def dot(d1: Pos, d2: Pos, d3: Pos) -> int or float:
     return (d2 - d1) ** (d3 - d2)
 
@@ -39,6 +43,11 @@ def on_seg(d1: Pos, d2: Pos, d3: Pos) -> bool:
 
 
 def cross_check(d1: Pos, d2: Pos, d3: Pos, d4: Pos) -> bool:
+    if parallel(d1, d2, d3, d4) and cross(d1, d2, d3) == 0:
+        if d3 < d1 < d4 and d3 < d2 < d4:
+            return False
+        if d1 < d3 < d2 and d1 < d4 < d2:
+            return False
     f1 = cross(d1, d2, d3) * cross(d2, d1, d4) > 0
     f2 = cross(d3, d4, d1) * cross(d4, d3, d2) > 0
     f3 = on_seg(d1, d2, d3) or \
@@ -50,13 +59,13 @@ def cross_check(d1: Pos, d2: Pos, d3: Pos, d4: Pos) -> bool:
 
 def find_pos(s: int, q: int) -> Pos:
     if s == 1:
-        return Pos(q, 50)
-    elif s == 2:
         return Pos(q, 0)
+    elif s == 2:
+        return Pos(q, 50)
     elif s == 3:
-        return Pos(0, 50 - q)
+        return Pos(0, q)
     elif s == 4:
-        return Pos(50, 50 - q)
+        return Pos(50, q)
 
 
 if __name__ == '__main__':
@@ -65,6 +74,8 @@ if __name__ == '__main__':
     for _ in range(N // 2):
         s1, q1, s2, q2 = map(int, sys.stdin.readline().strip().split())
         p1, p2 = find_pos(s1, q1), find_pos(s2, q2)
+        if p2 < p1:
+            p1, p2 = p2, p1
         SEG.append([p1, p2])
     total = 0
     many = 0
@@ -77,4 +88,4 @@ if __name__ == '__main__':
                 cnt += cross_check(p1, p2, p3, p4)
         many = max(many, cnt)
         total += cnt
-    print(f'{total // 2}\n{many}')
+    # print(f'{total // 2}\n{many}')
