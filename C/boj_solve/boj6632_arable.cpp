9da10aa6b52f7aee55cb56a67294e8418bd99184
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-14 00:44:09 +0900

    2402140044

--- C/boj_solve/boj6632_arable.cpp
+++ C/boj_solve/boj6632_arable.cpp
@@ -1,133 +1,171 @@
-/*
- * CTU Open 2009
- * Sample Solution: Arable Area
- * Josef Cibulka
- */
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+typedef long long ll;
+const ll MAX = 1e16;
+const int LEN = 100;
+int N;
+int board[205][205];
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
-#define MAX(a,b) (((a)>(b))?(a):(b))
-#define MAXN 100
-#define MAXCOOR 100
-#define D 0
-
-typedef struct Pt
-{
-	int xp, xq; /* x coordinate = xp/xq */
-	int edge;
-} Pt;
-
-int n;
-int x[MAXN + 2], y[MAXN + 2];
-int inc[MAXN + 2]; /* inc[i]=1 if the interior is to the right of edge i->i+1, otherwise -1 */
-struct Pt cr[MAXN + 2];
-int crcnt;
-
-int por_pt(const void* e1, const void* e2)
-{
-	struct Pt* p1 = (struct Pt*)e1;
-	struct Pt* p2 = (struct Pt*)e2;
-	int ret = p1->xp * p2->xq - p2->xp * p1->xq;
-	if (ret == 0) return inc[p1->edge] - inc[p2->edge];
-	else return ret;
+struct Pos {
+	ll x, y;
+	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ll& n) const { return { x * n, y * n }; }
+	Pos operator / (const ll& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator ~ () const { return { -y, x }; }
+	ll operator ! () const { return x * y; }
+	ll Euc() const { return x * x + y * y; }
+	ll Man() const { return std::abs(x) + std::abs(y); }
+	ll area() const { return std::abs(x * y); }
+	//ld mag() const { return hypot(x, y); }
+}; const Pos O = { 0, 0 };
+std::vector<Pos> H;
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll area(std::vector<Pos> H, const int& sz) {
+	ll A = 0;
+	for (int i = 0; i < sz; i++) A += cross(O, H[i], H[(i + 1) % sz]);
+	return A;
 }
-
-/* round the number p/q */
-int my_round_down(int p, int q)
-{
-	assert(q > 0);
-	if (p < 0) return (p - q + 1) / q;
-	else return p / q;
-}
-
-int my_round_up(int p, int q)
-{
-	assert(q > 0);
-	if (p < 0) return p / q;
-	else return (p + q - 1) / q;
-}
-
-int main(void)
-{
-	int i, j, ii, tmp;
-	int tmpp1, tmpq1, tmpp2, tmpq2;
-	long long area;
-	int miny, maxy;
-	int cw;
-	for (ii = 1;; ii++)
-	{
-		scanf("%d ", &n);
-		if (n == 0) break;
-		maxy = -10 * MAXCOOR; miny = 10 * MAXCOOR;
-		for (i = 0; i < n; i++)
-		{
-			scanf(" %d %d ", x + i, y + i);
-			assert(x[i] >= -MAXCOOR && x[i] <= MAXCOOR && y[i] >= -MAXCOOR && y[i] <= MAXCOOR);
-			if (y[i] > maxy) maxy = y[i];
-			if (y[i] < miny) miny = y[i];
-		}
-		x[n] = x[0]; y[n] = y[0];
-
-		/* first determine whether we are going clockwise */
-		area = 0;
-		for (i = 0; i < n; i++)
-		{
-			/* count 2 * (the area to the right of edge from i to i+1) */
-			area += (2 * MAXCOOR - (x[i] + x[i + 1])) * (y[i + 1] - y[i]);
-		}
-		if (area > 0) cw = 1; else cw = 0;
-
-		for (i = 0; i < n; i++) if (cw == (y[i + 1] > y[i])) inc[i] = 1; else inc[i] = -1;
-
-
-		area = 0;
-		for (i = miny; i < maxy; i++)
-		{
-			/* find all crossings of edges with line y=i and line y=i+1 */
-			crcnt = 0;
-			for (j = 0; j < n; j++) if ((y[j] <= i && y[j + 1] > i) || (y[j + 1] <= i && y[j] > i))
-			{
-				if (y[j + 1] == y[j]) continue; /* vertical edges have no effect */
-				tmpp1 = x[j] * (y[j + 1] - y[j]) + (i - y[j]) * (x[j + 1] - x[j]);
-				tmpq1 = y[j + 1] - y[j];
-				tmpp2 = x[j] * (y[j + 1] - y[j]) + (i + 1 - y[j]) * (x[j + 1] - x[j]);
-				tmpq2 = y[j + 1] - y[j];
-
-				if ((inc[j] == 1 && tmpp1 * tmpq2 >= tmpp2 * tmpq1) || (inc[j] == -1 && (tmpp1 * tmpq2 <= tmpp2 * tmpq1)))
-				{
-					cr[crcnt].xp = tmpp1;	cr[crcnt].xq = tmpq1;
+ll GCD(ll x, ll y) { return !y ? x : GCD(y, x % y); }
+ll Pick_s_theorem(const Pos& cur, const Pos& nxt) {\
+	//Pick`s Theorem : a = i + b/2 - 1
+	Pos d = nxt - cur;
+	if (!d.x) return 0;
+	if (!d.y) return -d.x * cur.y; 
+	ll A = d.area();
+	ll C = GCD(std::abs(d.x), std::abs(d.y));
+	//ll C = std::gcd(std::abs(d.x), std::abs(d.y));
+	ll B = d.Man() + C;
+	ll inner = (A - B + 2) >> 1;//2i = A - b + 2
+	ll box = std::abs(d.x) * std::min(cur.y, nxt.y);
+	//printf("box: %d\n", box);
+	Pos s = cur, e = nxt;
+	if (e < s) std::swap(s, e);
+	int i = s.x, j = s.y;
+	if (std::abs(s.x - e.x) <= std::abs(s.y - e.y)) {
+		if (s.y < e.y) {
+			while (i < e.x) {
+				while (j < e.y && cross(s, e, Pos(i, j + 1)) > 0 && cross(s, e, Pos(i + 1, j)) < 0) {
+					board[i][j] += 1;
+					j++;
 				}
-				else
-				{
-					cr[crcnt].xp = tmpp2;	cr[crcnt].xq = tmpq2;
-				}
-				if (cr[crcnt].xq < 0) { cr[crcnt].xq *= -1; cr[crcnt].xp *= -1; }
-				cr[crcnt].edge = j;
-				crcnt++;
+				if (cross(s, e, Pos(i + 1, j)) > 0 && cross(s, e, Pos(i + 2, j - 1)) < 0) j--;
+				i++;
 			}
-			qsort(cr, crcnt, sizeof(cr[0]), por_pt);
-#if(D) 
-			for (j = 0; j < crcnt; j++) printf("cr in %d/%d with %d\n", cr[j].xp, cr[j].xq, cr[j].edge);
-#endif
-
-			tmp = 0;
-			for (j = 0; j < crcnt; j++)
-			{
-				assert(tmp <= 1);
-#if(D>1)
-				if (tmp > 0) printf("j==%d %d %d\n", j, my_round_up(cr[j - 1].xp, cr[j - 1].xq), my_round_down(cr[j].xp, cr[j].xq));
-#endif
-				if (tmp > 0) area += MAX(0, (my_round_down(cr[j].xp, cr[j].xq) - my_round_up(cr[j - 1].xp, cr[j - 1].xq)));
-				tmp += inc[cr[j].edge];
-			}
-
-#if(D) 
-			printf("%d area %lld\n", i, area);
-#endif
 		}
-		printf("%lld\n", area);
+		if (s.y > e.y) {
+			while (i < e.x) {
+				while (j > e.y && cross(s, e, Pos(i + 1, j)) > 0 && cross(s, e, Pos(i, j - 1)) < 0) {
+					board[i][j - 1] += 1;
+					j--;
+				}
+				if (cross(s, e, Pos(i + 2, j + 1)) > 0 && cross(s, e, Pos(i + 1, j)) < 0) j++;
+				i++;
+			}
+		}
 	}
-	return 0;
+	else {
+		if (s.y < e.y) {
+			while (j < e.y) {
+				while (i < e.x && cross(s, e, Pos(i, j + 1)) > 0 && cross(s, e, Pos(i + 1, j)) < 0) {
+					board[i][j] += 1;
+					i++;
+				}
+				if (cross(s, e, Pos(i - 1, j + 2)) > 0 && cross(s, e, Pos(i, j + 1)) < 0) i--;
+				j++;
+			}
+		}
+		if (s.y > e.y) {
+			while (j > e.y) {
+				while (i < e.x && cross(s, e, Pos(i + 1, j)) > 0 && cross(s, e, Pos(i, j - 1)) < 0) {
+					board[i][j - 1] += 1;
+					i++;
+				}
+				if (cross(s, e, Pos(i, j - 1)) > 0 && cross(s, e, Pos(i - 1, j - 2)) < 0) i--;
+				j--;
+			}
+		}
+	}
+	//if (d.x < 0) std::cout << inner + C - 1 + box << "\n";
+	//else if (d.x > 0) std::cout << inner + C - 1 - A - box << "\n";
+	if (d.x < 0) return inner + C - 1 + box;
+	else if (d.x > 0) return inner + C - 1 - A - box;
 }
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	//freopen("boj6632.txt", "r", stdin);
+	//freopen("boj6632.out", "w", stdout);
+	int cnt = 0;
+	while (1) {
+		std::cin >> N;
+		if (!N) return;
+		H.resize(N);
+		memset(board, 0, sizeof board);
+		for (int i = 0; i < N; i++) std::cin >> H[i].x >> H[i].y, H[i].x += 101, H[i].y += 101;
+		if (area(H, N) < 0) std::reverse(H.begin(), H.end());
+		ll box = 0;
+		std::cout << cnt++ << "\n";
+		for (int i = 0; i < N; i++) std::cout << H[i].x << " " << H[i].y << "\n";
+ 		for (int i = 0; i < N; i++) 
+			box += Pick_s_theorem(H[i], H[(i + 1) % N]);
+		for (int i = 0; i < 205; i++)
+			for (int j = 0; j < 205; j++)
+				box += board[i][j] >> 1;
+		std::cout << box << "\n";
+		for (int i = 99; i < 120; i++) {
+			for (int j = 99; j < 120; j++) {
+				std::cout << board[i][j];
+			}
+			std::cout << "\n";
+		}
+	}
+}
+int main() { solve(); return 0; }//boj6632
+
+
+/*
+
+4
+3 3
+13 14
+2 2
+14 13
+
+3
+0 0
+10 10
+9 10
+
+13
+1 7
+1 1
+2 4
+3 1
+3 4
+5 1
+4 4
+7 1
+5 4
+9 1
+6 4
+1 1
+2 7
+0
+
+
+*/
+
+
