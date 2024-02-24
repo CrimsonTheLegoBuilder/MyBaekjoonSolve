63185baef268bc1477e31418a4426b5f0110d28d
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-24 11:59:35 +0900

    2402241159

--- Cpp/boj_solve/__Geometry_int__.cpp
+++ Cpp/boj_solve/__Geometry_int__.cpp
@@ -236,7 +236,7 @@ void rotating_calipers() {
 	std::cout << ans << "\n";
 	return;
 }
-Pos find_tangent_bi_search(Pos H[], const int& sz, const Pos& p) {
+Info find_tangent_bi_search(Pos H[], const int& sz, const Pos& p) {
 	int i1{ 0 }, i2{ 0 };
 	int ccw1 = ccw(p, H[0], H[1]), ccwN = ccw(p, H[0], H[sz - 1]);
 	if (ccw1 * ccwN >= 0) {
@@ -294,7 +294,7 @@ Pos find_tangent_bi_search(Pos H[], const int& sz, const Pos& p) {
 		if (!ccw(p, H[i2], H[(i2 + 1) % sz]) && dot(p, H[(i2 + 1) % sz], H[i2]) > 0) i2 = (i2 + 1) % sz;
 	}
 	if (i2 < i1) std::swap(i2, i1);//normalize
-	return { i2, i1 };
+	return { 0, i2, i1 };
 }
 Info get_inner_area(Pos H[], ll memo[], const int& sz, const Pos& p) {
 	Info tangent = find_tangent_bi_search(H, sz, p);
@@ -605,4 +605,106 @@ std::vector<Face> convex_hull_3D(std::vector<Pos3D>& candi) {
 	std::vector<Face> hull3D;
 	for (int i = 0; i < faces.size(); i++) if (active[i]) hull3D.push_back(faces[i]);
 	return hull3D;
+}
+
+using pi = std::pair<int, int>;
+#define sz(v) ((int)((v).size()))
+const int MAXN = 1005;
+
+struct vec3 {
+	ll x, y, z;
+	vec3() : x(0), y(0), z(0) {}
+	vec3(ll a, ll b, ll c) : x(a), y(b), z(c) {}
+	vec3 operator*(const vec3& v) const { return vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+	vec3 operator-(const vec3& v) const { return vec3(x - v.x, y - v.y, z - v.z); }
+	vec3 operator-() const { return vec3(-x, -y, -z); }
+	ll dot(const vec3& v) const { return x * v.x + y * v.y + z * v.z; }
+};
+
+struct twoset {
+	int a, b;
+	void insert(int x) { (a == -1 ? a : b) = x; }
+	bool contains(int x) { return a == x || b == x; }
+	void erase(int x) { (a == x ? a : b) = -1; }
+	int size() { return (a != -1) + (b != -1); }
+} E[MAXN][MAXN]; // i < j
+
+struct face {
+	vec3 norm;
+	ll disc;
+	int I[3];
+};
+
+face make_face(int i, int j, int k, int ii, std::vector<vec3>& A) { // p^T * norm < disc
+	E[i][j].insert(k); E[i][k].insert(j); E[j][k].insert(i);
+	face f; f.I[0] = i, f.I[1] = j, f.I[2] = k;
+	f.norm = (A[j] - A[i]) * (A[k] - A[i]);
+	f.disc = f.norm.dot(A[i]);
+	if (f.norm.dot(A[ii]) > f.disc) {
+		f.norm = -f.norm;
+		f.disc = -f.disc;
+	}
+	return f;
+}
+
+std::vector<face> get_hull(std::vector<vec3>& A) {
+	int N = A.size();
+	std::vector<face> faces; memset(E, -1, sizeof(E));
+	faces.push_back(make_face(0, 1, 2, 3, A));
+	faces.push_back(make_face(0, 1, 3, 2, A));
+	faces.push_back(make_face(0, 2, 3, 1, A));
+	faces.push_back(make_face(1, 2, 3, 0, A));
+	for (int i = 4; i < N; ++i) {
+		for (int j = 0; j < faces.size(); ++j) {
+			face f = faces[j];
+			if (f.norm.dot(A[i]) > f.disc) {
+				E[f.I[0]][f.I[1]].erase(f.I[2]);
+				E[f.I[0]][f.I[2]].erase(f.I[1]);
+				E[f.I[1]][f.I[2]].erase(f.I[0]);
+				faces[j--] = faces.back();
+				faces.pop_back();
+			}
+		}
+		int nf = faces.size();
+		for (int j = 0; j < nf; ++j) {
+			face f = faces[j];
+			for (int a = 0; a < 3; ++a) for (int b = a + 1; b < 3; ++b) {
+				int c = 3 - a - b;
+				if (E[f.I[a]][f.I[b]].size() == 2) continue;
+				faces.push_back(make_face(f.I[a], f.I[b], i, f.I[c], A));
+			}
+		}
+	}
+	return faces;
+}
+
+
+void solve() {
+	int n; scanf("%d", &n);
+	if (n == 0) exit(0);
+	std::vector<vec3> inp;
+	for (int i = 1; i <= n; i++) {
+		int x, y, z; scanf("%d %d %d", &x, &y, &z);
+		inp.emplace_back(x, y, z);
+	}
+	auto h = get_hull(inp);
+	int q; scanf("%d", &q);
+	while (q--) {
+		int x, y, z; scanf("%d %d %d", &x, &y, &z);
+		vec3 qry(x, y, z);
+		double ret = 1e18;
+		for (auto& i : h) {
+			double dist = i.disc - i.norm.dot(qry);
+			dist = dist * dist / i.norm.dot(i.norm);
+			ret = std::min(ret, dist);
+		}
+		printf("%.4f\n", sqrt(ret));
+	}
+}
+
+int main() {
+	int tc = 1e9;// scanf("%d",&tc);
+	for (int i = 1; i <= tc; i++) {
+		solve();
+	}
 }
