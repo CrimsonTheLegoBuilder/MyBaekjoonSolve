caf71d48c97a7d2ee67aca3c9bbdaae580a6a846
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-25 11:59:48 +0900

    2402251159

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -1,16 +1,34 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
-#include <vector>
-#include <cmath>
-typedef long long ll;
-ll N, ans;
+//#include <iostream>
+//#include <algorithm>
+//#include <cstring>
+//#include <vector>
+//#include <cmath>
+//typedef long long ll;
+//ll N, ans;
+//
+//int main() {
+//	std::cin >> N;
+//	ans = N % 8;
+//	if (!ans) std::cout << "2\n";
+//	else if (1 <= ans && ans <= 5) std::cout << ans << "\n";
+//	//else std::cout << 10 - ans << "\n";
+//	return 0;
+//}
 
-int main() {
-	std::cin >> N;
-	ans = N % 8;
-	if (!ans) std::cout << "2\n";
-	else if (1 <= ans && ans <= 5) std::cout << ans << "\n";
-	//else std::cout << 10 - ans << "\n";
-	return 0;
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 1, i, a, m;
+    cin >> m;
+    for (i = 1; i <= m; i++)
+    {
+        cin >> a;
+        if (i == 1 || i == m)
+        {
+            n = n * a;
+        }
+    }
+    cout << n;
+    return 0;
 }
