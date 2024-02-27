be8cc217520bd12f2c4e5c458feac2f8e4148978
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-28 00:10:39 +0900

    2310290010

--- Cpp/boj_solve/boj18252_ch_2ptr.cpp
+++ Cpp/boj_solve/boj18252_ch_2ptr.cpp
@@ -5,35 +5,16 @@
 #include <cmath>
 #include <cstring>
 typedef long long ll;
-typedef long double ld;
-const ll INF = 1e17;
 const int LEN = 1e5 + 1;
-const ld TOL = 1e-7;
 int N, M, T, Q;
-bool zero(const ld& x) { return std::abs(x) < TOL; }
-int dcmp(const ld& x) { return std::abs(x) < TOL ? 0 : x > 0 ? 1 : -1; }
-int dcmp(const ll& x) { return !x ? 0 : x > 0 ? 1 : -1; }
 
 struct Pos {
 	ll x, y;
 	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
-	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ll& n) const { return { x * n, y * n }; }
-	Pos operator / (const ll& n) const { return { x / n, y / n }; }
-	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
 	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
-	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
-	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
-	Pos operator ~ () const { return { -y, x }; }
-	ll operator ! () const { return x * y; }
 	ll Euc() const { return x * x + y * y; }
-	ll Man() const { return std::abs(x) + std::abs(y); }
-	ld mag() const { return hypot(x, y); }
 	friend std::istream& operator >> (std::istream& is, Pos& p);
 	friend std::ostream& operator << (std::ostream& os, const Pos& p);
 } NH[LEN], MH[LEN], seq[LEN]; const Pos O = { 0, 0 };
@@ -67,9 +48,68 @@ std::vector<Pos> graham_scan(std::vector<Pos>& C) {
 	}
 	return H;
 }
+//void solve() {
+//	Pos top, bot;
+//	ll y, xst, xen;
+//	std::cin >> N >> top >> bot;
+//	C = { top, bot };
+//	for (int i = 0; i < N; i++) {
+//		std::cin >> y >> xst >> xen;
+//		if (ccw(bot, top, Pos(xst, y)) < 0) C.push_back(Pos(xst, y));
+//		if (ccw(bot, top, Pos(xen, y)) > 0) C.push_back(Pos(xen, y));
+//	}
+//	H = graham_scan(C);
+//	int sz = H.size();
+//	if (sz == 2) { std::cout << "0.0000000\n"; return; }
+//	if (sz == 3) {
+//		ll ret = cross(H[0], H[1], H[2]);
+//		std::cout << (ret >> 1) << (ret & 1 ? ".5000000\n" : ".0000000\n"); return;
+//	}
+//
+//	ll pre = 0, ans = 0;
+//	for (int i = 0, j, k; i < sz; i++) {//O(N^2)
+//		j = (i + 2) % sz;
+//		k = (i + 1) % sz;
+//		pre = 0;
+//		while ((j + 1) % sz != i) {
+//			while ((k + 1) % sz != j && cross(H[i], H[(k + 1) % sz], H[j]) > cross(H[i], H[k], H[j])) k = (k + 1) % sz;
+//			ll cur = cross(H[i], H[k], H[j]);
+//			if (pre > cur) break;
+//			pre = cur;
+//			ans = std::max(ans, pre);
+//			j = (j + 1) % sz;
+//		}
+//	}
+//	std::cout << (ans >> 1) << (ans & 1 ? ".5000000\n" : ".0000000\n");
+//	return;
+//}
+//int main() { solve(); return 0; }//boj18252 The Starry Night
+
+/*
+
+4
+2 4 0 0
+1 2 4
+1 2 3
+2 3 5
+3 2 4
+
+8
+10 10 10 0
+2 6 7
+4 4 5
+6 4 5
+8 6 7
+2 13 14
+4 15 16
+6 15 16
+8 13 14
+
+*/
+
 void solve() {
 	Pos top, bot;
-	int y, xst, xen;
+	ll y, xst, xen;
 	std::cin >> N >> top >> bot;
 	C = { top, bot };
 	for (int i = 0; i < N; i++) {
@@ -86,12 +126,12 @@ void solve() {
 	}
 
 	ll pre = 0, ans = 0;
-	for (int i = 0, j, k; i < sz; i++) {
-		j = (i + 2) % sz;
-		k = (i + 1) % sz;
+	int i = 0, j = 2, k = 1;
+	for (i, j, k; i < sz; i++) {//O(N)
 		pre = 0;
-		while (j != i) {
-			while ((k + 1) % sz != j && cross(H[i], H[k], H[j]) > pre) k = (k + 1) % sz;
+		j = (j - 1 + sz) % sz;
+		while ((j + 1) % sz != i) {
+			while ((k + 1) % sz != j && cross(H[i], H[(k + 1) % sz], H[j]) > cross(H[i], H[k], H[j])) k = (k + 1) % sz;
 			ll cur = cross(H[i], H[k], H[j]);
 			if (pre > cur) break;
 			pre = cur;
@@ -99,18 +139,58 @@ void solve() {
 			j = (j + 1) % sz;
 		}
 	}
-	//std::cout << ans << "\n";
 	std::cout << (ans >> 1) << (ans & 1 ? ".5000000\n" : ".0000000\n"); return;
 }
-int main() { solve(); return 0; }
+int main() { solve(); return 0; }//boj18252 The Starry Night
 
-/*
+			//while ((k - 1 + sz) % sz != i && cross(H[i], H[(k - 1 + sz) % sz], H[j]) > cross(H[i], H[k], H[j])) k = (k - 1 + sz) % sz;
+//j = (i + 2) % sz;
+//k = (i + 1) % sz;
 
-4
-2 4 0 0
-1 2 4
-1 2 3
-2 3 5
-3 2 4
-
-*/
+//void solve() {
+//	Pos top, bot;
+//	ll y, xst, xen;
+//	freopen("boj9484_triangle_in.txt", "r", stdin);
+//	while (1) {
+//		std::cin >> N;
+//		if (!N) return;
+//		//std::cin >> top >> bot;
+//		//C = { top, bot };
+//		C.resize(N);
+//		for (int i = 0; i < N; i++) {
+//			std::cin >> C[i];
+//			//std::cin >> y >> xst >> xen;
+//			//if (ccw(bot, top, Pos(xst, y)) < 0) C.push_back(Pos(xst, y));
+//			//if (ccw(bot, top, Pos(xen, y)) > 0) C.push_back(Pos(xen, y));
+//		}
+//		H = graham_scan(C);
+//		int sz = H.size();
+//		if (sz == 2) { std::cout << "0.0000000\n"; continue; }
+//		if (sz == 3) {
+//			ll ret = cross(H[0], H[1], H[2]);
+//			std::cout << (ret >> 1) << (ret & 1 ? ".5000000\n" : ".0000000\n"); continue;
+//		}
+//
+//		ll pre = 0, ans = 0;
+//		int i = 0, j = 2, k = 1;
+//		for (i, j, k; i < sz; i++) {
+//			//j = (i + 2) % sz;
+//			//k = (i + 1) % sz;
+//			pre = 0;
+//			j = (j - 1 + sz) % sz;
+//			while ((j + 1) % sz != i) {
+//				//while ((k - 1 + sz) % sz != i && cross(H[i], H[(k - 1 + sz) % sz], H[j]) > cross(H[i], H[k], H[j])) k = (k - 1 + sz) % sz;
+//				//while ((j - 1 + sz) % sz != k && cross(H[i], H[k], H[(j - 1 + sz) % sz]) > cross(H[i], H[k], H[j])) j = (j - 1 + sz) % sz;
+//				while ((k + 1) % sz != j && cross(H[i], H[(k + 1) % sz], H[j]) > cross(H[i], H[k], H[j])) k = (k + 1) % sz;
+//				ll cur = cross(H[i], H[k], H[j]);
+//				if (pre > cur) break;
+//				pre = cur;
+//				ans = std::max(ans, pre);
+//				j = (j + 1) % sz;
+//			}
+//		}
+//		//std::cout << ans << "\n";
+//		std::cout << (ans >> 1) << (ans & 1 ? ".5000000\n" : ".0000000\n");
+//	}
+//}
+//int main() { solve(); return 0; }
