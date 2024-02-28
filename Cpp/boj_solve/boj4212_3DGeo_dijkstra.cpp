51853ccc89267d9079f7c3048cd7f8292fecda74
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-02-29 01:40:08 +0900

    2402290140

---
+++ Cpp/boj_solve/boj4212_3DGeo_dijkstra.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstring>
+typedef long long ll;
+typedef long double ld;
+const ld INF = 1e17;
+const ld TOL = 1e-7;
+const ld PI = acos(-1);
+const ld ERAD = 6370;
+const int LEN = 800;
+int N, M, T, Q;
+ld R, th;
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int dcmp(const ld& x) { return std::abs(x) < TOL ? 0 : x > 0 ? 1 : -1; }
+struct Seq { int x, y; Seq(int X = 0, int Y = 0) : x(X), y(Y) {} };
+std::vector<Seq> seq;
+
+struct Pos3D {
+	ld x, y, z;
+	Pos3D(ld X = 0, ld Y = 0, ld Z = 0) : x(X), y(Y), z(Z) {}
+	bool operator == (const Pos3D& p) const { return zero(x - p.x) && zero(y - p.y) && zero(z - p.z); }
+	bool operator != (const Pos3D& p) const { return !zero(x - p.x) || !zero(y - p.y) || !zero(z - p.z); }
+	bool operator < (const Pos3D& p) const { return zero(x - p.x) ? zero(y - p.y) ? z < p.z : y < p.y : x < p.x; }
+	ld operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
+	Pos3D operator / (const Pos3D& p) const {
+		Pos3D ret;
+		ret.x = y * p.z - z * p.y;
+		ret.y = z * p.x - x * p.z;
+		ret.z = x * p.y - y * p.x;
+		return ret;
+	}
+	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
+	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
+	Pos3D operator * (const ld& scalar) const { return { x * scalar, y * scalar, z * scalar }; }
+	Pos3D operator / (const ld& scalar) const { return { x / scalar, y / scalar, z / scalar }; }
+	Pos3D& operator += (const Pos3D& p) { x + p.x; y + p.y; z + p.z; return *this; }
+	Pos3D& operator *= (const ld& scalar) { x* scalar; y* scalar; z* scalar; return *this; }
+	ld Euc() const { return x * x + y * y + z * z; }
+	ld mag() const { return sqrtl(Euc()); }
+	ld lon() const { return atan2(y, x); }
+	ld lat() const { return atan2(sqrtl(x * x + y * y), z); }
+	Pos3D unit() const { return *this / mag(); }
+	Pos3D norm(const Pos3D& p) const { return (*this / p).unit(); }
+	Pos3D rotate(const ld& th, const Pos3D& axis) const {
+		ld SIN = sin(th), COS = cos(th);
+		Pos3D norm = axis.unit();
+		return norm * (*this * norm) * (1 - COS) + (*this * COS) - *this / norm * SIN;
+	}
+	friend std::istream& operator >> (std::istream& is, Pos3D& p) { is >> p.x >> p.y >> p.z; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos3D& p) { os << p.x << " " << p.y << " " << p.z << "\n"; return os; }
+}; const Pos3D MAXP3D = { INF, INF, INF };
+std::vector<Pos3D> poses;//3D
+struct Line3D {
+	Pos3D dir, p0;
+	Line3D(Pos3D DIR = Pos3D(0, 0, 0), Pos3D P0 = Pos3D(0, 0, 0)) : dir(DIR), p0(P0) {}
+};
+struct Plane {
+	ld a, b, c, d;
+	Plane(ld A = 0, ld B = 0, ld C = 0, ld D = 0) : a(A), b(B), c(C), d(D) {}
+	Pos3D norm() const { return Pos3D(a, b, c); };
+	friend std::istream& operator >> (std::istream& is, Plane& f) { is >> f.a >> f.b >> f.c >> f.d; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Plane& f) { os << f.a << " " << f.b << " " << f.c << " " << f.d << "\n"; return os; }
+};
+int above(const Plane& S, const Pos3D& p) {
+	ld ret = p * S.norm() + S.d;
+	return dcmp(ret);
+}
+
+std::vector<Pos3D> circle_intersection(Pos3D a, Pos3D perp, ld r) {
+	Pos3D diff = a - perp * (a * perp);
+}
