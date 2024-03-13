f584b98f93eff2a32fb0b4c2150a21db5fd3220b
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-03-13 20:18:20 +0900

    2403132018

--- Cpp/boj_solve/boj14633_brute.cpp
+++ Cpp/boj_solve/boj14633_brute.cpp
@@ -201,10 +201,10 @@ void brute() {
                 Pos d3 = H[(k + 2) % N];
                 if (collinear(I, J, d1, d2)) {
                     ld dot1 = projection(I, J, d1), dot2 = projection(I, J, d2);
-                    if (dot1 >= 0 && dot1 >= dot2 && ccw(I, J, d0) < 0) jj = std::min(jj, projection(I, J, d1));
-                    if (dot1 <= 0 && dot1 <= dot2 && ccw(I, J, d0) > 0) ii = std::min(ii, -projection(I, J, d1));
-                    if (dot2 >= 0 && dot2 >= dot1 && ccw(I, J, d3) > 0) jj = std::min(jj, projection(I, J, d2));
-                    if (dot2 <= 0 && dot2 <= dot1 && ccw(I, J, d3) < 0) ii = std::min(ii, -projection(I, J, d2));
+                    if ((dot1 > 0 || zero(dot1)) && dot1 > dot2 && ccw(I, J, d0) < 0) jj = std::min(jj, projection(I, J, d1));
+                    if ((dot1 < 0 || zero(dot1)) && dot1 < dot2 && ccw(I, J, d0) > 0) ii = std::min(ii, -projection(I, J, d1));
+                    if ((dot2 > 0 || zero(dot2)) && dot2 > dot1 && ccw(I, J, d3) > 0) jj = std::min(jj, projection(I, J, d2));
+                    if ((dot2 < 0 || zero(dot2)) && dot2 < dot1 && ccw(I, J, d3) < 0) ii = std::min(ii, -projection(I, J, d2));
                 }
                 else if (ccw(d1, d2, I) >= 0 && ccw(I, J, d2) < 0 &&
                     (ccw(I, J, d1) > 0 || (!ccw(I, J, d1) && ccw(I, J, d0) > 0))) {
