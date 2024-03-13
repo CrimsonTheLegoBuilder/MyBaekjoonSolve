4721de77de2355da064b9032882f06e450127000
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-03-14 01:13:46 +0900

    2403140113

--- Cpp/boj_solve/boj28046_Minokwski.cpp
+++ Cpp/boj_solve/boj28046_Minokwski.cpp
@@ -53,11 +53,33 @@ int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
 	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
 }
-int inner_check_bi_search(std::vector<Pos>& H, const Pos& p) {//convex
+std::vector<Pos> graham_scan(std::vector<Pos>& C) {
+	std::vector<Pos> H;
+	if (C.size() < 3) {
+		std::sort(C.begin(), C.end());
+		return C;
+	}
+	std::swap(C[0], *min_element(C.begin(), C.end()));
+	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
+		int ret = ccw(C[0], p, q);
+		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
+		return ret > 0;
+		}
+	);
+	//C.erase(unique(C.begin(), C.end()), C.end());
+	int sz = C.size();
+	for (int i = 0; i < sz; i++) {
+		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
+			H.pop_back();
+		H.push_back(C[i]);
+	}
+	return H;
+}
+int inner_check_bi_search(const std::vector<Pos>& H, const Pos& p) {//convex
 	int sz = H.size();
-	if (!sz) return -1;
-	if (sz == 1) return p == H[0] ? 0 : -1;
-	if (sz == 2) return on_seg_strong(H[0], H[1], p) ? 0 : -1;
+	//if (!sz) return -1;
+	//if (sz == 1) return p == H[0] ? 0 : -1;
+	//if (sz == 2) return on_seg_strong(H[0], H[1], p) ? 0 : -1;
 	if (cross(H[0], H[1], p) < 0 || cross(H[0], H[sz - 1], p) > 0) return -1;
 	if (on_seg_strong(H[0], H[1], p) || on_seg_strong(H[0], H[sz - 1], p)) return 0;
 	int s = 0, e = sz - 1, m;
@@ -70,31 +92,39 @@ int inner_check_bi_search(std::vector<Pos>& H, const Pos& p) {//convex
 	else if (on_seg_strong(H[s], H[e], p)) return 0;
 	else return -1;
 }
+ll area(std::vector<Pos>& H) {
+	Pos P = { 0, 0 };
+	ll ret = 0;
+	int h = H.size();
+	for (int i = 0; i < h; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % h];
+		ret += cross(P, cur, nxt);
+	}
+	return ret;
+}
+void norm(std::vector<Pos>& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
 std::vector<Pos> Minkowski_sum(std::vector<Pos>& A, std::vector<Pos>& B, const bool& f = 0) {
 	int sza = A.size();
 	int szb = B.size();
 	std::vector<Pos> minkowsum;
 
 	if (f) { for (Pos& p : A) p *= 2; for (Pos& p : B) p *= -1; }
-	//std::cout << "DEBUG\n";
 	int a = 0, b = 0;
 	for (int i = 0; i < sza; i++) if (A[i] < A[a]) a = i;
-	for (int j = 0; j < szb; j++) if (B[j] < B[b]) b = j;
-	//std::cout << "DEBUG\n"; 
+	for (int j = 0; j < szb; j++) if (B[j] < B[b]) b = j; 
 	minkowsum.push_back(A[a] + B[b]);
 	//std::cout << sza + szb << "\n";
 	for (int k = 1; k < sza + szb; k++) {
 		//std::cout << "DEBUG\n";
 		Pos a1 = A[(a + 1) % sza] + B[b];
 		Pos b1 = B[(b + 1) % szb] + A[a];
-		Pos& cur = *(minkowsum.end() - 1);
-		if (ccw(cur, a1, b1) >= 0) minkowsum.push_back(a1), a = (a + 1) % sza;// , std::cout << a1;
+		if (ccw(minkowsum.back(), a1, b1) > 0) minkowsum.push_back(a1), a = (a + 1) % sza;// , std::cout << a1;
 		else minkowsum.push_back(b1), b = (b + 1) % szb;// , std::cout << b1;
 	}
 
 	if (f) { for (Pos& p : A) p /= 2; for (Pos& p : B) p *= -1; }
-
-	return minkowsum;
+	return graham_scan(minkowsum);
+	//return minkowsum;
 }
 void query() {
 	Pos p;
@@ -111,8 +141,10 @@ void solve() {
 	std::cin >> N;
 	H1.resize(N);
 	for (int i = 0; i < N; i++) std::cin >> H1[i];
+	//norm(H1);
 	std::cin >> M;
 	H2.resize(M);
+	//norm(H2);
 	for (int j = 0; j < M; j++) std::cin >> H2[j];
 	MM = Minkowski_sum(H1, H2);
 	ML = Minkowski_sum(H1, H2, 1);
@@ -121,9 +153,9 @@ void solve() {
 	while (Q--) query();
 	return;
 }
-int main() { solve(); return 0; }//boj28046 Gravitational Wave Detector
+int main() { solve(); return 0; }//Gravitational Wave Detector
 
-
+//boj28046 Gravitational Wave Detector
 //std::vector<Pos> graham_scan(std::vector<Pos>& C) {
 //	std::vector<Pos> H;
 //	if (C.size() < 3) {
