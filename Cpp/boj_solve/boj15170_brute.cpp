6347682982165183efff48b9938d68319bbfee29
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-03-16 00:15:46 +0900

    2403160015

--- Cpp/boj_solve/boj15170_brute.cpp
+++ Cpp/boj_solve/boj15170_brute.cpp
@@ -47,6 +47,7 @@ struct Pos {
 } POS[LEN << 1], T[LEN]; const Pos O = { 0, 0, -1 };
 struct Vec {
 	ld vy, vx;
+	Vec(ld Y = 0, ld X = 0) : vy(Y), vx(X) {}
 	bool operator < (const Vec& v) const { return zero(vy - v.vy) ? vx < v.vx : vy < v.vy; }
 	bool operator == (const Vec& v) const { return (zero(vy - v.vy) && zero(vx - v.vx)); }
 	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }
@@ -55,6 +56,7 @@ struct Vec {
 struct Line {
 	Vec s;
 	ld c;
+	Line(Vec V = Vec(0, 0), ld C = 0) : s(V), c(C) {}
 	bool operator < (const Line& l) const {
 		bool f1 = Zero < s;
 		bool f2 = Zero < l.s;
@@ -119,8 +121,33 @@ Circle C(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ld r = (c - d1).Euc();
 	return { c, r };
 }
+Circle enclose_ssibal(const Pos& u, const Pos& v, const Pos& w) {
+	Pos b = v - u, c = w - u;
+	ld B = b * b, C = c * c;
+	ld D = b / c;
+	if (zero(D)) return { { 0, 0 }, -1 };
+	ld X = (c.y * B - b.y * C) / (2 * D);
+	ld Y = (b.x * C - c.x * B) / (2 * D);
+	Pos m = Pos(X, Y);
+	return Circle(u + m, m * m);
+}
+Circle enclose(const Pos& u, const Pos& v, const Pos& w) {
+	Pos B = v - u, C = w - u;
+	Line B_ = Line({ B.x, B.y }, B.Euc() / 2);
+	Line C_ = Line({ C.x, C.y }, C.Euc() / 2);
+	Pos inx = intersection(B_, C_);
+	return Circle(inx + u, inx.Euc());
+}
+Circle enclose_circle(const Pos& u, const Pos& v, const Pos& w) {
+	Line l1 = rotate90(L(u, v), (u + v) * .5);
+	Line l2 = rotate90(L(v, w), (v + w) * .5);
+	if (zero(l1 / l2)) return { { 0, 0 }, -1 };
+	Pos c = intersection(l1, l2);
+	ld r = (c - u).Euc();
+	return Circle(c, r);
+}
+char F;
 int main() {
-	char F;
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cin >> N;
@@ -153,7 +180,8 @@ int main() {
 		for (int j = i + 1; j < N; j++) {
 			for (int k = j + 1; k < N; k++) {
 				Pos d1 = T[i], d2 = T[j], d3 = T[k];
-				Circle c = C(d1, d2, d3);
+				//Circle c = C(d1, d2, d3);
+				Circle c = enclose_circle(d1, d2, d3);
 				if (c.r < 0) continue;
 				bool f = 1;
 				for (int l = 0; l < N; l++) {
@@ -172,6 +200,192 @@ int main() {
 	std::cout << "Yes\n";
 	return 0;
 }
+
+//#include <iostream>
+//#include <algorithm>
+//#include <cmath>
+//typedef long long ll;
+////typedef long double ld;
+//typedef double ld;
+//const int LEN = 121;
+//const ld TOL = 1e-9;
+//const ld PI = acos(-1);
+//int N, M;
+//ld x, y;
+//bool zero(const ld& x) { return std::abs(x) < TOL; }
+//int dcmp(const ld& x) { return std::abs(x) < TOL ? 0 : x > 0 ? 1 : -1; }
+//int dcmp(const ll& x) { return !x ? 0 : x > 0 ? 1 : -1; }
+//ld norm(ld& th) {
+//	while (th < -TOL) th += PI * 2;
+//	while (th > PI * 2) th -= PI * 2;
+//	return th;
+//}
+//
+//struct Pos {
+//	ld x, y;
+//	int i;
+//	Pos(ld X = 0, ld Y = 0, int I = 0) : x(X), y(Y), i(I) {}
+//	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+//	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+//	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y, 0 }; }
+//	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y, 0 }; }
+//	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar, 0 }; }
+//	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar, 0 }; }
+//	ld operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+//	ld operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+//	Pos operator ~ () const { return { -y, x, 0 }; }
+//	ld operator ! () const { return x * y; }
+//	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+//	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
+//	ld Euc() const { return x * x + y * y; }
+//	ld mag() const { return hypot(x, y); }
+//	friend std::istream& operator >> (std::istream& is, Pos& p) {
+//		is >> p.x >> p.y;
+//		return is;
+//	}
+//	friend std::ostream& operator << (std::ostream& os, const Pos& p) {
+//		os << p.x << " " << p.y << "\n";
+//		return os;
+//	}
+//} POS[LEN << 1], T[LEN]; const Pos O = { 0, 0, -1 };
+//struct Vec {
+//	ld vy, vx;
+//	Vec(ld Y = 0, ld X = 0) : vy(Y), vx(X) {}
+//	bool operator < (const Vec& v) const { return zero(vy - v.vy) ? vx < v.vx : vy < v.vy; }
+//	bool operator == (const Vec& v) const { return (zero(vy - v.vy) && zero(vx - v.vx)); }
+//	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }
+//	Vec operator ~ () const { return { -vx, vy }; }
+//}; const Vec Zero = { 0, 0 };
+//struct Line {
+//	Vec s;
+//	ld c;
+//	Line(Vec V = Vec(0, 0), ld C = 0) : s(V), c(C) {}
+//	bool operator < (const Line& l) const {
+//		bool f1 = Zero < s;
+//		bool f2 = Zero < l.s;
+//		if (f1 != f2) return f1;
+//		ld CCW = s / l.s;
+//		return zero(CCW) ? c * hypot(l.s.vy, l.s.vx) < l.c * hypot(s.vy, s.vx) : CCW > 0;
+//	}
+//	ld operator / (const Line& l) const { return s / l.s; }
+//	Line operator ~ () const { return { { s.vx, -s.vy }, c }; }  //rotate pi/2
+//};
+//struct Circle {
+//	Pos c;
+//	ld r;
+//	Circle(Pos C = Pos(0, 0, 0), ld R = 0) : c(C), r(R) {}
+//	bool operator == (const Circle& p) const { return c == p.c && std::abs(r - p.r) < TOL; }
+//	bool operator != (const Circle C) const { return std::abs(C.c.x - c.x) >= TOL || std::abs(C.c.y - c.y) >= TOL || std::abs(r - C.r) >= TOL; }
+//	Circle operator + (const Circle& C) const { return { {c.x + C.c.x, c.y + C.c.y}, r + C.r }; }
+//	Circle operator - (const Circle& C) const { return { {c.x - C.c.x, c.y - C.c.y}, r - C.r }; }
+//	ld H(const ld& th) const { return sin(th) * c.x + cos(th) * c.y + r; }// coord trans | check right
+//	friend std::ostream& operator << (std::ostream& os, const Circle& c) {
+//		//os << c.c.x << " " << c.c.y << " " << c.r << "\n";
+//		os << c.c.x << " " << c.c.y << " " << sqrtl(c.r) << "\n";
+//		return os;
+//	}
+//};
+//Pos mid(const Pos& d1, const Pos& d2) { return (d1 + d2) * .5; }
+//Line L(const Pos& d1, const Pos& d2) {
+//	return {
+//{ d2.y - d1.y, d1.x - d2.x },
+//(d2.y - d1.y) * d1.x - (d1.x - d2.x) * d1.y };
+//}
+//Line L(const Line& v, const Pos& p) {
+//	return
+//	{
+//	{ v.s.vy, v.s.vx },
+//	v.s.vy * p.x + v.s.vx * p.y
+//	};
+//}
+//Pos intersection(const Line& l1, const Line& l2) {
+//	Vec v1 = l1.s, v2 = l2.s;
+//	ld det = v1 / v2;
+//	return {
+//		(l1.c * v2.vx - l2.c * v1.vx) / det,
+//		(l2.c * v1.vy - l1.c * v2.vy) / det,
+//		0
+//	};
+//}
+//Line rotate90(const Line& l, const Pos& p) {
+//	Vec s = ~l.s;
+//	ld c = s.vy * p.x + s.vx * p.y;
+//	return { s, c };
+//}
+//Circle C(const Pos& d1, const Pos& d2) {
+//	Pos c = mid(d1, d2);
+//	ld r = (c - d1).Euc();
+//	return { c, r };
+//}
+//Circle C(const Pos& d1, const Pos& d2, const Pos& d3) {
+//	Line l1 = rotate90(L(d1, d2), mid(d1, d2)), l2 = rotate90(L(d2, d3), mid(d2, d3));
+//	if (zero(l1 / l2)) return { { 0, 0 }, -1 };
+//	Pos c = intersection(l1, l2);
+//	ld r = (c - d1).Euc();
+//	return { c, r };
+//}
+//Circle enclose(const Pos& u, const Pos& v, const Pos& w) {
+//	Pos B = v - u, C = w - u;
+//	if (zero(B / C)) return { { 0, 0 }, -1 };
+//	Line B_ = Line({ B.x, B.y }, B.Euc() / 2);
+//	Line C_ = Line({ C.x, C.y }, C.Euc() / 2);
+//	Pos inx = intersection(B_, C_);
+//	return Circle(inx + u, (inx - u).Euc());
+//}
+//int main() {
+//	char F;
+//	std::cin.tie(0)->sync_with_stdio(0);
+//	std::cout.tie(0);
+//	std::cin >> N;
+//	if (N <= 2) {
+//		std::cout << "No\n";
+//		return 0;
+//	}
+//	for (int i = 0; i < N; i++) {
+//		std::cin >> T[i].x >> T[i].y >> F;
+//		T[i].i = F == 'I' ? 1 : 0;
+//	}
+//	for (int i = 0; i < N; i++) {
+//		for (int j = i + 1; j < N; j++) {
+//			Pos d1 = T[i], d2 = T[j];
+//			Circle c = C(d1, d2);
+//			bool f = 1;
+//			for (int k = 0; k < N; k++) {
+//				ld d = (c.c - T[k]).Euc();
+//				if ((T[k].i && d > c.r) || (!T[k].i && d < c.r)) {
+//					f = 0; break;
+//				}
+//			}
+//			if (f) {
+//				std::cout << "No\n";
+//				return 0;
+//			}
+//		}
+//	}
+//	for (int i = 0; i < N; i++) {
+//		for (int j = i + 1; j < N; j++) {
+//			for (int k = j + 1; k < N; k++) {
+//				Pos d1 = T[i], d2 = T[j], d3 = T[k];
+//				//Circle c = C(d1, d2, d3);
+//				Circle c = enclose(d1, d2, d3);
+//				if (c.r < 0) continue;
+//				bool f = 1;
+//				for (int l = 0; l < N; l++) {
+//					ld d = (c.c - T[l]).Euc();
+//					if ((T[l].i && d > c.r) || (!T[l].i && d < c.r)) {
+//						f = 0; break;
+//					}
+//				}
+//				if (f) {
+//					std::cout << "No\n";
+//					return 0;
+//				}
+//			}
+//		}
+//	}
+//	std::cout << "Yes\n";
+//	return 0;
+//}
 
 //#include <iostream>
 //#include <algorithm>
---
+++ Cpp/boj_solve/boj2389_MEC.cpp
@@ -0,0 +1,210 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <cstdlib>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+const ld INF = 1e17;
+const ld TOL = 1e-7;
+const ld PI = acos(-1);
+const int LEN = 1e3;
+int N;
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+
+struct Pos {
+	ld x, y;
+	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
+	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
+	ld operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+	ld operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos operator ~ () const { return { -y, x }; }
+	ld operator ! () const { return x * y; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
+	ld Euc() const { return x * x + y * y; }
+	ld mag() const { return hypot(x, y); }
+	Pos unit() const { return *this / mag(); }
+	friend ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+	friend ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+	friend int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+		ld ret = cross(d1, d2, d3); return zero(ret) ? 0 : ret > 0 ? 1 : -1;
+	}
+	friend bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
+		ld ret = dot(d1, d3, d2);
+		return zero(cross(d1, d2, d3)) && (ret > 0 || zero(ret));
+	}
+	friend bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
+		ld ret = dot(d1, d3, d2);
+		return zero(cross(d1, d2, d3)) && ret > 0;
+	}
+	friend ld projection(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d1) / (d2 - d1).mag(); }
+	friend int collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
+		return !ccw(d1, d2, d3) && !ccw(d1, d2, d4);
+	}
+	friend std::istream& operator >> (std::istream& is, Pos& p) {
+		is >> p.x >> p.y;
+		return is;
+	}
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) {
+		os << p.x << " " << p.y;
+		return os;
+	}
+}; const Pos O = { 0, 0 };
+struct Vec {
+	ld vy, vx;
+	Vec(ld Y = 0, ld X = 0) : vy(Y), vx(X) {}
+	bool operator == (const Vec& v) const { return (zero(vy - v.vy) && zero(vx - v.vx)); }
+	bool operator < (const Vec& v) const { return zero(vy - v.vy) ? vx < v.vx : vy < v.vy; }
+	ld operator * (const Vec& v) const { return vy * v.vy + vx * v.vx; }
+	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }
+	Vec operator ~ () const { return { -vx, vy }; }
+	Vec& operator *= (const ld& scalar) { vy *= scalar; vx *= scalar; return *this; }
+	Vec& operator /= (const ld& scalar) { vy /= scalar; vx /= scalar; return *this; }
+}; const Vec Zero = { 0, 0 };
+struct Line {//ax + by = c
+	Vec s;
+	ld c;
+	Line(Vec V = Vec(0, 0), ld C = 0) : s(V), c(C) {}
+	bool operator < (const Line& l) const {
+		bool f1 = Zero < s;
+		bool f2 = Zero < l.s;
+		if (f1 != f2) return f1;
+		ld CCW = s / l.s;
+		return zero(CCW) ? c * hypot(l.s.vy, l.s.vx) < l.c * hypot(s.vy, s.vx) : CCW > 0;
+	}
+	ld operator / (const Line& l) const { return s / l.s; }
+	ld operator * (const Line& l) const { return s * l.s; }
+	Line operator + (const ld& scalar) const { return Line(s, c + hypot(s.vy, s.vx) * scalar); }
+	Line operator - (const ld& scalar) const { return Line(s, c - hypot(s.vy, s.vx) * scalar); }
+	Line operator * (const ld& scalar) const { return Line({ s.vy * scalar, s.vx * scalar }, c * scalar); }
+	Line& operator += (const ld& scalar) { c += hypot(s.vy, s.vx) * scalar; return *this; }
+	Line& operator -= (const ld& scalar) { c -= hypot(s.vy, s.vx) * scalar; return *this; }
+	Line& operator *= (const ld& scalar) { s *= scalar, c *= scalar; return *this; }
+	ld dist(const Pos& p) const { return s.vy * p.x + s.vx * p.y; }
+	ld above(const Pos& p) const { return s.vy * p.x + s.vx * p.y - c; }
+	friend std::ostream& operator << (std::ostream& os, const Line& l) {
+		os << l.s.vy << " " << l.s.vx << " " << l.c;
+		return os;
+	}
+};
+const Line Xaxis = { { 0, -1 }, 0 };
+const Line Yaxis = { { 1, 0 }, 0 };
+Line L(const Pos& s, const Pos& e) {
+	ld dy, dx, c;
+	dy = e.y - s.y;
+	dx = s.x - e.x;
+	c = dy * s.x + dx * s.y;
+	return { { dy, dx } , c };
+}
+Line rotate90(const Line& l, const Pos& p) {
+	Vec s = ~l.s;
+	ld c = s.vy * p.x + s.vx * p.y;
+	return { s, c };
+}
+Pos intersection(const Line& l1, const Line& l2) {
+	Vec v1 = l1.s, v2 = l2.s;
+	ld det = v1 / v2;
+	return {
+		(l1.c * v2.vx - l2.c * v1.vx) / det,
+		(l2.c * v1.vy - l1.c * v2.vy) / det,
+	};
+}
+struct Circle {
+	Pos c;
+	ld r;
+	Circle(Pos C = Pos(0, 0), ld R = 0) : c(C), r(R) {}
+	bool operator == (const Circle& p) const { return c == p.c && std::abs(r - p.r) < TOL; }
+	bool operator != (const Circle C) const { return std::abs(C.c.x - c.x) >= TOL || std::abs(C.c.y - c.y) >= TOL || std::abs(r - C.r) >= TOL; }
+	bool operator > (const Pos& p) const { return r > (c - p).mag(); }
+	bool operator >= (const Pos& p) const { return r + TOL > (c - p).mag(); }
+	bool operator < (const Pos& p) const { return r < (c - p).mag(); }
+	Circle operator + (const Circle& C) const { return { {c.x + C.c.x, c.y + C.c.y}, r + C.r }; }
+	Circle operator - (const Circle& C) const { return { {c.x - C.c.x, c.y - C.c.y}, r - C.r }; }
+	ld H(const ld& th) const { return sin(th) * c.x + cos(th) * c.y + r; }// coord trans | check right
+	friend std::istream& operator >> (std::istream& is, Circle& c) {
+		is >> c.c.x >> c.c.y >> c.r;
+		return is;
+	}
+	friend std::ostream& operator << (std::ostream& os, const Circle& c) {
+		os << c.c.x << " " << c.c.y << " " << c.r; return os;
+	}
+};
+Circle enclose_circle(const Pos& u, const Pos& v) {
+	Pos C = (u + v) * .5;
+	return Circle(C, (C - u).mag());
+}
+//Circle enclose(const Pos& u, const Pos& v, const Pos& w) {
+//	Pos B = v - u, C = w - u;
+//	Line B_ = Line({ B.x, B.y }, B.Euc() / 2);
+//	Line C_ = Line({ C.x, C.y }, C.Euc() / 2);
+//	Pos inx = intersection(B_, C_);
+//	return Circle(inx + u, inx.mag());
+//}
+Circle enclose_circle(const Pos& u, const Pos& v, const Pos& w) {
+	Line l1 = rotate90(L(u, v), (u + v) * .5);
+	Line l2 = rotate90(L(v, w), (v + w) * .5);
+	if (zero(l1 / l2)) return { { 0, 0 }, -1 };
+	Pos c = intersection(l1, l2);
+	ld r = (c - u).mag();
+	return Circle(c, r);
+}
+bool valid_check(const Circle& C, const std::vector<Pos>& P) {
+	for (const Pos& p : P) if (C < p) return 0;
+	return 1;
+}
+Circle get_min_circle(std::vector<Pos>& P) {
+	int sz = P.size();
+	assert(sz <= 3);
+	if (!sz) return Circle(Pos(0, 0), 0);
+	if (sz == 1) return Circle(P[0], 0);
+	if (sz == 2) return enclose_circle(P[0], P[1]);
+	for (int i = 0; i < 2; i++) {
+		for (int j = i + 1; j < 3; j++) {
+			Circle C = enclose_circle(P[i], P[j]);
+			if (valid_check(C, P)) return C;
+		}
+	}
+	return enclose_circle(P[0], P[1], P[2]);
+}
+Circle welzl(std::vector<Pos>& P, std::vector<Pos> R, int sz) {
+	if (!sz || R.size() == 3) return get_min_circle(R);
+	int idx = rand() % sz;
+	Pos p = P[idx];
+	std::swap(P[idx], P[sz - 1]);
+	Circle mec = welzl(P, R, sz - 1);
+	if (mec >= p) return mec;
+	R.push_back(p);
+	return welzl(P, R, sz - 1);
+}
+Circle welzl(std::vector<Pos>& P) {
+	shuffle(P.begin(), P.end(), std::mt19937(0x14004));
+	return welzl(P, {}, P.size());
+}
+std::vector<Pos> pos;
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(9);
+	std::cin >> N;
+	pos.resize(N);
+	for (int i = 0; i < N; i++) std::cin >> pos[i];
+	Circle MEC = welzl(pos);
+	std::cout << MEC;
+	return;
+}
+int main() { solve(); return 0; }//boj2389 at the center of the world...
