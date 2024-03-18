c35b0829446f4b241784682cdcc9ef3101e4e8e1
crimson <redcube231@gmail.com>
2024-03-18 13:41:55 +0900

    2403181341

---
+++ Cpp/boj_solve/boj1768_sort.cpp
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <array>
+#include <tuple>
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<int, int> pi;
+const ll INF = 1e17;
+const int LEN = 1e5 + 1;
+const ld TOL = 1e-7;
+int N, M, T, Q;
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int dcmp(const ld& x) { return std::abs(x) < TOL ? 0 : x > 0 ? 1 : -1; }
+int dcmp(const ll& x) { return !x ? 0 : x > 0 ? 1 : -1; }
+//ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll gcd(ll a, ll b) {
+	while (b) {
+		ll tmp = a % b;
+		a = b;
+		b = tmp;
+	}
+	return a;
+}
+struct Info { ll area, l, r; };
+
+struct Pos {
+	ll x, y;
+	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ll& n) const { return { x * n, y * n }; }
+	Pos operator / (const ll& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator ~ () const { return { -y, x }; }
+	ll operator ! () const { return x * y; }
+	ll Euc() const { return x * x + y * y; }
+	ll Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) {
+		is >> p.x >> p.y;
+		return is;
+	}
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) {
+		os << p.x << " " << p.y;
+		return os;
+	}
+} pos[LEN]; const Pos O = { 0, 0 };
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin >> T;
+	while (T--) {
+		std::cin >> N;
+		for (int i = 0; i < N; i++) std::cin >> pos[i], pos[i] *= 2;
+		std::sort(pos, pos + N);
+		Pos mid = (pos[0] + pos[(N - 1) % N]) / 2;
+		bool f = 1;
+		for (int i = 0; i <= (N >> 1) + 1; i++) {
+			Pos v1 = mid - pos[i], v2 = pos[N - 1 - i] - mid;
+			if (v1 != v2) { std::cout << "no\n"; f = 0; break; }
+		}
+		if (f) std::cout << "yes\n"; return;
+	}
+}
+int main() { solve(); return 0; }//boj1768
