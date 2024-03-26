af477277aa4e1c903d5ef4538e5832fe6b0a2985
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-03-27 00:43:34 +0900

    2403270043

--- Python/__test1.py
+++ Python/__test1.py
@@ -4,3 +4,5 @@ print((Decimal(2) ** Decimal(.5)))
 b = a / (Decimal(3) ** Decimal(.5))
 print(a + a + b + b)
 print(f'{a + a + b * Decimal(2):.20f}')
+
+print(Decimal(100000000) / (Decimal(3) ** Decimal(.5)))
