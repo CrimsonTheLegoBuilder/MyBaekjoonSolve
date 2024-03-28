b6e2fd4148d2a89c25620eefe234e101ed21c4c1
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-03-28 19:38:14 +0900

    2403281938

--- Cpp/boj_solve/boj25002_Minkowski_Merge_sort_tree.cpp
+++ Cpp/boj_solve/boj25002_Minkowski_Merge_sort_tree.cpp
@@ -6,69 +6,44 @@
 #include <cassert>
 #include <vector>
 typedef long long ll;
-typedef long double ld;
+typedef double ld;
 const ll INF = 1e17;
 const int LEN = 250'005;
 const ld TOL = 1e-7;
 int N, MIN = 1e9, MAX = -1e9;
-ll EUC;
 
 struct Pos {
-	ll x, y;
-	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	int x, y;
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
-	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	//bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	//bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ll& n) const { return { x * n, y * n }; }
-	Pos operator / (const ll& n) const { return { x / n, y / n }; }
-	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
-	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
 	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
 	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
-	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+	Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
 	Pos operator ~ () const { return { -y, x }; }
-	ll operator ! () const { return x * y; }
-	ll Euc() const { return x * x + y * y; }
-	ll Man() const { return std::abs(x) + std::abs(y); }
+	ll operator ! () const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	int Man() const { return std::abs(x) + std::abs(y); }
 	ld mag() const { return hypot(x, y); }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
 std::vector<Pos> C[LEN], H[LEN << 2];//container, merge sort tree
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ll ret = cross(d1, d2, d3);
 	return !ret ? 0 : ret > 0 ? 1 : -1;
 }
-//std::vector<Pos> graham_scan(std::vector<Pos>& C) {
-//	std::vector<Pos> H;
-//	if (C.size() < 3) {
-//		std::sort(C.begin(), C.end());
-//		return C;
-//	}
-//	std::swap(C[0], *min_element(C.begin(), C.end()));
-//	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
-//		int ret = ccw(C[0], p, q);
-//		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
-//		return ret > 0;
-//		}
-//	);
-//	C.erase(unique(C.begin(), C.end()), C.end());
-//	int sz = C.size();
-//	for (int i = 0; i < sz; i++) {
-//		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
-//			H.pop_back();
-//		H.push_back(C[i]);
-//	}
-//	return H;
-//}
 void graham_scan(std::vector<Pos>& C, std::vector<Pos>& H) {
 	if (C.size() < 3) {
 		std::sort(C.begin(), C.end());
@@ -82,7 +57,7 @@ void graham_scan(std::vector<Pos>& C, std::vector<Pos>& H) {
 		return ret > 0;
 		}
 	);
-	C.erase(unique(C.begin(), C.end()), C.end());
+	//C.erase(unique(C.begin(), C.end()), C.end());
 	int sz = C.size();
 	for (int i = 0; i < sz; i++) {
 		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
@@ -91,37 +66,36 @@ void graham_scan(std::vector<Pos>& C, std::vector<Pos>& H) {
 	}
 	return;
 }
+//ll Minkowski_sum(std::vector<Pos>& A, std::vector<Pos>& B);
 ll Minkowski_sum(std::vector<Pos>& A, std::vector<Pos>& B) {
 	int sza = A.size();
 	int szb = B.size();
 	if (!sza || !szb) return -INF;
 	ll ret = -INF;
 
-	for (Pos& p : B) p *= -1;
-
 	if (sza <= 2 || szb <= 2) {
 		for (const Pos& pa : A)
 			for (const Pos& pb : B)
-				ret = std::max(ret, (pa + pb).Euc());
-		for (Pos& p : B) p *= -1;
+				ret = std::max(ret, (pa - pb).Euc());
 		return ret;
 	}
 
-	std::vector<Pos> MS;
+	for (Pos& p : B) p *= -1;
+
+	Pos cur;
 	int a = 0, b = 0;
 	for (int i = 0; i < sza; i++) if (A[i] < A[a]) a = i;
 	for (int j = 0; j < szb; j++) if (B[j] < B[b]) b = j;
-	MS.push_back(A[a] + B[b]);
-	ret = std::max(ret, MS.back().Euc());
-	//std::cout << sza + szb << "\n";
+	cur = A[a] + B[b];
+	ret = std::max(ret, cur.Euc());
 	for (int k = 1; k < sza + szb; k++) {
-		//std::cout << "DEBUG\n";
 		Pos a1 = A[(a + 1) % sza] + B[b];
 		Pos b1 = B[(b + 1) % szb] + A[a];
-		if (ccw(MS.back(), a1, b1) > 0) MS.push_back(a1), a = (a + 1) % sza;// , std::cout << a1;
-		else MS.push_back(b1), b = (b + 1) % szb;// , std::cout << b1;
-		ret = std::max(ret, MS.back().Euc());
+		if (ccw(cur, a1, b1) > 0) cur = a1, a = (a + 1) % sza;
+		else cur = b1, b = (b + 1) % szb;
+		ret = std::max(ret, cur.Euc());
 	}
+
 	for (Pos& p : B) p *= -1;
 
 	return ret;
@@ -187,3 +161,124 @@ int main() { solve(); return 0; }//boj25002 Board Game
 50
 
 */
+
+
+//struct Pos {
+//	ll x, y;
+//	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+//	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+//	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+//	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+//	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+//	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+//	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+//	Pos operator * (const ll& n) const { return { x * n, y * n }; }
+//	Pos operator / (const ll& n) const { return { x / n, y / n }; }
+//	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+//	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+//	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+//	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+//	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+//	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+//	Pos operator ~ () const { return { -y, x }; }
+//	ll operator ! () const { return x * y; }
+//	ll Euc() const { return x * x + y * y; }
+//	ll Man() const { return std::abs(x) + std::abs(y); }
+//	ld mag() const { return hypot(x, y); }
+//	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+//	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+//}; const Pos O = { 0, 0 };
+
+//std::vector<Pos> graham_scan(std::vector<Pos>& C) {
+//	std::vector<Pos> H;
+//	if (C.size() < 3) {
+//		std::sort(C.begin(), C.end());
+//		return C;
+//	}
+//	std::swap(C[0], *min_element(C.begin(), C.end()));
+//	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
+//		int ret = ccw(C[0], p, q);
+//		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
+//		return ret > 0;
+//		}
+//	);
+//	C.erase(unique(C.begin(), C.end()), C.end());
+//	int sz = C.size();
+//	for (int i = 0; i < sz; i++) {
+//		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
+//			H.pop_back();
+//		H.push_back(C[i]);
+//	}
+//	return H;
+//}
+
+//ll Minkowski_sum(std::vector<Pos>& A, std::vector<Pos>& B) {
+//	int sza = A.size();
+//	int szb = B.size();
+//	if (!sza || !szb) return -INF;
+//	ll ret = -INF;
+//
+//	for (Pos& p : B) p *= -1;
+//
+//	if (sza <= 2 || szb <= 2) {
+//		for (const Pos& pa : A)
+//			for (const Pos& pb : B)
+//				ret = std::max(ret, (pa + pb).Euc());
+//		for (Pos& p : B) p *= -1;
+//		return ret;
+//	}
+//
+//	std::vector<Pos> MS;
+//	int a = 0, b = 0;
+//	for (int i = 0; i < sza; i++) if (A[i] < A[a]) a = i;
+//	for (int j = 0; j < szb; j++) if (B[j] < B[b]) b = j;
+//	MS.push_back(A[a] + B[b]);
+//	ret = std::max(ret, MS.back().Euc());
+//	//std::cout << sza + szb << "\n";
+//	for (int k = 1; k < sza + szb; k++) {
+//		//std::cout << "DEBUG\n";
+//		Pos a1 = A[(a + 1) % sza] + B[b];
+//		Pos b1 = B[(b + 1) % szb] + A[a];
+//		if (ccw(MS.back(), a1, b1) > 0) MS.push_back(a1), a = (a + 1) % sza;// , std::cout << a1;
+//		else MS.push_back(b1), b = (b + 1) % szb;// , std::cout << b1;
+//		ret = std::max(ret, MS.back().Euc());
+//	}
+//	for (Pos& p : B) p *= -1;
+//
+//	return ret;
+//}
+
+//ll Minkowski_sum(std::vector<Pos>& A, std::vector<Pos>& B) {
+//	int sza = A.size();
+//	int szb = B.size();
+//	if (!sza || !szb) return -INF;
+//	ll ret = -INF;
+//
+//	if (sza <= 2 || szb <= 2) {
+//		for (const Pos& pa : A)
+//			for (const Pos& pb : B)
+//				ret = std::max(ret, (pa - pb).Euc());
+//		return ret;
+//	}
+// 
+//	for (Pos& p : B) p *= -1;
+//
+//	std::vector<Pos> MS, tmp;
+//	int a = 0, b = 0;
+//	for (int i = 0; i < sza; i++) if (A[i] < A[a]) a = i;
+//	for (int j = 0; j < szb; j++) if (B[j] < B[b]) b = j;
+//	MS.push_back(A[a] + B[b]);
+//	for (int k = 1; k < sza + szb; k++) {
+//		Pos a1 = A[(a + 1) % sza] + B[b];
+//		Pos b1 = B[(b + 1) % szb] + A[a];
+//		if (ccw(MS.back(), a1, b1) > 0) MS.push_back(a1), a = (a + 1) % sza;// , std::cout << a1;
+//		else MS.push_back(b1), b = (b + 1) % szb;// , std::cout << b1;
+//	}
+//
+//	graham_scan(MS, tmp);
+//	for (const Pos& p : tmp) ret = std::max(ret, p.Euc());
+// 
+//	for (Pos& p : B) p *= -1;
+//
+//	return ret;
+//}
