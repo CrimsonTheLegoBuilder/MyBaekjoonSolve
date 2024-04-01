1ca75248943bd22d0d6be90c4b84a51c9554fc9f
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-04-01 19:27:09 +0900

    2404011927

--- Cpp/boj_solve/boj15824.cpp
+++ Cpp/boj_solve/boj15824.cpp
@@ -14,6 +14,17 @@ ll pow_m(ll A, ll B, const ll& mod = MOD) {  //divide & conquer
 	if (B % 2) return (((A_ * A_) % mod) * A) % mod;
 	else return (A_ * A_) % mod;
 }
+
+ll pow_fuck(ll a, ll b) {
+	ll ret = 1;
+	while (b) {
+		if (b & 1) ret = ret * a % MOD;
+		a = a * a % MOD;
+		b >>= 1;
+	}
+	return ret;
+}
+
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
@@ -23,7 +34,8 @@ void solve() {
 	ll ans = 0;
 	for (int i = 0; i < N; i++) {
 		ll tmp = sco[i];
-		ans = (ans + tmp * ((pow_m(2, i) - pow_m(2, N - i - 1)) % MOD) + MOD) % MOD;
+		//ans = (ans + tmp * ((pow_m(2, i) - pow_m(2, N - i - 1)) % MOD) + MOD) % MOD;
+		ans = (ans + tmp * ((pow_fuck(2, i) - pow_fuck(2, N - i - 1)) % MOD) + MOD) % MOD;
 	}
 	std::cout << ans << "\n";
 	return;
