465f2ffc7eb99a0a796dd22578df840f8eb9ec38
crimson <redcube231@gmail.com>
2024-04-05 08:01:21 +0900

    2404050801

--- Cpp/boj_solve/boj13907_dijkstra.cpp
+++ Cpp/boj_solve/boj13907_dijkstra.cpp
@@ -5,18 +5,17 @@
 #include <vector>
 #include <queue>
 typedef long long ll;
-const ll INF = 1e18;
-const int LEN = 1001;
+const ll INF = 1e9;
+const int LEN = 1005;
 int N, M, K, S, E, P;
 int G[LEN][LEN], C[LEN][LEN];
-int CNT[LEN];
 
 struct Info {
-	int u, v; //cur, prev
-	int c;    //cost
-	int cnt;  //count
-	Info(int u, int v, int c, int cnt) : u(u), v(v), c(c), cnt(cnt) {}
-	bool operator < (const Info& x) const { return c == x.c ? cnt > x.cnt : c > x.c; }
+	int i;
+	int c;
+	int cnt;
+	Info(int i, int c, int cnt) : i(i), c(c), cnt(cnt) {}
+	bool operator < (const Info& x) const { return c > x.c; }
 };
 std::priority_queue<Info> PQ;
 void dijkstra(int s = S, int e = E, int n = N) {
@@ -24,23 +23,17 @@ void dijkstra(int s = S, int e = E, int n = N) {
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++)
 			C[i][j] = INF;
-	for (int i = 1; i <= n; i++) {
-		if (~G[s][i]) C[s][i] = G[s][i], PQ.push(Info(i, s, G[s][i], 1));
-	}
+	C[0][s] = 0;
+	PQ.push(Info(s, 0, 1));
 	while (PQ.size()) {
 		Info p = PQ.top(); PQ.pop();
-		if (C[p.v][p.u] < p.c) continue;
-		//if (p.u == e) CNT[p.v] = p.cnt;
-		if (p.u == e) {
-			CNT[p.v] = p.cnt;
-			continue;
-		}
+		if (C[p.cnt][p.i] < p.c) continue;
 		for (int i = 1; i <= n; i++) {
-			if (~G[p.u][i]) continue;
-			int cost = p.c + G[p.u][i];
-			if (C[p.u][i] > cost) {
-				C[p.u][i] = cost;
-				PQ.push(Info(i, p.u, cost, p.cnt + 1));
+			if (~G[p.i][i]) continue;
+			int cost = p.c + G[p.i][i];
+			if (C[p.cnt][i] > cost) {
+				C[p.cnt][i] = cost;
+				PQ.push(Info(i, cost, p.cnt + 1));
 			}
 		}
 	}
@@ -50,22 +43,27 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	memset(G, -1, sizeof G);
-	memset(CNT, -1, sizeof CNT);
 	std::cin >> N >> M >> K >> S >> E;
 	int a, b;
 	int w;
 	while (M--) {
 		std::cin >> a >> b >> w;
-		G[a][b] = w;
-		G[b][a] = w;
+		if (~G[a][b]) {
+			G[a][b] = std::min(G[a][b], w);
+			G[b][a] = std::min(G[b][a], w);
+		}
+		else {
+			G[a][b] = w;
+			G[b][a] = w;
+		}
 	}
 	dijkstra();
 	while (K--) {
 		std::cin >> P;
 		int MIN = INF;
 		for (int i = 1; i <= N; i++) {
-			if (!~CNT[i]) { 
-				C[i][E] += P * CNT[i];
+			if (C[i][E] < INF) {
+				C[i][E] += P * i;
 				MIN = std::min(MIN, C[i][E]);
 			}
 		}
