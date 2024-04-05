e8678433dfebe2cb7e93b80c75435015005c75aa
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-04-05 21:43:45 +0900

    2404052143

---
+++ Cpp/boj_solve/boj1688_conncave_inner.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <vector>
+typedef long long ll;
+typedef long double ld;
+const int LEN = 1e5 + 1;
+int N;
+
+struct Pos {
+	int x, y;
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	ll operator * (const Pos& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator ~ () const { return { -y, x }; }
+	ll operator ! () const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	ll Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = { 0, 0 };
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+	ll ret = cross(d1, d2, d3);
+	return !ret ? 0 : ret > 0 ? 1 : -1;
+}
+bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
+}
+bool inner_check(const std::vector<Pos>& H, const Pos& t) {//for concave
+	int sz = H.size();
+	int cnt = 0;
+	for (int i = 0; i < sz; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % sz];
+		if (on_seg_strong(cur, nxt, t)) return 1;
+		if (cur.y == nxt.y) continue;
+		if (nxt.y < cur.y) std::swap(cur, nxt);
+		if (cur.y > t.y || nxt.y <= t.y) continue;
+		cnt += ccw(cur, nxt, t) >= 0;
+	}
+	return cnt & 1;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	Pos fuck[3];
+	std::cin >> N;
+	std::vector<Pos> H(N);
+	int i = 0;
+	while (N--) std::cin >> H[i++];
+	std::cin >> fuck[0];
+	std::cin >> fuck[1];
+	std::cin >> fuck[2];
+	for (int i = 0; i < 3; i++) std::cout << inner_check(H, fuck[i]) << "\n";
+	return;
+}
+int main() { solve(); }
