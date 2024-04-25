3b2131579563a176d29b3a6eff0f00927ce585c8
crimson <redcube231@gmail.com>
2024-04-25 14:13:25 +0900

    2404251413flappy2

--- Cpp/boj_solve/boj25250_flappy_bird.cpp
+++ Cpp/boj_solve/boj25250_flappy_bird.cpp
@@ -23,75 +23,18 @@ struct Pos {
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const int& n) const { return { x * n, y * n }; }
-	Pos operator / (const int& n) const { return { x / n, y / n }; }
 	ll operator * (const Pos& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
 	ll operator / (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
-	ll operator ^ (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
-	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
-	Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
-	Pos operator - () const { return { -x, -y }; }
-	Pos operator ~ () const { return { -y, x }; }
-	Pos operator ! () const { return { y, x }; }
-	ll xy() const { return (ll)x * y; }
-	ll Euc() const { return (ll)x * x + (ll)y * y; }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
-std::vector<Pos> C, H;
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) ^ (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ll ret = cross(d1, d2, d3);
-	return !ret ? 0 : ret > 0 ? 1 : -1;
-}
-bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
-	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
-}
-int inner_check_bi_search(const std::vector<Pos>& H, const Pos& p) {//convex
-	int sz = H.size();
-	if (!sz) return -1;
-	if (sz == 1) return p == H[0] ? 0 : -1;
-	if (sz == 2) return on_seg_strong(H[0], H[1], p) ? 0 : -1;
-	if (cross(H[0], H[1], p) < 0 || cross(H[0], H[sz - 1], p) > 0) return -1;
-	if (on_seg_strong(H[0], H[1], p) || on_seg_strong(H[0], H[sz - 1], p)) return 0;
-	int s = 0, e = sz - 1, m;
-	while (s + 1 < e) {
-		m = s + e >> 1;
-		if (cross(H[0], H[m], p) > 0) s = m;
-		else e = m;
-	}
-	if (cross(H[s], H[e], p) > 0) return 1;
-	else if (on_seg_strong(H[s], H[e], p)) return 0;
-	else return -1;
-}
-std::vector<Pos> graham_scan(std::vector<Pos>& C) {
-	std::vector<Pos> H;
-	if (C.size() < 3) {
-		std::sort(C.begin(), C.end());
-		return C;
-	}
-	std::swap(C[0], *min_element(C.begin(), C.end()));
-	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
-		int ret = ccw(C[0], p, q);
-		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
-		return ret > 0;
-		}
-	);
-	C.erase(unique(C.begin(), C.end()), C.end());
-	int sz = C.size();
-	for (int i = 0; i < sz; i++) {
-		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
-			H.pop_back();
-		H.push_back(C[i]);
-	}
-	return H;
+	return ret < 0 ? -1 : !!ret;
 }
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
@@ -106,23 +49,59 @@ void solve() {
 		D[i] = Pos(x, yd);
 		U[i] = Pos(x, yu);
 	}
+
 	std::deque<Pos> HU, HD;
-	HU.push_back(s);
-	HU.push_back(U[0]);
+	HU.push_back(s); HU.push_back(U[0]);
 	U.push_back(e);
-	HD.push_back(s);
-	HD.push_back(D[0]);
+	HD.push_back(s); HD.push_back(D[0]);
 	D.push_back(e);
 
 	std::vector<Pos> ret;
+	int ptr = 1;
 	ret.push_back(s);
 	for (int i = 1; i <= N; i++) {
-		while (HU.size() > 2 && ccw(HU[HU.size() - 2], HU.back(), U[i]) >= 0) HU.pop_back();
-		while (HD.size() > 2 && ccw(HD[HD.size() - 2], HD.back(), D[i]) <= 0) HU.pop_back();
-
+		while (HU.size() > 1 && ccw(HU[HU.size() - 2], HU.back(), U[i]) <= 0) HU.pop_back();
+		HU.push_back(U[i]);
+		while (HD.size() > 1 && ccw(HD[HD.size() - 2], HD.back(), D[i]) >= 0) HD.pop_back();
+		HD.push_back(D[i]);
+		ptr = 1;
+		if (HU.size() <= 2 && HD.size() <= 2) continue;
+		else if (HU.size() == 2) {
+			//std::cout << "DEBUG::\n";
+			if (ccw(HU[0], HU[1], HD[1]) <= 0) continue;
+			while (ptr < HD.size() && ccw(HU[0], HU[1], HD[ptr]) > 0) ptr++;
+			ptr--;
+			while (ptr--) ret.push_back(HD[0]), HD.pop_front();
+			ret.push_back(HD[0]);
+			HU[0] = HD[0];
+			//std::cout << "DEBUG:: " << HU[0] << "\n";
+		}
+		else if (HD.size() == 2) {
+			//std::cout << "DEBUG::\n";
+			if (ccw(HD[0], HD[1], HU[1]) >= 0) continue;
+			while (ptr < HU.size() && ccw(HD[0], HD[1], HU[ptr]) < 0) ptr++;
+			ptr--;
+			while (ptr--) ret.push_back(HU[0]), HU.pop_front();
+			ret.push_back(HU[0]);
+			HD[0] = HU[0];
+			//std::cout << "DEBUG:: " << HD[0] << "\n";
+		}
 	}
+	ret.push_back(e);
 	std::sort(ret.begin(), ret.end());
+	ret.erase(unique(ret.begin(), ret.end()), ret.end());
 	for (const Pos& p : ret) std::cout << p << "\n";
 	return;
 }
-int main() { solve(); return 0; }//GCPC 2021 F Flappy Bird boj25250 
+int main() { solve(); return 0; }//GCPC 2021 F Flappy Bird boj25250
+
+/*
+
+0 0 10 0
+4
+2 1 3
+4 2 3
+7 0 2
+9 -2 -1
+
+*/
