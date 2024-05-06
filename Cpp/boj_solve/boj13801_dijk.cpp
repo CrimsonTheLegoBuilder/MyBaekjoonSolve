1c1c39d1ced1d9371551bf0c298c77b2e4f696bd
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-05-06 23:04:55 +0900

    2405061740alicebomb

---
+++ Cpp/boj_solve/boj13801_dijk.cpp
@@ -0,0 +1,166 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <array>
+#include <tuple>
+#include <queue>
+typedef long long ll;
+typedef double ld;
+const ll INF = 1e17;
+const int LEN = 505;
+const ld TOL = 1e-7;
+const ll MOD = 1'000'000'007;
+int N, M, T;
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+
+ld COST[LEN];
+struct Info {
+	int i;
+	ld c;
+	bool operator < (const Info& x) const { return c > x.c; }
+};
+std::vector<Info> G[LEN];
+std::priority_queue<Info> Q;
+ld dijkstra(int v, int g) {
+	for (int i = 0; i < LEN; i++) COST[i] = INF;
+	Q.push({ v, 0 });
+	COST[v] = 0;
+	while (Q.size()) {
+		Info p = Q.top(); Q.pop();
+		if (p.c > COST[p.i]) continue;
+		for (const Info& w : G[p.i]) {
+			ld cost = p.c + w.c;
+			if (COST[w.i] > cost) {
+				COST[w.i] = cost;
+				Q.push({ w.i, cost });
+			}
+		}
+	}
+	return COST[g];
+}
+struct Pos {
+	int x, y;
+	int i;
+	Pos(int X = 0, int Y = 0, ll I = 0) : x(X), y(Y), i(I) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	int Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return y > 0 || y == 0 && x >= 0; }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+} pos[LEN]; const Pos O = Pos(0, 0);
+bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
+bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
+bool cmpi(const Pos& p, const Pos& q) { return p.i < q.i; }
+typedef std::vector<Pos> Polygon;
+std::vector<Pos> H[105];
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+	ll ret = cross(d1, d2, d3);
+	return ret < 0 ? -1 : !!ret;
+}
+bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
+}
+bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0;
+}
+int collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
+	return !ccw(d1, d2, d3) && !ccw(d1, d2, d4);
+}
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	return f1 && f2;
+	//bool f3 = on_seg_strong(s1, s2, d1) ||
+	//	on_seg_strong(s1, s2, d2) ||
+	//	on_seg_strong(d1, d2, s1) ||
+	//	on_seg_strong(d1, d2, s2);
+	//return (f1 && f2) || f3;
+}
+ll area(std::vector<Pos>& H) {
+	ll ret = 0;
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % sz];
+		ret += cross(O, cur, nxt);
+	}
+	return ret;
+}
+void norm(std::vector<Pos>& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
+bool inner_check(const std::vector<Pos>& H, const Pos& p) {//concave
+	int cnt = 0, sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % sz];
+		if (on_seg_strong(cur, nxt, p)) return 1;
+		if (cur.y == nxt.y) continue;
+		if (nxt.y < cur.y) std::swap(cur, nxt);
+		if (nxt.y <= p.y || cur.y > p.y) continue;
+		cnt += ccw(cur, nxt, p) > 0;
+	}
+	return cnt & 1;
+}
+void init() {
+	memset(pos, 0, sizeof pos);
+	while (Q.size()) Q.pop();
+	for (int i = 0; i < 100; i++) std::vector<Pos>().swap(H[i]);
+	return;
+}
+void query() {
+	Pos s;
+	init();
+	std::cin >> N;
+	std::cin >> s;
+	s.i = 0;
+	T = 1;
+	for (int i = 0; i < N; i++) {
+		std::cin >> M;
+		H[i].resize(M);
+		for (Pos& p : H[i]) {
+			std::cin >> p;
+			T++;
+			p.i = T;
+		}
+	}
+	return;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(7);
+	while (1) query();
+	return;
+}
+int main() { solve(); return 0; }//JAG Practice Contest 2010 E boj13801
