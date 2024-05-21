5035208f2517f934410e44084592420bac803922
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-05-21 23:05:35 +0900

    2405212305seg

--- Cpp/boj_solve/boj16975_lazy.cpp
+++ Cpp/boj_solve/boj16975_lazy.cpp
@@ -2,17 +2,19 @@
 #include <algorithm>
 #include <cmath>
 #include <vector>
+#include <queue>
 #include <cassert>
 typedef long long ll;
 const int LEN = 1e5 + 5;
 const ll INF = 1e17;
 
-int N, M;
-int o, s, t;
-ll tree[LEN << 2], lazy[LEN << 2];
+int N, M, K;
+int a, b, c;
+ll d;
+ll arr[LEN], tree[LEN << 2], lazy[LEN << 2];
 void lazy_propagate(int s, int e, int i) {
 	if (lazy[i]) {
-		if (lazy[i] & 1) tree[i] = ((ll)e - s + 1) - tree[i];
+		tree[i] += lazy[i] * ((ll)e - s + 1);
 		if (s ^ e) {
 			lazy[i << 1] += lazy[i];
 			lazy[i << 1 | 1] += lazy[i];
@@ -21,11 +23,11 @@ void lazy_propagate(int s, int e, int i) {
 	}
 	return;
 }
-void update(int l, int r, ll diff = 1, int s = 1, int e = N, int i = 1) {
+void update(int l, int r, ll diff, int s = 1, int e = N, int i = 1) {
 	lazy_propagate(s, e, i);
 	if (r < s || e < l) return;
 	if (l <= s && e <= r) {
-		tree[i] = ((ll)e - s + 1) - tree[i];
+		tree[i] += diff * ((ll)e - s + 1);
 		if (s ^ e) {
 			lazy[i << 1] += diff;
 			lazy[i << 1 | 1] += diff;
@@ -45,16 +47,32 @@ ll search(int l, int r, int s = 1, int e = N, int i = 1) {
 	int m = s + e >> 1;
 	return search(l, r, s, m, i << 1) + search(l, r, m + 1, e, i << 1 | 1);
 }
+ll init(int s = 1, int e = N, int i = 1) {
+	if (s == e) return tree[i] = arr[s];
+	int m = s + e >> 1;
+	tree[i << 1] = init(s, m, i << 1);
+	tree[i << 1 | 1] = init(m + 1, e, i << 1 | 1);
+	return tree[i] = tree[i << 1] + tree[i << 1 | 1];
+}
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> N >> M;
-	while (M--) {
-		std::cin >> o >> s >> t;
-		if (s > t) std::swap(s, t);
-		if (o == 0) update(s, t);
-		else if (o == 1) std::cout << search(s, t) << "\n";
+	std::cin >> N;
+	for (int i = 1; i <= N; i++) std::cin >> arr[i];
+	init();
+	int cnt = M;
+	while (cnt--) {
+		std::cin >> a;
+		if (a == 1) {
+			std::cin >> b >> c >> d;
+			if (c < b) std::swap(b, c);
+			update(b, c, d);
+		}
+		else if (a == 2) {
+			std::cin >> b;
+			std::cout << search(b, b) << "\n";
+		}
 	}
 	return;
 }
-int main() { solve(); return 0; }//1395
+int main() { solve(); return 0; }//16975
