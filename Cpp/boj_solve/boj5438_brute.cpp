37e63ba5bd9b10e78ba0692c59ed7f7ae195acc2
crimson <redcube231@gmail.com>
2024-05-24 13:48:30 +0900

    2405241306_geobrute

--- Cpp/boj_solve/boj5438_brute.cpp
+++ Cpp/boj_solve/boj5438_brute.cpp
@@ -14,9 +14,14 @@ const ld INF = 1e17;
 const ld TOL = 1e-6;
 const ld PI = acos(-1);
 const int LEN = 2e4;
-int N;
+int Q, N;
 bool zero(const ld& x) { return std::abs(x) < TOL; }
 ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ld norm(ld th) {
+	while (th < 0) th += PI * 2;
+	while (th > PI * 2 - TOL) th -= PI * 2;
+	return th;
+}
 
 struct Pos {
 	ld x, y;
@@ -24,24 +29,35 @@ struct Pos {
 	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
 	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
 	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
 	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
 	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
-	ld operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
-	ld operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
-	Pos operator ~ () const { return { -y, x }; }
-	ld operator ! () const { return x * y; }
+	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
 	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
 	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
 	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
 	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ld xy() const { return x * y; }
+	Pos rot(ld the) { return { x * cos(the) - y * sin(the), x * sin(the) + y * cos(the) }; }
 	ld Euc() const { return x * x + y * y; }
-	ld mag() const { return hypot(x, y); }
+	ld mag() const { return sqrt(Euc()); }
 	Pos unit() const { return *this / mag(); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	bool close(const Pos& p) const { return zero((*this - p).Euc()); }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
+typedef std::vector<Pos> Polygon;
 struct Vec {
 	ld vy, vx;
 	Vec(ld Y = 0, ld X = 0) : vy(Y), vx(X) {}
@@ -52,6 +68,7 @@ struct Vec {
 	Vec operator ~ () const { return { -vx, vy }; }
 	Vec& operator *= (const ld& scalar) { vy *= scalar; vx *= scalar; return *this; }
 	Vec& operator /= (const ld& scalar) { vy /= scalar; vx /= scalar; return *this; }
+	ld mag() const { return hypot(vy, vx); }
 }; const Vec Zero = { 0, 0 };
 struct Line {//ax + by = c
 	Vec s;
@@ -64,8 +81,8 @@ struct Line {//ax + by = c
 		ld CCW = s / l.s;
 		return zero(CCW) ? c * hypot(l.s.vy, l.s.vx) < l.c * hypot(s.vy, s.vx) : CCW > 0;
 	}
-	ld operator / (const Line& l) const { return s / l.s; }
 	ld operator * (const Line& l) const { return s * l.s; }
+	ld operator / (const Line& l) const { return s / l.s; }
 	Line operator + (const ld& scalar) const { return Line(s, c + hypot(s.vy, s.vx) * scalar); }
 	Line operator - (const ld& scalar) const { return Line(s, c - hypot(s.vy, s.vx) * scalar); }
 	Line operator * (const ld& scalar) const { return Line({ s.vy * scalar, s.vx * scalar }, c * scalar); }
@@ -74,17 +91,48 @@ struct Line {//ax + by = c
 	Line& operator *= (const ld& scalar) { s *= scalar, c *= scalar; return *this; }
 	ld dist(const Pos& p) const { return s.vy * p.x + s.vx * p.y; }
 	ld above(const Pos& p) const { return s.vy * p.x + s.vx * p.y - c; }
+	ld mag() const { return s.mag(); }
 	friend std::ostream& operator << (std::ostream& os, const Line& l) { os << l.s.vy << " " << l.s.vx << " " << l.c; return os; }
 };
 const Line Xaxis = { { 0, -1 }, 0 };
 const Line Yaxis = { { 1, 0 }, 0 };
+typedef std::vector<Line> Lines;
+Line L(const Pos& s, const Pos& e) {
+	ld dy, dx, c;
+	dy = e.y - s.y;
+	dx = s.x - e.x;
+	c = dy * s.x + dx * s.y;
+	return Line(Vec(dy, dx), c);
+}
+Line L(const Vec& s, const Pos& p) {
+	ld c = s.vy * p.x + s.vx * p.y;
+	return Line(s, c);
+}
+Line rotate(const Line& l, const Pos& p, ld the) {
+	Vec s = l.s;
+	ld x = -s.vx, y = s.vy;
+	ld vx = -(x * cos(the) - y * sin(the));
+	ld vy = x * sin(the) + y * cos(the);
+	ld c = vy * p.x + vx * p.y;
+	return Line(Vec(vy, vx), c);
+}
+Line rot90(const Line& l, const Pos& p) {
+	Vec s = ~l.s;
+	ld c = s.vy * p.x + s.vx * p.y;
+	return Line(s, c);
+}
 Pos intersection(const Line& l1, const Line& l2) {
 	Vec v1 = l1.s, v2 = l2.s;
 	ld det = v1 / v2;
 	return {
 		(l1.c * v2.vx - l2.c * v1.vx) / det,
-		(l2.c * v1.vy - l1.c * v2.vy) / det
+		(l2.c * v1.vy - l1.c * v2.vy) / det,
 	};
+}
+ld rad(const Line& b, const Line& l) {
+	ld x = (b * l) / b.mag();//dot
+	ld y = (b / l) / b.mag();//cross
+	return atan2(y, x);
 }
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
@@ -97,7 +145,12 @@ void solve() {
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(10);
-	std::cin >> N;
+	std::cin >> Q;
+	while (Q--) {
+		std::cin >> N;
+		Polygon H(N);
+		Lines V(N);
+	}
 	return;
 }
 int main() { solve(); return 0; }//boj5438 Secret Island Base
