4adbd5012c50c93b6234a120be1423cd51bbea78
crimson <redcube231@gmail.com>
2024-05-28 11:50:49 +0900

    2405281150geogreedy

---
+++ Cpp/boj_solve/boj8182.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <deque>
+typedef long long ll;
+typedef double ld;
+//typedef long double ld;
+const ld INF = 1e17;
+const ld TOL = 1e-10;
+const ld PI = acos(-1);
+const int LEN = 1e5 + 1;
+int N, M, T, Q;
+inline bool zero(const ld& x) { return std::abs(x) < TOL; }
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline ll sq(int x) { return (ll)x * x; }
+inline ld norm(ld th) {
+	while (th < 0) th += PI * 2;
+	while (th > PI * 2 - TOL) th -= PI * 2;
+	return th;
+}
+
+//#define DEBUG
+//#define ASSERT
+
+struct Pii {
+	int x, y;
+	Pii(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pii& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pii& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pii& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pii& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pii operator + (const Pii& p) const { return { x + p.x, y + p.y }; }
+	Pii operator - (const Pii& p) const { return { x - p.x, y - p.y }; }
+	Pii operator * (const int& n) const { return { x * n, y * n }; }
+	Pii operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pii& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pii& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
+	Pii& operator += (const Pii& p) { x += p.x; y += p.y; return *this; }
+	Pii& operator -= (const Pii& p) { x -= p.x; y -= p.y; return *this; }
+	Pii& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
+	Pii& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
+	Pii operator - () const { return { -x, -y }; }
+	Pii operator ~ () const { return { -y, x }; }
+	Pii operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	inline ll Euc() const { return (ll)x * x + (ll)y * y; }
+	inline ld rad() const { return norm(atan2(y, x)); }
+	int Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	inline friend std::istream& operator >> (std::istream& is, Pii& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pii& p) { os << p.x << " " << p.y; return os; }
+};
+const Pii Oii = { 0, 0 };
+const Pii INF_PT = { (int)INF, (int)INF };
+typedef std::vector<Pii> Hull;
+inline ll cross(const Pii& d1, const Pii& d2, const Pii& d3) { return (d2 - d1) / (d3 - d2); }
+inline ll cross(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) { return (d2 - d1) / (d4 - d3); }
+inline ll dot(const Pii& d1, const Pii& d2, const Pii& d3) { return (d2 - d1) * (d3 - d2); }
+inline ll dot(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) { return (d2 - d1) * (d4 - d3); }
+inline int ccw(const Pii& d1, const Pii& d2, const Pii& d3) {
+	ll ret = cross(d1, d2, d3);
+	return !ret ? 0 : ret > 0 ? 1 : -1;
+}
+struct Pos {
+	ld x, y;
+	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
+	inline Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
+	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
+	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	inline ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
+	inline Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ld xy() const { return x * y; }
+	inline Pos rot(ld the) const { return Pos(x * cos(the) - y * sin(the), x * sin(the) + y * cos(the)); }
+	inline ld Euc() const { return x * x + y * y; }
+	ld mag() const { return sqrt(Euc()); }
+	//ld mag() const { return hypot(x, y); }
+	Pos unit() const { return *this / mag(); }
+	inline ld rad() const { return norm(atan2(y, x)); }
+	inline friend ld rad(const Pos& p1, const Pos& p2) { return norm(atan2(p1 / p2, p1 * p2)); }
+	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	bool close(const Pos& p) const { return zero((*this - p).Euc()); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+};
+const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+inline ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+inline int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+	ld ret = cross(d1, d2, d3);
+	return zero(ret) ? 0 : ret > 0 ? 1 : -1;
+}
+Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
+	ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
+	return (p1 * a2 + p2 * a1) / (a1 + a2);
+}
+std::vector<int> B[LEN];//blocked
+int G[LEN];
+struct Seg {
+
+};
+inline void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(10);
+	std::cin >> N >> M;
+	Hull H(N);
+	for (Pii& p : H) std::cin >> p;
+	int s, e;
+	while (M--) {
+		std::cin >> s >> e;
+		s--, e--;
+		B[s].push_back(e);
+	}
+	int X = N - 1;
+	for (int i = 0; i < N - 1; i++) {
+		std::sort(B[i].rbegin(), B[i].rend());
+		G[i] = N - 1;
+		for (const int& j : B[i]) {
+			if (X < j) break;
+			if (G[i] == j) G[i]--;
+		}
+
+	}
+
+
+}
+int main() { solve(); return 0; }//boj8182 Island
