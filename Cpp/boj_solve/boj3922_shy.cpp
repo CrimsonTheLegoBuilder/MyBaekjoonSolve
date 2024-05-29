4234e44c8ad09f3cf9745e9efe0e73e53fc73b2c
crimson <redcube231@gmail.com>
2024-05-29 13:00:24 +0900

    2405291300geo

--- Cpp/boj_solve/boj3922_shy.cpp
+++ Cpp/boj_solve/boj3922_shy.cpp
@@ -174,18 +174,34 @@ bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ld ret = dot(d1, d3, d2);
 	return !ccw(d1, d2, d3) && ret > 0;
 }
-ld projection(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d1) / (d2 - d1).mag(); }
-bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
+inline bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
 	return !ccw(d1, d2, d3) && !ccw(d1, d2, d4);
 }
-ld dist(const Pos& d1, const Pos& d2, const Pos& t) {
-	return cross(d1, d2, t) / (d1 - d2).mag();
+ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
+	return (d2 - d1) * (d4 - d3) / (d2 - d1).mag();
+}
+ld dist(const Pos& d1, const Pos& d2, const Pos& t, bool f = 0) {
+	if (f) return std::abs(cross(d1, d2, t)) / (d1 - d2).mag();
+	if (sign(projection(d1, d2, d2, t)) <= 0 &&
+		sign(projection(d2, d1, d1, t)) <= 0)
+		return std::abs(cross(d1, d2, t)) / (d1 - d2).mag();
+	return std::min((d1 - t).mag(), (d2 - t).mag());
 }
 Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
 	ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
 	return (p1 * a2 + p2 * a1) / (a1 + a2);
 }
 Pos intersection(Linear& l1, Linear& l2) { return intersection(l1[0], l1[1], l2[0], l2[1]); }
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	//return f1 && f2;
+	bool f3 = on_seg_strong(s1, s2, d1) ||
+		on_seg_strong(s1, s2, d2) ||
+		on_seg_strong(d1, d2, s1) ||
+		on_seg_strong(d1, d2, s2);
+	return (f1 && f2) || f3;
+}
 bool inner_check(const std::vector<Pos>& H, const Pos& p) {//concave
 	int cnt = 0, sz = H.size();
 	for (int i = 0; i < sz; i++) {
@@ -209,6 +225,44 @@ ld area(std::vector<Pos>& H) {
 	return ret / 2;
 }
 void norm(std::vector<Pos>& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
+bool valid_check(const Polygon& A, const Polygon& B, const ld& D) {
+	int sza = A.size(), szb = B.size();
+	for (int i = 0; i < sza; i++) {
+		for (int j = 0; j < szb; j++) {
+			int ii = (i + 1) % sza;
+			int jj = (j + 1) % szb;
+			if (intersect(A[i], A[ii], B[j], B[jj])) return 0;
+			if (dist(A[i], A[ii], B[j]) < D) return 0;
+			if (dist(A[i], A[ii], B[jj]) < D) return 0;
+			if (dist(B[j], B[jj], A[i]) < D) return 0;
+			if (dist(B[j], B[jj], A[ii]) < D) return 0;
+		}
+	}
+	return !inner_check(A, B[0]);
+}
+std::vector<Pos> circle_line_intersection(const Pos& o, const ld& r, const Pos& p1, const Pos& p2) {
+	ld d = dist(p1, p2, o, 1);
+	if (std::abs(d) > r) return {};
+	Pos vec = p2 - p1;
+	Pos m = intersection(p1, p2, o, o + ~vec);
+	ld distance = vec.mag();
+	ld ratio = sqrt(r * r - d * d);
+	Pos m1 = m - vec * ratio / distance;
+	Pos m2 = m + vec * ratio / distance;
+	if (dot(p1, p2, m1, m2) < 0) std::swap(m1, m2);
+	return { m1, m2 };//p1->p2
+}
+std::vector<ld> dists(const Pos& a1, const Pos& a2, const Pos& b, const ld& D) {
+	std::vector<ld> ret;
+	std::vector<Pos> tmp;
+	Pos v = ~(a1 - a2).unit();
+	Pos p1 = a1 + v * D, p2 = a2 + v * D;
+	Pos q1 = a1 - v * D, q2 = a2 - v * D;
+	Pos b1 = b + Pos(1, 0);
+	if (ccw(b, b1, p1) * ccw(b, b1, p2) <= 0) tmp.push_back(intersection(b, b1, p1, p2));
+	if (ccw(b, b1, q1) * ccw(b, b1, q2) <= 0) tmp.push_back(intersection(b, b1, q1, q2));
+	
+}
 bool query() {
 	ld D;
 	std::cin >> D;
