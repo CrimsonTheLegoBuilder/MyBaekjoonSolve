8e2ce3791f580a202dfce75e738b58e480a0d8c8
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-05-29 20:56:02 +0900

    2405292055geobrute

--- Cpp/boj_solve/boj1879_shy.cpp
+++ Cpp/boj_solve/boj1879_shy.cpp
@@ -24,25 +24,25 @@ struct Pos {
 	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
 	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
 	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
-	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
-	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
-	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
-	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
-	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	inline Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	inline Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	inline Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
+	inline Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
+	inline ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	inline ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
+	inline Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
 	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
 	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
 	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
 	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
-	Pos operator - () const { return { -x, -y }; }
-	Pos operator ~ () const { return { -y, x }; }
-	Pos operator ! () const { return { y, x }; }
+	inline Pos operator - () const { return { -x, -y }; }
+	inline Pos operator ~ () const { return { -y, x }; }
+	inline Pos operator ! () const { return { y, x }; }
 	ld xy() const { return x * y; }
 	Pos rot(ld the) { return { x * cos(the) - y * sin(the), x * sin(the) + y * cos(the) }; }
-	ld Euc() const { return x * x + y * y; }
-	ld mag() const { return sqrt(Euc()); }
-	Pos unit() const { return *this / mag(); }
+	inline ld Euc() const { return x * x + y * y; }
+	inline ld mag() const { return sqrt(Euc()); }
+	inline Pos unit() const { return *this / mag(); }
 	ld rad() const { return atan2(y, x); }
 	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
 	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
@@ -52,147 +52,40 @@ struct Pos {
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
 typedef std::vector<Pos> Polygon;
-struct Vec {
-	ld vy, vx;
-	Vec(ld Y = 0, ld X = 0) : vy(Y), vx(X) {}
-	bool operator == (const Vec& v) const { return (zero(vy - v.vy) && zero(vx - v.vx)); }
-	bool operator < (const Vec& v) const { return zero(vy - v.vy) ? vx < v.vx : vy < v.vy; }
-	ld operator * (const Vec& v) const { return vy * v.vy + vx * v.vx; }
-	ld operator / (const Vec& v) const { return vy * v.vx - vx * v.vy; }
-	Vec operator ~ () const { return { -vx, vy }; }
-	Vec& operator *= (const ld& scalar) { vy *= scalar; vx *= scalar; return *this; }
-	Vec& operator /= (const ld& scalar) { vy /= scalar; vx /= scalar; return *this; }
-	ld mag() const { return hypot(vy, vx); }
-}; const Vec Zero = { 0, 0 };
-struct Line {//ax + by = c
-	Vec s;
-	ld c;
-	Line(Vec V = Vec(0, 0), ld C = 0) : s(V), c(C) {}
-	bool operator < (const Line& l) const {
-		bool f1 = Zero < s;
-		bool f2 = Zero < l.s;
-		if (f1 != f2) return f1;
-		ld CCW = s / l.s;
-		return zero(CCW) ? c * hypot(l.s.vy, l.s.vx) < l.c * hypot(s.vy, s.vx) : CCW > 0;
-	}
-	ld operator * (const Line& l) const { return s * l.s; }
-	ld operator / (const Line& l) const { return s / l.s; }
-	Line operator + (const ld& scalar) const { return Line(s, c + hypot(s.vy, s.vx) * scalar); }
-	Line operator - (const ld& scalar) const { return Line(s, c - hypot(s.vy, s.vx) * scalar); }
-	Line operator * (const ld& scalar) const { return Line({ s.vy * scalar, s.vx * scalar }, c * scalar); }
-	Line& operator += (const ld& scalar) { c += hypot(s.vy, s.vx) * scalar; return *this; }
-	Line& operator -= (const ld& scalar) { c -= hypot(s.vy, s.vx) * scalar; return *this; }
-	Line& operator *= (const ld& scalar) { s *= scalar, c *= scalar; return *this; }
-	ld dist(const Pos& p) const { return s.vy * p.x + s.vx * p.y; }
-	ld above(const Pos& p) const { return s.vy * p.x + s.vx * p.y - c; }
-	ld mag() const { return s.mag(); }
-	friend std::ostream& operator << (std::ostream& os, const Line& l) { os << l.s.vy << " " << l.s.vx << " " << l.c; return os; }
-};
-struct Linear {//ps[0] -> ps[1] :: refer to bulijiojiodibuliduo
-	Pos ps[2];
-	Pos dir_;
-	Pos& operator[](int i) { return ps[i]; }
-	Pos dir() const { return dir_; }
-	Linear(Pos a = Pos(0, 0), Pos b = Pos(0, 0)) {
-		ps[0] = a;
-		ps[1] = b;
-		dir_ = (ps[1] - ps[0]).unit();
-	}
-	bool include(const Pos& p) const { return sign(dir_ / (p - ps[0])) > 0; }
-	Linear push() const {//push eps outward
-		const double eps = 1e-8;
-		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
-		return Linear(ps[0] + delta, ps[1] + delta);
-	}
-	Linear operator + (const double eps) const {//push eps outward
-		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
-		return Linear(ps[0] + delta, ps[1] + delta);
-	}
-	Linear operator - (const double eps) const {//pull eps inward
-		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
-		return Linear(ps[0] - delta, ps[1] - delta);
-	}
-	friend bool parallel(const Linear& l0, const Linear& l1) { return zero(l0.dir() / l1.dir()); }
-	friend bool same_dir(const Linear& l0, const Linear& l1) { return parallel(l0, l1) && l0.dir() * l1.dir() > 0; }
-	bool operator < (const Linear& l0) const {
-		if (same_dir(*this, l0)) return l0.include(ps[0]);
-		else return cmpq(this->dir(), l0.dir());
-	}
-};
-const Line Xaxis = { { 0, -1 }, 0 };
-const Line Yaxis = { { 1, 0 }, 0 };
-Line L(const Pos& s, const Pos& e) {
-	ld dy, dx, c;
-	dy = e.y - s.y;
-	dx = s.x - e.x;
-	c = dy * s.x + dx * s.y;
-	return Line(Vec(dy, dx), c);
-}
-Line L(const Vec& s, const Pos& p) {
-	ld c = s.vy * p.x + s.vx * p.y;
-	return Line(s, c);
-}
-Line rotate(const Line& l, const Pos& p, ld the) {
-	Vec s = l.s;
-	ld x = -s.vx, y = s.vy;
-	ld vx = -(x * cos(the) - y * sin(the));
-	ld vy = x * sin(the) + y * cos(the);
-	ld c = vy * p.x + vx * p.y;
-	return Line(Vec(vy, vx), c);
-}
-Line rotate90(const Line& l, const Pos& p) {
-	Vec s = ~l.s;
-	ld c = s.vy * p.x + s.vx * p.y;
-	return Line(s, c);
-}
-Pos intersection(const Line& l1, const Line& l2) {
-	Vec v1 = l1.s, v2 = l2.s;
-	ld det = v1 / v2;
-	return {
-		(l1.c * v2.vx - l2.c * v1.vx) / det,
-		(l2.c * v1.vy - l1.c * v2.vy) / det,
-	};
-}
-ld rad(const Line& b, const Line& l) {
-	ld x = (b * l) / b.mag();//dot
-	ld y = (b / l) / b.mag();//cross
-	return atan2(y, x);
-}
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+inline ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+inline ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+inline int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ld ret = cross(d1, d2, d3);
 	return zero(ret) ? 0 : ret > 0 ? 1 : -1;
 }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
-bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
+inline ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+inline ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+inline bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ld ret = dot(d1, d3, d2);
 	return !ccw(d1, d2, d3) && (ret > 0 || zero(ret));
 }
-bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
+inline bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ld ret = dot(d1, d3, d2);
 	return !ccw(d1, d2, d3) && ret > 0;
 }
 inline bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
 	return !ccw(d1, d2, d3) && !ccw(d1, d2, d4);
 }
-ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
+inline ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) {
 	return (d2 - d1) * (d4 - d3) / (d2 - d1).mag();
 }
-ld dist(const Pos& d1, const Pos& d2, const Pos& t, bool f = 0) {
+inline ld dist(const Pos& d1, const Pos& d2, const Pos& t, bool f = 0) {
 	if (f) return std::abs(cross(d1, d2, t)) / (d1 - d2).mag();
 	if (sign(projection(d1, d2, d2, t)) <= 0 &&
 		sign(projection(d2, d1, d1, t)) <= 0)
 		return std::abs(cross(d1, d2, t)) / (d1 - d2).mag();
 	return std::min((d1 - t).mag(), (d2 - t).mag());
 }
-Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
+inline Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
 	ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
 	return (p1 * a2 + p2 * a1) / (a1 + a2);
 }
-Pos intersection(Linear& l1, Linear& l2) { return intersection(l1[0], l1[1], l2[0], l2[1]); }
-bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+inline bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
 	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
 	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
 	//return f1 && f2;
@@ -202,7 +95,7 @@ bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
 		on_seg_strong(d1, d2, s2);
 	return (f1 && f2) || f3;
 }
-bool inner_check(const std::vector<Pos>& H, const Pos& p) {//concave
+inline bool inner_check(const std::vector<Pos>& H, const Pos& p) {//concave
 	int cnt = 0, sz = H.size();
 	for (int i = 0; i < sz; i++) {
 		Pos cur = H[i], nxt = H[(i + 1) % sz];
@@ -214,7 +107,7 @@ bool inner_check(const std::vector<Pos>& H, const Pos& p) {//concave
 	}
 	return cnt & 1;
 }
-ld area(std::vector<Pos>& H) {
+inline ld area(std::vector<Pos>& H) {
 	Pos pivot = Pos(0, 0);
 	ld ret = 0;
 	int h = H.size();
@@ -224,8 +117,8 @@ ld area(std::vector<Pos>& H) {
 	}
 	return ret / 2;
 }
-void norm(std::vector<Pos>& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
-bool valid_check(const Polygon& A, const Polygon& B, const ld& D) {
+inline void norm(std::vector<Pos>& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
+inline bool valid_check(const Polygon& A, const Polygon& B, const ld& D) {
 	int sza = A.size(), szb = B.size();
 	for (int i = 0; i < sza; i++) {
 		for (int j = 0; j < szb; j++) {
@@ -240,7 +133,7 @@ bool valid_check(const Polygon& A, const Polygon& B, const ld& D) {
 	}
 	return !inner_check(A, B[0]) && !inner_check(B, A[0]);
 }
-std::vector<Pos> circle_line_intersection(const Pos& o, const ld& r, const Pos& p1, const Pos& p2) {
+inline std::vector<Pos> circle_line_intersection(const Pos& o, const ld& r, const Pos& p1, const Pos& p2) {
 	ld d = dist(p1, p2, o, 1);
 	if (std::abs(d) > r) return {};
 	Pos vec = p2 - p1;
@@ -252,7 +145,7 @@ std::vector<Pos> circle_line_intersection(const Pos& o, const ld& r, const Pos& 
 	if (dot(p1, p2, m1, m2) < 0) std::swap(m1, m2);
 	return { m1, m2 };//p1->p2
 }
-std::vector<ld> dists(const Pos& a1, const Pos& a2, const Pos& b, const ld& D) {
+inline std::vector<ld> dists(const Pos& a1, const Pos& a2, const Pos& b, const ld& D) {
 	std::vector<ld> ret;
 	std::vector<Pos> tmp;
 	Pos v = ~(a1 - a2).unit();
@@ -271,11 +164,13 @@ std::vector<ld> dists(const Pos& a1, const Pos& a2, const Pos& b, const ld& D) {
 	}
 	return ret;
 }
-bool query() {
+inline bool query() {
 	ld D;
 	ld maxx = -INF, minx = INF;
 	ld amaxx = -INF, aminx = INF;
+	ld amaxy = -INF, aminy = INF;
 	ld bmaxx = -INF, bminx = INF;
+	ld bmaxy = -INF, bminy = INF;
 
 	std::cin >> D;
 	if (zero(D)) return 0;
@@ -287,6 +182,8 @@ bool query() {
 		minx = std::min(minx, p.x);
 		amaxx = std::max(amaxx, p.x);
 		aminx = std::min(aminx, p.x);
+		amaxy = std::max(amaxy, p.y);
+		aminy = std::min(aminy, p.y);
 	}
 	norm(A);
 	std::cin >> M;
@@ -297,8 +194,16 @@ bool query() {
 		minx = std::min(minx, p.x);
 		bmaxx = std::max(bmaxx, p.x);
 		bminx = std::min(bminx, p.x);
+		bmaxy = std::max(bmaxy, p.y);
+		bminy = std::min(bminy, p.y);
 	}
 	norm(B);
+
+	if (sign((aminy - bmaxy) - D) >= 0 || sign((bminy - amaxy) - D) >= 0) {
+		ld ans = std::min(amaxx - aminx, bmaxx - bminx);
+		std::cout << ans << "\n";
+		return 1;
+	}
 
 	//brute
 	ld ans = INF;
@@ -335,18 +240,16 @@ bool query() {
 		}
 	}
 	if (!F) ans = std::min(amaxx - aminx, bmaxx - bminx);
-	ans *= 100;
-	ans += 0.5;
-	ll a = ans;
-	ans = a / 100.;
 	std::cout << ans << "\n";
 	return 1;
 }
-void solve() {
+inline void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
+	//freopen("shy_in.txt", "r", stdin);
+	//freopen("shy_out.txt", "w", stdout);
 	query();
 	return;
 }
@@ -367,5 +270,13 @@ int main() { solve(); return 0; }//boj1879 Shy Polygon
 0
 
 114.882476
+
+	if (!F) ans = std::min(amaxx - aminx, bmaxx - bminx);
+	ans *= 100;
+	ans += 0.5;
+	ll a = ans;
+	ans = a / 100.;
+	std::cout << ans << "\n";
+	return 1;
 
 */
