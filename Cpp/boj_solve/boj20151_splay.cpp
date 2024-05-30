2ce0a0c17b5e56e84f5cd60e71ff251c5efa4bc8
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-05-30 19:58:13 +0900

    2405301958splay

--- Cpp/boj_solve/boj20151_splay.cpp
+++ Cpp/boj_solve/boj20151_splay.cpp
@@ -25,7 +25,8 @@ struct Pos {
 	Pos(int X = 0, int Y = 0, int I = 0, int D = 0) : x(X), y(Y), i(I), d(D) {}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
-	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	//bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator < (const Pos& p) const { return x == p.x ? y == p.y ? d < p.d : y < p.y : x < p.x; }
 	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
@@ -52,29 +53,14 @@ struct Pos {
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 } pos[LEN << 1]; const Pos O = Pos(0, 0);
-bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
-bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
+//bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
+//bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
 //bool cmpi(const Pos& p, const Pos& q) { return p.i < q.i; }
 typedef std::vector<Pos> Polygon;
-struct Seg {
-	Pos s, e;
-	Seg(Pos S = Pos(0, 0), Pos E = Pos(0, 0)) : s(S), e(E) {}
-	bool operator == (const Seg& S) const { return s == S.s && e == S.e; }
-	bool operator != (const Seg& S) const { return !(*this == S); }
-	//bool operator < (const Seg& S) const { return (s == S.s) ? e < S.e : s < S.s; }
-	bool operator < (const Seg& x) const {
-		if (x.s < s) return cross(x.e, x.s, s) > 0;
-		return cross(x.e, x.s, e) > 0;
-	}
-	friend std::ostream& operator << (std::ostream& os, const Seg& S) {
-		os << "DEBUG::Seg s: " << S.s << " | e: " << S.e << " DEBUG::Seg\n";
-		return os;
-	}
-} seg[LEN];
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+//ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+//ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 	ll ret = cross(d1, d2, d3);
 	return !ret ? 0 : ret > 0 ? 1 : -1;
@@ -82,9 +68,9 @@ int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
 bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
 	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
 }
-bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
-	return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0;
-}
+//bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
+//	return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0;
+//}
 bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
 	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
 	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
@@ -95,11 +81,26 @@ bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
 		on_seg_strong(d1, d2, s2);
 	return (f1 && f2) || f3;
 }
-bool intersect(int a, int b) {
-	Pos d1 = seg[a].s, d2 = seg[a].e, d3 = seg[b].s, d4 = seg[b].e;
+struct Seg {
+	Pos s, e;
+	Seg(Pos S = Pos(0, 0), Pos E = Pos(0, 0)) : s(S), e(E) {}
+	bool operator == (const Seg& S) const { return s == S.s && e == S.e; }
+	bool operator != (const Seg& S) const { return !(*this == S); }
+	//bool operator < (const Seg& S) const { return (s == S.s) ? e < S.e : s < S.s; }
+	bool operator < (const Seg& rhs) const {
+		if (rhs.s == s) return cross(rhs.s, rhs.e, e) < 0;
+		if (rhs.s < s) return cross(rhs.s, rhs.e, s) < 0;
+		return cross(rhs.s, rhs.e, e) < 0;
+	}
+	friend std::ostream& operator << (std::ostream& os, const Seg& S) {
+		os << "DEBUG::Seg s: " << S.s << " | e: " << S.e << " DEBUG::Seg\n";
+		return os;
+	}
+} seg[LEN];
+bool intersect(const int& a, const int& b) {
+	const Pos& d1 = seg[a].s, d2 = seg[a].e, d3 = seg[b].s, d4 = seg[b].e;
 	return intersect(d1, d2, d3, d4);
 }
-
 class SplayTree {
 	struct Node {
 		int i;
@@ -108,7 +109,7 @@ class SplayTree {
 		Node* p;
 		Node(int i) : i(i), l(0), r(0), p(0) {}
 		~Node() { if (l) delete l; if (r) delete r; }
-	}*root;
+	} *root;
 
 	void rotate(Node* x) {
 		Node* p = x->p;     //p : parent of x
@@ -210,7 +211,7 @@ public:
 		if (p->l && p->r) {
 			Node* l = p->l;
 			Node* r = p->r;
-			root = p->l; root->p = 0;
+			root = l; root->p = 0;
 			while (l->r) l = l->r;
 			l->r = r;
 			r->p = l;
@@ -232,7 +233,7 @@ public:
 		splay(p);
 		return p->i;
 	}
-} I;
+} ST;
 bool solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
@@ -240,21 +241,22 @@ bool solve() {
 	for (int i = 0; i < N; i++) {
 		std::cin >> seg[i].s >> seg[i].e;
 		if (seg[i].e < seg[i].s) std::swap(seg[i].s, seg[i].e);
-		seg[i].s.i = i, seg[i].e.i = i;
+		seg[i].s.i = seg[i].e.i = i;
 		seg[i].s.d = 1, seg[i].e.d = -1;
 		pos[i << 1] = seg[i].s;
 		pos[i << 1 | 1] = seg[i].e;
 	}
-	std::sort(pos, pos + (N << 1));
-	for (int i = 0; i < (N << 1); i++) {
+	N <<= 1;
+	std::sort(pos, pos + N);
+	for (int i = 0; i < N; i++) {
 		if (~pos[i].d) {
-			if (I.insert(pos[i].i)) {
+			if (ST.insert(pos[i].i)) {
 				std::cout << 1;
 				return 0;
 			}
 		}
 		else {
-			if (I.pop(pos[i].i)) {
+			if (ST.pop(pos[i].i)) {
 				std::cout << 1;
 				return 0;
 			}
