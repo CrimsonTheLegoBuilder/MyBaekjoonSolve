3564b420a29e9c965ca8f8dfa613040d45730d7c
crimson <redcube231@gmail.com>
2024-06-17 08:26:36 +0900

    2406170826nuclear1

---
+++ Cpp/boj_solve/boj2260_nuclear.cpp
@@ -0,0 +1,145 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+typedef long long ll;
+typedef double ld;
+const ll INF = 1e17;
+const int LEN = 1e2 + 1;
+const ld TOL = 1e-7;
+const ll MOD = 1'000'000'007;
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+inline ll sq(int x) { return (ll)x * x; }
+
+int N, M, T, Q;
+ll G[LEN][LEN];
+struct Pos {
+	int x, y;
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	int Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return y > 0 || y == 0 && x >= 0; }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+Pos nu = Pos();
+bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
+bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
+//bool cmpi(const Pos& p, const Pos& q) { return p.i < q.i; }
+typedef std::vector<Pos> Polygon;
+std::vector<Pos> C, H;
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ll ret = cross(d1, d2, d3); return ret < 0 ? -1 : !!ret; }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { ll ret = cross(d1, d2, d3, d4); return ret < 0 ? -1 : !!ret; }
+bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0; }
+bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0; }
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	//return f1 && f2;
+	bool f3 = on_seg_strong(s1, s2, d1) ||
+		on_seg_strong(s1, s2, d2) ||
+		on_seg_strong(d1, d2, s1) ||
+		on_seg_strong(d1, d2, s2);
+	return (f1 && f2) || f3;
+}
+struct Seg {
+	Pos s, e;
+	ll cost;
+	Seg(Pos S = Pos(), Pos E = Pos(), ll COST = 0) : cost(COST) {
+		s = S, e = E;
+		assert(!on_seg_strong(s, e, nu));
+		if (on_seg_strong(s, nu, e)) std::swap(s, e);
+		if (ccw(nu, s, e) < 0) std::swap(s, e);
+	}
+	bool operator == (const Seg& S) const { return s == S.s && e == S.e; }
+	bool operator != (const Seg& S) const { return !(*this == S); }
+	friend std::ostream& operator << (std::ostream& os, const Seg& S) {
+		os << "DEBUG::Seg s: " << S.s << " | e: " << S.e << " DEBUG::Seg\n";
+		return os;
+	}
+} seg[LEN];
+int ccw(const Seg& S, const Seg& E) { return ccw(S.s, S.e, E.s, E.e); }
+bool check(const Seg& S, const Seg& E) {
+	bool f1 = intersect(S.s, S.e, E.s, E.e);
+	int f2 = ccw(S.s, S.e, E.s, E.e);
+	if (!f2 && f1) return (on_seg_strong(S.s, S.e, E.s));
+	return f1 && f2 > 0;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin >> N >> nu;
+	for (int i = 0; i < N; i++) {
+		Pos s, e;
+		ll cost;
+		std::cin >> s >> e >> cost;
+		seg[i] = Seg(s, e, cost);
+	}
+	for (int i = 0; i < N; i++) {
+		G[i][i] = 0;
+		for (int j = 0; j < N; j++) {
+			if (i == j) continue;
+			G[i][j] = INF;
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (check(seg[i], seg[j])) G[i][j] = seg[i].cost;
+		}
+	}
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				G[i][j] = std::min(G[i][j], G[i][k] + G[k][j]);
+			}
+		}
+	}
+	ll ret = INF;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (i == j) continue;
+			if (check(seg[i], seg[j])) ret = std::min(ret, G[i][j] + G[j][i]);
+		}
+	}
+	std::cout << (ret > 1e16 ? -1 : ret) << "\n";
+	return;
+}
+int main() { solve(); return 0; }//boj2260 nuclear bomb
+
+/*
+달디달고 달디단
+bomb 양갱
+bomb 양갱
+bomb 양갱이야~~~
+*/
