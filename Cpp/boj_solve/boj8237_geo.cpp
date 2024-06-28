a8f9a8c36d9d46a5235f9631ed914158bfeffee1
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-06-29 03:37:10 +0900

    2406290337tape6

--- Cpp/boj_solve/boj8237_geo.cpp
+++ Cpp/boj_solve/boj8237_geo.cpp
@@ -7,7 +7,6 @@
 #include <vector>
 typedef long long ll;
 typedef long double ld;
-typedef std::pair<int, int> pi;
 const ll INF = 1e17;
 const int LEN = 1e3 + 1;
 const ld TOL = 1e-7;
@@ -27,12 +26,12 @@ struct BigInt {
 	bool operator < (const BigInt& p) const { return hi == p.hi ? lo < p.lo : hi < p.hi; }
 	bool operator == (const BigInt& p) const { return hi == p.hi && lo == p.lo; }
 };
-BigInt mul(ll num, ll den) {
-	assert(0 < den && den < DEN);
-	int m = num < 0 ? -1 : 1;
-	if (m < 0) num *= -1;
-	ll hi = (num / DEN) * den;
-	ll lo = (num % DEN) * den;
+inline BigInt mul(ll a, ll b) {
+	assert(0 < b && b < DEN);
+	int m = a < 0 ? -1 : 1;
+	if (m < 0) a *= -1;
+	ll hi = (a / DEN) * b;
+	ll lo = (a % DEN) * b;
 	hi += lo / DEN;
 	lo %= DEN;
 	return BigInt(m * hi, m * lo);
@@ -41,93 +40,102 @@ struct Pos {
 	ll x, y;
 	ll den;
 	bool dark;
-	Pos(ll X = 0, ll Y = 0, ll d = 1, bool S = 0) : x(X), y(Y), den(d), dark(S) {}
-	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
-	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
-	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
-	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
-	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ll& n) const { return { x * n, y * n }; }
-	Pos operator / (const ll& n) const { return { x / n, y / n }; }
-	ll operator * (const Pos& p) const { return x * p.x + y * p.y; }
-	ll operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
-	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
-	Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
-	Pos operator - () const { return { -x, -y, den }; }
-	Pos operator ~ () const { return { -y, x, den }; }
-	Pos operator ! () const { return { y, x }; }
-	Pos s() const { return { y, -x }; }
-	ld xy() const { return x * y; }
-	ll Euc() const { return x * x + y * y; }
-	ll Man() const { return std::abs(x) + std::abs(y); }
-	ld mag() const { return hypot(x, y); }
-	ld rad() const { return atan2(y, x); }
-	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
-	int quad() const { return y > 0 || y == 0 && x >= 0; }
-	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
-	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
-	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+	inline Pos(ll X = 0, ll Y = 0, ll d = 1, bool S = 0) : x(X), y(Y), den(d), dark(S) {}
+	inline bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	inline bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	inline bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	//inline bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	inline Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	inline Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	//inline Pos operator * (const ll& n) const { return { x * n, y * n }; }
+	//inline Pos operator / (const ll& n) const { return { x / n, y / n }; }
+	inline ll operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	inline ll operator / (const Pos& p) const { return x * p.y - y * p.x; }
+	//inline Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	//inline Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	//inline Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	//inline Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
+	//inline Pos& operator /= (const ll& scale) { x /= scale; y /= scale; return *this; }
+	inline Pos operator - () const { return { -x, -y, den }; }
+	inline Pos operator ~ () const { return { -y, x, den }; }
+	//inline Pos operator ! () const { return { y, x }; }
+	inline Pos s() const { return { y, -x }; }
+	//inline ld xy() const { return x * y; }
+	inline ll Euc() const { return x * x + y * y; }
+	//inline ll Man() const { return std::abs(x) + std::abs(y); }
+	//inline ld mag() const { return hypot(x, y); }
+	//inline ld rad() const { return atan2(y, x); }
+	//inline friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	//inline int quad() const { return y > 0 || y == 0 && x >= 0; }
+	//inline friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	inline friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	inline friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
 typedef std::vector<Pos> Polygon;
 Pos H[LEN];
 struct Line {//ax + by = c
 	Pos s;
 	ll c;
-	Line(Pos ps = Pos(0, 0), Pos pe = Pos(0, 0)) {
+	inline Line(Pos ps = Pos(0, 0), Pos pe = Pos(0, 0)) {
 		s = (pe - ps).s();
 		c = s * ps;
 	}
 	friend std::ostream& operator << (std::ostream& os, const Line& l) { os << l.s << " " << l.c; return os; }
 };
-Pos intersection(const Line& l1, const Line& l2) {
-	ll den = l1.s * l2.s;
+inline Pos intersection(const Line& l1, const Line& l2) {
+	ll den = l1.s / l2.s;
 	Pos s = l1.s * l2.c - l2.s * l1.c;
 	s.den = den;
 	return ~s;
 }
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ll ret = cross(d1, d2, d3); return !ret ? 0 : ret > 0 ? 1 : -1; }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { ll ret = cross(d1, d2, d3, d4); return !ret ? 0 : ret > 0 ? 1 : -1; }
-ll inner_check(const Pos& p1, const Pos& p2, const Pos& p3, const Pos& q) {
-	return ccw(p1, p2, q) < 0 && ccw(p2, p3, q) < 0 && ccw(p3, p1, q) < 0;
-}
-BigInt dot(const Pos& vec, const Pos& inx) { return BigInt(vec * inx, inx.den); }
+//Pos intersection(const Line& l1, const Line& l2) {
+//	Pos v1 = l1.s, v2 = l2.s;
+//	ld det = v1 / v2;
+//	return {
+//		(l1.c * v2.y - l2.c * v1.y) / det,
+//		(l2.c * v1.x - l1.c * v2.x) / det,
+//	};
+//}
+inline ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+inline ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+inline ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+//inline ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+inline BigInt dot(const Pos& vec, const Pos& inx) { return BigInt(vec * inx, inx.den); }
+inline int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ll ret = cross(d1, d2, d3); return !ret ? 0 : ret > 0 ? 1 : -1; }
+//inline int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { ll ret = cross(d1, d2, d3, d4); return !ret ? 0 : ret > 0 ? 1 : -1; }
+inline ll inner_check(const Pos& p1, const Pos& p2, const Pos& p3, const Pos& q) { return ccw(p1, p2, q) < 0 && ccw(p2, p3, q) < 0 && ccw(p3, p1, q) < 0; }
 inline bool cmp_frac(BigInt p, BigInt q) {
 	if (p.lo < 0) p = -p;//p.den < 0
 	if (q.lo < 0) q = -q;//q.den < 0
 	return mul(p.hi, q.lo) < mul(q.hi, p.lo);
 }
-inline bool frac_eq(BigInt p, BigInt q) {
+inline bool eq_frac(BigInt p, BigInt q) {
 	if (p.lo < 0) p = -p;//p.den < 0
 	if (q.lo < 0) q = -q;//q.den < 0
 	return mul(p.hi, q.lo) == mul(q.hi, p.lo);
 }
-bool above(const Line& l, const Pos& inx, bool rvs = 0) {
+inline bool above(const Line& l, const Pos& inx, bool rvs = 0) {
 	BigInt dist = dot(l.s, inx);
 	if (rvs) return cmp_frac(dist, BigInt(l.c, 1));
 	return cmp_frac(BigInt(l.c, 1), dist);
 }
-bool on_line(const Line& l, const Pos& inx) {
+inline bool on_line(const Line& l, const Pos& inx) {
 	BigInt dist = dot(l.s, inx);
-	return frac_eq(dist, BigInt(l.c, 1));
+	return eq_frac(dist, BigInt(l.c, 1));
 }
-bool closer(const Pos& p1, const Pos& p2, const Pos& q, const Pos& candi) {
-	ll ccwq = cross(p1, p2, q);
-	ll numq = ccwq * ccwq;
-	if (ccwq < 0) numq *= -1;
-	ll denq = (q - p1).Euc();
-	ll ccwx = cross(p1, p2, q);
-	ll numx = ccwx * ccwx;
-	if (ccwx < 0) numx *= -1;
-	ll denx = (candi - p1).Euc();
-	return cmp_frac(BigInt(numq, denq), BigInt(numx, denx));
+//inline bool closer(const Pos& p1, const Pos& p2, const Pos& q, const Pos& candi) {
+//	ll ccwq = cross(p1, p2, q);
+//	ll numq = ccwq * ccwq;
+//	if (ccwq < 0) numq *= -1;
+//	ll denq = (q - p1).Euc();
+//	ll ccwx = cross(p1, p2, q);
+//	ll numx = ccwx * ccwx;
+//	if (ccwx < 0) numx *= -1;
+//	ll denx = (candi - p1).Euc();
+//	return cmp_frac(BigInt(numq, denq), BigInt(numx, denx));
+//}
+inline bool closer(const Pos& p1, const Pos& p2, const Pos& q, const Pos& candi) {
+	return std::abs(cross(p1, p2, q)) > std::abs(cross(p1, p2, candi));
 }
 //std::string all_light_check() {
 //	for (int i = 0; i < N; i++) {//half_plane_intersection must include i1-i2
@@ -151,7 +159,7 @@ bool closer(const Pos& p1, const Pos& p2, const Pos& q, const Pos& candi) {
 //	}
 //	return "NIE";
 //}
-std::string half_plane_intersection_except_x(int x = -1) {
+std::string half_plane_intersection_except_x(const int& x = -1) {
 	int sz = (x == -1 ? N : x + 1);
 	int i = (x == -1 ? 0 : x);
 	for (i; i < sz; i++) {//half_plane_intersection must include i1-i2
@@ -178,18 +186,17 @@ std::string half_plane_intersection_except_x(int x = -1) {
 std::string query() {
 	std::cin >> N;
 	for (int i = 0; i < N; i++) std::cin >> H[i];
-	bool all_light = 1, all_dark = 1;
+	int illum = 0;
 	for (int i = 0; i < N; i++) {
 		std::cin >> sc;
 		H[i].dark = sc == 'C';
-		if (!H[i].dark) all_dark = 0;
-		if (H[i].dark) all_light = 0;
+		if (!H[i].dark) illum++;
 	}
 
-	if (all_dark) return "NIE";
-	if (all_light) return half_plane_intersection_except_x();
+	if (illum < 3) return "NIE";
+	if (illum == N) return half_plane_intersection_except_x();
 
-	int i1 = 0, i2 = 0, i3 = 0, i4 = 0;
+	int i1 = 0, i2 = 0, i3 = 0;
 	while (1) {//merge dark walls
 		if (N <= 3) return "NIE";//at least one must be dark.
 
@@ -232,26 +239,24 @@ std::string query() {
 
 		if (dark_hi) {//merge dark walls
 			for (int i = c2; i < N; i++) H[i - (c2 - c1 - 1)] = H[i];
-			//H[c1].dark = 1;
 			N -= (c2 - c1 - 1);
 		}
-		else {//merge dark walls
+		if (dark_lo) {//merge dark walls
 			for (int i = c1; i <= c2; i++) H[i - c1] = H[i];
-			N = (c2 - c1 - 1);
-			//H[N - 1].dark = 1;
+			N = c2 - c1 - 1;
 		}
 	}
 
 	//for (int i = 0; i < N; i++) if (H[i].dark && H[(i + 1) % N].dark) return "NIE";
 
-	for (int i = 0; i < N; i++) {
-		/*
-			If there is a light wall in the same direction
-			on either side of a dark wall, a half-plane intersection is not formed.
-		*/
-		Pos& prev = H[i], & w1 = H[(i + 1) % N], & w2 = H[(i + 2) % N], & nxt = H[(i + 3) % N];
-		if (w1.dark && ccw(prev, w1, w2) == ccw(w1, w2, nxt)) return "NIE";
-	}
+	//for (int i = 0; i < N; i++) {
+	//	/*
+	//		If there is a light wall in the same direction
+	//		on either side of a dark wall, a half-plane intersection is not formed.
+	//	*/
+	//	Pos& prev = H[i], & w1 = H[(i + 1) % N], & w2 = H[(i + 2) % N], & nxt = H[(i + 3) % N];
+	//	if (w1.dark && ccw(prev, w1, w2) == ccw(w1, w2, nxt)) return "NIE";
+	//}
 
 	i1 = 0, i2 = 0;
 	while (!H[i1].dark) i1++;
@@ -265,7 +270,7 @@ std::string query() {
 		*/
 		Pos& w1 = H[i1], & w2 = H[(i1 + 1) % N];
 		Pos& w3 = H[i2], & w4 = H[(i2 + 1) % N];
-		if (!ccw(w1, w2, w3, w4)) return "NIE";
+		if (!cross(w1, w2, w3, w4)) return "NIE";
 		Pos inx = intersection(Line(w1, w2), Line(w3, w4));
 		for (int i = 0; i < N; i++) {
 			Pos& prev = H[(i - 1 + N) % N], & nxt = H[(i + 2) % N];
@@ -287,21 +292,6 @@ std::string query() {
 		Pos& p1 = H[i1], & p2 = H[(i1 + 1) % N];
 		if (p1.dark && ccw(prev, p1, p2) == ccw(p1, p2, nxt)) return "NIE";
 		return half_plane_intersection_except_x(i1);
-		//Pos vec = w2 - w1;
-		//BigInt hi = { INF, 1 }, lo = { -INF, 1 };
-		//for (int i = 0; i < N; i++) {
-		//	if (i == i1) continue;
-		//	Pos& p1 = H[i], & p2 = H[(i + 1) % N];
-		//	ll det = cross(w1, w2, p1, p2);
-		//	if (!det) {
-		//		if (ccw(p1, p2, w1) >= 0) return "NIE";
-		//		continue;
-		//	}
-		//	BigInt inx = dot(vec, intersection(Line(w1, w2), Line(p1, p2)));
-		//	if (det < 0 && cmp_frac(inx, hi)) hi = inx;
-		//	else if (det > 0 && cmp_frac(lo, inx)) lo = inx;
-		//}
-		//if (!cmp_frac(lo, hi)) return "NIE";
 	}
 	return "TAK";
 }
