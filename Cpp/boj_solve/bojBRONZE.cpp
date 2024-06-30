1df6126b79ce273db089d71cf0792b1ce80e3324
crimson <redcube231@gmail.com>
2024-07-01 08:33:58 +0900

    2407010833

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -4,15 +4,18 @@
 #include <vector>
 #include <cmath>
 typedef long long ll;
-int N, A, B, C, D, E;
+typedef long double ld;
+const ld PI = acos(-1);
+ll N, a1, p1, r1, p2;
 
 int main() {
 	std::cout << std::fixed;
 	std::cout.precision(7);
-	std::cin >> A >> B >> C >> D >> E;
-	N = 0;
-	if (A < 0) N = -A * C + D + B * E;
-	else if (A > 0) N = (B - A) * E;
-	std::cout << N << "\n";
+	std::cin >> N;
+	while (N--) {
+		std::cin >> a1 >> p1 >> r1 >> p2;
+		if ((ld)p1 / a1 < (ld)p2 / (PI * r1 * r1)) std::cout << "Slice of pizza\n";
+		//else std::cout << "Whole pizza\n";
+	}
 	return 0;
 }
