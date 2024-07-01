b7239f0360e96f1d2c5fa7ae8d42e0ab6c1618dc
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-07-01 23:03:08 +0900

    2407012303geo

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-#include <vector>
 #include <cmath>
+#include <cstring>
 typedef long long ll;
-typedef long double ld;
-const ld PI = acos(-1);
-ll N, a1, p1, r1, p2;
+int T, N, W, H, D, L;
+inline ll sq(int x) { return (ll)x * x; }
 
+struct Pos {
+	int x, y;
+	inline Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	inline bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	inline bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	inline Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	inline Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	inline ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	inline ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	inline ll Euc() const { return (ll)x * x + (ll)y * y; }
+	inline friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	inline friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+} p;
 int main() {
-	std::cout << std::fixed;
-	std::cout.precision(7);
-	std::cin >> N;
-	while (N--) {
-		std::cin >> a1 >> p1 >> r1 >> p2;
-		if ((ld)p1 / a1 < (ld)p2 / (PI * r1 * r1)) std::cout << "Slice of pizza\n";
-		//else std::cout << "Whole pizza\n";
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin >> T;
+	while (T--) {
+		std::cin >> N;
+		ll all = 0;
+		while (N--) {
+			std::cin >> p;
+			ll d = p.Euc();
+			if (d <= sq(20)) all += 10;
+			else if (d <= sq(40)) all += 9;
+			else if (d <= sq(60)) all += 8;
+			else if (d <= sq(80)) all += 7;
+			else if (d <= sq(100)) all += 6;
+			else if (d <= sq(120)) all += 5;
+			else if (d <= sq(140)) all += 4;
+			else if (d <= sq(160)) all += 3;
+			else if (d <= sq(180)) all += 2;
+			else if (d <= sq(200)) all += 1;
+		}
+		//std::cout << all << "\n";
 	}
 	return 0;
 }
