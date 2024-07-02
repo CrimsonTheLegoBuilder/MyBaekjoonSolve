52fc5da1ec2574a486f416793bcb1165ec3fb204
crimson <redcube231@gmail.com>
2024-07-02 17:45:30 +0900

    geo

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -3,44 +3,17 @@
 #include <cmath>
 #include <cstring>
 typedef long long ll;
-int T, N, W, H, D, L;
-inline ll sq(int x) { return (ll)x * x; }
+ll N, H, V;
 
-struct Pos {
-	int x, y;
-	inline Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
-	inline bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
-	inline bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	inline Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
-	inline Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	inline ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
-	inline ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
-	inline ll Euc() const { return (ll)x * x + (ll)y * y; }
-	inline friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
-	inline friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
-} p;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> T;
-	while (T--) {
-		std::cin >> N;
-		ll all = 0;
-		while (N--) {
-			std::cin >> p;
-			ll d = p.Euc();
-			if (d <= sq(20)) all += 10;
-			else if (d <= sq(40)) all += 9;
-			else if (d <= sq(60)) all += 8;
-			else if (d <= sq(80)) all += 7;
-			else if (d <= sq(100)) all += 6;
-			else if (d <= sq(120)) all += 5;
-			else if (d <= sq(140)) all += 4;
-			else if (d <= sq(160)) all += 3;
-			else if (d <= sq(180)) all += 2;
-			else if (d <= sq(200)) all += 1;
-		}
-		//std::cout << all << "\n";
-	}
+	std::cin >> N >> H >> V;
+	ll a, b, c, d;
+	a = H * V;
+	b = (N - H) * V;
+	c = H * (N - V);
+	d = (N - H) * (N - V);
+	std::cout << (std::max({ a, b, c, d }) << 2) << "\n";
 	return 0;
 }
--- Python/__test1.py
+++ Python/__test1.py
@@ -1,42 +1,46 @@
 import sys
-from math import hypot, pi
+from math import hypot, pi, atan, atan2
 TOL = 1e-7
 
+d = hypot(3280, 2464)
+print(d)
+t = 2 * atan(3 / (2 * 0.87))
+print(t / pi * 180)
 
-class Pos:
-    def __init__(self, x_: float, y_: float):
-        self.x = x_
-        self.y = y_
-
-    def __sub__(self, p: 'Pos') -> 'Pos':
-        return Pos(self.x - p.x, self.y - p.y)
-
-    def __floordiv__(self, p: 'Pos') -> int or float:
-        return self.x * p.y - self.y * p.x
-
-    def __pow__(self, p: 'Pos') -> int or float:
-        return self.x * p.x + self.y * p.y
-
-    def mag(self) -> float:
-        return hypot(self.x, self.y)
-
-    def euc(self) -> int or float:
-        return self.x * self.x + self.y * self.y
-
-
-def cross(d1: Pos, d2: Pos, d3: Pos) -> int or float:
-    return (d2 - d1) // (d3 - d2)
-
-
-# p1: Pos = Pos(17, 7)
-# p2: Pos = Pos(19, 90)
-# vec = p2 - p1
-# h = Pos(-vec.y, vec.x)
-# l = h.mag()
-# h = Pos(h.x / l * 100, h.y / l * 100)
-# print(cross(p1, p2, h) / 2)
-
-a = Pos(1, 2)
-b = Pos(10, 27)
-c = Pos(28, 26)
-print((a - b).mag() + (b - c).mag())
+# class Pos:
+#     def __init__(self, x_: float, y_: float):
+#         self.x = x_
+#         self.y = y_
+#
+#     def __sub__(self, p: 'Pos') -> 'Pos':
+#         return Pos(self.x - p.x, self.y - p.y)
+#
+#     def __floordiv__(self, p: 'Pos') -> int or float:
+#         return self.x * p.y - self.y * p.x
+#
+#     def __pow__(self, p: 'Pos') -> int or float:
+#         return self.x * p.x + self.y * p.y
+#
+#     def mag(self) -> float:
+#         return hypot(self.x, self.y)
+#
+#     def euc(self) -> int or float:
+#         return self.x * self.x + self.y * self.y
+#
+#
+# def cross(d1: Pos, d2: Pos, d3: Pos) -> int or float:
+#     return (d2 - d1) // (d3 - d2)
+#
+#
+# # p1: Pos = Pos(17, 7)
+# # p2: Pos = Pos(19, 90)
+# # vec = p2 - p1
+# # h = Pos(-vec.y, vec.x)
+# # l = h.mag()
+# # h = Pos(h.x / l * 100, h.y / l * 100)
+# # print(cross(p1, p2, h) / 2)
+#
+# a = Pos(1, 2)
+# b = Pos(10, 27)
+# c = Pos(28, 26)
+# print((a - b).mag() + (b - c).mag())
