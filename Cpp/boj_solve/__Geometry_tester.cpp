a74d96dd4120f09215ba75416e91746bbec79252
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-07-06 21:25:05 +0900

    debug

--- Cpp/boj_solve/__Geometry_tester.cpp
+++ Cpp/boj_solve/__Geometry_tester.cpp
@@ -8,6 +8,7 @@
 #include <random>
 #include <array>
 #include <tuple>
+#include <stack>
 typedef long long ll;
 typedef long double ld;
 typedef std::pair<int, int> pi;
@@ -20,13 +21,240 @@ int dcmp(const ld& x) { return std::abs(x) < TOL ? 0 : x > 0 ? 1 : -1; }
 int dcmp(const ll& x) { return !x ? 0 : x > 0 ? 1 : -1; }
 struct Info { ll area, l, r; };
 
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+
+pll operator-(pll x, pll y) { return { x.first - y.first, x.second - y.second }; }
+ll operator/(pll x, pll y) { return x.first * y.second - x.second * y.first; }
+
+
+bool ccw(pll x, pll y, pll z)
+{
+    pll v = y - x, u = z - y;
+    return v / u > 0;
+}
+
+ll n, i, j, k, maxi;
+pll p[100001], a;
+
+
+bool cmp(pll x, pll y)
+{
+    x = x - a, y = y - a;
+    if (x / y == 0)
+    {
+        if (x.first == y.first)
+            return x.second < y.second;
+        return x.first < y.first;
+    }
+    return x / y > 0;
+}
+
+ll dis(pll x, pll y)
+{
+    return ((x.first - y.first) * (x.first - y.first) + (x.second - y.second) * (x.second - y.second));
+}
+
+int main() {
+
+    cin >> n;
+    for (i = 1; i <= n; i++)
+        scanf("%lld %lld", &p[i].first, &p[i].second);
+
+    if (n == 1)
+        printf("0");
+    else
+    {
+
+        sort(p + 1, p + (n + 1));
+        a = p[1];
+
+        ll imsi = 1;
+        while (p[imsi] == p[1])
+        {
+            imsi++;
+            if (imsi > n)
+                break;
+        }
+        if (imsi > n)
+        {
+            printf("0");
+        }
+        else
+        {
+            sort(p + imsi, p + (n + 1), cmp);
+
+            stack<ll> s;
+
+            s.push(1), s.push(imsi);
+
+            ll first, second;
+            for (i = imsi + 1; i <= n; i++)
+            {
+                second = s.top();
+                s.pop();
+                first = s.top();
+
+                while (!ccw(p[first], p[second], p[i]))
+                {
+                    second = s.top();
+                    s.pop();
+                    if (s.empty())
+                        break;
+                    first = s.top();
+
+                }
+                s.push(second), s.push(i);
+
+            }
+
+            vector<ll> convex;
+
+            while (!s.empty())
+                convex.push_back(s.top()), s.pop();
+
+            ll aa = convex.size();
+
+            for (i = 0; i < aa; i++)
+                convex.push_back(convex[i]);
+
+            if (aa == 2)
+                maxi = dis(p[convex[0]], p[convex[1]]);
+            else
+            {
+                ll small = 10000000, large = -10000000, lpoint, spoint;
+
+                for (i = 0; i < aa; i++)
+                {
+                    if (p[convex[i]].first < small)
+                        small = p[convex[i]].first, spoint = i;
+                    if (p[convex[i]].first > large)
+                        large = p[convex[i]].first, lpoint = i;
+                }
+
+
+                ll lp = lpoint, sp = spoint;
+
+                while ((lp < lpoint + aa) || (sp < spoint + aa))
+                {
+                    if (maxi < dis(p[convex[lp]], p[convex[sp]]))
+                        maxi = dis(p[convex[lp]], p[convex[sp]]);
+
+                    pll lpp = p[convex[lp + 1]] - p[convex[lp]];
+                    pll spp = p[convex[sp + 1]] - p[convex[sp]];
+
+                    if (lpp / spp == 0)
+                    {
+                        if (lp == 2 * aa - 1)
+                            sp++;
+                        else
+                            lp++;
+                    }
+                    else if (lpp / spp > 0)
+                        lp++;
+                    else
+                        sp++;
+
+
+
+                }
+
+
+            }
+
+            cout << maxi;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+////struct Pos3D {
+////	ll x, y, z;
+////	Pos3D(ll X = 0, ll Y = 0, ll Z = 0) : x(X), y(Y), z(Z) {}
+////	bool operator == (const Pos3D& p) const { return x == p.x && y == p.y && z == p.z; }
+////	bool operator != (const Pos3D& p) const { return x != p.x || y != p.y || z != p.z; }
+////	bool operator < (const Pos3D& p) const { return x == p.x ? y == p.y ? z < p.z : y < p.y : x < p.x; }
+////	ll operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
+////	Pos3D operator / (const Pos3D& p) const {
+////		Pos3D ret;
+////		ret.x = y * p.z - z * p.y;
+////		ret.y = z * p.x - x * p.z;
+////		ret.z = x * p.y - y * p.x;
+////		return ret;
+////	}
+////	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
+////	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
+////	Pos3D operator * (const ll& scalar) const { return { x * scalar, y * scalar, z * scalar }; }
+////	Pos3D operator / (const ll& scalar) const { return { x / scalar, y / scalar, z / scalar }; }
+////	Pos3D& operator += (const Pos3D& p) { x += p.x; y += p.y; z += p.z; return *this; }
+////	Pos3D& operator -= (const Pos3D& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
+////	Pos3D& operator *= (const ll& scalar) { x *= scalar; y *= scalar; z *= scalar; return *this; }
+////	Pos3D& operator /= (const ll& scalar) { x /= scalar; y /= scalar; z /= scalar; return *this; }
+////	ll Euc() const { return x * x + y * y + z * z; }
+////	ld mag() const { return sqrtl(Euc()); }
+////	friend std::istream& operator >> (std::istream& is, Pos3D& p) {
+////		is >> p.x >> p.y >> p.z;
+////		return is;
+////	}
+////	friend std::ostream& operator << (std::ostream& os, const Pos3D& p) {
+////		os << p.x << " " << p.y << " " << p.z << "\n";
+////		return os;
+////	}
+////} candi[LEN], willy, MAXP{ INF, INF, INF };
+////const Pos3D O3D = { 0, 0, 0 };
+////using Face = std::array<int, 3>;
+////std::vector<Face> Hull3D;
+////struct Edge {
+////	int face_num, edge_num;
+////	Edge(int t = 0, int v = 0) : face_num(t), edge_num(v) {}
+////};
+////Pos3D cross(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) / (d3 - d2); }
+////Pos3D cross(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& d4) { return (d2 - d1) / (d4 - d3); }
+////ll dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) * (d3 - d2); }
+////ll dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& d4) { return (d2 - d1) * (d4 - d3); }
+////bool on_seg_strong(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) {
+////	ll ret = dot(d1, d3, d2);
+////	return !cross(d1, d2, d3).mag() && ret >= 0;
+////}
+////bool on_seg_weak(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) {
+////	ll ret = dot(d1, d3, d2);
+////	return !cross(d1, d2, d3).mag() && ret > 0;
+////}
+////int ccw(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& norm) {
+////	Pos3D CCW = cross(d1, d2, d3);
+////	std::cout << CCW;
+////	ll ret = CCW * norm;
+////	std::cout << "ret: " << ret << "\n";
+////	return !ret ? 0 : ret > 0 ? 1 : -1;
+////}
+////ld area(const std::vector<Pos3D>& H, const Pos3D& norm) {
+////	ll ret = 0;
+////	if (H.size() < 3) return ret;
+////	Pos3D O = H[0];
+////	int sz = H.size();
+////	for (int i = 0; i < sz; i++) {
+////		Pos3D cur = H[i], nxt = H[(i + 1) % sz];
+////		ret += cross(O, cur, nxt) * norm / norm.Euc();
+////	}
+////	return std::abs(ret * .5);
+////}
+////bool above(const Pos3D& a, const Pos3D& b, const Pos3D& c, const Pos3D& p) {// is p strictly above plane
+////	return cross(a, b, c) * (p - a) > 0;
+////}
+//
 //struct Pos3D {
-//	ll x, y, z;
-//	Pos3D(ll X = 0, ll Y = 0, ll Z = 0) : x(X), y(Y), z(Z) {}
-//	bool operator == (const Pos3D& p) const { return x == p.x && y == p.y && z == p.z; }
-//	bool operator != (const Pos3D& p) const { return x != p.x || y != p.y || z != p.z; }
-//	bool operator < (const Pos3D& p) const { return x == p.x ? y == p.y ? z < p.z : y < p.y : x < p.x; }
-//	ll operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
+//	ld x, y, z;
+//	Pos3D(ld X = 0, ld Y = 0, ld Z = 0) : x(X), y(Y), z(Z) {}
+//	bool operator == (const Pos3D& p) const { return zero(x - p.x) && zero(y - p.y) && zero(z - p.z); }
+//	bool operator != (const Pos3D& p) const { return !zero(x - p.x) || !zero(y - p.y) || !zero(z - p.z); }
+//	bool operator < (const Pos3D& p) const { return zero(x - p.x) ? zero(y - p.y) ? z < p.z : y < p.y : x < p.x; }
+//	ld operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
 //	Pos3D operator / (const Pos3D& p) const {
 //		Pos3D ret;
 //		ret.x = y * p.z - z * p.y;
@@ -36,14 +264,23 @@ struct Info { ll area, l, r; };
 //	}
 //	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
 //	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
-//	Pos3D operator * (const ll& scalar) const { return { x * scalar, y * scalar, z * scalar }; }
-//	Pos3D operator / (const ll& scalar) const { return { x / scalar, y / scalar, z / scalar }; }
+//	Pos3D operator * (const ld& scalar) const { return { x * scalar, y * scalar, z * scalar }; }
+//	Pos3D operator / (const ld& scalar) const { return { x / scalar, y / scalar, z / scalar }; }
 //	Pos3D& operator += (const Pos3D& p) { x += p.x; y += p.y; z += p.z; return *this; }
 //	Pos3D& operator -= (const Pos3D& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
-//	Pos3D& operator *= (const ll& scalar) { x *= scalar; y *= scalar; z *= scalar; return *this; }
-//	Pos3D& operator /= (const ll& scalar) { x /= scalar; y /= scalar; z /= scalar; return *this; }
-//	ll Euc() const { return x * x + y * y + z * z; }
+//	Pos3D& operator *= (const ld& scalar) { x *= scalar; y *= scalar; z *= scalar; return *this; }
+//	Pos3D& operator /= (const ld& scalar) { x /= scalar; y /= scalar; z /= scalar; return *this; }
+//	ld Euc() const { return x * x + y * y + z * z; }
 //	ld mag() const { return sqrtl(Euc()); }
+//	ld lon() const { return atan2(y, x); }
+//	ld lat() const { return atan2(z, sqrtl(x * x + y * y)); }
+//	Pos3D unit() const { return *this / mag(); }
+//	Pos3D norm(const Pos3D& p) const { return (*this / p).unit(); }
+//	Pos3D rotate(const ld& th, const Pos3D& axis) const {
+//		ld SIN = sin(th), COS = cos(th);
+//		Pos3D u = axis.unit();
+//		return u * (*this * u) * (1 - COS) + (*this * COS) - (*this / u) * SIN;
+//	}
 //	friend std::istream& operator >> (std::istream& is, Pos3D& p) {
 //		is >> p.x >> p.y >> p.z;
 //		return is;
@@ -52,131 +289,48 @@ struct Info { ll area, l, r; };
 //		os << p.x << " " << p.y << " " << p.z << "\n";
 //		return os;
 //	}
-//} candi[LEN], willy, MAXP{ INF, INF, INF };
-//const Pos3D O3D = { 0, 0, 0 };
-//using Face = std::array<int, 3>;
-//std::vector<Face> Hull3D;
-//struct Edge {
-//	int face_num, edge_num;
-//	Edge(int t = 0, int v = 0) : face_num(t), edge_num(v) {}
 //};
+//const Pos3D O3D = { 0, 0, 0 };
 //Pos3D cross(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) / (d3 - d2); }
-//Pos3D cross(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& d4) { return (d2 - d1) / (d4 - d3); }
-//ll dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) * (d3 - d2); }
-//ll dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& d4) { return (d2 - d1) * (d4 - d3); }
-//bool on_seg_strong(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) {
-//	ll ret = dot(d1, d3, d2);
-//	return !cross(d1, d2, d3).mag() && ret >= 0;
-//}
-//bool on_seg_weak(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) {
-//	ll ret = dot(d1, d3, d2);
-//	return !cross(d1, d2, d3).mag() && ret > 0;
-//}
+//ld dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) * (d3 - d2); }
 //int ccw(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& norm) {
 //	Pos3D CCW = cross(d1, d2, d3);
-//	std::cout << CCW;
-//	ll ret = CCW * norm;
-//	std::cout << "ret: " << ret << "\n";
-//	return !ret ? 0 : ret > 0 ? 1 : -1;
-//}
-//ld area(const std::vector<Pos3D>& H, const Pos3D& norm) {
-//	ll ret = 0;
-//	if (H.size() < 3) return ret;
-//	Pos3D O = H[0];
-//	int sz = H.size();
-//	for (int i = 0; i < sz; i++) {
-//		Pos3D cur = H[i], nxt = H[(i + 1) % sz];
-//		ret += cross(O, cur, nxt) * norm / norm.Euc();
-//	}
-//	return std::abs(ret * .5);
+//	ld ret = CCW * norm;
+//	return zero(ret) ? 0 : ret > 0 ? 1 : -1;
 //}
 //bool above(const Pos3D& a, const Pos3D& b, const Pos3D& c, const Pos3D& p) {// is p strictly above plane
 //	return cross(a, b, c) * (p - a) > 0;
 //}
-
-struct Pos3D {
-	ld x, y, z;
-	Pos3D(ld X = 0, ld Y = 0, ld Z = 0) : x(X), y(Y), z(Z) {}
-	bool operator == (const Pos3D& p) const { return zero(x - p.x) && zero(y - p.y) && zero(z - p.z); }
-	bool operator != (const Pos3D& p) const { return !zero(x - p.x) || !zero(y - p.y) || !zero(z - p.z); }
-	bool operator < (const Pos3D& p) const { return zero(x - p.x) ? zero(y - p.y) ? z < p.z : y < p.y : x < p.x; }
-	ld operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
-	Pos3D operator / (const Pos3D& p) const {
-		Pos3D ret;
-		ret.x = y * p.z - z * p.y;
-		ret.y = z * p.x - x * p.z;
-		ret.z = x * p.y - y * p.x;
-		return ret;
-	}
-	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
-	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
-	Pos3D operator * (const ld& scalar) const { return { x * scalar, y * scalar, z * scalar }; }
-	Pos3D operator / (const ld& scalar) const { return { x / scalar, y / scalar, z / scalar }; }
-	Pos3D& operator += (const Pos3D& p) { x += p.x; y += p.y; z += p.z; return *this; }
-	Pos3D& operator -= (const Pos3D& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
-	Pos3D& operator *= (const ld& scalar) { x *= scalar; y *= scalar; z *= scalar; return *this; }
-	Pos3D& operator /= (const ld& scalar) { x /= scalar; y /= scalar; z /= scalar; return *this; }
-	ld Euc() const { return x * x + y * y + z * z; }
-	ld mag() const { return sqrtl(Euc()); }
-	ld lon() const { return atan2(y, x); }
-	ld lat() const { return atan2(z, sqrtl(x * x + y * y)); }
-	Pos3D unit() const { return *this / mag(); }
-	Pos3D norm(const Pos3D& p) const { return (*this / p).unit(); }
-	Pos3D rotate(const ld& th, const Pos3D& axis) const {
-		ld SIN = sin(th), COS = cos(th);
-		Pos3D u = axis.unit();
-		return u * (*this * u) * (1 - COS) + (*this * COS) - (*this / u) * SIN;
-	}
-	friend std::istream& operator >> (std::istream& is, Pos3D& p) {
-		is >> p.x >> p.y >> p.z;
-		return is;
-	}
-	friend std::ostream& operator << (std::ostream& os, const Pos3D& p) {
-		os << p.x << " " << p.y << " " << p.z << "\n";
-		return os;
-	}
-};
-const Pos3D O3D = { 0, 0, 0 };
-Pos3D cross(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) / (d3 - d2); }
-ld dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) * (d3 - d2); }
-int ccw(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& norm) {
-	Pos3D CCW = cross(d1, d2, d3);
-	ld ret = CCW * norm;
-	return zero(ret) ? 0 : ret > 0 ? 1 : -1;
-}
-bool above(const Pos3D& a, const Pos3D& b, const Pos3D& c, const Pos3D& p) {// is p strictly above plane
-	return cross(a, b, c) * (p - a) > 0;
-}
-int main() {
-	std::cout << std::fixed;
-	std::cout.precision(15);
-	Pos3D a, b, c;
-	std::cin >> a >> b >> c;
-	std::cout << "above: " << above(a, b, c, O3D) << "\n";
-	Pos3D n = cross(a, b, c);
-	int x = ccw(a, b, c, n);
-	std::cout << x << "\n";
-	std::cout << (a - b);
-	std::cout << (b - c);
-	std::cout << "ccw a b: " << ccw(a, b, O3D, n) << "\n";
-	std::cout << "ccw b c: " << ccw(b, c, O3D, n) << "\n";
-	std::cout << "ccw c a: " << ccw(c, a, O3D, n) << "\n";
-	std::cout << x << "\n";
-	std::cout << n.Euc() << "\n";
-	std::cout << n;
-	std::cout << n.mag() << "\n";
-	std::cout << n * (O3D - a) << "\n";
-}
-
-/*
-
-1 3
--280000 -450000 0
--437600 259800 148400
-275680 452688 1484
-
-9223372036854775807
-9011369165176521728
-312533749465952362168320
-
-*/
+//int main() {
+//	std::cout << std::fixed;
+//	std::cout.precision(15);
+//	Pos3D a, b, c;
+//	std::cin >> a >> b >> c;
+//	std::cout << "above: " << above(a, b, c, O3D) << "\n";
+//	Pos3D n = cross(a, b, c);
+//	int x = ccw(a, b, c, n);
+//	std::cout << x << "\n";
+//	std::cout << (a - b);
+//	std::cout << (b - c);
+//	std::cout << "ccw a b: " << ccw(a, b, O3D, n) << "\n";
+//	std::cout << "ccw b c: " << ccw(b, c, O3D, n) << "\n";
+//	std::cout << "ccw c a: " << ccw(c, a, O3D, n) << "\n";
+//	std::cout << x << "\n";
+//	std::cout << n.Euc() << "\n";
+//	std::cout << n;
+//	std::cout << n.mag() << "\n";
+//	std::cout << n * (O3D - a) << "\n";
+//}
+//
+///*
+//
+//1 3
+//-280000 -450000 0
+//-437600 259800 148400
+//275680 452688 1484
+//
+//9223372036854775807
+//9011369165176521728
+//312533749465952362168320
+//
+//*/
