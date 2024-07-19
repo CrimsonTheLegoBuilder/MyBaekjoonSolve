8c28137cc2450c221930c252f6984f819a788016
crimson <redcube231@gmail.com>
2024-07-19 13:01:35 +0900

    2407191301kuru1

---
+++ Cpp/boj_solve/boj22427_kurukuru.cpp
@@ -0,0 +1,254 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <deque>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+const ld INF = 1e17;
+const ld TOL = 1e-10;
+const ld PI = acos(-1);
+const int LEN = 3e3 + 5;
+int N, M, T, Q;
+bool V[LEN];
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+
+///========================================================///
+//half plane intersection - refer to bulijiojiodibuliduo
+//O(N^2logN + 6QN) power-diagram query
+///========================================================///
+
+struct Pos {
+	ld x, y;
+	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ld& scalar) const { return { x * scalar, y * scalar }; }
+	Pos operator / (const ld& scalar) const { return { x / scalar, y / scalar }; }
+	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ld xy() const { return x * y; }
+	Pos rot(ld the) { return { x * cos(the) - y * sin(the), x * sin(the) + y * cos(the) }; }
+	ld Euc() const { return x * x + y * y; }
+	ld mag() const { return sqrt(Euc()); }
+	Pos unit() const { return *this / mag(); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = { 0, 0 };
+typedef std::vector<Pos> Polygon;
+struct Linear {//ps[0] -> ps[1] :: refer to bulijiojiodibuliduo
+	Pos ps[2];
+	Pos dir_;
+	Pos& operator[](int i) { return ps[i]; }
+	Pos dir() const { return dir_; }
+	Linear(Pos a = Pos(0, 0), Pos b = Pos(0, 0)) {
+		ps[0] = a;
+		ps[1] = b;
+		dir_ = (ps[1] - ps[0]).unit();
+	}
+	bool include(const Pos& p) const { return sign(dir_ / (p - ps[0])) > 0; }
+	Linear push() const {//push eps outward
+		const double eps = 1e-8;
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] + delta, ps[1] + delta);
+	}
+	Linear operator + (const double eps) const {//push eps outward
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] + delta, ps[1] + delta);
+	}
+	Linear operator - (const double eps) const {//pull eps inward
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] - delta, ps[1] - delta);
+	}
+	friend bool parallel(const Linear& l0, const Linear& l1) { return zero(l0.dir() / l1.dir()); }
+	friend bool same_dir(const Linear& l0, const Linear& l1) { return parallel(l0, l1) && l0.dir() * l1.dir() > 0; }
+	bool operator < (const Linear& l0) const {
+		if (same_dir(*this, l0)) return l0.include(ps[0]);
+		else return cmpq(this->dir(), l0.dir());
+	}
+};
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+	ld ret = cross(d1, d2, d3);
+	return sign(ret);
+}
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+ld dist(const Pos& d1, const Pos& d2, const Pos& t) {
+	return cross(d1, d2, t) / (d1 - d2).mag();
+}
+Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
+	ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
+	return (p1 * a2 + p2 * a1) / (a1 + a2);
+}
+Pos intersection(Linear& l1, Linear& l2) { return intersection(l1[0], l1[1], l2[0], l2[1]); }
+Polygon convex_cut(const std::vector<Pos>& ps, const Pos& b1, const Pos& b2) {
+	std::vector<Pos> qs;
+	int n = ps.size();
+	for (int i = 0; i < n; i++) {
+		Pos p1 = ps[i], p2 = ps[(i + 1) % n];
+		int d1 = ccw(b1, b2, p1), d2 = ccw(b1, b2, p2);
+		if (d1 >= 0) qs.push_back(p1);
+		if (d1 * d2 < 0) qs.push_back(intersection(p1, p2, b1, b2));
+	}
+	return qs;
+}
+Polygon sutherland_hodgman(const std::vector<Pos>& C, const std::vector<Pos>& clip) {
+	int sz = clip.size();
+	std::vector<Pos> ret = C;
+	for (int i = 0; i < sz; i++) {
+		Pos b1 = clip[i], b2 = clip[(i + 1) % sz];
+		ret = convex_cut(ret, b1, b2);
+	}
+	return ret;
+}
+std::vector<Pos> circle_line_intersection(const Pos& o, const ld& r, const Pos& p1, const Pos& p2) {
+	ld d = dist(p1, p2, o);
+	if (std::abs(d) > r) return {};
+	Pos vec = p2 - p1;
+	Pos m = intersection(p1, p2, o, o + ~vec);
+	ld distance = vec.mag();
+	ld ratio = sqrt(r * r - d * d);
+	Pos m1 = m - vec * ratio / distance;
+	Pos m2 = m + vec * ratio / distance;
+	if (dot(p1, p2, m1, m2) < 0) std::swap(m1, m2);
+	return { m1, m2 };//p1->p2
+}
+std::vector<Pos> half_plane_intersection(std::vector<Linear>& HP) {//refer to bulijiojiodibuliduo
+	auto check = [&](Linear& u, Linear& v, Linear& w) -> bool {
+		return w.include(intersection(u, v));
+		};
+	std::sort(HP.begin(), HP.end());
+	std::deque<Linear> dq;
+	int sz = HP.size();
+	for (int i = 0; i < sz; ++i) {
+		if (i && same_dir(HP[i], HP[(i - 1) % sz])) continue;
+		while (dq.size() > 1 && !check(dq[dq.size() - 2], dq[dq.size() - 1], HP[i])) dq.pop_back();
+		while (dq.size() > 1 && !check(dq[1], dq[0], HP[i])) dq.pop_front();
+		dq.push_back(HP[i]);
+	}
+	while (dq.size() > 2 && !check(dq[dq.size() - 2], dq[dq.size() - 1], dq[0])) dq.pop_back();
+	while (dq.size() > 2 && !check(dq[1], dq[0], dq[dq.size() - 1])) dq.pop_front();
+	sz = dq.size();
+	if (sz < 3) return {};
+	std::vector<Pos> HPI;
+	for (int i = 0; i < sz; ++i) HPI.push_back(intersection(dq[i], dq[(i + 1) % sz]));
+	return HPI;
+}
+struct Circle {
+	Pos c;
+	int r;
+	Circle(Pos C = Pos(0, 0), int R = 0) : c(C), r(R) {}
+	bool operator == (const Circle& C) const { return c == C.c && std::abs(r - C.r) < TOL; }
+	bool operator != (const Circle& C) const { return !(*this == C); }
+	bool operator < (const Circle& q) const {
+		ld dist = (c - q.c).mag();
+		return r < q.r && dist + r < q.r + TOL;
+	}
+	bool operator > (const Pos& p) const { return r > (c - p).mag(); }
+	bool operator >= (const Pos& p) const { return r + TOL > (c - p).mag(); }
+	bool operator < (const Pos& p) const { return r < (c - p).mag(); }
+	Circle operator + (const Circle& C) const { return { c + C.c, r + C.r }; }
+	Circle operator - (const Circle& C) const { return { c - C.c, r - C.r }; }
+	ld H(const ld& th) const { return sin(th) * c.x + cos(th) * c.y + r; }//coord trans | check right
+	ld A() const { return 1. * r * r * PI; }
+	friend std::istream& operator >> (std::istream& is, Circle& c) { is >> c.c >> c.r; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Circle& c) { os << c.c << " " << c.r; return os; }
+} INVAL = { { 0, 0 }, -1 };
+bool cmpr(const Circle& p, const Circle& q) { return p.r > q.r; }//sort descending order
+std::vector<Pos> pd[LEN];//power diagram (Laguerre-Voronoi diagram)
+std::vector<Circle> disks;
+ld circle_cut(const Circle& c, const Pos& p1, const Pos& p2) {
+	Pos v1 = p1 - c.c, v2 = p2 - c.c;
+	ld r = c.r;
+	std::vector<Pos> inx = circle_line_intersection(O, r, v1, v2);
+	if (inx.empty()) return r * r * rad(v1, v2) * .5;
+	Pos m1 = inx[0], m2 = inx[1];
+	bool d1 = dot(m1, v1, m2) > -TOL, d2 = dot(m1, v2, m2) > -TOL;
+	if (d1 && d2) return (v1 / v2) * .5;
+	else if (d1) return (v1 / m2 + r * r * rad(m2, v2)) * .5;
+	else if (d2) return (r * r * rad(v1, m1) + m1 / v2) * .5;
+	else if (dot(v1, m1, v2) > 0 && dot(v1, m2, v2) > 0)
+		return (r * r * (rad(v1, m1) + rad(m2, v2)) + m1 / m2) * .5;
+	else return (r * r * rad(v1, v2)) * .5;
+}
+void query() {
+	ll x, y, w, h;
+	std::vector<Pos> box;
+	std::cin >> x >> y >> w >> h;
+	box = { Pos(x, y), Pos(x + w, y), Pos(x + w, y + h), Pos(x, y + h) };
+	ld ret = 0;
+	for (int i = 0; i < N; i++) {
+		if (pd[i].empty()) continue;
+		std::vector<Pos> rem = sutherland_hodgman(pd[i], box);
+		int sz = rem.size();
+		if (sz < 3) continue;
+		for (int j = 0; j < sz; j++)
+			ret += circle_cut(disks[i], rem[j], rem[(j + 1) % sz]);
+	}
+	std::cout << ret * 100 / w / h << "\n";
+	return;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(15);
+	std::cin >> N >> Q;
+	std::vector<Circle> tmp(N);
+	for (Circle& c : tmp) std::cin >> c;
+	memset(V, 0, sizeof V);
+	for (int i = 0; i < N; i++) {//remove
+		if (V[i]) continue;
+		for (int j = 0; j < N; j++) {
+			if (i < j && tmp[i] == tmp[j]) V[i] = 1;
+			if (tmp[i] < tmp[j]) V[i] = 1;
+			if (tmp[j] < tmp[i]) V[j] = 1;
+		}
+	}
+	for (int i = 0; i < N; i++) if (!V[i]) disks.push_back(tmp[i]);
+	N = disks.size();
+	int bnd = 3e6;
+	for (int i = 0; i < N; i++) {//compose power diagram
+		std::vector<Linear> HP;
+		HP.push_back(Linear(Pos(bnd, bnd), Pos(-bnd, bnd)));
+		HP.push_back(Linear(Pos(-bnd, bnd), Pos(-bnd, -bnd)));
+		HP.push_back(Linear(Pos(-bnd, -bnd), Pos(bnd, -bnd)));
+		HP.push_back(Linear(Pos(bnd, -bnd), Pos(bnd, bnd)));
+		for (int j = 0; j < N; j++) {
+			if (i == j) continue;
+			Pos& ca = disks[i].c, cb = disks[j].c;
+			ll ra = disks[i].r, rb = disks[j].r;
+			Pos vec = cb - ca;//vec a -> b
+			ld distance = vec.mag();
+			ld X = (ra * ra - rb * rb + vec.Euc()) / (2 * distance);
+			Pos m = ca + vec * X / distance;
+			HP.push_back(Linear(m, m + ~vec));
+		}
+		pd[i] = half_plane_intersection(HP);
+	}
+	while (Q--) query();
+	return;
+}
+int main() { solve(); return 0; }
