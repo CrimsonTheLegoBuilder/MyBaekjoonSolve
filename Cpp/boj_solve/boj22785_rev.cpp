c096379fea2eeba033e6480522e53ca77763fa70
crimson <redcube231@gmail.com>
2024-07-22 09:32:12 +0900

    2407220932rev

--- Cpp/boj_solve/boj22785_rev.cpp
+++ Cpp/boj_solve/boj22785_rev.cpp
@@ -104,6 +104,75 @@ struct Line {//ax + by = c
 	friend std::ostream& operator << (std::ostream& os, const Line& l) { os << l.s.vy << " " << l.s.vx << " " << l.c; return os; }
 };
 typedef std::vector<Line> Lines;
+struct Linear {//ps[0] -> ps[1] :: refer to bulijiojiodibuliduo
+	Pos ps[2];
+	Pos dir_;
+	Pos& operator[](int i) { return ps[i]; }
+	Pos dir() const { return dir_; }
+	Linear(Pos a = Pos(0, 0), Pos b = Pos(0, 0)) {
+		ps[0] = a;
+		ps[1] = b;
+		dir_ = (ps[1] - ps[0]).unit();
+	}
+	bool include(const Pos& p) const { return sign(dir_ / (p - ps[0])) > 0; }
+	Linear push() const {//push eps outward
+		const double eps = 1e-8;
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] + delta, ps[1] + delta);
+	}
+	Linear operator + (const double eps) const {//push eps outward
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] + delta, ps[1] + delta);
+	}
+	Linear operator - (const double eps) const {//pull eps inward
+		Pos delta = ~(ps[1] - ps[0]).unit() * eps;
+		return Linear(ps[0] - delta, ps[1] - delta);
+	}
+	friend bool parallel(const Linear& l0, const Linear& l1) { return zero(l0.dir() / l1.dir()); }
+	friend bool same_dir(const Linear& l0, const Linear& l1) { return parallel(l0, l1) && l0.dir() * l1.dir() > 0; }
+	bool operator < (const Linear& l0) const {
+		if (same_dir(*this, l0)) return l0.include(ps[0]);
+		else return cmpq(this->dir(), l0.dir());
+	}
+};
+typedef std::vector<Linear> Linears;
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
+	ld ret = cross(d1, d2, d3);
+	return sign(ret);
+}
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+ld dist(const Pos& d1, const Pos& d2, const Pos& t) {
+	return cross(d1, d2, t) / (d1 - d2).mag();
+}
+Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
+	ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
+	return (p1 * a2 + p2 * a1) / (a1 + a2);
+}
+Pos intersection(Linear& l1, Linear& l2) { return intersection(l1[0], l1[1], l2[0], l2[1]); }
+
+std::vector<Pos> half_plane_intersection(std::vector<Linear>& HP) {//refer to bulijiojiodibuliduo
+	auto check = [&](Linear& u, Linear& v, Linear& w) -> bool {
+		return w.include(intersection(u, v));
+		};
+	std::sort(HP.begin(), HP.end());
+	std::deque<Linear> dq;
+	int sz = HP.size();
+	for (int i = 0; i < sz; ++i) {
+		if (i && same_dir(HP[i], HP[(i - 1) % sz])) continue;
+		while (dq.size() > 1 && !check(dq[dq.size() - 2], dq[dq.size() - 1], HP[i])) dq.pop_back();
+		while (dq.size() > 1 && !check(dq[1], dq[0], HP[i])) dq.pop_front();
+		dq.push_back(HP[i]);
+	}
+	while (dq.size() > 2 && !check(dq[dq.size() - 2], dq[dq.size() - 1], dq[0])) dq.pop_back();
+	while (dq.size() > 2 && !check(dq[1], dq[0], dq[dq.size() - 1])) dq.pop_front();
+	sz = dq.size();
+	if (sz < 3) return {};
+	std::vector<Pos> HPI;
+	for (int i = 0; i < sz; ++i) HPI.push_back(intersection(dq[i], dq[(i + 1) % sz]));
+	return HPI;
+}
 Line L(const Pos& s, const Pos& e) {
 	ld dy, dx, c;
 	dy = e.y - s.y;
@@ -119,9 +188,6 @@ Pos intersection(const Line& l1, const Line& l2) {
 		(l2.c * v1.vy - l1.c * v2.vy) / det,
 	};
 }
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ld ret = cross(d1, d2, d3); return sign(ret); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 ld area(const Polygon& H) {
 	ld a = 0;
 	int sz = H.size();
@@ -168,6 +234,15 @@ bool polygon_intersection(const Polygon& H1, const Polygon& H2, Polygon& hull) {
 	for (int i = 0; i < sz; i++) hp.push_back(L(H2[i], H2[(i + 1) % sz]));
 	return half_plane_intersection(hp, hull);
 }
+Polygon polygon_intersection(const Polygon& H1, const Polygon& H2) {
+	int sz;
+	Linears hp;
+	sz = H1.size();
+	for (int i = 0; i < sz; i++) hp.push_back(Linear(H1[i], H1[(i + 1) % sz]));
+	sz = H2.size();
+	for (int i = 0; i < sz; i++) hp.push_back(Linear(H2[i], H2[(i + 1) % sz]));
+	return half_plane_intersection(hp);
+}
 Pos centroid(const Polygon& H) {
 	Pos cen = Pos(0, 0);
 	ld A = 0;
@@ -187,8 +262,8 @@ void query() {
 	std::cin >> N;
 	Polygon H0(N), H1, H2;
 	for (Pos& p : H0) std::cin >> p;
-	std::cout << N << "\n";
-	for (Pos& p : H0) std::cout << "(" << p.x << ", " << p.y << "),\n";
+	//std::cout << N << "\n";
+	//for (Pos& p : H0) std::cout << "(" << p.x << ", " << p.y << "),\n";
 	norm(H0);
 	ld a, b, c;
 	std::cin >> a >> b >> c;
@@ -207,8 +282,9 @@ void query() {
 	if (f1 && f2) {
 		//std::cout << "zzz\n";
 		Polygon H3, tmp;
-		//for (Pos& p : H0) std::cout << p << " ";
-		//std::cout << "\n";
+		//std::cout << "A = [\n";
+		//for (Pos& p : H1) std::cout << "(" << p.x << ", " << p.y << "),\n";
+		//std::cout << "]\n";
 		Pos vec = Pos(l.s.vy, l.s.vx).unit();
 		int sz = H1.size();
 		for (int i = 0; i < sz; i++) {
@@ -217,14 +293,21 @@ void query() {
 			tmp.push_back(mr);
 		}
 		norm(tmp);
- 		bool f3 = polygon_intersection(tmp, H2, H3);
-		if (!f3) { std::cout << "err\n"; return; }
+ 	//	bool f3 = polygon_intersection(tmp, H2, H3);
+		//if (!f3) { std::cout << "err\n"; return; }
+		H3 = polygon_intersection(tmp, H2);
 		//std::cout << tmp.size() << " " << H2.size() << " " << H3.size() << "\n";
 		//for (Pos& p : tmp) std::cout << p << " ";
 		//std::cout << "\n";
-		//for (Pos& p : H2) std::cout << p << " ";
+		//std::cout << "B = [\n";
+		//for (Pos& p : H2) std::cout << "(" << p.x << ", " << p.y << "),\n";
+		//std::cout << "]\n";
 		//std::cout << "\n";
+		//for (Pos& p : H2) std::cout << p << " ";
 		//for (Pos& p : H3) std::cout << p << " ";
+		//std::cout << "C = [\n";
+		//for (Pos& p : H3) std::cout << "(" << p.x << ", " << p.y << "),\n";
+		//std::cout << "]\n";
 		//std::cout << "\n";
 		Pos cen;
 		cen = centroid(H1);
@@ -249,7 +332,7 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(10);
+	std::cout.precision(1);
 	//freopen("pol.in", "r", stdin);
 	//freopen("pol_my.out", "w", stdout);
 	std::cin >> T;
