75cbb75cd7e50b657e7b9a7c2627e5b7217c5697
crimson <redcube231@gmail.com>
2024-07-24 14:39:49 +0900

    2407241439

---
+++ Cpp/boj_solve/boj15767_circle.cpp
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <unordered_map>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+typedef std::pair<int, int> pi;
+typedef std::vector<int> Vint;
+typedef std::vector<ld> Vld;
+const ld INF = 1e17;
+const ld TOL = 1e-10;
+const ld PI = acos(-1);
+const int LEN = 3e3 + 5;
+int N, M, T, Q;
+bool V[LEN];
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+ll sq(const ll& x) { return x * x; }
+
+struct Pos {
+	ll x, y;
+	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	ll Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return y > 0 || y == 0 && x >= 0; }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+struct Circle {
+	Pos c;
+	ll r;
+	Circle(Pos C = Pos(0, 0), ll R = 0) : c(C), r(R) {}
+	bool operator == (const Circle& C) const { return c == C.c && r == C.r; }
+	bool operator != (const Circle& C) const { return !(*this == C); }
+	bool operator < (const Circle& q) const {
+		ll dist = (c - q.c).Euc();
+		return r < q.r && dist > sq(q.r - r);
+	}
+	bool operator > (const Pos& p) const { return r > (c - p).mag(); }
+	bool operator >= (const Pos& p) const { return r + TOL > (c - p).mag(); }
+	bool operator < (const Pos& p) const { return r < (c - p).mag(); }
+	Circle operator + (const Circle& C) const { return { c + C.c, r + C.r }; }
+	Circle operator - (const Circle& C) const { return { c - C.c, r - C.r }; }
+	ld H(const ld& th) const { return sin(th) * c.x + cos(th) * c.y + r; }//coord trans | check right
+	ld A() const { return 1. * r * r * PI; }
+	friend std::istream& operator >> (std::istream& is, Circle& c) { is >> c.c >> c.r; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Circle& c) { os << c.c << " " << c.r; return os; }
+} INVAL = { { 0, 0 }, -1 };
+bool cmpr(const Circle& p, const Circle& q) { return p.r > q.r; }//sort descending order
+typedef std::vector<Circle> Disks;
+std::unordered_map<ll, Vint> idxs;
+
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(15);
+	std::cin >> N;
+	Polygon R(N);
+	Disks C(N);
+	int i = 0;
+	for (Circle& c : C) {
+		std::cin >> c;
+		c.c += Pos(1ll << 30, 1ll << 30);
+		R[i] = Pos(-c.r, i);
+		i++;
+	}
+	std::sort(C.begin(), C.end());
+
+	return;
+}
+int main() { solve(); return 0; }//NAC 2021 B Apple Orchard
