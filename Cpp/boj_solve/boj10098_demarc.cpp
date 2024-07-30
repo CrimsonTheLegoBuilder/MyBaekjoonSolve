5ee339866eab803a8199932f4b3b4d9bee74d4ed
crimson <redcube231@gmail.com>
2024-07-31 08:32:58 +0900

    2407310832demarc2

--- Cpp/boj_solve/boj10098_demarc.cpp
+++ Cpp/boj_solve/boj10098_demarc.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <vector>
 #include <cassert>
+#include <cmath>
+#include <cstring>
 
 #define JAY_MODULE_DEBUG
 
@@ -11,11 +13,9 @@ typedef double ld;
 const int LEN = 1e5 + 1;
 
 int N;
+ll memo[LEN];
 std::vector<int> g[LEN]; // above lines
 struct Pos {
-    /// <summary>
-    /// 
-    /// </summary>
     int x, y;
     Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
     bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
@@ -97,16 +97,42 @@ void norm(Pos H[], const int& sz) {
     if (A < 0) std::reverse(H, H + sz);
     return;
 }
-
-void match(int u, int v, int l, int r) {
-#ifdef JAY_MODULE_DEBUG
-    std::cout << "facing pair: " << u << ' ' << v << '\n';
-    std::cout << "    facing range: " << l << ' ' << r << '\n';
-#endif
-    g[u].push_back(v);
+void norm(Polygon& H) {
+    ll A = area(H);
+    if (A < 0) std::reverse(H.begin(), H.end());
+    auto s = std::min_element(H.begin(), H.end());
+    std::rotate(H.begin(), s, H.end());
 }
-
-ll dist(const Pos& p1, const Pos& p2) { return (ll)(p1.x - p2.x) * (p1.x - p2.x) + (ll)(p1.y - p2.y) * (p1.y - p2.y); }
+void move(const Pos& s, Pos H[]) { for (int i = 0; i < N; i++) H[i] += s; }
+void move(const Pos& s, Polygon& H) {
+    int sz = H.size();
+    for (int i = 0; i < N; i++) H[i] += s;
+}
+void rotate() { for (int i = 0; i < N; i++) pos[i] = ~pos[i]; }
+void rotate(Polygon& H) {
+    int sz = H.size();
+    for (int i = 0; i < sz; i++) H[i] = ~H[i];
+}
+bool polygon_cmp(const Polygon& A, const Polygon& B) {
+    int a = A.size(), b = B.size();
+    if (a != b) return 0;
+    for (int i = 0; i < a; i++) {
+        if (A[i] != B[i]) return 0;
+    }
+    return 1;
+}
+bool operator == (const Polygon& p, const Polygon& q) { return polygon_cmp(p, q); }
+Polygon make_polygon(int u, int v, Pos s, Pos e, bool rvs = 0) {
+    if (rvs) std::swap(s, e);
+    Polygon H;
+    for (int i = u; i != (v + 1) % N; i++) {
+        Pos& cur = pos[i], nxt = pos[(i + 1) % N];
+        if (on_seg_weak(cur, nxt, s)) H.push_back(s);
+        H.push_back(cur);
+        if (on_seg_weak(cur, nxt, e)) H.push_back(s);
+    }
+    return H;
+}
 
 struct Line {
     int y;      // y coord
@@ -127,9 +153,7 @@ struct Line {
         return y == o.y ? l < o.l : y > o.y;
     }
 };
-
 int Q;
-
 /// <summary>
 /// event horizon
 /// </summary>
@@ -139,7 +163,6 @@ struct Event {
     Line l; // line
     bool operator<(const Event& r) const { return l < r.l; }
 } events[LEN];
-
 /// <summary>
 /// segment struct defined to use in SplayTree node.
 /// simplified version of line event (ommited y coord)
@@ -147,7 +170,7 @@ struct Event {
 struct Seg {
     int l, r; // x coords
     int i;
-
+    Seg(int l = 0, int r = 0, int i = 0) : l(l), r(r), i(i) {}
     /// <summary>
     /// only compare x coords
     /// </summary>
@@ -157,7 +180,40 @@ struct Seg {
         return l < o.l;
     }
 };
-
+std::vector<Seg> Segs;
+void match(int u, int v, int l, int r) {
+#ifdef JAY_MODULE_DEBUG
+    std::cout << "facing pair: " << u << ' ' << v << '\n';
+    std::cout << "    facing range: " << l << ' ' << r << '\n';
+#endif
+    g[u].push_back(v);
+    Segs.push_back(Seg(l, r, v));
+}
+bool find_split(const int i, const Seg& S, Pos& s, Pos& e) {
+    int j = S.i, k = i;
+    bool f = 0;
+    if (k < j) f = 1, std::swap(j, k);
+    ll a1 = memo[k] - memo[j] + pos[k] / pos[j];
+    if (f) a1 = memo[N] - a1;
+    Pos& p1 = pos[i], p2 = pos[(i + 1) % N];
+    assert(p1.x < p2.x);
+    int r = std::min(p2.x, S.r);
+    int l = std::max(p1.x, S.l);
+    int h = std::abs(pos[S.i].y - p1.y);
+    int w1 = p2.x - l, w2 = pos[i].x - l;
+    int w3 = p2.x - r, w4 = pos[i].x - r;
+    ll amax = a1 + 1ll * h * (w1 + w2);
+    ll amin = a1 + 1ll * h * (w3 + w4);
+    if (amin <= (memo[N] >> 1) && (memo[N] >> 1) <= amax) {
+        ll B = (memo[N] >> 1) - amin;
+        ll w = B / h;
+        if (w & 1) return 0;
+        s = Pos(r - (w >> 1), p2.y);
+        e = Pos(r - (w >> 1), pos[S.i].y);
+        return 1;
+    }
+    return 0;
+}
 class SplayTree {
     struct Node {
         Node* l;
@@ -322,8 +378,77 @@ public:
         }
     }
 } sp;
+bool vertical_split(Pos& s, Pos& e) {
+    memo[0] = 0;
+    for (int i = 0; i < N; i++) {
+        memo[i + 1] = memo[i] + pos[i] / pos[(i + 1) % N];
+    }
+    // horizontal check
+    Q = 0;
+    for (int i = 0; i < N; ++i) {
+        int j = (i + 1) % N;
+        if (pos[i].y == pos[j].y) {
+            if (pos[i].x < pos[j].x) { // right halfline
+                events[Q++] = { i, 1, { pos[i].y, pos[i].x, pos[j].x } };
+            }
+            if (pos[i].x > pos[j].x) { // left halfline
+                events[Q++] = { i, 0, { pos[i].y, pos[j].x, pos[i].x } };
+            }
+        }
+    }
 
-int main() {
+    std::sort(events, events + Q);
+
+    Pos s, e;
+    for (int i = 0; i < Q; ++i) {
+        std::cout << (events[i].d == 0 ? "insert" : "query") << " segment: (h, l, r) ";
+        std::cout << events[i].l.y << ' ' << events[i].l.l << ' ' << events[i].l.r << '\n';
+        if (events[i].d == 0) sp.insert(events[i].l.l, events[i].l.r, events[i].i);
+        if (events[i].d == 1) {
+            Segs.clear();
+            sp.query(events[i].l.l, events[i].l.r, events[i].i);
+            for (Seg& S : Segs) {
+                if (find_split(events[i].i, S, s, e)) {
+
+                }
+            }
+        }
+    }
+    return 0;
+}
+void solve() {
+    freopen("demarcation.in.1", "r", stdin);
+    freopen("demarcation.out", "w", stdout);
+    std::cin.tie(0)->sync_with_stdio(0);
+    //std::cout.tie(0);
+    std::cin >> N;
+    for (int i = 0; i < N; ++i) std::cin >> pos[i].x >> pos[i].y;
+    ll A = area(pos, N);
+    assert(A);
+    if (A < 0) std::reverse(pos, pos + N);
+    A >>= 1;
+    if (A & 1) { std::cout << "NO\n"; return; }
+
+    Pos s, e;
+    if (vertical_split(s, e)) {
+        std::cout << "YES\n";
+        std::cout << s << e << "\n";
+        return;
+    }
+    rotate();
+    if (vertical_split(s, e)) {
+        std::cout << "YES\n";
+        std::cout << s << e << "\n";
+        return;
+    }
+    std::cout << "NO\n";
+    return;
+}
+int main() { solve(); return 0; }
+
+/*
+
+void solve() {
     freopen("demarcation.in.1", "r", stdin);
     freopen("demarcation.out", "w", stdout);
     std::cin.tie(0)->sync_with_stdio(0);
@@ -331,12 +456,6 @@ int main() {
     std::cin >> N;
     for (int i = 0; i < N; ++i) std::cin >> pos[i].x >> pos[i].y;
     norm(pos, N);
-    
-    std::cout << "points = [\n";
-    for (int i = 0; i < N; i++) {
-        std::cout << "  (" << pos[i].x << ", " << pos[i].y << "), \n";
-    }
-    std::cout << "]\n";
 
     // horizontal check
     Q = 0;
@@ -359,7 +478,12 @@ int main() {
         if (events[i].d == 0) sp.insert(events[i].l.l, events[i].l.r, events[i].i);
         if (events[i].d == 1) sp.query(events[i].l.l, events[i].l.r, events[i].i);
     }
-
+#ifdef JAY_MODULE_DEBUG
+    std::cout << "points = [\n";
+    for (int i = 0; i < N; i++) {
+        std::cout << "  (" << pos[i].x << ", " << pos[i].y << "), \n";
+    }
+    std::cout << "]\n";
     for (int i = 0; i <= N; ++i) {
         if (g[i].size()) {
             std::cout << "right half-line from " << i << '\n';
@@ -368,9 +492,6 @@ int main() {
             std::cout << '\n';
         }
     }
+#endif
 }
-
-/*
-
-
 */
