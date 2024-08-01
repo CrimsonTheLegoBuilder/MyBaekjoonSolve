24cc0cc4bea6b07e08fa70e04d8ddc08893d946b
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-08-01 21:03:26 +0900

    2408012103demarc777

--- Cpp/boj_solve/boj10098_demarc.cpp
+++ Cpp/boj_solve/boj10098_demarc.cpp
@@ -18,9 +18,7 @@
 
 typedef long long ll;
 typedef double ld;
-typedef std::pair<int, int> pi;
 typedef std::vector<int> Vint;
-typedef std::vector<ld> Vld;
 const int LEN = 1e5 + 1;
 
 int N;
@@ -32,89 +30,44 @@ struct Pos {
     bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
     bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
     bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-    bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
     Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
     Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
     Pos operator * (const int& n) const { return { x * n, y * n }; }
     Pos operator / (const int& n) const { return { x / n, y / n }; }
     ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
     ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
-    Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
     Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-    Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
     Pos& operator *= (const int& scale) { x *= scale; y *= scale; return *this; }
-    Pos& operator /= (const int& scale) { x /= scale; y /= scale; return *this; }
     Pos operator - () const { return { -x, -y }; }
     Pos operator ~ () const { return { -y, x }; }
-    Pos operator ! () const { return { y, x }; }
-    ll xy() const { return (ll)x * y; }
-    ll Euc() const { return (ll)x * x + (ll)y * y; }
-    int Man() const { return std::abs(x) + std::abs(y); }
-    ld mag() const { return hypot(x, y); }
-    ld rad() const { return atan2(y, x); }
-    friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
-    int quad() const { return y > 0 || y == 0 && x >= 0; }
-    friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
     friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
     friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 } pos[LEN];
 const Pos O = Pos(0, 0);
-const Pos INVAL = Pos(-1, -1);
 typedef std::vector<Pos> Polygon;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ll ret = cross(d1, d2, d3); return ret < 0 ? -1 : !!ret; }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { ll ret = cross(d1, d2, d3, d4); return ret < 0 ? -1 : !!ret; }
-bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0; }
-bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0; }
-bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return !ccw(d1, d2, d3) && !ccw(d1, d2, d4); }
-bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2, const bool f = 1) {
-    //f : include end of seg, !f : ignore end of seg
-    bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
-    bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
-    if (!f) return f1 && f2;
-    bool f3 = on_seg_strong(s1, s2, d1) ||
-        on_seg_strong(s1, s2, d2) ||
-        on_seg_strong(d1, d2, s1) ||
-        on_seg_strong(d1, d2, s2);
-    return (f1 && f2) || f3;
-}
-bool inner_check(Pos p0, Pos p1, Pos p2, const Pos& t) {
-    if (ccw(p0, p1, p2) < 0) std::swap(p1, p2);
-    return ccw(p0, p1, t) >= 0 && ccw(p1, p2, t) >= 0 && ccw(p2, p0, t) >= 0;
-}
 ll area(Pos H[], const int& sz) {
     ll ret = 0;
-    for (int i = 0; i < sz; i++) {
-        Pos cur = H[i], nxt = H[(i + 1) % sz];
-        ret += cross(O, cur, nxt);
-    }
+    for (int i = 0; i < sz; i++) ret += H[i] / H[(i + 1) % sz];
     return ret;
 }
 ll area(std::vector<Pos>& H) {
     ll ret = 0;
     int sz = H.size();
-    for (int i = 0; i < sz; i++) {
-        Pos cur = H[i], nxt = H[(i + 1) % sz];
-        ret += cross(O, cur, nxt);
-    }
+    for (int i = 0; i < sz; i++) ret += H[i] / H[(i + 1) % sz];
     return ret;
 }
 void norm(Pos H[], const int& sz) {
-    ll A = area(H, sz);
-    assert(A);
+    ll A = area(H, sz); assert(A);
     if (A < 0) std::reverse(H, H + sz);
-    return;
 }
 void norm(Polygon& H) {
-    ll A = area(H);
+    ll A = area(H); assert(A);
     if (A < 0) std::reverse(H.begin(), H.end());
     auto s = std::min_element(H.begin(), H.end());
     std::rotate(H.begin(), s, H.end());
 }
-void move(Pos H[], const Pos& vec) { for (int i = 0; i < N; i++) H[i] += vec; }
 void move(Polygon& H, const Pos& vec) {
     int sz = H.size();
     for (int i = 0; i < sz; i++) H[i] += vec;
@@ -124,25 +77,22 @@ void rotate(Polygon& H) {
     int sz = H.size();
     for (int i = 0; i < sz; i++) H[i] = ~H[i];
 }
-void mirror() { for (int i = 0; i < N; i++) pos[i].x = -pos[i].x; }
 void mirror(Polygon& H) {
     int sz = H.size();
     for (int i = 0; i < sz; i++) H[i].x = -H[i].x;
 }
-bool polygon_cmp(const Polygon& A, const Polygon& B) {
+inline bool polygon_cmp(const Polygon& A, const Polygon& B) {
     int a = A.size(), b = B.size();
     if (a != b) return 0;
-    for (int i = 0; i < a; i++) {
-        if (A[i] != B[i]) return 0;
-    }
+    for (int i = 0; i < a; i++) if (A[i] != B[i]) return 0;
     return 1;
 }
 bool operator == (const Polygon& p, const Polygon& q) { return polygon_cmp(p, q); }
-Polygon make_polygon(int u, int v, Pos s, Pos e) {
+Polygon cut_polygon(int u, int v, Pos s, Pos e) {
     Polygon H, tmp;
     Vint V;
-    //std::cout << "fuck:: make poly " << u << " " << v << "\n";
-    //std::cout << "fuck:: " << s << " " << e << "\n";
+    //std::cout << "make poly:: " << u << " " << v << "\n";
+    //std::cout << "s, e:: " << s << " " << e << "\n";
     H.push_back(e);
     H.push_back(s);
     for (int i = (u + 1) % N; i != (v + 1) % N; i = (i + 1) % N) {
@@ -152,18 +102,17 @@ Polygon make_polygon(int u, int v, Pos s, Pos e) {
     int sz = H.size();
     V.resize(sz, 0);
     for (int i = 0; i < sz; i++) {
-        const Pos& pre = H[(i - 1 + sz) % sz], cur = H[i], nxt = H[(i + 1) % sz];
+        const Pos& pre = H[(i - 1 + sz) % sz], & cur = H[i], & nxt = H[(i + 1) % sz];
         if (!ccw(pre, cur, nxt)) V[i] = 1;
     }
     for (int i = 0; i < sz; i++) if (!V[i]) tmp.push_back(H[i]);
     return tmp;
 }
 bool all_polygon_cmp(int u, int v, Pos s, Pos e) {
-    Polygon A = make_polygon(u, v, s, e);
-    Polygon B = make_polygon(v, u, e, s);
+    Polygon A = cut_polygon(u, v, s, e);
+    Polygon B = cut_polygon(v, u, e, s);
 #ifdef CRIMSON_MODULE_DEBUG
-    std::cout << "FUCK:: s:: " << s << " e:: " << e << "\n";
-    std::cout << "FUCK::\n";
+    std::cout << "FUCK:: s:: " << s << " e:: " << e << " x::\n";
     std::cout << "A = [\n";
     for (Pos& p : A) {
         std::cout << "(" << p.x << ", " << p.y << "), \n";
@@ -176,13 +125,13 @@ bool all_polygon_cmp(int u, int v, Pos s, Pos e) {
     std::cout << "]\n";
 #endif
     if (A.size() != B.size()) return 0;
-    norm(A), norm(B);
-    Pos b = *std::min_element(B.begin(), B.end());
+    norm(B);
+    const Pos& b = B[0];
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 4; j++) {
             rotate(A);
             norm(A);
-            Pos a = *std::min_element(A.begin(), A.end());
+            const Pos& a = A[0];
             move(A, b - a);
             if (A == B) return 1;
         }
@@ -236,18 +185,26 @@ struct Seg {
         return l < o.l;
     }
 };
-std::vector<Seg> Segs;
+std::vector<Seg> segs;
 void match(int u, int v, int l, int r) {
 #ifdef JAY_MODULE_DEBUG
     std::cout << "facing pair: " << u << ' ' << v << '\n';
     std::cout << "    facing range: " << l << ' ' << r << '\n';
     g[u].push_back(v);
 #endif
-    Segs.push_back(Seg(l, r, v));
+    segs.push_back(Seg(l, r, v));
 }
+/// <summary>
+/// find a segment that divides the area of hole polygon in half.
+/// </summary>
+/// <param name="i">query's idx</param>
+/// <param name="S">target segment</param>
+/// <param name="s">split seg's end point 1</param>
+/// <param name="e">split seg's end point 2</param>
+/// <returns>boolean, if true, compose split segment { s - e }.</returns>
 bool find_split(const int i, const Seg& S, Pos& s, Pos& e) {
     int j = i, k = S.i;
-    Pos& J = pos[j], & K = pos[k];
+    const Pos& J = pos[j], & K = pos[k];
     bool f = 0;
     if (k < j) f = 1, std::swap(j, k);
     ll a1 = memo[k] - memo[j];
@@ -471,9 +428,9 @@ bool vertical_split(Pos& s, Pos& e) {
         //std::cout << events[i].l.y << ' ' << events[i].l.l << ' ' << events[i].l.r << '\n';
         if (events[i].d == 0) sp.insert(events[i].l.l, events[i].l.r, events[i].i);
         if (events[i].d == 1) {
-            Segs.clear();
+            segs.clear();
             sp.query(events[i].l.l, events[i].l.r, events[i].i);
-            for (Seg& S : Segs) {
+            for (Seg& S : segs) {
                 //std::cout << "segs::\n";
                 if (find_split(events[i].i, S, s, e)) {
                     //std::cout << "polygon cmp before::\n";
@@ -483,6 +440,17 @@ bool vertical_split(Pos& s, Pos& e) {
             }
         }
     }
+
+#ifdef JAY_MODULE_DEBUG
+    for (int i = 0; i <= N; ++i) {
+        if (g[i].size()) {
+            std::cout << "right half-line from " << i << '\n';
+            std::cout << "    matching left half-lines from: ";
+            for (const int& j : g[i]) std::cout << j << ' ';
+            std::cout << '\n';
+        }
+    }
+#endif
     return 0;
 }
 void solve() {
@@ -498,7 +466,6 @@ void solve() {
     A *= -1;
     A >>= 1;
 #ifdef CRIMSON_MODULE_DEBUG
-    std::cout << "FUCK::\n";
     std::cout << "pos = [\n";
     for (int i = 0; i < N; i++) {
         std::cout << "(" << pos[i].x << ", " << pos[i].y << "), \n";
@@ -509,7 +476,6 @@ void solve() {
 
     Pos s, e;
     if (vertical_split(s, e)) {
-        //std::cout << "YES\n";
         for (int i = 0; i < N; i++) {
             if (pos[i].x == e.x && s.y < pos[i].y && e.y > pos[i].y)
                 e = pos[i];
@@ -518,9 +484,7 @@ void solve() {
         return;
     }
     rotate();
-    //std::cout << "FUCK::\n";
     if (vertical_split(s, e)) {
-        //std::cout << "YES\n";
         for (int i = 0; i < N; i++) {
             if (pos[i].x == e.x && s.y < pos[i].y && e.y > pos[i].y)
                 e = pos[i];
@@ -528,70 +492,7 @@ void solve() {
         std::cout << -~s << " " << -~e << "\n";
         return;
     }
-    //std::cout << "FUCK::\n";
     std::cout << "NO\n";
     return;
 }
 int main() { solve(); return 0; }//boj10098 Demarcation
-
-/*
-
-8
-0 0
-6 0
-6 2
-5 2
-5 1
-1 1
-1 4
-0 4
-
-
-
-void solve() {
-    freopen("demarcation.in.1", "r", stdin);
-    freopen("demarcation.out", "w", stdout);
-    std::cin.tie(0)->sync_with_stdio(0);
-    //std::cout.tie(0);
-    std::cin >> N;
-    for (int i = 0; i < N; ++i) std::cin >> pos[i].x >> pos[i].y;
-    norm(pos, N);
-
-    // horizontal check
-    Q = 0;
-    for (int i = 0; i < N; ++i) {
-        int j = (i + 1) % N;
-        if (pos[i].y == pos[j].y) {
-            if (pos[i].x < pos[j].x) { // right halfline
-                events[Q++] = { i, 1, { pos[i].y, pos[i].x, pos[j].x } };
-            }
-            if (pos[i].x > pos[j].x) { // left halfline
-                events[Q++] = { i, 0, { pos[i].y, pos[j].x, pos[i].x } };
-            }
-        }
-    }
-
-    std::sort(events, events + Q);
-    for (int i = 0; i < Q; ++i) {
-        std::cout << (events[i].d == 0 ? "insert" : "query") << " segment: (h, l, r) ";
-        std::cout << events[i].l.y << ' ' << events[i].l.l << ' ' << events[i].l.r << '\n';
-        if (events[i].d == 0) sp.insert(events[i].l.l, events[i].l.r, events[i].i);
-        if (events[i].d == 1) sp.query(events[i].l.l, events[i].l.r, events[i].i);
-    }
-#ifdef JAY_MODULE_DEBUG
-    std::cout << "points = [\n";
-    for (int i = 0; i < N; i++) {
-        std::cout << "  (" << pos[i].x << ", " << pos[i].y << "), \n";
-    }
-    std::cout << "]\n";
-    for (int i = 0; i <= N; ++i) {
-        if (g[i].size()) {
-            std::cout << "right half-line from " << i << '\n';
-            std::cout << "    matching left half-lines from: ";
-            for (const int& j : g[i]) std::cout << j << ' ';
-            std::cout << '\n';
-        }
-    }
-#endif
-}
-*/
