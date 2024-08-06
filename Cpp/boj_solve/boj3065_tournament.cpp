0828eb138d4b154993bbb485e2d6b131e6d53850
crimson <redcube231@gmail.com>
2024-08-06 12:57:34 +0900

    2408061257tree

---
+++ Cpp/boj_solve/boj3065_tournament.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstring>
+typedef long long ll;
+typedef std::vector<int> vint;
+const int LEN = 1 << 8;
+
+#define DEBUG
+
+int T, N, Q;
+int tree[LEN];
+void query() {
+	memset(tree, 0, sizeof tree);
+	std::cin >> N;
+	int c = 1 << (N - 1);
+	int n = 1;
+	for (int i = c + c; i < c + c + c + c; i++) {
+		tree[i] = n;
+		n++;
+	}
+	for (int i = N - 1; i >= 0; i--) {
+		int w;
+		for (int j = 0; j < c; j++) {
+			std::cin >> w;
+			tree[(1 << i) + j] = w;
+		}
+		c >>= 1;
+	}
+
+#ifdef DEBUG
+	std::cout << "\nDEBUG::\n";
+	for (int i = 1; i <= (1 << (N + 1)); i++) {
+		std::cout << tree[i] << " ";
+	}
+	std::cout << "\nDEBUG::\n";
+#endif
+
+	std::cin >> Q;
+	while (Q--) {
+		int q, a1 = 0, a2 = 0;
+		//
+		std::cin >> q;
+		std::cout << "Player " << q;
+		std::cout << " can be ranked as high as " << a1;
+		std::cout << " or as low as " << a2 << ".\n";
+	}
+	return;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin >> T;
+	while (T--) query();
+	return;
+}
+int main() { solve(); return 0; }//boj3065
