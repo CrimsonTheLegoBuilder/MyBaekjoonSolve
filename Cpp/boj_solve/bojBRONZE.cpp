0132d040fa3d4e0dc786d95c138677aad871de11
crimson <redcube231@gmail.com>
2024-08-20 12:59:28 +0900

    2408201259

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -1,49 +1,14 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <cstring>
-typedef long long ll;
-typedef long double ld;
-const ld TOL = 1e-6;
-int N;
 
+int N, h, w, ans;
 int main() {
 	std::cin >> N;
-	for (int j = 0; j < N; j++) {
-		for (int k = 0; k < N; k++) {
-			std::cout << "G";
-		}
-		for (int k = 0; k < N * 3; k++) {
-			std::cout << ".";
-		}
-		std::cout << "\n";
+	ans = 0;
+	while (N--) {
+		std::cin >> w >> h;
+		ans = std::max(ans, w * h);
 	}
-	for (int j = 0; j < N; j++) {
-		for (int k = 0; k < N; k++) {
-			std::cout << ".";
-		}
-		for (int k = 0; k < N; k++) {
-			std::cout << "I";
-		}
-		for (int k = 0; k < N; k++) {
-			std::cout << ".";
-		}
-		for (int k = 0; k < N; k++) {
-			std::cout << "T";
-		}
-		std::cout << "\n";
-	}
-	for (int j = 0; j < N; j++) {
-		for (int k = 0; k < N * 2; k++) {
-			std::cout << ".";
-		}
-		for (int k = 0; k < N; k++) {
-			std::cout << "S";
-		}
-		for (int k = 0; k < N; k++) {
-			std::cout << ".";
-		}
-		//std::cout << "\n";
-	}
-	return 0;
+	//std::cout << ans << "\n";
 }
