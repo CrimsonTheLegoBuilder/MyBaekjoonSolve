7d2aabf60fe61a019c41edbdd658aa8243d4afee
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-09-06 01:37:20 +0900

    2409060137easy6

--- Cpp/boj_solve/boj9583_easy.cpp
+++ Cpp/boj_solve/boj9583_easy.cpp
@@ -9,7 +9,7 @@ typedef long long ll;
 typedef long double ld;
 //typedef double ld;
 const ld INF = 1e17;
-const ld TOL = 1e-7;
+const ld TOL = 1e-11;
 const ld PI = acos(-1);
 const int LEN = 1e5 + 5;
 inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
@@ -45,12 +45,13 @@ typedef std::vector<Pos> Polygon;
 Polygon U, L;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
-std::vector<Pos> monotone_chain(std::vector<Pos>& C, int f = LOW) {
-	std::vector<Pos> H;
+Polygon monotone_chain(Polygon& C, int f = LOW) {
+	Polygon H;
 	std::sort(C.begin(), C.end());
 	if (f == UPPER) std::reverse(C.begin(), C.end());
 	//C.erase(unique(C.begin(), C.end()), C.end());
 	if (C.size() <= 2) {
+		assert(0);
 		for (const Pos& pos : C) H.push_back(pos);
 		return H;
 	}
@@ -66,18 +67,18 @@ std::vector<Pos> monotone_chain(std::vector<Pos>& C, int f = LOW) {
 }
 int idx_bi_search(const Polygon& H, const ld& x) {
 	int s = 1, e = H.size() - 1;
-	if (sign(H[1].x - x) > 0) return 0;
-	if (sign(H[2].x - x) > 0) return 1;
-	if (sign(H.back().x - x) == 0) return e;
+	if (H[1].x > x) return 0;
+	if (H[2].x > x) return 1;
+	if (!sign(H.back().x - x)) return e;
 	while (s < e) {
 		int m = s + e >> 1;
-		if (sign(H[m].x - x) > 0) s = m + 1;
+		if (sign(H[m].x - x) < 0) s = m + 1;
 		else e = m;
 	}
 	return s;
 }
 ld height_search(const ld& x, const ld& w, bool f = 0) {
-	auto cal_x_y = [&](const Polygon& V, const int i, const ld& x_, ld& y_) -> void {
+	auto cal_y = [&](const Polygon& V, const int i, const ld& x_, ld& y_) -> void {
 		ll den = V[i + 1].x - V[i].x;
 		ll num = V[i + 1].y - V[i].y;
 		y_ = (ld)V[i].y + (x_ - V[i].x) * (ld)num / den;
@@ -90,23 +91,24 @@ ld height_search(const ld& x, const ld& w, bool f = 0) {
 	ld x1 = x, x2 = x + w;
 	ld y1, y2, y3, y4;
 	if (zero(L[l1].x - x1)) y1 = L[l1].y;
-	else cal_x_y(L, l1, x1, y1);
+	else cal_y(L, l1, x1, y1);
 	if (zero(U[u1].x - x1)) y2 = U[u1].y;
-	else cal_x_y(U, u1, x1, y2);
+	else cal_y(U, u1, x1, y2);
 	if (zero(L[l2].x - x2)) y3 = L[l2].y;
-	else cal_x_y(L, l2, x2, y3);
+	else cal_y(L, l2, x2, y3);
 	if (zero(U[u2].x - x2)) y4 = U[u2].y;
-	else cal_x_y(U, u2, x2, y4);
-	ld yu = std::min(y2, y4);
+	else cal_y(U, u2, x2, y4);
+ 	ld yu = std::min(y2, y4);
 	ld yl = std::max(y1, y3);
-	if (f) X1 = x, X2 = x + w, Y1 = yl, Y2 = yu;
+	if (f && sign(yu - yl) > 0) X1 = x, X2 = x + w, Y1 = yl, Y2 = yu;
+	X1 = x, X2 = x + w, Y1 = yl, Y2 = yu;
 	return std::max(yu - yl, (ld)0);
 }
 ld area_ternary_search(const ld& w) {
 	ld s = L[0].x, e = L.back().x - w;
 	ld x1, x2;
-	int cnt = 100;
-	while (cnt--) {
+	//while (sign(e - s) > 0) {
+	int cnt = 100; while (cnt--) {
 		x1 = (s + s + e) / 3;
 		x2 = (s + e + e) / 3;
 		ld h1 = height_search(x1, w);
@@ -121,13 +123,13 @@ ld area_ternary_search(const ld& w) {
 void area_ternary_search() {
 	ld s = 0, e = L.back().x - L[0].x;
 	ld w1, w2;
-	int cnt = 100;
-	while (cnt--) {
+	//while (sign(e - s) > 0) {
+	int cnt = 100; while (cnt--) {
 		w1 = (s + s + e) / 3;
 		w2 = (s + e + e) / 3;
 		ld a1 = area_ternary_search(w1);
 		ld a2 = area_ternary_search(w2);
-		if (sign(a2 - a1) >= 0) s = w1;
+		if (sign(a2 - a1) > 0) s = w1;
 		else e = w2;
 	}
 	return;
@@ -139,10 +141,35 @@ void solve() {
 	std::cout.precision(9);
 	std::cin >> N;
 	Polygon H(N);
+	for (int i = 0; i < N; i++) std::cin >> H[i];
 	L = monotone_chain(H, LOW);
 	U = monotone_chain(H, UPPER);
 	area_ternary_search();
-	std::cout << X1 << " " << Y1 << " " << X2 << " " << Y2 << "\n";
+	std::cout << X1 << " " << Y1 << "\n" << X2 << " " << Y2 << "\n";
 	return;
 }
 int main() { solve(); return 0; }//boj9583
+
+/*
+
+4
+-1 0
+1 0
+2 -10
+-2 -10
+
+-1.000000000 -10.000000000
+1.000000000 -0.000000000
+
+6
+233127 -972446
+-357996 -933723
+-929678 -368373
+-947555 319593
+-563980 825789
+837792 545990
+
+-707706.1603165409 -587886.2601078567
+386264.71217350144 636116.556677073
+
+*/
