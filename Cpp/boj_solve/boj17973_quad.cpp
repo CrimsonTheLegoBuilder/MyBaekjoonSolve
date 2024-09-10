7257edcf69258d7a9648c0209b60867d5cc6a0a5
crimson <redcube231@gmail.com>
2024-09-10 14:12:17 +0900

    2409101412quad5

--- Cpp/boj_solve/boj17973_quad.cpp
+++ Cpp/boj_solve/boj17973_quad.cpp
@@ -16,7 +16,7 @@ inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL ? 1 : 0; }
 inline bool zero(const ld& x) { return !sign(x); }
 
-int N, M, E, OD[LEN], Q[LEN];
+int N, E, OD[LEN], Q[LEN];
 struct Pos {
 	int x, y;
 	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
@@ -33,10 +33,6 @@ struct Pos {
 	Pos operator ~ () const { return { -y, x }; }
 	Pos operator ! () const { return { y, x }; }
 	ll Euc() const { return (ll)x * x + (ll)y * y; }
-	int Man() const { return std::abs(x) + std::abs(y); }
-	ld mag() const { return hypot(x, y); }
-	ld rad() const { return atan2(y, x); }
-	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 } P[LEN];
@@ -44,27 +40,19 @@ const Pos O = { 0, 0 };
 typedef std::vector<Pos> Polygon;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { ll ret = cross(d1, d2, d3); return ret < 0 ? -1 : !!ret; }
+bool same_halfplane(const Pos& p1, const Pos& p2, const Pos& p3, const Pos& p4) { return ccw(p1, p2, p3) == ccw(p1, p2, p4); }
 struct Seg {
 	short int u, v;//idx
 	Seg(short int U = 0, short int V = 0) { u = U; v = V; if (P[v] < P[u]) std::swap(u, v); }
 	Pos s() const { return P[v] - P[u]; }
-	Pos p() const { return Pos(u, v); }
 	int ccw(const Pos& p0) const { return sign(cross(P[u], P[v], p0)); }
 	bool operator < (const Seg& S) const {
-		//assert(O != s()); assert(O != S.s());
-		//bool f1 = O < s();
-		//bool f2 = O < S.s();
-		//if (f1 != f2) return f1;
 		ll CCW = s() / S.s();
 		return !CCW ? S.ccw(P[u]) > 0 : CCW < 0;
 	}
 	bool operator == (const Seg& S) const { return s() / S.s() == 0 && s() * S.s() > 0; }
 } events[LEN * LEN];
-typedef std::vector<Seg> vseg;
-vseg segs[LEN][LEN];
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
@@ -91,19 +79,11 @@ void solve() {
 		Q[ou] = v; Q[ov] = u;
 		assert(ov - ou == 1);
 		quad += (ll)ou * (N - 1ll - ov);
-		int j, k, J, K;
-		j = std::max(0, ou - 2);
-		J = ou;
-		k = ov + 1;
-		K = std::min(N, ov + 3);
-		for (; j < J; j++) {
-			for (; k < K; k++) {
+		for (int j = std::max(0, ou - 2); j < ou; j++) {
+			for (int k = ov + 1; k < std::min(N, ov + 3); k++) {
 				ll A = std::abs(cross(P[Q[j]], P[Q[ou]], P[Q[ov]])) + std::abs(cross(P[Q[k]], P[Q[ou]], P[Q[ov]]));
 				if (MIN > A) MIN = A, cnt = 0;
-				if (MIN == A) {
-					cnt++;
-					if (ccw(P[Q[j]], P[Q[k]], P[Q[ov]]) == ccw(P[Q[j]], P[Q[k]], P[Q[ou]])) cnt++;
-				}
+				if (MIN == A) cnt += 1ll + same_halfplane(P[Q[j]], P[Q[k]], P[Q[ou]], P[Q[ov]]);
 			}
 		}
 	}
