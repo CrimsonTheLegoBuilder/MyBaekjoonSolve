aca945cffc280dbcf5e1c82e2aa92ed20dc101c3
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-09-18 19:54:37 +0900

    2409181954seg

--- Cpp/boj_solve/boj10049_cut.cpp
+++ Cpp/boj_solve/boj10049_cut.cpp
@@ -5,14 +5,11 @@
 #include <cstring>
 #include <cassert>
 #include <vector>
-#include <set>
 #include <unordered_set>
 typedef long long ll;
 typedef std::vector<int> Vint;
-typedef std::vector<ll> Vll;
-typedef std::set<int> Sint;
 typedef std::unordered_set<int> USint;
-const int LEN = 1e5 + 5;
+const int LEN = 1e5 + 10;
 
 #define ADD 1
 #define DEL -1
@@ -27,9 +24,8 @@ bool join(int i, int j) {
 	else P[j] += P[i], P[i] = j;
 	return 1;
 }
-int CX[LEN], CY[LEN], xi, yi;//compressed
-int idx_bi_search(int A[], int sz, const int& x) {
-	int s = 0, e = sz - 1, m;
+int idx_bi_search(const Vint& A, const int& x) {
+	int s = 0, e = A.size() - 1, m;
 	while (s <= e) {
 		m = s + e >> 1;
 		if (A[m] == x) return m;
@@ -46,33 +42,19 @@ struct H_event {
 struct V_event {
 	int x, y, ev, i;
 	V_event(int x_ = 0, int y_ = 0, int ev_ = 0, int i_ = 0) : x(x_), y(y_), ev(ev_), i(i_) {}
-	bool operator < (const V_event& v) const { return y == v.y ? x < v.x : v < v.y; }
+	bool operator < (const V_event& v) const { return y == v.y ? x < v.x : y < v.y; }
 } V[LEN << 1]; int vp;
-int max_y[LEN];
+int xi, yi;
+int hi_y[LEN];//highest y coord' of vertical event
 USint inxs[LEN << 4];//seg_tree
-//Sint inxs[LEN << 4];//seg_tree
 int seg_v[LEN << 4];//seg_tree
 int seg_p[LEN << 4];//seg_tree
-int sum_horizontal(int l, int r, int idx, int s = 0, int e = xi - 1, int i = 1) {
-	if (r < s || e < l) return 0;
-	if (l <= s && e <= r) {
-		if (seg_v[i] != -1) join(idx, seg_v[i]);//find new intersection
-		for (const int& v : inxs[i]) {
-			join(idx, v);
-			if (seg_v[i] == -1 || max_y[v] > max_y[seg_v[i]]) seg_v[i] = v;
-		}
-		inxs[i].clear();
-		return seg_p[i];
-	}
-	int m = s + e >> 1;
-	return sum_horizontal(l, r, idx, s, m, i << 1) + sum_horizontal(l, r, idx, m + 1, r, i << 1 | 1);
-}
-void update_vertical(int x, int ev, int idx, int s = 0, int e = xi - 1, int i = 1) {
+void update_vertical(const int& x, const int& ev, const int& idx, int s = 0, int e = xi - 1, int i = 1) {
 	if (x < s || e < x) return;
 	if (ev == ADD) inxs[i].insert(idx);
 	else if (ev == DEL) {
 		inxs[i].erase(idx);
-		if (seg_v[i] == idx) seg_v[i] = -1;
+		if (seg_v[i] == idx) seg_v[i] = -1;//erase all vertical
 	}
 	if (s == e) { seg_p[i] += ev; return; }
 	int m = s + e >> 1;
@@ -81,23 +63,37 @@ void update_vertical(int x, int ev, int idx, int s = 0, int e = xi - 1, int i = 
 	seg_p[i] = seg_p[i << 1] + seg_p[i << 1 | 1];
 	return;
 }
+int sum_horizontal(const int& l, const int& r, const int& idx, int s = 0, int e = xi - 1, int i = 1) {
+	if (r < s || e < l) return 0;
+	if (l <= s && e <= r) {
+		if (seg_v[i] != -1) join(idx, seg_v[i]);//find new intersection
+		for (const int& v : inxs[i]) {
+			join(idx, v);
+			if (seg_v[i] == -1 || hi_y[v] > hi_y[seg_v[i]]) seg_v[i] = v;//highest y coord'
+		}
+		inxs[i].clear();
+		return seg_p[i];
+	}
+	int m = s + e >> 1;
+	return sum_horizontal(l, r, idx, s, m, i << 1) + sum_horizontal(l, r, idx, m + 1, e, i << 1 | 1);
+}
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> h >> w >> N;
+	std::cin >> w >> h >> N;
 	Vint X = { 0, w };
 	Vint Y = { 0, h, h + 1 };
 	hp = 0; vp = 0;
 	for (int i = 0, a, b, c, d; i < N; i++) {
 		std::cin >> a >> b >> c >> d;
-		if (b == d) {
+		if (a != c) {
 			H[hp++] = H_event(a, c, b, i);
 		}
-		else if (a == c) {
-			V[vp++] = V_event(a, b, ADD, i);
-			V[vp++] = V_event(a, d + 1, DEL, i);
-			max_y[i] = d;
+		if (b != d) {
+			hi_y[i] = d;
 			d++;
+			V[vp++] = V_event(a, b, ADD, i);
+			V[vp++] = V_event(a, d, DEL, i);
 		}
 		X.push_back(a); X.push_back(c);
 		Y.push_back(b); Y.push_back(d);
@@ -109,30 +105,27 @@ void solve() {
 	V[vp++] = V_event(0, h + 1, DEL, N);
 	V[vp++] = V_event(w, h + 1, DEL, N);
 
-	std::sort(X.begin(), X.end());
-	X.erase(unique(X.begin(), X.end()), X.end());
-	std::sort(Y.begin(), Y.end());
-	Y.erase(unique(Y.begin(), Y.end()), Y.end());
-	int tmp;
-	tmp = -1; for (const int& x : X) if (x != tmp) tmp = CX[xi++] = x;
-	tmp = -1; for (const int& y : Y) if (y != tmp) tmp = CY[yi++] = y;
+	std::sort(X.begin(), X.end()); X.erase(unique(X.begin(), X.end()), X.end());
+	std::sort(Y.begin(), Y.end()); Y.erase(unique(Y.begin(), Y.end()), Y.end());
 	std::sort(H, H + hp);
 	std::sort(V, V + vp);
 	memset(P, -1, sizeof P);
 	memset(seg_v, -1, sizeof seg_v);
 	memset(seg_p, 0, sizeof seg_p);
 
-	int e = 0, v = 0, chi = 0;
+	xi = X.size();
+	yi = Y.size();
+	ll e = 0, v = 0, chi = 0;
 	for (int y = 0, i = 0, j = 0; y < yi; y++) {
-		while (i < vp && idx_bi_search(CY, yi, V[i].y) == y) {
+		while (i < vp && V[i].y == Y[y]) {//vertical events
 			v++;
 			e += V[i].ev == DEL;
-			update_vertical(idx_bi_search(CX, xi, V[i].x), V[i].ev, V[i].i);
+			update_vertical(idx_bi_search(X, V[i].x), V[i].ev, V[i].i);
 			i++;
 		}
-		while (j < hp && idx_bi_search(CY, yi, H[j].y) == y) {
-			int l_ = idx_bi_search(CX, xi, H[j].l);
-			int r_ = idx_bi_search(CX, xi, H[j].r);
+		while (j < hp && H[j].y == Y[y]) {//horizontal events
+			int l_ = idx_bi_search(X, H[j].l);
+			int r_ = idx_bi_search(X, H[j].r);
 			int cnt = sum_horizontal(l_, r_, H[j].i);
 			e += cnt * 2 + 1;
 			v += cnt + 2;
@@ -143,4 +136,4 @@ void solve() {
 	std::cout << chi - v + e << "\n";//X = v - e + f
 	return;
 }
-int main() { solve(); return 0; }//boj10049
+int main() { solve(); return 0; }//boj10049 cutting  refer to Jay0202
