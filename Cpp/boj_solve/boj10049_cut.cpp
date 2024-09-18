7533a0d867e7f59f33bc30162e74a94fe0d9cd8c
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-09-18 17:34:42 +0900

    2409181734seg

--- Cpp/boj_solve/boj10049_cut.cpp
+++ Cpp/boj_solve/boj10049_cut.cpp
@@ -6,13 +6,12 @@
 #include <cassert>
 #include <vector>
 #include <set>
-#include <queue>
+#include <unordered_set>
 typedef long long ll;
 typedef std::vector<int> Vint;
 typedef std::vector<ll> Vll;
 typedef std::set<int> Sint;
-typedef std::priority_queue<int> HQint;
-typedef std::priority_queue<ll> HQll;
+typedef std::unordered_set<int> USint;
 const int LEN = 1e5 + 5;
 
 #define ADD 1
@@ -28,7 +27,6 @@ bool join(int i, int j) {
 	else P[j] += P[i], P[i] = j;
 	return 1;
 }
-Vint X, Y;//all x, y coord's
 int CX[LEN], CY[LEN], xi, yi;//compressed
 int idx_bi_search(int A[], int sz, const int& x) {
 	int s = 0, e = sz - 1, m;
@@ -51,10 +49,11 @@ struct V_event {
 	bool operator < (const V_event& v) const { return y == v.y ? x < v.x : v < v.y; }
 } V[LEN << 1]; int vp;
 int max_y[LEN];
-Sint inxs[LEN << 2];//seg_tree
-int seg_v[LEN << 2];//seg_tree
-int seg_p[LEN << 2];//seg_tree
-int add_horizontal(int l, int r, int idx, int s = 0, int e = xi - 1, int i = 1) {
+USint inxs[LEN << 4];//seg_tree
+//Sint inxs[LEN << 4];//seg_tree
+int seg_v[LEN << 4];//seg_tree
+int seg_p[LEN << 4];//seg_tree
+int sum_horizontal(int l, int r, int idx, int s = 0, int e = xi - 1, int i = 1) {
 	if (r < s || e < l) return 0;
 	if (l <= s && e <= r) {
 		if (seg_v[i] != -1) join(idx, seg_v[i]);//find new intersection
@@ -66,7 +65,7 @@ int add_horizontal(int l, int r, int idx, int s = 0, int e = xi - 1, int i = 1) 
 		return seg_p[i];
 	}
 	int m = s + e >> 1;
-	return add_horizontal(l, r, idx, s, m, i << 1) + add_horizontal(l, r, idx, m + 1, r, i << 1 | 1);
+	return sum_horizontal(l, r, idx, s, m, i << 1) + sum_horizontal(l, r, idx, m + 1, r, i << 1 | 1);
 }
 void update_vertical(int x, int ev, int idx, int s = 0, int e = xi - 1, int i = 1) {
 	if (x < s || e < x) return;
@@ -86,8 +85,8 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cin >> h >> w >> N;
-	X = { 0, w };
-	Y = { 0, h, h + 1 };
+	Vint X = { 0, w };
+	Vint Y = { 0, h, h + 1 };
 	hp = 0; vp = 0;
 	for (int i = 0, a, b, c, d; i < N; i++) {
 		std::cin >> a >> b >> c >> d;
@@ -111,12 +110,12 @@ void solve() {
 	V[vp++] = V_event(w, h + 1, DEL, N);
 
 	std::sort(X.begin(), X.end());
+	X.erase(unique(X.begin(), X.end()), X.end());
 	std::sort(Y.begin(), Y.end());
+	Y.erase(unique(Y.begin(), Y.end()), Y.end());
 	int tmp;
-	tmp = -1;
-	for (const int& x : X) if (x != tmp) tmp = CX[xi++] = x;
-	tmp = -1;
-	for (const int& y : Y) if (y != tmp) tmp = CY[yi++] = y;
+	tmp = -1; for (const int& x : X) if (x != tmp) tmp = CX[xi++] = x;
+	tmp = -1; for (const int& y : Y) if (y != tmp) tmp = CY[yi++] = y;
 	std::sort(H, H + hp);
 	std::sort(V, V + vp);
 	memset(P, -1, sizeof P);
@@ -127,21 +126,21 @@ void solve() {
 	for (int y = 0, i = 0, j = 0; y < yi; y++) {
 		while (i < vp && idx_bi_search(CY, yi, V[i].y) == y) {
 			v++;
-			e += V[i].ev == ADD;
+			e += V[i].ev == DEL;
 			update_vertical(idx_bi_search(CX, xi, V[i].x), V[i].ev, V[i].i);
 			i++;
 		}
 		while (j < hp && idx_bi_search(CY, yi, H[j].y) == y) {
 			int l_ = idx_bi_search(CX, xi, H[j].l);
 			int r_ = idx_bi_search(CX, xi, H[j].r);
-			int cnt = add_horizontal(l_, r_, H[j].i);
+			int cnt = sum_horizontal(l_, r_, H[j].i);
 			e += cnt * 2 + 1;
 			v += cnt + 2;
 			j++;
 		}
 	}
 	for (int i = 0; i <= N; i++) if (i == find(i)) chi++;
-	std::cout << chi - v + e << "\n";
+	std::cout << chi - v + e << "\n";//X = v - e + f
 	return;
 }
 int main() { solve(); return 0; }//boj10049
