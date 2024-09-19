a584b53ac7862464a52bbba0c312f5efbef7a8eb
crimson <redcube231@gmail.com>
2024-09-19 15:26:50 +0900

    2409191526

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -7,12 +7,17 @@ typedef long long ll;
 typedef long double ld;
 const ld METER = 1609.344;
 const ld LITER = 3.785411784;
+const ld PI = acos(-1);
 ld A;
 
 int main() {
 	std::cout << std::fixed;
 	std::cout.precision(7);
-	std::cin >> A;
+	//std::cin >> A;
+
+	ld t = atan2(12.566, 56.5);
+	std::cout << t * 180 / PI << "\n";
+
 	//std::cout << 100000. / (A / LITER * METER) << "\n";
 	return 0;
 }
