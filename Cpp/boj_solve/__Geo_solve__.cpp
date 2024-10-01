767621799c79cd30ef6a307b9a69c68bddc32401
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-10-01 16:40:52 +0900

    2410011640

--- Cpp/boj_solve/__Geo_solve__.cpp
+++ Cpp/boj_solve/__Geo_solve__.cpp
@@ -5,39 +5,128 @@
 #include <cstring>
 #include <vector>
 typedef long long ll;
+typedef long double ld;
 inline int sign(const ll& x) { return x < 0 ? -1 : x > 0; }
 inline ll sq(const int& x) { return (ll)x * x; }
 inline ll sq(const ll& x) { return x * x; }
 
-int d, T, N;
+int T, N, M, K;
 struct Pos {
 	int x, y;
 	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
 	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
 	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
 	ll Euc() const { return (ll)x * x + (ll)y * y; }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
-};
+} p1, p2;
 typedef std::vector<Pos> Polygon;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
-Polygon dir = { Pos(-1, -1), Pos(1, 0), Pos(-1, 0), Pos(0, -1), Pos(0, 1) };
+bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0;
+}
+bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) {
+	return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0;
+}
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	bool f3 = on_seg_strong(s1, s2, d1) ||
+		on_seg_strong(s1, s2, d2) ||
+		on_seg_strong(d1, d2, s1) ||
+		on_seg_strong(d1, d2, s2);
+	return f1 && f2 || f3;
+}
+Polygon graham_scan(Polygon& C) {
+	Polygon H;
+	if (C.size() < 3) {
+		std::sort(C.begin(), C.end());
+		return C;
+	}
+	std::swap(C[0], *min_element(C.begin(), C.end()));
+	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
+		int ret = ccw(C[0], p, q);
+		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
+		return ret > 0;
+		}
+	);
+	C.erase(unique(C.begin(), C.end()), C.end());
+	int sz = C.size();
+	for (int i = 0; i < sz; i++) {
+		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
+			H.pop_back();
+		H.push_back(C[i]);
+	}
+	return H;
+}
+ll rotating_calipers(const Polygon& H) {
+	int sz = H.size();
+	auto CCW = [&](const int& i, const int& j) -> ll {
+		return cross(H[i], H[(i + 1) % sz], H[j], H[(j + 1) % sz]);
+		};
+	ll MD = 0;
+	for (int i = 0, j = 0; i < sz; i++) {
+		while (CCW(i, j) > 0) {
+			if (MD < (H[i] - H[j]).Euc()) {
+				MD = (H[i] - H[j]).Euc();
+				p1 = H[i]; p2 = H[j];
+			}
+			j = (j + 1) % sz;
+		}
+		if (MD < (H[i] - H[j]).Euc()) {
+			MD = (H[i] - H[j]).Euc();
+			p1 = H[i]; p2 = H[j];
+		}
+	}
+	return MD;
+}
+int inner_check_bi_search(const Polygon& H, const Pos& p) {//convex
+	int sz = H.size();
+	if (!sz) return -1;
+	if (sz == 1) return p == H[0] ? 0 : -1;
+	if (sz == 2) return on_seg_strong(H[0], H[1], p) ? 0 : -1;
+	if (cross(H[0], H[1], p) < 0 || cross(H[0], H[sz - 1], p) > 0) return -1;
+	if (on_seg_strong(H[0], H[1], p) || on_seg_strong(H[0], H[sz - 1], p)) return 0;
+	int s = 0, e = sz - 1, m;
+	while (s + 1 < e) {
+		m = s + e >> 1;
+		if (cross(H[0], H[m], p) >= 0) s = m;
+		else e = m;
+	}
+	if (cross(H[s], H[e], p) > 0) return 1;
+	else if (on_seg_strong(H[s], H[e], p)) return 0;
+	else return -1;
+}
+int inner_check(const Polygon& H, const Pos& p) {//concave
+	int cnt = 0, sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % sz];
+		if (on_seg_strong(cur, nxt, p)) return 1;
+		if (cur.y == nxt.y) continue;
+		if (nxt.y < cur.y) std::swap(cur, nxt);
+		if (nxt.y <= p.y || cur.y > p.y) continue;
+		cnt += ccw(cur, nxt, p) > 0;
+	}
+	return (cnt & 1) * 2;
+}
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cin >> N;
-	Polygon H = { Pos(0, 0) };
-	for (int _ = 0; _ < 6; _++) {
-		std::cin >> d >> T;
-		Pos cur = H.back() + dir[d] * T;
-		H.push_back(cur);
+	Polygon A(N);
+	for (Pos& p : A) std::cin >> p;
+	for (int _ = 0; _ < 3; _++) {
+		std::cin >> p1;
+		std::cout << (inner_check(A, p1) > 0) << "\n";
 	}
-	ll ret = 0;
-	for (int i = 0; i < 7; i++) ret += H[i] / H[(i + 1) % 7];
-	std::cout << (ret * N >> 1)<< "\n";
 	return 0;
 }
--- Python/geo_solve.py
+++ Python/geo_solve.py
@@ -1,14 +1,36 @@
 import sys
 INPUT = sys.stdin.readline
 
-if __name__ == "__main__":
-    while 1:
-        A = list(map(int, INPUT().strip().split()))
-        A.sort()
-        if A[0] == 0 and A[1] == 0 and A[2] == 0:
-            break
-        else:
-            if A[2] ** 2 == A[0] ** 2 + A[1] ** 2:
-                print("right")
-            else:
-                print("wrong")
+
+class Pos:
+    def __init__(self, x_: int, y_: int):
+        self.x = x_
+        self.y = y_
+
+    def cross(self, d1: 'Pos', d2: 'Pos') -> int:
+        ret = (d1.x - self.x) * (d2.y - d1.y) - (d1.y - self.y) * (d2.x - d1.x)
+        return ret
+
+    def dist(self, d1: 'Pos') -> float:
+        return ((self.x - d1.x)**2 + (self.y - d1.y)**2)**.5
+
+
+def monotone_chain() -> list[Pos]:
+    D.sort(key=lambda pos: (pos.x, pos.y))
+    uh, dh = [], []
+    for d in D:
+        while len(dh) > 1 and Pos.cross(dh[-2], dh[-1], d) <= 0:
+            dh.pop()
+        dh.append(d)
+    for d in reversed(D):
+        while len(uh) > 1 and Pos.cross(uh[-2], uh[-1], d) <= 0:
+            uh.pop()
+        uh.append(d)
+    return dh[:-1] + uh[:-1]
+
+
+N: int = int(INPUT().strip())
+D: list[Pos] = [Pos(*map(int, INPUT().strip().split())) for _ in range(N)]
+hull: list[Pos] = monotone_chain()
+
+print(len(hull))
