9a9f373e015856e73e795ab0003c32ad341cf045
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-10-14 00:24:25 +0900

    2410140024

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -9,53 +9,21 @@ typedef long long ll;
 typedef long double ld;
 const int LEN = 500;
 
-int N, X;
+int Q, C, V;
+
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> N;
-	for (int i = 0; i < N - 1; i++) {
-		if (i == 0) {
-			for (int j = 0; j < N; j++) std::cout << "*";
-			for (int j = 0; j < (N - 1) * 2 - 1; j++) std::cout << " ";
-			for (int j = 0; j < N; j++) std::cout << "*";
-		}
-		else {
-			for (int j = 0; j < i; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < N - 2; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < (N - i - 1) * 2 - 1; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < N - 2; j++) std::cout << " ";
-			std::cout << "*";
-		}
-		std::cout << "\n";
-	}
-	for (int i = 0; i < N - 1; i++) std::cout << " ";
-	std::cout << "*";
-	for (int i = 0; i < N - 2; i++) std::cout << " ";
-	std::cout << "*";
-	for (int i = 0; i < N - 2; i++) std::cout << " ";
-	std::cout << "*";
-	std::cout << "\n";
-	for (int i = N - 2; i >= 0; i--) {
-		if (i == 0) {
-			for (int j = 0; j < N; j++) std::cout << "*";
-			for (int j = 0; j < (N - 1) * 2 - 1; j++) std::cout << " ";
-			for (int j = 0; j < N; j++) std::cout << "*";
-		}
-		else {
-			for (int j = 0; j < i; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < N - 2; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < (N - i - 1) * 2 - 1; j++) std::cout << " ";
-			std::cout << "*";
-			for (int j = 0; j < N - 2; j++) std::cout << " ";
-			std::cout << "*";
-		}
-		std::cout << "\n";
+	std::cout << std::fixed;
+	std::cout.precision(2);
+	std::cin >> Q;
+	while (Q--) {
+		std::cin >> C >> V;
+		std::cout << "You get ";
+		std::cout << C / V;
+		std::cout << " piece(s) and your dad gets ";
+		std::cout << C % V;
+		std::cout << " piece(s).\n";
 	}
 	return 0;
 }
