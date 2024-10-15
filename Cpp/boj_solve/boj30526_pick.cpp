05a887f0140a0767d2993b66e004e852cceb69de
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-10-16 01:27:08 +0900

    2410160127geo

--- Cpp/boj_solve/boj30526_pick.cpp
+++ Cpp/boj_solve/boj30526_pick.cpp
@@ -25,14 +25,22 @@ ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
 //ll gcd(ll a, ll b) { while (b) { ll tmp = a % b; a = b; b = tmp; } return a; }
 inline ld tol(const ld& x) { return x + TOL * (x / std::abs(x)); }
 
-#define DEBUG
+//#define DEBUG
+//#define BIG
 #define WHAT_THE_FUCK
 
 //Pick`s Theorem : A = i + b/2 - 1
 int N, M, T, Q;
 struct Pos {
 	int x, y;
-	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+#ifdef BIG
+	int den;
+#endif
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {
+#ifdef BIG
+		den = 1;
+#endif
+	}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
@@ -61,6 +69,15 @@ struct Pos {
 	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+#ifdef BIG
+	int modx() const { return std::abs(x) % std::abs(den); }
+	int mody() const { return std::abs(y) % std::abs(den); }
+	bool x_int() const { return !modx(); }
+	bool y_int() const { return !mody(); }
+	bool is_int() const { return !modx() && !mody(); }
+	ld x() const { return x * 1. / den; }
+	ld y() const { return y * 1. / den; }
+#endif
 }; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
@@ -82,6 +99,16 @@ std::pair<ld, ld> intersection(const Pos& p1, const Pos& p2, const Pos& q1, cons
 	ld y = (p1.y * a2 + p2.y * a1) * (ld)1. / (a1 + a2);
 	return { x, y };
 }
+#ifdef BIG
+Pos intersection_big(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) {
+	int a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2);
+	int x = (p1.x * a2 + p2.x * a1);
+	int y = (p1.y * a2 + p2.y * a1);
+	Pos ret = Pos(x, y);
+	ret.den = a1 + a2;
+	return ret;
+}
+#endif
 ld get_y(Pos p0, Pos p1, ld x) {
 	if (p1.x < p0.x) std::swap(p1, p0);
 	Pos vec = p1 - p0;
@@ -101,17 +128,20 @@ ll pick(const Pos& p0, const Pos& p1, const ll& y) {
 	assert(!(i & 1ll));
 	return i >> 1;
 }
-ll remain_count(Pos p0, Pos p1, Pos s, ll x, ll y) {
+ll remain_count(Pos p0, Pos p1, Pos s, ll x, ll y, ld xf) {
 #ifdef DEBUG
 	std::cout << "FUCK:: remain\n";
 	std::cout << "p0:: " << p0 << "\n";
 	std::cout << "p1:: " << p1 << "\n";
+	std::cout << " s:: " << s << "\n";
 #endif
-	if (p0.y == p1.y) return (s.y - y - 1) * (std::abs(s.x - x) + 1);
+	if (p1 == s) return 0;
+	if (p0.y == p1.y) return (s.y - y - 1) * (std::abs(s.x - x) + !zero(x - xf));
 	if (std::abs(s.x - x) == 0) return (s.y - y - 1);
-	ll dx = (s.x < x) ? 1ll : -1ll;
+	ll dx = ll(p1.x - p0.x) / std::abs(p1.x - p0.x);
 	ll dy = ll(p1.y - p0.y) / std::abs(p1.y - p0.y);
 	int sz = std::abs(x - s.x);
+	if (zero(x - xf)) sz--;
 	ll cnt = 0;
 	while (sz-- >= 0) {
 		while (s.x * ccw(p0, p1, s) < 0) { 
@@ -174,17 +204,19 @@ ll tri_count(Pos p0, Pos p1, Pos p2, Pos p3) {
 	if (v0.x) {
 		int n0 = (ll)tol(dx0) / v0.x;
 		c1 = std::abs((ll)tol(dx1) / v0.x);
+		if (zero(x - (p1.x + c1 * v0.x)) && zero(y - (p1.y + c1 * v0.y))) c1--;
 		Pos q0 = p0 + v0 * n0;
 		if (ccw(p3, p2, q0) > 0) q0 -= v0;
 		t1 = pick(p1, q0, Y);
 		ll X = x0;
+		//if (zero((ll)tol(x) - x)) X -= v0.x / std::abs(v0.x);
 #ifdef DEBUG
 		std::cout << "FUCK:: v0\n";
 		std::cout << "FUCK:: t1:: " << t1 << "\n";
 		std::cout << "FUCK:: t1:: p0:: " << p0 << " p1:: " << p1 << " X:: "  << X << "\n";
 		std::cout << "FUCK:: t1:: q0:: " << q0 << "\n";
 #endif
-		t1 += remain_count(p0, p1, q0, X, Y);
+		t1 += remain_count(p0, p1, q0, X, Y, x);
 #ifdef DEBUG
 		std::cout << "FUCK:: t1:: " << t1 << "\n";
 #endif
@@ -192,17 +224,19 @@ ll tri_count(Pos p0, Pos p1, Pos p2, Pos p3) {
 	if (v3.x) {
 		int n3 = (ll)tol(dx3) / v3.x;
 		c2 = std::abs((ll)tol(dx2) / v3.x);
+		if (zero(x - (p2.x + c2 * v3.x)) && zero(y - (p2.y + c2 * v3.y))) c2--;
 		Pos q3 = p3 + v3 * n3;
 		if (ccw(p0, p1, q3) < 0) q3 -= v3;
 		t2 = pick(p2, q3, Y);
 		ll X = x3;
+		//if (zero((ll)tol(x) - x)) X -= v3.x / std::abs(v3.x);
 #ifdef DEBUG
 		std::cout << "FUCK:: v3\n";
 		std::cout << "FUCK:: t2:: " << t2 << "\n";
-		std::cout << "FUCK:: t1:: p2:: " << p2 << " p3:: " << p3 << " X:: " << X << "\n";
-		std::cout << "FUCK:: t1:: q3:: " << q3 << "\n";
+		std::cout << "FUCK:: t2:: p2:: " << p2 << " p3:: " << p3 << " X:: " << X << "\n";
+		std::cout << "FUCK:: t2:: q3:: " << q3 << "\n";
 #endif
-		t2 += remain_count(p3, p2, q3, X, Y);
+		t2 += remain_count(p3, p2, q3, X, Y, x);
 #ifdef DEBUG
 		std::cout << "FUCK:: t2:: " << t2 << "\n";
 #endif
@@ -220,53 +254,55 @@ ll tri_count(Pos p0, Pos p1, Pos p2, Pos p3) {
 		ld my = get_y(a0, a1, x);
 		if (sign(my - y) > 0) {
 			cnt = t0 - t1 - t2;
-			cnt -= c1 + c2 - 2;
+			cnt -= c1 + c2;
 			if (zero((ll)tol(x) - x)) {
 				ll dy = (ll)tol(y - Y);
 				cnt += dy;
+				if (zero((ll)tol(y) - y)) cnt++;
 			}
 		}
 		else {
 			cnt = t1 + t2 - t0;
 			cnt -= c0 - 1;
 			if (zero((ll)tol(x) - x)) {
-				ll dy = (ll)tol(y - Y);
-				cnt -= dy;
+				ll dy = (ll)tol(y - ll(my + 1));
+				cnt += dy + 1;
+				if (zero((ll)tol(y) - y)) cnt--;
 			}
 		}
 	}
 	else if (v2.x > 0) {
 		cnt += t0;
-		if (sign(tol(x) - a1.x) > 0) {
-			cnt -= t1 - 1;
-			cnt -= c1;
+		if (sign(x - a1.x) > 0) {
 			cnt += t2;
-			ll dy = a1.y - Y;
+			cnt -= t1;
+			cnt -= c1;
+			ll dy = a1.y - Y - 1;
 			cnt += dy;
 		}
-		else if (sign(tol(x) - a0.x) < 0) {
-			cnt -= t2 - 1;
-			cnt -= c2;
+		else if (sign(x - a0.x) < 0) {
 			cnt += t1;
-			ll dy = a0.y - Y;
+			cnt -= t2;
+			cnt -= c2;
+			ll dy = a0.y - Y - 1;
 			cnt += dy;
 		}
 	}
 	else if (v2.x < 0) {
 		cnt -= t0;
 		cnt -= c0 - 1;
-		if (sign(tol(x) - a1.x) > 0) {
-			cnt -= t1 - 1;
-			cnt -= c1;
+		if (sign(x - a1.x) > 0) {
 			cnt += t2;
-			ll dy = a1.y - Y + 1;
+			cnt -= t1;
+			cnt -= c1;
+			ll dy = a1.y - Y;
 			cnt -= dy;
 		}
-		else if (sign(tol(x) - a0.x) < 0) {
-			cnt -= t2 - 1;
-			cnt -= c2;
+		else if (sign(x - a0.x) < 0) {
 			cnt += t1;
-			ll dy = a0.y - Y + 1;
+			cnt -= t2;
+			cnt -= c2;
+			ll dy = a0.y - Y;
 			cnt -= dy;
 		}
 	}
@@ -282,15 +318,22 @@ void solve() {
 	for (Pos& p : H) std::cin >> p;
 	norm(H);
 	ll ret = 0;
+	if (N == 3) { std::cout << "infinitely many\n"; return; }
+	if (N == 4) {
+		const Pos& p0 = H[0], p1 = H[1], p2 = H[2], p3 = H[3];
+		if (!cross(p0, p1, p2, p3) && !cross(p1, p2, p3, p0) && area(H) == 2) { std::cout << "0\n"; return; }
+	}
 	for (int i = 0; i < N; i++) {
 		const Pos& p0 = H[(i - 1 + N) % N], p1 = H[i], p2 = H[(i + 1) % N], p3 = H[(i + 2) % N];
-		if (4 >= N || ccw(p0, p1, p2, p3) <= 0) {
+		if (ccw(p0, p1, p2, p3) <= 0) {
 			std::cout << "infinitely many\n";
 			return;
 		}
 		ll tmp = tri_count(p0, p1, p2, p3);
 #ifdef DEBUG
-		std::cout << "cnt:: " << tmp << "\n";
+		std::cout << "\n";
+		std::cout << "CNT:: " << tmp << "\n";
+		std::cout << "\n";
 #endif
 		ret += tmp;
 	}
