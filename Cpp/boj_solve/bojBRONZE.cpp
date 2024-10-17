6833884536ce7b220677896d9cacd90b410bc2ec
crimson <redcube231@gmail.com>
2024-10-18 08:17:02 +0900

    2410180816

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -7,23 +7,25 @@
 #include <cassert>
 typedef long long ll;
 typedef long double ld;
-const int LEN = 500;
+const int LEN = 1e6 + 1;
 
-int Q, C, V;
+ll N, A[LEN], B, C;
 
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> Q;
-	while (Q--) {
-		std::cin >> C >> V;
-		std::cout << "You get ";
-		std::cout << C / V;
-		std::cout << " piece(s) and your dad gets ";
-		std::cout << C % V;
-		std::cout << " piece(s).\n";
+	ll ret = 0;
+	std::cin >> N;
+	for (int i = 0; i < N; i++) std::cin >> A[i];
+	std::cin >> B >> C;
+	for (int i = 0; i < N; i++) {
+		ll D = A[i] - B;
+		ret++;
+		if (D <= 0) continue;
+		else ret += D / C + (D % C != 0);
 	}
+	std::cout << ret << "\n";
 	return 0;
 }
