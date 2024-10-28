f490b50591150d9f51b816b3fd3a8d2571e35bf6
crimson <redcube231@gmail.com>
2024-10-29 08:10:39 +0900

    2410290810

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -9,24 +9,23 @@ typedef long long ll;
 typedef long double ld;
 const int LEN = 1e6 + 1;
 
-int H1, M1, S1, H2, M2, S2, H3, M3, S3;
-ll T1, T2, T3;
+int N, M[1001], X;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	scanf("%d:%d:%d", &H1, &M1, &S1);
-	scanf("%d:%d:%d", &H2, &M2, &S2);
-	T1 = H1 * 60ll * 60 + M1 * 60ll + S1;
-	T2 = H2 * 60ll * 60 + M2 * 60ll + S2;
-	T3 = T2 - T1;
-	if (T3 < 0) T3 += 24 * 60 * 60;
-	H3 = T3 / (60 * 60);
-	T3 %= (60 * 60);
-	M3 = T3 / 60;
-	T3 %= 60;
-	S3 = T3;
-	printf("%02d:%02d:%02d", H3, M3, S3);
+	memset(M, 0, sizeof M);
+	int cnt = 0;
+	for (int i = 0; i < 10; i++) {
+		std::cin >> X;
+		M[X]++;
+		N += X;
+	}
+	std::cout << N / 10 << "\n";
+	for (int i = 1000; i > 0; i -= 10) {
+		if (cnt < M[i]) { N = i; cnt = M[i]; }
+	}
+	//std::cout << N << "\n";
 	return 0;
 }
