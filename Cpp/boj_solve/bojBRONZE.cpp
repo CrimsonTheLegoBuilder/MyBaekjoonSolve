d3f4507fb2705ed2a3b012be08b879fca2b34ba4
crimson <redcube231@gmail.com>
2024-10-29 13:01:00 +0900

    2410291301

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -8,24 +8,28 @@
 typedef long long ll;
 typedef long double ld;
 const int LEN = 1e6 + 1;
+int sq(int x) { return x * x; }
 
-int N, M[1001], X;
+int a, b, c;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(2);
-	memset(M, 0, sizeof M);
-	int cnt = 0;
-	for (int i = 0; i < 10; i++) {
-		std::cin >> X;
-		M[X]++;
-		N += X;
+	std::cout.precision(4);
+	int t = 1;
+	while (1) {
+		std::cin >> a >> b >> c;
+		if (!a && !b && !c) break;
+		//std::cout << "Triangle #" << t << "\n";
+		if (a == -1 || b == -1) {
+			if (b == -1) std::swap(a, b);
+			if (b >= c) std::cout << "Impossible.\n";
+			else std::cout << sqrt(sq(c) - sq(b)) << "\n";
+		}
+		else {
+			std::cout << sqrt(sq(a) + sq(b)) << "\n";
+		}
+		std::cout << "\n";
 	}
-	std::cout << N / 10 << "\n";
-	for (int i = 1000; i > 0; i -= 10) {
-		if (cnt < M[i]) { N = i; cnt = M[i]; }
-	}
-	//std::cout << N << "\n";
 	return 0;
 }
