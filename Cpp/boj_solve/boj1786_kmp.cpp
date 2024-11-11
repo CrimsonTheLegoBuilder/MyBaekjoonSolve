dffccb8e99a036e9ebe7416a2878693f8c2bc4a0
crimson <redcube231@gmail.com>
2024-11-12 08:10:59 +0900

    2411120810

--- Cpp/boj_solve/boj1786_kmp.cpp
+++ Cpp/boj_solve/boj1786_kmp.cpp
@@ -1,14 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <vector>
-#include <cstdio>
 #include <string>
 #include <cstring>
+#include <cassert>
+#include <vector>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+typedef std::pair<int, int> pi;
 typedef std::vector<int> Vint;
+typedef std::vector<ld> Vld;
+const ll INF = 1e17;
+const int LEN = 1e5 + 1;
+const ld TOL = 1e-7;
+const ll MOD = 1'000'000'007;
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+inline ll sq(int x) { return (ll)x * x; }
 
 int N;
-Vint get_pi(const std::string& s) {
+struct Pos {
+	int x, y;
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+Vint get_pi(const Polygon& s) {
 	int m = s.size(), j = 0;
 	Vint pi(m, 0);
 	for (int i = 1; i < m; i++) {
@@ -17,7 +48,7 @@ Vint get_pi(const std::string& s) {
 	}
 	return pi;
 }
-Vint kmp(const std::string& T, const std::string& P) {
+Vint kmp(const Polygon& T, const Polygon& P) {
 	Vint ans;
 	Vint pi = get_pi(P);
 	int t = T.size(), p = P.size(), j = 0;
@@ -33,12 +64,10 @@ Vint kmp(const std::string& T, const std::string& P) {
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::string T, P;
-	std::getline(std::cin, T);
-	std::getline(std::cin, P);
+	Polygon T, P;
 	Vint matched = kmp(T, P);
 	std::cout << matched.size() << "\n";
 	for (const int& i : matched) std::cout << i + 1 << " ";
 	return;
 }
-int main() { solve(); return 0; }//boj1786 refer to bowbowbow
+int main() { solve(); return 0; }//boj29309
