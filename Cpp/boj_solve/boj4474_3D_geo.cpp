85eb6196b726276a18f1a6f3a384040967b16e54
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-11-16 15:51:48 +0900

    2411161551

---
+++ Cpp/boj_solve/boj4474_3D_geo.cpp
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <array>
+#include <tuple>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+typedef std::pair<int, int> pi;
+typedef std::vector<int> Vint;
+typedef std::vector<ld> Vld;
+const ll INF = 1e17;
+const int LEN = 1e5 + 1;
+const ld TOL = 1e-7;
+const ll MOD = 1'000'000'007;
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+inline bool eq(const ld& x, const ld& y) { return zero(x - y); }
+inline ld sq(const ld& x) { return x * x; }
+inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
+//ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll gcd(ll a, ll b) { while (b) { ll tmp = a % b; a = b; b = tmp; } return a; }
+
+int N, M, T, Q;
+struct Pos3D {
+	ll x, y, z;
+	Pos3D(ll X = 0, ll Y = 0, ll Z = 0) : x(X), y(Y), z(Z) {}
+	bool operator == (const Pos3D& p) const { return x == p.x && y == p.y && z == p.z; }
+	bool operator != (const Pos3D& p) const { return x != p.x || y != p.y || z != p.z; }
+	bool operator < (const Pos3D& p) const { return x == p.x ? y == p.y ? z < p.z : y < p.y : x < p.x; }
+	ll operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
+	Pos3D operator / (const Pos3D& p) const {
+		Pos3D ret;
+		ret.x = y * p.z - z * p.y;
+		ret.y = z * p.x - x * p.z;
+		ret.z = x * p.y - y * p.x;
+		return ret;
+	}
+	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
+	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
+	Pos3D operator * (const ll& n) const { return { x * n, y * n, z * n }; }
+	Pos3D operator / (const ll& n) const { return { x / n, y / n, z / n }; }
+	Pos3D& operator += (const Pos3D& p) { x += p.x; y += p.y; z += p.z; return *this; }
+	Pos3D& operator -= (const Pos3D& p) { x -= p.x; y -= p.y; z -= p.z; return *this; }
+	Pos3D& operator *= (const ll& n) { x *= n; y *= n; z *= n; return *this; }
+	Pos3D& operator /= (const ll& n) { x /= n; y /= n; z /= n; return *this; }
+	ll Euc() const { return x * x + y * y + z * z; }
+	ld mag() const { return sqrtl(Euc()); }
+	friend std::istream& operator >> (std::istream& is, Pos3D& p) { is >> p.x >> p.y >> p.z; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos3D& p) { os << p.x << " " << p.y << " " << p.z << "\n"; return os; }
+} candi[LEN];
+const Pos3D O3D = { 0, 0, 0 };
+const Pos3D X_axis = { 1, 0, 0 };
+const Pos3D Y_axis = { 0, 1, 0 };
+const Pos3D Z_axis = { 0, 0, 1 };
+const Pos3D MAXP3D = { INF, INF, INF };
+typedef std::vector<Pos3D> Polygon3D;
+typedef std::vector<Polygon3D> Polyhedron;
+bool query() {
+	std::cin >> N;
+	int cnt = 0;
+	if (!N) return 0;
+	Polyhedron H(N);
+	for (int i = 0; i < N; i++) {
+		Polygon3D& C = H[i];
+		C.resize(3);
+		for (Pos3D& p : C) std::cin >> p;
+	}
+	std::cout << cnt << "\n";
+	return 1;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	while (query());
+	return;
+}
+int main() { solve(); return 0; }//boj4474
