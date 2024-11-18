fac5d8a39a9e05a0acdaa2b2bd13657369f0e3e2
crimson <redcube231@gmail.com>
2024-11-18 17:29:24 +0900

    24

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -7,23 +7,29 @@
 #include <cassert>
 typedef long long ll;
 typedef double ld;
+typedef std::vector<int> Vint;
 const int LEN = 1e6 + 1;
 int sq(int x) { return x * x; }
 ld sq(ld x) { return x * x; }
 
-int N, M[100], T, D;
-
+int N, a, b, c;
+Vint v;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(7);
 	std::cin >> N;
-	int s = 5;
-	for (int i = 2; i <= N; i++) {
-		s = s + i * 3 + 1;
-		s %= 45678;
+	v.resize(3);
+	for (int i = 1; i <= N; i++) {
+		std::cin >> v[0] >> v[1] >> v[2];
+		std::sort(v.begin(), v.end());
+		a = v[0], b = v[1], c = v[2];
+		std::cout << "Case #" << i << ": ";
+		if (a + b <= c) std::cout << "invalid!\n";
+		else if (a == b == c)std::cout << "equilateral\n";
+		else if (a == b || b == c) std::cout << "isosceles\n";
+		else std::cout << "scalene\n";
 	}
-	std::cout << s << "\n";
 	return 0;
 }
