beaca97da3d24041aac84b4db6a5ed30aff33f12
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-11-27 21:16:52 +0900

    2411271943

--- Cpp/boj_solve/boj4026_bi_search.cpp
+++ Cpp/boj_solve/boj4026_bi_search.cpp
@@ -119,7 +119,11 @@ ld bi_search(Polygon H, ld& h, ld& w) {
 			rotate(C.begin(), C.begin() + i, C.end());
 			//std::cout << C[0] << "\n";
 			//for (Pos& p : C) p -= v;
-			ld t = -1e-4 + std::min(rad(C[1] - C[0], C[2] - C[0]), PI * .5 - rad(C[2] - C[0], C[3] - C[0]));
+			ld t1 = rad(C[1] - C[0], C[2] - C[0]);
+			ld t2 = PI * .5 - rad(C[2] - C[0], C[3] - C[0]);
+			ld t3 = rad(C[3] - C[2], C[0] - C[2]);
+			ld t4 = PI * .5 - rad(C[0] - C[2], C[1] - C[2]);
+			ld t = -1e-4 + std::min({ t1, t2, t3, t4 });
 			//assert(t >= 0);
 			ld s = (C[2] - C[0]).rad(), e = s - t;
 			int cnt = 35; while (cnt--) {
@@ -148,8 +152,8 @@ ld ratio(const Pos& p1, const Pos& p2, const ld& t, ld& h, ld& w) {
 	Pos ho = Pos(1, 0).rot(t);
 	Pos ve = ~ho;
 	Pos b1 = intersection(O, ho, p1, p1 + ve);
-	Pos b2 = intersection(p2, p2 + ho, b1, b1 + ve);
-	if (ccw(p2, b2, p1) >= 0) return -1;
+	Pos b2 = intersection(p2, p2 + ho, p1, p1 + ve);
+	if (ccw(p2, b2, p1) >= 0) return 1.1;
 	h = b1.mag();
 	w = (b1 - b2).mag();
 	return h / w;
@@ -184,9 +188,9 @@ void query(int tc) {
 		return;
 	}
 	ld d1 = v1.Euc(), d2 = v2.Euc();
-	if (d2 < d1) std::swap(d1, d2); 
-	if (sign(d1 - (d1 + d1)) >= 0) { std::cout << "too long\n"; }
-	if (sign(d1 - (d1 + d1)) >= 0) { ans.push_back(-1); return; }
+	if (d2 < d1) std::swap(d1, d2);
+	if (sign(d2 - (d1 + d1)) >= 0) { std::cout << "too long\n"; }
+	if (sign(d2 - (d1 + d1)) >= 0) { ans.push_back(-1); return; }
 	ld t = std::abs(rad(v1, v2));
 	t = std::min(t, PI - t);
 	ld l1 = v1.mag(), l2 = v2.mag();
@@ -201,7 +205,6 @@ void query(int tc) {
 	//bool f1 = 0;
 	//if (dot(inx, p2, p1) > 0 && )
 	ld s = 0, e = -(PI * .5 - t), m;
-	//s -= TOL; e += TOL;
 	int cnt = 35;
 	while (cnt--) {
 		m = (s + e) * .5;
@@ -212,12 +215,29 @@ void query(int tc) {
 	//if (eq(h, w)) std::cout << w + 10 << "\n";
 	//else std::cout << "no solution\n";
 	std::cout << h << " " << w << "\n";
+	bool f2 = 1;
+	for (int i = 0; i < 4; i++) {
+		Pos u = P[(i + 1) % 4] - P[i];
+		Pos v = ~u;
+		int R = 0, U = 0, L = 0;
+		for (int j = i; j < 4; j++) if (sign(dot(P[i], P[(i + 1) % 4], P[j % 4], P[(j + 1) % 4])) <= 0) { R = j; break; }
+		for (int k = R; k < R + 4; k++) if (ccw(P[i], P[(i + 1) % 4], P[k % 4], P[(k + 1) % 4]) <= 0) { U = k; break; }
+		for (int l = U; l <= U + 4; l++) if (sign(dot(P[i], P[(i + 1) % 4], P[l % 4], P[(l + 1) % 4])) >= 0) { L = l; break; }
+		R %= 4;	U %= 4; L %= 4;
+		Pos a0 = intersection(P[i], P[i] + u, P[R], P[R] + v);
+		Pos a1 = intersection(P[R], P[R] + v, P[U], P[U] + u);
+		Pos a2 = intersection(P[U], P[U] + u, P[L], P[L] + v);
+		Pos a3 = intersection(P[L], P[L] + v, P[i], P[i] + u);
+		ld b1 = (a0 - a3).mag();
+		ld b2 = (a0 - a1).mag();
+		std::cout << "h:: " << h << " w:: " << w << " b1:: " << b1 << " b2:: " << b2 << "\n";
+		if (eq(h, w) && eq(b1, b2) && eq(b1, h)) f2 = 0;
+	}
+	if (f2 && eq(h, w)) { ans.push_back(h + 10); return; }
+	h = -1, w = -2;
+	bi_search(P, h, w);
+	std::cout << h << " " << w << "\n";
 	if (eq(h, w)) { ans.push_back(h + 10); return; }
-	//if (!f0) {
-		bi_search(P, h, w);
-		std::cout << h << " " << w << "\n";
-		if (eq(h, w)) { ans.push_back(h + 10); return; }
-	//}
 	ans.push_back(-1);
 	return;
 }
