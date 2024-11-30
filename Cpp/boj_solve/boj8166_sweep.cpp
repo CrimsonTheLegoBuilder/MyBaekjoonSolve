3ea08db2c7c9d34ad76c6a81666b4e1481ee13c4
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-12-01 02:26:35 +0900

    24

---
+++ Cpp/boj_solve/boj8166_sweep.cpp
@@ -0,0 +1,93 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+typedef std::vector<ll> Vll;
+typedef std::vector<ld> Vld;
+const ld INF = 1e17;
+const ld TOL = 1e-9;
+const ld PI = acosl(-1);
+const int LEN = 3005;
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+inline bool eq(const ld& x, const ld& y) { return zero(x - y); }
+inline ld sq(const ld& x) { return x * x; }
+inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
+
+int N, M, F;
+struct Pos {
+	ll x, y;
+	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const int& n) { x *= n; y *= n; return *this; }
+	Pos& operator /= (const int& n) { x /= n; y /= n; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	int Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return y > 0 || y == 0 && x >= 0; }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+} A[LEN]; const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+ll cross(const Pos& p1, const Pos& p2, const Pos& p3) { return (p2 - p1) / (p3 - p2); }
+bool cmp(const Pos& p, const Pos& q) {
+	bool f1 = O < p;
+	bool f2 = O < q;
+	if (f1 != f2) return f1;
+	ll ccw = p / q;
+	return ccw ? ccw > 0 : p.Euc() < q.Euc();
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cin >> N;
+	Polygon H(N);
+	for (Pos& p : H) std::cin >> p;
+	if (N == 3) {
+		ll a = std::abs(cross(H[0], H[1], H[2]));
+		std::cout << (a >> 1) << "." << (a & 1 ? 5 : 1) << "\n";
+		return;
+	}
+	ll T = 0;
+	for (int i = 0; i < N; i++) {
+		Polygon P;
+		for (int j = 0; j < N; j++) if (j != i) P.push_back(H[j] - H[i]);
+		std::sort(P.begin(), P.end(), cmp);
+		M = N - 1;
+		for (int j = 1; j <= M; j++) A[j] = A[j - 1] + P[j - 1];
+		for (int j = 0, k = 1; j < M; j++) {
+			while (P[j] / P[k] > 0) k = (k + 1) % M;
+			Pos v = A[(k - 1) % M] - A[j];
+			if (k < j) v += A[M];
+			T += -P[j] / v;
+		}
+	}
+	std::cout << (T >> 1) << "." << (T & 1 ? 5 : 0) << "\n";
+	return;
+}
+int main() { solve(); return 0; }//boj4039
