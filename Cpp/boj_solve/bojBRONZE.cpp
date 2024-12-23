b2b2c3a6c6bf44df8a5417a682b7f6db5ae3870e
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-12-23 21:55:39 +0900

    24

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -10,26 +10,16 @@ typedef long double ld;
 const int LEN = 1e6 + 1;
 ll sq(ll x) { return x * x; }
 
-int N, Q, ret[LEN];
-ll X[LEN], Y[LEN], Z[LEN];
+int N, X, Y;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(3);
 	std::cin >> N;
-	for (int i = 0; i < N; i++) std::cin >> X[i] >> Y[i] >> Z[i];
-	std::cin >> Q;
-	for (int q = 0; q < Q; q++) {
-		ll x, y, z, r;
-		std::cin >> x >> y >> z >> r;
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			ll d = sq(x - X[i]) + sq(y - Y[i]) + sq(z - Z[i]);
-			cnt += d <= sq(r);
-		}
-		ret[q] = cnt;
+	while (N--) {
+		scanf("%d,%d", &X, &Y);
+		//printf("%d\n", X + Y);
 	}
-	//for (int q = 0; q < Q; q++) std::cout << ret[q] << "\n";
 	return 0;
 }
