4d1a8b4076d2bfbe66c19f45c2add35d0227e7a3
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2024-12-29 22:05:50 +0900

    24

--- Cpp/boj_solve/boj12620_lights.cpp
+++ Cpp/boj_solve/boj12620_lights.cpp
@@ -21,7 +21,7 @@ typedef std::vector<ld> Vld;
 typedef std::vector<bool> Vbool;
 const ld INF = 1e17;
 const ld TOL = 1e-10;
-const int LEN = 1e5 + 5;
+const int LEN = 55;
 const ld PI = acos(-1);
 inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
@@ -33,6 +33,8 @@ inline int128 sq(const int& x) { return (int128)x * x; }
 inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
 #define LINE 1
 #define CIRCLE 2
+#define STRONG 0
+#define WEAK 1
 #define BLACK 0
 #define RED 1
 #define GREEN 2
@@ -70,6 +72,7 @@ struct Pos {
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 } R, G;
 typedef std::vector<Pos> Polygon;
+Polygon TR[LEN * LEN], TG[LEN * LEN];
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
 ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
@@ -79,6 +82,28 @@ bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, 
 bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return !ccw(d1, d2, d3) && !ccw(d1, d2, d4); }
 ld projection(const Pos& d1, const Pos& d2, const Pos& d3) { return dot(d1, d2, d3) / (d1 - d2).mag(); }
 Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) { ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2); return (p1 * a2 + p2 * a1) / (a1 + a2); }
+bool inside(const Pos& p0, const Pos& p1, const Pos& p2, const Pos& q, const int& f = STRONG) {
+	if (ccw(p0, p1, p2) < 0) return ccw(p0, p1, q) >= f || ccw(p1, p2, q) >= f;
+	return ccw(p0, p1, q) >= f && ccw(p1, p2, q) >= f;
+}
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	return f1 && f2;
+	//bool f3 = on_seg_strong(s1, s2, d1) ||
+	//	on_seg_strong(s1, s2, d2) ||
+	//	on_seg_strong(d1, d2, s1) ||
+	//	on_seg_strong(d1, d2, s2);
+	//return (f1 && f2) || f3;
+}
+bool inner_check(const Polygon& H, const Pos& q) {
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		int j = (i + sz) % sz;
+		if (ccw(H[i], H[j], q) < 0) return 0;
+	}
+	return 1;
+}
 struct Seg {
 	Pos s, e;
 	Seg(Pos s_ = Pos(), Pos e_ = Pos()) : s(s_), e(e_) {}
@@ -90,8 +115,10 @@ struct Seg {
 		return m.y * d * (s.x - e.x);
 	}
 };
+Seg SR[LEN], SG[LEN];
 ld dot(const Seg& p, const Seg& q) { return dot(p.s, p.e, q.s, q.e); }
 bool collinear(const Seg& p, const Seg& q) { return collinear(p.s, p.e, q.s, q.e); }
+bool intersect(const Seg& u, const Seg& v) { return intersect(u.s, u.e, v.s, v.e); }
 struct Circle {
 	Pos c;
 	int r;
@@ -114,6 +141,7 @@ struct Circle {
 	inline friend std::ostream& operator << (std::ostream& os, const Circle& c) { os << c.c << " " << c.r; return os; }
 };
 typedef std::vector<Circle> Circles;
+Circles C;
 Vld circle_line_intersections(const Circle& q, const Seg& l, const int& t = LINE) {
 	//https://math.stackexchange.com/questions/311921/get-location-of-vector-circle-intersection
 	Pos s = l.s, e = l.e;
@@ -143,23 +171,41 @@ Vld circle_line_intersections(const Circle& q, const Seg& l, const int& t = LINE
 	}
 	return ret;
 }
-ld circle_cut(const Circle& c, const Seg& s) {
-	Pos v1 = s.s - c.c, v2 = s.e - c.c;
-	ll r = c.r;
-	//Vld inx = circle_line_intersections(c, s, CIRCLE);
-	Vld inx = circle_line_intersections(c, s, LINE);
-	if (inx.empty()) return r * r * rad(v1, v2) * .5;
-	Pos m1, m2;
-	if (inx.size() == 2) m1 = s.p(inx[0]), m2 = s.p(inx[1]);
-	else m1 = m2 = c.p(inx[0]);
-	m1 -= c.c; m2 -= c.c;
-	bool d1 = dot(m1, v1, m2) > -TOL, d2 = dot(m1, v2, m2) > -TOL;
-	if (d1 && d2) return (v1 / v2) * .5;
-	else if (d1) return (v1 / m2 + r * r * rad(m2, v2)) * .5;
-	else if (d2) return (r * r * rad(v1, m1) + m1 / v2) * .5;
-	else if (dot(v1, m1, v2) > 0 && dot(v1, m2, v2) > 0)
-		return (r * r * (rad(v1, m1) + rad(m2, v2)) + m1 / m2) * .5;
-	else return (r * r * rad(v1, v2)) * .5;
+ld intersection(const Seg& s1, const Seg& s2) {
+	const Pos& p1 = s1.s, p2 = s1.e, q1 = s2.s, q2 = s2.e;
+	ld det = (q2 - q1) / (p2 - p1);
+	if (zero(det)) return -1;
+	ld a1 = ((q2 - q1) / (q1 - p1)) / det;
+	ld a2 = ((p2 - p1) / (p1 - q1)) / -det;
+	if (0 < a1 && a1 < 1 && -TOL < a2 && a2 < 1 + TOL) return a1;
+	return -1;
+}
+Vld circle_line_intersections(const Seg& l, const Circle& q, const int& t = LINE) {
+	//https://math.stackexchange.com/questions/311921/get-location-of-vector-circle-intersection
+	Pos s = l.s, e = l.e;
+	Pos vec = e - s;
+	Pos OM = s - q.c;
+	ld a = vec.Euc();
+	ld b = vec * OM;
+	ld c = OM.Euc() - q.r * q.r;
+	ld J = b * b - a * c;
+	if (J < -TOL) return {};
+	ld det = sqrt(std::max((ld)0, J));
+	ld lo = (-b - det) / a;
+	ld hi = (-b + det) / a;
+	Vld ret;
+	if (t == LINE) {
+		if (0 < lo && lo < 1) ret.push_back(lo);
+		if (zero(det)) return ret;
+		if (0 < hi && hi < 1) ret.push_back(hi);
+	}
+	else {//circle
+		auto the = [&](ld rt) { return q.rad(s + (e - s) * rt); };
+		if (-TOL < lo && lo < 1 + TOL) ret.push_back(the(lo));
+		if (zero(det)) return ret;
+		if (-TOL < hi && hi < 1 + TOL) ret.push_back(the(hi));
+	}
+	return ret;
 }
 struct Arc {
 	ld lo, hi;
@@ -180,13 +226,35 @@ Vld tangents(const Pos& p, const Circle& c, Polygon& vp, const bool& f = 0) {
 	if (f) vp.push_back(c.p(lo)), vp.push_back(c.p(hi));
 	return { lo, hi };
 }
+Pos get_pos(const Pos& l, const Seg& p, const Seg& q) {
+	Pos r;
+	Pos p1 = p.s, p2 = p.e;
+	Pos q1 = q.s, q2 = q.e;
+
+	return r;
+}
 void query(const int& q) {
 	std::cin >> R >> G;
 	std::cin >> N;
-	Circles C;
+	Polygon B = { Pos(0, 0), Pos(100, 0), Pos(100, 100), Pos(0, 100) };
 	for (Circle& c : C) std::cin >> c;
 	for (int i = 0; i < N; i++) {
+		Polygon V, vp;
+		Pos s, e;
+		tangents(R, C[i], vp, 1);
+		s = vp[1], e = vp[1];
+		SR[i] = Seg(s, e);
+		vp.clear();
+		tangents(G, C[i], vp, 1);
+		s = vp[1], e = vp[1];
+		SG[i] = Seg(s, e);
+	}
+	for (int i = 0; i < N; i++) {
+		Polygon VR, VG;
+		for (int j = 0; j < N; j++) {
+			if (i == j) continue;
 
+		}
 	}
 }
 void solve() {
