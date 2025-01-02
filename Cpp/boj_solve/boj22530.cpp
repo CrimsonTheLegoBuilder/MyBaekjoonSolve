43f06a3a4c09883f7fe8500e76cb12f9ee5155ab
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-01-03 00:06:43 +0900

    2501030006

--- Cpp/boj_solve/boj22530.cpp
+++ Cpp/boj_solve/boj22530.cpp
@@ -9,6 +9,7 @@
 #include <array>
 #include <tuple>
 #include <deque>
+#include <queue>
 #include <iomanip>
 typedef long long ll;
 //typedef long double ld;
@@ -19,8 +20,8 @@ typedef std::vector<int> Vint;
 typedef std::vector<ld> Vld;
 typedef std::vector<bool> Vbool;
 const ld INF = 1e17;
-const ld TOL = 1e-10;
-const int LEN = 55;
+const ld TOL = 1e-7;
+const int LEN = 25;
 const ld PI = acos(-1);
 inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
@@ -37,10 +38,11 @@ inline ld fit(const ld& x, const ld& lo, const ld& hi) { return std::min(hi, std
 #define LO x
 #define HI y
 
-int N, M, T, Q;
+int N, M, T;
 struct Pos {
 	ld x, y;
-	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) {}
+	int i;
+	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) { i = -1; }
 	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
 	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
 	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
@@ -72,16 +74,20 @@ struct Pos {
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
+bool cmpt(const Pos& p, const Pos& q) {
+	bool f1 = O < p;
+	bool f2 = O < q;
+	if (f1 != f2) return f1;
+	return p / q > 0;
+}
 typedef std::vector<Pos> Polygon;
 bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
 bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
 //bool cmpi(const Pos& p, const Pos& q) { return p.i < q.i; }
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3) {
-	ld ret = cross(d1, d2, d3);
-	return zero(ret) ? 0 : ret > 0 ? 1 : -1;
-}
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return sign(cross(d1, d2, d3, d4)); }
 ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) {
@@ -106,10 +112,10 @@ bool inside(const Pos& p0, const Pos& p1, const Pos& p2, const Pos& q, const int
 bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2) {
 	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
 	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
-	bool f3 = on_seg_strong(s1, s2, d1) ||
-		on_seg_strong(s1, s2, d2) ||
-		on_seg_strong(d1, d2, s1) ||
-		on_seg_strong(d1, d2, s2);
+	bool f3 = on_seg_weak(s1, s2, d1) ||
+		on_seg_weak(s1, s2, d2) ||
+		on_seg_weak(d1, d2, s1) ||
+		on_seg_weak(d1, d2, s2);
 	return (f1 && f2) || f3;
 }
 ld area(const Polygon& H) {
@@ -117,6 +123,54 @@ ld area(const Polygon& H) {
 	ld a = 0;
 	for (int i = 0; i < sz; i++) a += H[i] / H[(i + 1) % sz];
 	return a * .5;
+}
+bool inner_check(const Polygon& H, const Pos& p) {
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) if (ccw(H[i], H[(i + 1) % sz], p) <= 0) return 0;
+	return 1;
+}
+int inner_check_concave(const Polygon& H, const Pos& p) {//concave
+	int cnt = 0, sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		Pos cur = H[i], nxt = H[(i + 1) % sz];
+		if (on_seg_strong(cur, nxt, p)) return 1;
+		if (zero(cur.y - nxt.y)) continue;
+		if (nxt.y < cur.y) std::swap(cur, nxt);
+		if (nxt.y - TOL < p.y || cur.y > p.y) continue;
+		cnt += ccw(cur, nxt, p) > 0;
+	}
+	return (cnt & 1) * 2;
+}
+bool cross_check(const Polygon& H, const Pos& s, const Pos& e) {
+	if (inner_check(H, s)) return 1;
+	if (inner_check(H, e)) return 1;
+	int sz = H.size();
+	assert(sz == 4);
+	bool f0 = intersect(s, e, H[0], H[1]);
+	bool f1 = intersect(s, e, H[1], H[2]);
+	bool f2 = intersect(s, e, H[2], H[3]);
+	bool f3 = intersect(s, e, H[3], H[0]);
+	return (f0 + f1 + f2 + f3) > 1;
+}
+Polygon convex_cut(const Polygon& ps, const Pos& b1, const Pos& b2) {
+	Polygon qs;
+	int n = ps.size();
+	for (int i = 0; i < n; i++) {
+		Pos p1 = ps[i], p2 = ps[(i + 1) % n];
+		int d1 = ccw(b1, b2, p1), d2 = ccw(b1, b2, p2);
+		if (d1 >= 0) qs.push_back(p1);
+		if (d1 * d2 < 0) qs.push_back(intersection(p1, p2, b1, b2));
+	}
+	return qs;
+}
+Polygon sutherland_hodgman(const Polygon& C, const Polygon& clip) {
+	int sz = clip.size();
+	Polygon ret = C;
+	for (int i = 0; i < sz; i++) {
+		Pos b1 = clip[i], b2 = clip[(i + 1) % sz];
+		ret = convex_cut(ret, b1, b2);
+	}
+	return ret;
 }
 struct Seg {
 	Pos s, e;
@@ -128,6 +182,16 @@ struct Seg {
 		Pos m = p(ratio);
 		return m.y * d * (s.x - e.x);
 	}
+	Seg operator - (const ld& n) const  {
+		Pos v = ~(e - s).unit();
+		return { s + v * n, e + v * n };
+	}
+	Seg& operator -= (const ld& n) {
+		Pos v = ~(e - s).unit();
+		s += v * n; e += v * n;
+		return *this;
+	}
+	ld mag() const { return (e - s).mag(); }
 };
 Seg S[4][LEN];
 typedef std::vector<Seg> Segs;
@@ -167,7 +231,7 @@ struct Circle {
 	}
 	inline friend std::istream& operator >> (std::istream& is, Circle& c) { is >> c.c >> c.r; return is; }
 	inline friend std::ostream& operator << (std::ostream& os, const Circle& c) { os << c.c << " " << c.r; return os; }
-};
+} C0;
 typedef std::vector<Circle> Circles;
 Circles C;
 Vld circle_line_intersections(const Circle& q, const Seg& l, const int& t = LINE) {
@@ -185,11 +249,11 @@ Vld circle_line_intersections(const Circle& q, const Seg& l, const int& t = LINE
 	ld hi = (-b + det) / a;
 	Vld ret;
 	if (t == LINE) {
-		//if (0 < lo && lo < 1) ret.push_back(lo);
-		//if (zero(det)) return ret;
-		//if (0 < hi && hi < 1) ret.push_back(hi);
-		ret.push_back(lo);
-		ret.push_back(hi);
+		if (0 < lo && lo < 1) ret.push_back(lo);
+		if (zero(det)) return ret;
+		if (0 < hi && hi < 1) ret.push_back(hi);
+		//ret.push_back(lo);
+		//ret.push_back(hi);
 	}
 	else {//circle
 		auto the = [&](ld rt) { return q.rad(s + (e - s) * rt); };
@@ -200,4 +264,283 @@ Vld circle_line_intersections(const Circle& q, const Seg& l, const int& t = LINE
 		//ret.push_back(the(hi));
 	}
 	return ret;
+}
+Pos VX[LEN * LEN * LEN]; int vp;
+Polygon ROT[LEN];
+struct Pii {
+	int i, j;
+	bool operator < (const Pii& o) const { return i == o.i ? j < o.j : i < o.i; }
+	bool operator == (const Pii& o) const { return i == o.i && j == o.j; }
+};
+std::vector<Pii> VP;
+struct Info {
+	int i;
+	int s1, e1, s2, e2;
+	Pii p1() const { return { s1, e1 }; }
+	Pii p2() const { return { s2, e2 }; }
+};
+Info ND[LEN * LEN * LEN]; int np;
+Vint G[LEN * LEN * LEN];
+bool V[LEN * LEN * LEN];
+void bfs() {
+	std::queue<int> Q;
+	memset(V, 0, sizeof V);
+	Q.push(0);
+	while (Q.size()) {
+		int p = Q.front(); Q.pop();
+		for (const int& w : G[p]) {
+			if (!V[w]) {
+				V[w] = 1;
+				Q.push(w);
+				Pii se;
+				se = ND[w].p1();
+				if (se.i != -1) VP.push_back(se);
+				se = ND[w].p2();
+				if (se.i != -1) VP.push_back(se);
+			}
+		}
+	}
+	return;
+}
+bool inside(const Pos& p0, const Pos& p1, const Pos& p2, const int& r, const Pos& q) {
+	Circle c = Circle(p1, r);
+	bool f1 = c >= q;
+	bool f2 = inside(p0, p1, p2, q);
+	return f1 && f2;
+}
+bool connectable(const Polygon& H, const Pos& s, const Pos& e, const int& r, const int& idx = -1, const int& f = LINE) {
+	if (s == e) return 1;
+	if (f == LINE) {
+		Pos v = ~(e - s).unit() * r;
+		Polygon clip = { s + v, s - v, e - v, e + v };
+		Polygon cut = sutherland_hodgman(H, clip);
+		return eq(area(clip), area(cut));
+	}
+	else if (f == CIRCLE) {
+		int sz = H.size();
+		if (s.i == -1 || e.i == -1) return 0;
+		const Pos& p0 = H[(idx - 1) & sz], & p1 = H[idx], & p2 = H[(idx + 1) % sz];
+		Circle c = Circle(p1, r);
+		ld lo = (s - p1).rad();
+		ld hi = (s - p2).rad();
+		ld m = (lo + hi) * .5;
+		Pos mid = c.p(m);
+		if (!inside(p0, p1, p2, mid)) return 0;
+		for (int i = 0; i < sz; i++) {
+			if ((i - 1 + sz) % sz == idx || i == idx || (i + 1) % sz == idx) continue;
+			Vld inxs = circle_line_intersections(c, Seg(H[i], H[(i + 1) % N]), CIRCLE);
+			if (inxs.size() < 2) continue;
+			Pos q1 = inxs[0];
+			Pos q2 = inxs[0];
+			if (inside(e, p1, s, r, q1) && inside(e, p1, s, r, q2)) return 0;
+		}
+		return 1;
+	}
+	return 1;
+}
+bool valid_check(const Polygon& H, const Circle& c) {
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		const Pos& p0 = H[i], & p1 = H[(i + 1) % sz];
+		if (c > p0) return 0;
+		Vld inxs = circle_line_intersections(c, Seg(p0, p1), CIRCLE);
+		if (inxs.size() > 1) return 0;
+	}
+	if (!inner_check_concave(H, c.c)) return 0;
+	return 1;
+}
+Polygon get_node(const Polygon& H, const int& r, const int& i, int& tq) {
+	const Pos& p0 = H[(i - 1 + N) % N], & p1 = H[i], & p2 = H[(i + 1) % N];
+	tq = ccw(p0, p1, p2);
+	if (tq > 0) return {};
+	Polygon ret;
+	Pos v;
+	v = -~(p1 - p0).unit() * r;
+	ret.push_back(p1 + v);
+	v = -~(p2 - p1).unit() * r;
+	ret.push_back(p1 + v);
+	return ret;
+}
+Pos get_node(const Seg& s1, const Seg& s2, const int& r) {
+	Seg e1 = s1 - r;
+	Seg e2 = s2 - r;
+	return intersection(e1, e2);
+}
+Polygon get_node(const Pos& p1, const Pos& p2, const int& r) {
+	Pos v = p2 - p1;
+	ld h = v.mag() * .5;
+	ld w = sqrt(r * r - h * h);
+	Pos m = (p1 + p2) * .5;
+	v = v.unit() * w;
+	return { m + v, m - v };
+}
+Polygon get_node(const Seg& s, const Pos& p, const int& r) {
+	if (ccw(s.s, s.e, p) <= 0) return {};
+	ld d = cross(s.s, s.e, p) / s.mag();
+	if (d >= r * 2) return {};
+	Pos v = s.e - s.s;
+	Polygon ret;
+	if (eq(d, r)) {
+		v = v.unit() * r;
+		Pos c1 = p + v, c2 = p - v;
+		if (dot(s.s, s.e, c1) < 0 && dot(s.e, s.s, c1) < 0) ret.push_back(c1);
+		if (dot(s.s, s.e, c2) < 0 && dot(s.e, s.s, c2) < 0) ret.push_back(c2);
+	}
+	if (d < r) {
+		ld h = r - d;
+		ld w = sqrt(r * r - h * h);
+		v = v.unit() * w;
+		Pos c1 = p + v, c2 = p - v;
+		if (dot(s.s, s.e, c1) < 0 && dot(s.e, s.s, c1) < 0) ret.push_back(c1);
+		if (dot(s.s, s.e, c2) < 0 && dot(s.e, s.s, c2) < 0) ret.push_back(c2);
+	}
+	if (d > r) {
+		ld h = d - r;
+		ld w = sqrt(r * r - h * h);
+		v = v.unit() * w;
+		Pos c1 = p + v, c2 = p - v;
+		if (dot(s.s, s.e, c1) < 0 && dot(s.e, s.s, c1) < 0) ret.push_back(c1);
+		if (dot(s.s, s.e, c2) < 0 && dot(s.e, s.s, c2) < 0) ret.push_back(c2);
+	}
+	return ret;
+}
+void connect(const Polygon& H, const int& r) {
+	assert(vp == np);
+	for (int i = 0; i < np; i++) {
+		for (int j = 0; j < np; j++) {
+			if (i == j) continue;
+			const Pos& s = VX[i];
+			const Pos& e = VX[j];
+			if (connectable(H, s, e, r)) {
+				G[i].push_back(j);
+				G[j].push_back(i);
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		Pos cen = H[i];
+		for (Pos& p : ROT[i]) p -= cen;
+		std::sort(ROT[i].begin(), ROT[i].end(), cmpt);
+		int sz = ROT[i].size();
+		for (int j = 0; j < sz; j++) {
+			const Pos& s = ROT[i][j], & e = ROT[i][(j + 1) % sz];
+			if (connectable(H, s, e, r, i, CIRCLE)) {
+				G[i].push_back(j);
+				G[j].push_back(i);
+			}
+		}
+	}
+	VP.clear();
+	bfs();
+	return;
+}
+ld green(const Polygon& H) {
+	std::sort(VP.begin(), VP.end());
+	VP.erase(unique(VP.begin(), VP.end()), VP.end());
+	int sz = VP.size();
+	for (int i = 0; i < sz; i++) {
+		const Pii& se0 = VP[(i - 1 + sz) % sz];
+		const Pii& se1 = VP[i];
+		const Pii& se2 = VP[(i + sz) % sz];
+		
+	}
+}
+bool query() {
+	int x, y, r;
+	std::cin >> N >> x >> y >> r;
+	if (!N && !x && !y && !r) return 0;
+	vp = 0; np = 0;
+	C0 = Circle(Pos(x, y), r);
+	VX[vp++] = Pos(x, y);
+	ND[np++] = { 0, -1, -1, -1, -1 };
+	Polygon H(N);
+	for (Pos& p : H) std::cin >> p;
+	Vbool F(N, 0);
+	for (int i = 0; i < N; i++) {
+		const Pos& p0 = H[(i - 1 + N) % N], & p1 = H[i], & p2 = H[(i + 1) % N];
+		int tq = ccw(p0, p1, p2);
+		if (!tq) F[i] = 1;
+	}
+	Polygon tmp;
+	for (int i = 0; i < N; i++) if (!F[i]) tmp.push_back(H[i]);
+	H = tmp;
+	N = H.size();
+	for (int i = 0; i < N; i++) {//node init
+		Polygon inxs;
+		Pos inx;
+		int tq;
+		inxs = get_node(H, r, i, tq);
+		if (tq < 0) {
+			assert(inxs.size() == 2);
+			Pos c1 = inxs[0];
+			if (valid_check(H, Circle(c1, r))) {
+				c1.i = np; ROT[i].push_back(c1);
+				VX[vp++] = c1;
+				ND[np] = { np, (i - 1 + N) % N, i, i, -1 }; np++;
+			}
+			else { c1.i = -1; ROT[i].push_back(c1); }
+			Pos c2 = inxs[0];
+			if (valid_check(H, Circle(c2, r))) {
+				c2.i = np; ROT[i].push_back(c2);
+				VX[vp++] = c2;
+				ND[np] = { np, i, i + 1, i, -1 }; np++;
+			}
+			else { c2.i = -1; ROT[i].push_back(c2); }
+		}
+		Pos i1 = H[i], i2 = H[(i + 1) % N];
+		Seg i12 = Seg(i1, i2);
+		for (int j = i + 1; j < N; j++) {
+			Pos j1 = H[j], j2 = H[(j + 1) % N];
+			if (i + 1 != j && j + 1 != i) {
+				inxs = get_node(i1, j1, r);
+				assert(inxs.size() == 2);
+				Pos c1 = inxs[0];
+				if (valid_check(H, Circle(c1, r))) {
+					c1.i = np;
+					ROT[i].push_back(c1);
+					ROT[j].push_back(c1);
+					VX[vp++] = c1;
+					ND[np] = { np, i, -1, j, -1 }; np++;
+				}
+				Pos c2 = inxs[0];
+				if (valid_check(H, Circle(c2, r))) {
+					c2.i = np; 
+					ROT[i].push_back(c2);
+					ROT[j].push_back(c2);
+					VX[vp++] = c2;
+					ND[np] = { np, i, -1, j, -1 }; np++;
+				}
+			}
+			Seg j12 = Seg(j1, j2);
+			inx = get_node(i12, j12, r);
+			if (valid_check(H, Circle(inx, r))) {
+				VX[vp++] = inx;
+				ND[np] = { np, i, i + 1, j, j + 1 }; np++;
+			}
+		}
+		for (int j = 0; j < N; j++) {
+			if (j == i || j == (i + 1) % N) continue;
+			Pos j1 = H[j];
+			inxs = get_node(i12, j1, r);
+			for (Pos& p : inxs) {
+				if (valid_check(H, Circle(p, r))) {
+					p.i = np;
+					ROT[j].push_back(p);
+					VX[vp++] = p;
+					ND[np] = { np, i, i + 1, j, -1 }; np++;
+				}
+			}
+		}
+	}
+	connect(H, r);
+	std::cout << green(H) << "\n";
+	return;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(9);
+	while (query());
+	return;
 }
