3d21f931c5efc7fc5c0ee58406fbdd563213f8af
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-01-11 22:54:56 +0900

    2401112254

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -9,15 +9,24 @@ typedef long long ll;
 typedef long double ld;
 const int LEN = 1e6 + 1;
 ll sq(ll x) { return x * x; }
+int diff(int x, int y) { return std::abs(x - y); }
 
-int X, Y;
+int N, P[11];
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(3);
-	std::cin >> X >> Y;
-	Y %= X;
-	//std::cout << X + Y << "\n";
-	return 0;
+	for (int i = 0; i < 10; i++) {
+		std::cin >> N;
+		for (int j = i; j < 10; j++) P[j] += N;
+	}
+	int df = 1e9, ret = 0;
+	for (int i = 0; i < 10; i++) {
+		int tmp = diff(P[i], 100);
+		if (tmp == df && ret < P[i]) df = tmp, ret = P[i];
+		else if (tmp < df) df = tmp, ret = P[i];
+
+	}
+	//std::cout << ret << "\n";
 }
