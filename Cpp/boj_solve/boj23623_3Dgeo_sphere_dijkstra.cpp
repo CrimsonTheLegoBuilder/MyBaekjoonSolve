878617ddff1437f39ed36168552d957fb1915190
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-01-11 17:24:24 +0900

    2401111724

--- Cpp/boj_solve/boj23623_3Dgeo_sphere_dijkstra.cpp
+++ Cpp/boj_solve/boj23623_3Dgeo_sphere_dijkstra.cpp
@@ -1,101 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <cmath>
 #include <cstring>
 #include <cassert>
+#include <vector>
 #include <queue>
-#include <iomanip>
 typedef long long ll;
 //typedef long double ld;
 typedef double ld;
-typedef std::pair<int, int> pi;
-typedef std::vector<size_t> Vidx;
-typedef std::vector<int> Vint;
-typedef std::vector<ld> Vld;
 typedef std::vector<bool> Vbool;
 const ld INF = 1e17;
-const ld TOL = 1e-7;
+const ld TOL = 1e-10;
 const ld PI = acos(-1);
-const int LEN = 5e3;
-inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
+const int LEN = 2e3;
 inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
 inline bool zero(const ld& x) { return !sign(x); }
 inline bool eq(const ld& x, const ld& y) { return zero(x - y); }
 inline ld sq(const ld& x) { return x * x; }
-inline ll sq(const ll& x) { return x * x; }
 inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
 inline ld fit(const ld& x, const ld& lo, const ld& hi) { return std::min(hi, std::max(lo, x)); }
-#define LINE 1
-#define CIRCLE 2
-#define STRONG 0
-#define WEAK 1
 #define LO x
 #define HI y
 
-int N, T, R;
+int N, R;
 struct Pos {
-	ld x, y;
-	int i;
-	Pos(ld X = 0, ld Y = 0) : x(X), y(Y) { i = -1; }
-	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
-	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
-	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
-	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
-	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
-	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ld& n) const { return { x * n, y * n }; }
-	Pos operator / (const ld& n) const { return { x / n, y / n }; }
-	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
+	ld x, y; int i;
+	Pos(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) { i = -1; }
+	bool operator < (const Pos& p) const { return eq(x, p.x) ? y < p.y : x < p.x; }
 	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
-	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const ld& n) { x *= n; y *= n; return *this; }
-	Pos& operator /= (const ld& n) { x /= n; y /= n; return *this; }
-	Pos operator - () const { return { -x, -y }; }
-	Pos operator ~ () const { return { -y, x }; }
-	Pos operator ! () const { return { y, x }; }
-	Pos rot(const ld& the) const { return { x * cos(the) - y * sin(the), x * sin(the) + y * cos(the) }; }
 	ld Euc() const { return x * x + y * y; }
-	ld mag() const { return sqrt(Euc()); }
-	Pos unit() const { return *this / mag(); }
 	ld rad() const { return norm(atan2(y, x)); }
-	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
-	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
-	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
 typedef std::vector<Pos> Polygon;
 Polygon ROT[25];
 bool cmpt(const Pos& p, const Pos& q) {
 	assert(eq(p.Euc(), q.Euc()));
-	bool f1 = O < p;
-	bool f2 = O < q;
-	if (f1 != f2) return f1;
-	return p / q > 0;
+	bool f1 = O < p, f2 = O < q;
+	return f1 == f2 ? p / q > 0 : f1;
 }
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return sign(cross(d1, d2, d3, d4)); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
 struct Pos3D {
 	ld x, y, z;
 	int r, i;
-	Pos3D(ld X = 0, ld Y = 0, ld Z = 0) : x(X), y(Y), z(Z) { r = 0; i = -1; }
-	bool operator == (const Pos3D& p) const { return zero(x - p.x) && zero(y - p.y) && zero(z - p.z); }
-	bool operator != (const Pos3D& p) const { return !zero(x - p.x) || !zero(y - p.y) || !zero(z - p.z); }
-	bool operator < (const Pos3D& p) const { return zero(x - p.x) ? zero(y - p.y) ? z < p.z : y < p.y : x < p.x; }
+	Pos3D(ld x_ = 0, ld y_ = 0, ld z_ = 0) : x(x_), y(y_), z(z_) { r = 0; i = -1; }
+	bool operator == (const Pos3D& p) const { return eq(x, p.x) && eq(y, p.y) && eq(z, p.z); }
+	bool operator < (const Pos3D& p) const { return eq(x, p.x) ? eq(y, p.y) ? z < p.z : y < p.y : x < p.x; }
 	ld operator * (const Pos3D& p) const { return x * p.x + y * p.y + z * p.z; }
-	Pos3D operator / (const Pos3D& p) const {
-		Pos3D ret;
-		ret.x = y * p.z - z * p.y;
-		ret.y = z * p.x - x * p.z;
-		ret.z = x * p.y - y * p.x;
-		return ret;
-	}
+	Pos3D operator / (const Pos3D& p) const { return { y * p.z - z * p.y, z * p.x - x * p.z, x * p.y - y * p.x }; }
 	Pos3D operator + (const Pos3D& p) const { return { x + p.x, y + p.y, z + p.z }; }
 	Pos3D operator - (const Pos3D& p) const { return { x - p.x, y - p.y, z - p.z }; }
 	Pos3D operator * (const ld& n) const { return { x * n, y * n, z * n }; }
@@ -117,38 +68,20 @@ struct Pos3D {
 	friend std::ostream& operator << (std::ostream& os, const Pos3D& p) { os << p.x << " " << p.y << " " << p.z; return os; }
 };
 const Pos3D O3D = { 0, 0, 0 };
-const Pos3D MAXP3D = { INF, INF, INF };
 typedef std::vector<Pos3D> Polyhedron;
-std::vector<Pos3D> pos;
 struct Line3D {
 	Pos3D dir, p0;
-	Line3D(Pos3D DIR = Pos3D(0, 0, 0), Pos3D P0 = Pos3D(0, 0, 0)) : dir(DIR), p0(P0) {}
+	Line3D(Pos3D dir_ = Pos3D(0, 0, 0), Pos3D p0_ = Pos3D(0, 0, 0)) : dir(dir_), p0(p0_) {}
 };
 struct Plane {
 	ld a, b, c, d;
-	Plane(ld A = 0, ld B = 0, ld C = 0, ld D = 0) : a(A), b(B), c(C), d(D) {}
-	Plane& operator *= (const ld& s) { a *= s; b *= s; c *= s; d *= s; return *this; }
+	Plane(ld a_ = 0, ld b_ = 0, ld c_ = 0, ld d_ = 0) : a(a_), b(b_), c(c_), d(d_) {}
 	Pos3D norm() const { return Pos3D(a, b, c); };
-	Plane operator + (const ld& n) const { return { a, b, c, d + n }; }
-	Plane operator - (const ld& n) const { return { a, b, c, d - n }; }
-	Plane& operator += (const ld& n) { d += n; return *this; }
-	Plane& operator -= (const ld& n) { d -= n; return *this; }
 	friend std::istream& operator >> (std::istream& is, Plane& f) { is >> f.a >> f.b >> f.c >> f.d; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Plane& f) { os << f.a << " " << f.b << " " << f.c << " " << f.d; return os; }
 };
-typedef std::vector<Plane> Surfaces;
 Plane plane(const Pos3D& p, const ld& n) { return Plane(p.x, p.y, p.z, n); }
 ld sc[4];
-void update_sc(const Plane& p) {
-	ld angle1 = -atan2l(p.b, p.a);
-	ld dx = sqrtl(p.a * p.a + p.b * p.b);
-	ld angle2 = -atan2l(dx, p.c);
-	sc[0] = sinl(angle1);
-	sc[1] = cosl(angle1);
-	sc[2] = sinl(angle2);
-	sc[3] = cosl(angle2);
-	return;
-}
 void update_sc(const Pos3D& p) {
 	ld angle1 = -atan2l(p.y, p.x);
 	ld dx = sqrtl(p.x * p.x + p.y * p.y);
@@ -163,16 +96,8 @@ Pos3D rotate(const Pos3D& p) {
 	ld x = p.x * sc[1] - p.y * sc[0], y = p.x * sc[0] + p.y * sc[1], z = p.z;
 	return Pos3D(z * sc[2] + x * sc[3], y, z * sc[3] - x * sc[2]);
 }
-Pos convert(Pos3D p, const Pos3D& v) {
-	p -= v;
-	p = rotate(p);
-	return Pos(p.x, p.y);
-}
-Pos convert(Pos3D q, const Pos3D& p, const Pos3D& v) {
-	update_sc(p);
-	q -= v; q = rotate(q);
-	return Pos(q.x, q.y);
-}
+Pos convert(Pos3D p, const Pos3D& v) { p = rotate(p - v); return Pos(p.x, p.y); }
+Pos convert(Pos3D q, const Pos3D& p, const Pos3D& v) { update_sc(p); return convert(q, v); }
 Pos3D recover(const Pos& p2D, const Pos3D& v) {
 	ld x = p2D.x * -sc[3];
 	ld y = p2D.y;
@@ -185,9 +110,8 @@ ld dot(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return (d2 - d1) * (
 int ccw(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3, const Pos3D& norm) { return sign(cross(d1, d2, d3) * norm); }
 bool on_seg_strong(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return zero(cross(d1, d2, d3).mag()) && sign(dot(d1, d3, d2)) >= 0; }
 bool on_seg_weak(const Pos3D& d1, const Pos3D& d2, const Pos3D& d3) { return zero(cross(d1, d2, d3).mag()) && sign(dot(d1, d3, d2)) > 0; }
-Line3D L(const Pos3D& p1, const Pos3D& p2) { return { p2 - p1, p1 }; }
+Line3D line(const Pos3D& p1, const Pos3D& p2) { return { p2 - p1, p1 }; }
 ld dist(const Plane& s, const Pos3D& p) { return (s.norm() * p + s.d) / s.norm().mag(); }
-Pos3D offset(const Plane& s, const Pos3D& p) { ld d = dist(s, p); return s.norm().unit() * -d; }
 Pos3D intersection(const Plane& S, const Line3D& l) {
 	ld det = S.norm() * l.dir;
 	if (zero(det)) return { INF, INF, INF };
@@ -195,7 +119,7 @@ Pos3D intersection(const Plane& S, const Line3D& l) {
 	return l.p0 + (l.dir * t);
 }
 Pos3D intersection(const Plane& S, const Pos3D& p1, const Pos3D& p2, const bool& f = 0) {
-	Line3D l = L(p1, p2);
+	Line3D l = line(p1, p2);
 	Pos3D inx = intersection(S, l);
 	if (f && !on_seg_strong(p1, p2, inx)) return { INF, INF, INF };
 	return inx;
@@ -219,11 +143,10 @@ int intersection(const Plane& p1, const Plane& p2, Line3D& l) {
 	return 1;
 }
 Pos3D s2c(const ld& phi, const ld& psi) {//Spherical to Cartesian
-	ld lat = phi * PI / 180;
-	ld lon = psi * PI / 180;
+	ld lat = phi * PI / 180, lon = psi * PI / 180;
 	return Pos3D(cos(lon) * cos(lat), sin(lon) * cos(lat), sin(lat));
 }
-bool plane_circle_intersection(const Pos3D& perp, const Pos3D& a, std::vector<Pos3D>& inxs) {
+bool plane_circle_intersection(const Pos3D& perp, const Pos3D& a, Polyhedron& inxs) {
 	inxs.clear();
 	Pos3D vec = a - (perp * (perp * a));
 	if (zero(vec.mag())) return 0;
@@ -248,10 +171,7 @@ ld angle(const Pos3D& a, const Pos3D& b) {
 	Pos3D perp = (a / b).unit();
 	Pos3D X = a.unit();//X-axis
 	Pos3D Y = (perp / a).unit();//Y-axis
-	ld x = X * b;
-	ld y = Y * b;
-	ld th = atan2(y, x);
-	return th;
+	return angle(X, Y, b);
 }
 bool inner_check(const Pos3D& d1, const Pos3D& d2, const Pos3D& t, const Pos3D& nrm) {
 	return ccw(O3D, d1, t, nrm) >= 0 && ccw(O3D, d2, t, nrm) <= 0;
@@ -259,10 +179,7 @@ bool inner_check(const Pos3D& d1, const Pos3D& d2, const Pos3D& t, const Pos3D& 
 bool connectable(const Polyhedron& P, const Pos3D& a, const Pos3D& b, const int& i, const int& j) {
 	if (a == b) return 1;
 	Pos3D perp = (a / b).unit();
-	Pos3D X = a.unit();//X-axis
-	Pos3D Y = (perp / a).unit();//Y-axis
-	ld ang = angle(X, Y, b);
-	std::vector<Pos3D> inxs;
+	Polyhedron inxs;
 	for (int k = 0; k < N; k++) {
 		if (k == i || k == j) continue;
 		Pos3D axis = (P[k] / perp).unit();
@@ -310,11 +227,11 @@ ld dijkstra(const int& v, const int& g) {
 	}
 	return C[g];
 }
-Pos3D VX[LEN]; int vp;
+int vp;
 struct Node {
 	int i;
 	ld t;
-	Node(int i_ = 0, ld t_ = 0) : t(t_), i(i_) {}
+	Node(int i_ = 0, ld t_ = 0) : i(i_), t(t_) {}
 	bool operator < (const Node& x) const { return t < x.t; }
 };
 std::vector<Node> ND[25];
@@ -333,10 +250,8 @@ Polyhedron circle_circle_intersections(const Pos3D& p, const Pos3D& q) {
 	assert(t1 <= PI * .5); assert(t2 <= PI * .5);
 	if (t1 + t2 < ang || std::abs(t1 - t2) >= ang) return {};
 	ld d1 = cos(t1);
-	Pos3D n1 = p * d1;
-	Plane p1 = plane(p, -d1);
 	ld d2 = cos(t2);
-	Pos3D n2 = q * d2;
+	Plane p1 = plane(p, -d1);
 	Plane p2 = plane(q, -d2);
 	Line3D inx;
 	if (intersection(p1, p2, inx) != 1) return {};
@@ -347,8 +262,7 @@ Polyhedron circle_circle_intersections(const Pos3D& p, const Pos3D& q) {
 }
 ld spherical_pythagorean(const ld& a, const ld& b, const ld& A, const ld& B) {
 	assert(sin(a) > 0); assert(cos(b) > 0);
-	ld cosc = cos(a) / cos(b);
-	cosc = fit(cosc, -1, 1);
+	ld cosc = fit(cos(a) / cos(b), -1, 1);
 	ld c = acos(cosc);
 	ld sinC = sin(c) * sin(A) / sin(a);
 	return asin(sinC);
@@ -374,7 +288,7 @@ Polyhedron tangents(const Pos3D& p, const Pos3D& q) {
 		Pos3D lo = m.rodrigues_rotate(-C, q);
 		return { hi, lo };
 	}
-	assert(p.r * q.r);
+	assert(p.r && q.r);
 	ld ang = angle(p, q);
 	ld t1 = (ld)p.r / R;
 	ld t2 = (ld)q.r / R;
@@ -384,8 +298,7 @@ Polyhedron tangents(const Pos3D& p, const Pos3D& q) {
 	bool f = inner_check(p, rq) || inner_check(rq, p);
 	if (ang > (t1 + t2) && !f) {//inner tangent
 		if (p.r == q.r) {
-			Pos3D m = ((p + q) * .5).unit();
-			m.r = 0;
+			Pos3D m = ((p + q) * .5).unit(); m.r = 0;
 			Polyhedron ptan = tangents(m, p);
 			Polyhedron qtan = tangents(m, q);
 			if (ptan.size() == 2 && qtan.size() == 2) {
@@ -394,14 +307,12 @@ Polyhedron tangents(const Pos3D& p, const Pos3D& q) {
 			}
 		}
 		else {
-			ld A = PI * .5;
 			ld a_ = angle(p, q);
 			ld bp = (ld)p.r / R;
 			ld bq = (ld)q.r / R;
 			ld ap = atan2(sin(a_), (sin(bq) / sin(bp)) + cos(a_));
 			Pos3D perp = (p / q).unit();
-			Pos3D m = p.rodrigues_rotate(ap, perp);
-			m.r = 0;
+			Pos3D m = p.rodrigues_rotate(ap, perp); m.r = 0;
 			Polyhedron ptan = tangents(m, p);
 			Polyhedron qtan = tangents(m, q);
 			if (ptan.size() == 2 && qtan.size() == 2) {
@@ -412,9 +323,7 @@ Polyhedron tangents(const Pos3D& p, const Pos3D& q) {
 	}
 	if (ful < PI) {//outer tangent
 		if (p.r == q.r) {
-			Pos3D top = (q - p).unit();
-			Pos3D bot = (p - q).unit();
-			top.r = 0; bot.r = 0;
+			Pos3D top = (q - p).unit(); top.r = 0;
 			Polyhedron qtan = tangents(top, q);
 			Polyhedron ptan = tangents(top, p);
 			if (ptan.size() == 2 && qtan.size() == 2) {
@@ -425,13 +334,10 @@ Polyhedron tangents(const Pos3D& p, const Pos3D& q) {
 		else {
 			Pos3D L = p.r > q.r ? p : q;
 			Pos3D S = p.r > q.r ? q : p;
-			ld A = PI * .5;
 			ld a_ = angle(L, S);
 			ld bl = (ld)L.r / R;
 			ld bs = (ld)S.r / R;
 			ld as = atan2(sin(a_), (sin(bl) / sin(bs)) - cos(a_));
-			ld B = asin(sin(A) * sin(bs) / sin(as));
-			ld al = as + a_;
 			Pos3D perp = (L / S).unit();
 			Pos3D top = S.rodrigues_rotate(as, perp);
 			top.r = 0;
@@ -451,22 +357,16 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(13);
+	std::cout.precision(15);
 	std::cin >> R >> N;
 	Polyhedron P(N);
 	Pos3D s, e;
 	int phi, psi, r;
-	for (Pos3D& p : P) {
-		std::cin >> phi >> psi >> r;
-		p = s2c(phi, psi); p.r = r;
-	}
-	std::cin >> phi >> psi;
-	s = s2c(phi, psi); s.r = 0; s.i = -1;
-	std::cin >> phi >> psi;
-	e = s2c(phi, psi); e.r = 0; e.i = -2;
 	vp = 0;
-	VX[vp++] = s;
-	VX[vp++] = e;
+	for (Pos3D& p : P) { std::cin >> phi >> psi >> r; p = s2c(phi, psi); p.r = r; }
+	std::cin >> phi >> psi; s = s2c(phi, psi); s.r = 0; s.i = -1; vp++;
+	std::cin >> phi >> psi; e = s2c(phi, psi); e.r = 0; e.i = -2; vp++;
+	if (connectable(P, s, e, -1, -1)) { std::cout << angle(s, e) * R << "\n"; return; }
 	std::sort(P.begin(), P.end(), [&](const Pos3D& p, const Pos3D& q) -> bool { return p.r > q.r; });
 	Vbool F(N);
 	for (int i = 0; i < N; i++)
@@ -474,10 +374,8 @@ void solve() {
 			if (inner_check(P[i], P[j]) || P[i] == P[j]) F[j] = 1;
 	Polyhedron tmp;
 	for (int i = 0; i < N; i++) if (!F[i]) tmp.push_back(P[i]);
-	P = tmp;
-	N = P.size();
+	P = tmp; N = P.size();
 	for (int i = 0; i < N; i++) P[i].i = i;
-	if (connectable(P, s, e, -1, -1)) { std::cout << angle(s, e) * R << "\n"; return; }
 	Polyhedron inxs;
 	for (int i = 0; i < N; i++) {
 		ld t1 = (ld)P[i].r / R;
@@ -490,8 +388,7 @@ void solve() {
 				G[0].push_back(Info(vp, t));
 				G[vp].push_back(Info(0, t));
 				Pos ix = convert(p, P[i], n1);
-				Node n = Node(vp, ix.rad());
-				ND[i].push_back(n);
+				ND[i].push_back(Node(vp, ix.rad()));
 				vp++;
 			}
 		}
@@ -502,8 +399,7 @@ void solve() {
 				G[1].push_back(Info(vp, t));
 				G[vp].push_back(Info(1, t));
 				Pos ix = convert(p, P[i], n1);
-				Node n = Node(vp, ix.rad());
-				ND[i].push_back(n);
+				ND[i].push_back(Node(vp, ix.rad()));
 				vp++;
 			}
 		}
@@ -521,7 +417,7 @@ void solve() {
 				ch = convert(h_, n1);
 				lo = cl.rad();
 				hi = ch.rad();
-				if (eq(lo, hi)) { lo = norm(lo - TOL); hi = norm(hi + TOL); }
+				if (eq(lo, hi)) lo = norm(lo - TOL), hi = norm(hi + TOL);
 				if (lo < hi) ROT[i].push_back(Pos(lo, hi));
 				else ROT[i].push_back(Pos(0, hi)), ROT[i].push_back(Pos(lo, 2 * PI));
 				update_sc(P[j]);
@@ -529,7 +425,7 @@ void solve() {
 				ch = convert(l_, n2);
 				lo = cl.rad();
 				hi = ch.rad();
-				if (eq(lo, hi)) { lo = norm(lo - TOL); hi = norm(hi + TOL); }
+				if (eq(lo, hi)) lo = norm(lo - TOL), hi = norm(hi + TOL);
 				if (lo < hi) ROT[j].push_back(Pos(lo, hi));
 				else ROT[j].push_back(Pos(0, hi)), ROT[j].push_back(Pos(lo, 2 * PI));
 			}
@@ -539,10 +435,9 @@ void solve() {
 				for (int k = 0; k < sz; k += 2) {
 					Pos3D I = tans[k + 0], J = tans[k + 1];
 					if (connectable(P, I, J, i, j)) {
-						ld t = angle(I, J) * R;
+						ld t = angle(I, J) * R; Pos ix;
 						G[vp].push_back(Info(vp + 1, t));
 						G[vp + 1].push_back(Info(vp, t));
-						Pos ix; Node n;
 						ix = convert(I, P[i], n1);
 						ND[i].push_back(Node(vp, ix.rad()));
 						ix = convert(J, P[j], n2);
@@ -556,23 +451,21 @@ void solve() {
 	for (int i = 0, k; i < N; i++) {
 		std::sort(ROT[i].begin(), ROT[i].end());
 		std::sort(ND[i].begin(), ND[i].end());
-		int szr = ROT[i].size();
-		int szn = ND[i].size();
+		int sz = ND[i].size();
 		ld hi = 0, lo = 2 * PI;
 		ld rr = R * sin((ld)P[i].r / R);
 		for (const Pos& p : ROT[i]) hi = std::max(hi, p.HI), lo = std::min(lo, p.LO);
-		if (hi < ND[i][szn - 1].t && ND[i][0].t < lo) {
-			ld t = norm((ND[i][0].t - ND[i][szn - 1].t)) * rr;
-			G[ND[i][0].i].push_back(Info(ND[i][szn - 1].i, t));
-			G[ND[i][szn - 1].i].push_back(Info(ND[i][0].i, t));
+		if (hi < ND[i][sz - 1].t && ND[i][0].t < lo) {
+			ld t = norm((ND[i][0].t - ND[i][sz - 1].t)) * rr;
+			G[ND[i][0].i].push_back(Info(ND[i][sz - 1].i, t));
+			G[ND[i][sz - 1].i].push_back(Info(ND[i][0].i, t));
 		}
 		ROT[i].push_back(Pos(2 * PI, 2 * PI));
-		hi = 0;
-		k = 0;
+		hi = 0; k = 0;
 		for (const Pos& p : ROT[i]) {
 			if (p.LO > hi) {
-				while (k < szn && ND[i][k].t < hi) { k++; }
-				while (k < szn - 1 && ND[i][k + 1].t <= p.LO) {
+				while (k < sz && ND[i][k].t < hi) k++;
+				while (k < sz - 1 && ND[i][k + 1].t < p.LO) {
 					ld t = (ND[i][k + 1].t - ND[i][k].t) * rr;
 					G[ND[i][k].i].push_back(Info(ND[i][k + 1].i, t));
 					G[ND[i][k + 1].i].push_back(Info(ND[i][k].i, t));
