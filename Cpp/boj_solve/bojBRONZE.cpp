5f8d65e6d13cd2b13d7afd36f17f943de13be2cb
crimson <redcube231@gmail.com>
2025-01-13 10:34:13 +0900

    2501131034

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -1,39 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <vector>
 #include <cstring>
-#include <cassert>
 typedef long long ll;
 typedef long double ld;
-const int LEN = 1e6 + 1;
-ll sq(ll x) { return x * x; }
-int diff(int x, int y) { return std::abs(x - y); }
+ll A, B, C;
+ld N, M;
 
-int A, B, D, N, M;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(3);
-	N = M = 0;
-	std::cin >> A >> B;
-	D = 1;
-	while (A) {
-		int r = A % 10;
-		if (r == 5 || r == 6) { N += 5 * D; M += 6 * D; }
-		else { N += r * D; M += r * D; }
-		D *= 10;
-		A /= 10;
-	}
-	D = 1;
-	while (B) {
-		int r = B % 10;
-		if (r == 5 || r == 6) { N += 5 * D; M += 6 * D; }
-		else { N += r * D; M += r * D; }
-		D *= 10;
-		B /= 10;
-	}
-	//std::cout << N << " " << M << "\n";
+	std::cout.precision(9);
+	std::cin >> A >> B >> C;
+	N = (ld)(A * B) / C;
+	M = A / (ld)(B * C);
+	std::cout << std::max(N, M) << "\n";
+	return 0;
 }
