7b6834085cf5460318d7397d57059bfab7914873
crimson <redcube231@gmail.com>
2025-01-22 13:19:45 +0900

    25

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -4,17 +4,21 @@
 #include <cstring>
 typedef long long ll;
 typedef long double ld;
-ll A, B, C;
-ld N, M;
+ll N, S[55], C, T;
 
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(9);
-	std::cin >> A >> B >> C;
-	N = (ld)(A * B) / C;
-	M = A / (ld)(B * C);
-	std::cout << std::max(N, M) << "\n";
+	std::cin >> N;
+	C = 1;
+	while (N) {
+		T++;
+		if (N < C) C = 1;
+		N -= C;
+		C++;
+	}
+	std::cout << T << "\n";
 	return 0;
 }
