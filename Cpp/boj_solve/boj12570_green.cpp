6258e37c845fc2196d6f02697d04f6cc0c370912
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-01-29 01:57:38 +0900

    25

--- Cpp/boj_solve/boj12570_green.cpp
+++ Cpp/boj_solve/boj12570_green.cpp
@@ -24,43 +24,71 @@ inline ld norm(ld th) {
 	while (sign(th - 2 * PI) >= 0) th -= 2 * PI;
 	return th;
 }
-inline ld power(ld a, ll b) {
-	ld ret = 1;
-	while (b) {
-		if (b & 1) ret *= a;
-		a *= a;
-		b >>= 1;
-	}
-	return ret;
-}
 
-int T, N, M, K, R, X[LEN], Y[LEN];
-ld A[LEN];
+int T, N, M;
 struct Pos {
 	ld x, y;
-	Pos(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) {}
+	Pos(ld X_ = 0, ld y_ = 0) : x(X_), y(y_) {}
 	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
 	Pos operator * (const ld& n) const { return { x * n, y * n }; }
 	Pos operator / (const ld& n) const { return { x / n, y / n }; }
-	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
-	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos rot(const ld& the) const { return { x * cos(the) - y * sin(the), x * sin(the) + y * cos(the) }; }
+	ld operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+	ld operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { -x, -y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ld& scale) { x *= scale; y *= scale; return *this; }
+	Pos& operator /= (const ld& scale) { x /= scale; y /= scale; return *this; }
+	Pos rot(const ld& t) { return { x * cos(t) - y * sin(t), x * sin(t) + y * cos(t) }; }
 	ld Euc() const { return x * x + y * y; }
 	ld mag() const { return sqrt(Euc()); }
+	Pos unit() const { return *this / mag(); }
 	ld rad() const { return atan2(y, x); }
 	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
+Polygon graham_scan(Polygon C) {
+	Polygon H;
+	if (C.size() < 3) {
+		std::sort(C.begin(), C.end());
+		return C;
+	}
+	std::swap(C[0], *min_element(C.begin(), C.end()));
+	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
+		int ret = ccw(C[0], p, q);
+		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
+		return ret > 0;
+		}
+	);
+	C.erase(unique(C.begin(), C.end()), C.end());
+	int sz = C.size();
+	for (int i = 0; i < sz; i++) {
+		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
+			H.pop_back();
+		H.push_back(C[i]);
+	}
+	return H;
+}
+bool inner_check(const Polygon& H, const Pos& p) {
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) if (ccw(H[i], H[(i + 1) % sz], p) < 0) return 0;
+	return 1;
+}
 struct Circle {
 	Pos c;
 	ld r;
 	Circle(Pos c_ = Pos(), ld r_ = 0) : c(c_), r(r_) {}
-	bool operator == (const Circle& q) const { return c == q.c && r == q.r; }
-	bool operator >= (const Pos& p) const { return sign(r - (c - p).mag()) >= 0; }
+	bool operator > (const Pos& p) const { return sign(r - (c - p).mag()) > 0; }
+	bool outside(const Circle& q) const { return sign((c - q.c).Euc() - sq((ll)r + q.r)) >= 0; }
 	Pos p(const ld& t) const { return c + Pos(r, 0).rot(t); }
 	ld rad(const Pos& p) const { return (p - c).rad(); }
 	ld area(const ld& lo, const ld& hi) const { return (hi - lo) * r * r * .5; }
@@ -91,61 +119,55 @@ Vld intersections(const Circle& a, const Circle& b) {
 	ret.push_back(norm(rd + h));
 	return ret;
 }
-struct Event {
-	ld x;
-	int f;
-	bool operator < (const Event& o) const { return eq(x, o.x) ? f < o.f : sign(x - o.x) < 0; }
-};
-typedef std::vector<Event> Ve;
+ld green(const Pos& q, const Pos& p0, const Pos& p1, const Pos& p2) {
+	Circle c0 = Circle(p0, (p0 - q).mag());
+	Circle c1 = Circle(p1, (p1 - q).mag());
+	Circle c2 = Circle(p2, (p2 - q).mag());
+	ld a = 0;
+	Vld V = { 0, 2 * PI };
+	Vld i10 = intersections(c1, c0);
+	Vld i12 = intersections(c1, c2);
+	for (const ld& x : i10) V.push_back(x);
+	for (const ld& x : i12) V.push_back(x);
+	std::sort(V.begin(), V.end());
+	int sz = V.size();
+	for (int i = 0; i < sz - 1; i++) {
+		const ld& s = V[i], & e = V[i + 1];
+		ld m = (s + e) * .5;
+		Pos mid = c1.p(m);
+		if (c0 > mid && c2 > mid) a += c1.green(s, e);
+	}
+	return a;
+}
+ld A[LEN];
 void query(const int& t) {
 	std::cin >> N >> M;
-	Circles C(N);
-	for (Circle& c : C) std::cin >> c.c.x >> c.c.y;
-	Polygon P(M);
-	for (Pos& p : P) std::cin >> p;
+	Polygon P(N); for (Pos& p : P) std::cin >> p;
+	Polygon Q(M); for (Pos& p : Q) std::cin >> p;
 	memset(A, 0, sizeof A);
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			C[j].r = (P[i] - C[i].c).mag();
+	for (int j = 0; j < M; j++) {
+		const Pos& q = Q[j];
+		Polygon R = P;
+		for (Pos& p : R) p -= q;
+		Polygon H = graham_scan(R);
+		if (inner_check(H, O)) { A[j] = 0; continue; }
+		std::sort(R.begin(), R.end(), [&](const Pos& p, const Pos& q) -> bool {
+			int tq = sign(p / q);
+			return !tq ? p.Euc() < q.Euc() : tq > 0;
+			});
+		Polygon S;
+		for (int i = 0; i < N; i++) {
+			while (S.size() > 1 && ccw(S[S.size() - 2], S[S.size() - 1], R[i]) > 0)
+				S.pop_back();
+			S.push_back(R[i]);
 		}
-	}
-	for (int i = 0; i < N; i++) {
-		Ve V = { { 0, 1 }, { PI * 2, -1 } };
-		Vld P = { 0, PI * 2 };
-		for (int j = 0; j < N; j++) {
-			if (j == i) continue;
-			ll d1 = sq((ll)X[i] - X[j]) + sq((ll)Y[i] - Y[j]);
-			ll d2 = sq(R * 2ll);
-			if (d1 > d2) continue;
-			Vld inxs = intersections(C[i], C[j]);
-			if (inxs.size() < 2) continue;
-			ld lo = inxs[0], hi = inxs[1];
-			if (lo < hi) {
-				V.push_back({ lo, 1 });
-				V.push_back({ hi, -1 });
-			}
-			else {
-				V.push_back({ 0, 1 });
-				V.push_back({ hi, -1 });
-				V.push_back({ lo, 1 });
-				V.push_back({ PI * 2, -1 });
-			}
-			P.push_back(hi); P.push_back(lo);
-		}
-		std::sort(V.begin(), V.end());
-		std::sort(P.begin(), P.end());
-		P.erase(unique(P.begin(), P.end()), P.end());
-		int szp = P.size(), szv = V.size(), cnt = 0;
-		for (int j = 0, k = 0; j < szp - 1; j++) {
-			const ld& s = P[j], e = P[j + 1];
-			while (k < szv && eq(V[k].x, s)) { cnt += V[k].f; k++; }
-			ld a = C[i].green(s, e);
-			if (cnt > 0) A[i] += a;
-			
+		int sz = S.size();
+		for (int i = 0; i < sz; i++) {
+			A[j] += green(O, S[(i - 1 + sz) % sz], S[i], S[(i + 1) % sz]);
 		}
 	}
 	std::cout << "Case #" << t << ": ";
-	for (int i = 0; i < M; i++) std::cout << A[i] << "\n";
+	for (int j = 0; j < M; j++) std::cout << A[j] << " ";
 	std::cout << "\n";
 	return;
 }
@@ -153,7 +175,7 @@ void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(15);
+	std::cout.precision(9);
 	std::cin >> T;
 	for (int t = 1; t <= T; t++) query(t);
 }
