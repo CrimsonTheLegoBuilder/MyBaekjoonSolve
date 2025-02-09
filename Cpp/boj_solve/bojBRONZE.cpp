77ef1113b9eca4c399e0366d5c6173f50747e80f
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-02-10 00:10:14 +0900

    2502100010

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -4,24 +4,17 @@
 #include <cstring>
 typedef long long ll;
 typedef long double ld;
-ll N, S[55], C, T;
+ll N, A, B, C, D, T, R, G, P, U, V, W, K, X, L;
+ld F;
 
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(9);
-	std::cin >> N;
-	while (N--) {
-		std::string s;
-		std::cin >> s;
-		int t = s.size();
-		int cnt = 0;
-		for (int i = 0; i < t; i++) {
-			if (s[i] == 'U') cnt++;
-			else break;
-		}
-		std::cout << cnt << "\n";
-	}
+	std::cin >> N >> U >> L;
+	if (N >= 1000 && (U >= 8000 || L >= 260)) std::cout << "Very Good\n";
+	else if (N >= 1000) std::cout << "Good\n";
+	else std::cout << "Bad\n";
 	return 0;
 }
