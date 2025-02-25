f02fff31af661765a9b76226cd01d286d15b37f4
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-02-26 00:23:22 +0900

    25

--- Cpp/boj_solve/boj33519_stack.cpp
+++ Cpp/boj_solve/boj33519_stack.cpp
@@ -31,8 +31,11 @@ Polygon S;
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
-bool invisible(Pos p1, Pos p2, const Pos& q) {
+bool invisible(Pos p1, Pos p2, const Pos& q, const bool& f = 1) {
 	if (p1.x > p2.x) std::swap(p1, p2);
+	if (f == RIGHT && q.d == RIGHT) {
+		return p1.x < q.x && q.x <= p2.x && ccw(p1, p2, q) < 0;
+	}
 	return p1.x <= q.x && q.x <= p2.x && ccw(p1, p2, q) < 0;
 }
 void solve() {
@@ -43,32 +46,61 @@ void solve() {
 	P[0] = P[1] + Pos(-1);
 	P[N + 1] = P[N] + Pos(1);
 	S.clear(); S.push_back(P[0]); S.push_back(P[1]);
+	if ((P[2] - P[1]).x < 0) S.back().d = LEFT;
 	bool fvis = 1, bvis = 1, rvs = 0;
 	for (int i = 1; i <= N; i++) {
 		Pos p0 = P[i - 1], p1 = P[i], p2 = P[i + 1];
 		Pos v0 = p1 - p0, v1 = p2 - p1;
 		int dir = sign(v1.x);
-		int hide = v0 / v1;
+		ll tq = v0 / v1;
 		if (fvis && bvis) {
 			if (dir < 0) {//move backward
 				p2.d = LEFT;
-				if (!rvs && hide < 0) {
+				if (!rvs && tq < 0) {
 					rvs = 1;
 					fvis = 0;
 					continue;
 				}
 				rvs = 1;
-				while (S.size() && invisible(p1, p2, S.back())) S.pop_back();
+				while (S.size() && invisible(p1, p2, S.back(), LEFT)) S.pop_back();
 			}
-			else if (!dir) {
+			else if (!dir) {//move vertical
+
 				p2.d = LEFT;
 			}
+			else if (dir > 0) {//move forward
+				if (rvs && tq > 0) {
+					p1.d = RIGHT;
+					S.push_back(p1);
+					rvs = 0;
+					bvis = 0;
+					continue;
+				}
+				if (S.size() && rvs && S.back().x < p1.x) S.push_back(p1);
+				rvs = 0;
+				if (S.size() < 1 || S.back().x < p2.x) S.push_back(p2);
+			}
 		}
-		while (S.size()) {
-
+		else if (!fvis) {
+			if (S.back().x <= p2.x) {
+				rvs = 0;
+				fvis = 1;
+				p2.d = LEFT;
+				S.push_back(p2);
+			}
 		}
-		S.push_back(P[i]);
+		else if (!bvis) {
+			if (S.back().x > p2.x) {
+				S.pop_back();
+				rvs = 1;
+				bvis = 1;
+				while (S.size() && invisible(p1, p2, S.back())) S.pop_back();
+			}
+		}
 	}
+	int cnt = 0;
+	for (const Pos& p : S) cnt += p.d != 0;
+	std::cout << cnt << "\n";
 	return;
 }
 int main() { solve(); return 0; }//boj33519 Andrew the Diver
