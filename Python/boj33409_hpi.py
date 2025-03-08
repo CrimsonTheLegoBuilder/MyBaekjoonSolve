96004891cbb3f22cadde5ea8157c03c1ee8852c9
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-03-08 15:41:38 +0900

    25

--- Python/boj33409_hpi.py
+++ Python/boj33409_hpi.py
@@ -9,22 +9,28 @@ def read_fraction() -> tuple:
     return Fraction(p_x, q_x), Fraction(p_y, q_y)
 
 
-def cross(d1, d2, d3) -> Fraction:
+def cross(d1: tuple, d2: tuple, d3: tuple) -> Fraction:
     return (d2[0] - d1[0]) * (d3[1] - d2[1]) - (d2[1] - d1[1]) * (d3[0] - d2[0])
 
 
-def intersection(p1_, p2_, q1_, q2_) -> tuple:
+def intersection(p1_: tuple, p2_: tuple, q1_: tuple, q2_: tuple) -> tuple:
     a1 = cross(q1_, q2_, p1_)
-    a2 = -cross(q1_, q2_, p2_)
+    a2 = cross(q1_, q2_, p2_) * Fraction(-1, 1)
     x_: Fraction = p1_[0] * a2 + p2_[0] * a1
     y_: Fraction = p1_[1] * a2 + p2_[1] * a1
     den: Fraction = a1 + a2
     return Fraction(x_, den), Fraction(y_, den)
 
 
-def cmp_(t1_, t2_) -> int:
-    p1_ = (t1_[0][0] - t1_[1][0], t1_[0][1] - t1_[1][1])
-    p2_ = (t2_[0][0] - t2_[1][0], t2_[0][1] - t2_[1][1])
+def collinear(l1_: tuple, l2_: tuple) -> bool:
+    tq1: Fraction = cross(l1_[0], l1_[1], l2_[0])
+    tq2: Fraction = cross(l1_[0], l1_[1], l2_[1])
+    return tq1 == Fraction(0, 1) and tq2 == Fraction(0, 1)
+
+
+def cmp_(t1_: tuple, t2_: tuple) -> int:
+    p1_: tuple = (t1_[0][0] - t1_[1][0], t1_[0][1] - t1_[1][1])
+    p2_: tuple = (t2_[0][0] - t2_[1][0], t2_[0][1] - t2_[1][1])
     f1: bool = (0, 0) < p1_
     f2: bool = (0, 0) < p2_
     if f1 != f2:
@@ -41,66 +47,62 @@ def cmp_(t1_, t2_) -> int:
 if __name__ == "__main__":
     N = int(INPUT())
     K: int = int((N * 2)**.5 + 1)
-    # print(K)
 
     H: list = []
     for i in range(N):
         p: tuple = read_fraction()
         H.append(p)
 
-    # print(len(H))
-
     L: list = []
     V: list = []
     for i in range(N):
         for j in range(i + 1, N):
-            if j == i:
-                continue
-            I: tuple = H[i]
-            J: tuple = H[j]
-            x: Fraction = J[0] - I[0]
-            y: Fraction = J[1] - I[1]
-            t: tuple = x, y
-            f: bool = False
-            for v in V:
-                det: Fraction = t[0] * v[1] - t[1] * v[0]
-                if det == 0:
-                    f = True
-                    break
-            if f:
-                continue
-            r: int = 0
-            z: int = 0
-            l: int = 0
-            for k in range(N):
-                if k == i or k == j:
-                    continue
-                tq: Fraction = cross(I, J, H[k])
-                if tq == 0:
-                    z += 1
-                if tq > 0:
-                    l += 1
-                if tq < 0:
-                    r += 1
-            # print("z", z, "l", l, "r", r)
-            if z == K - 3:
-                if l < r:
-                    I, J = J, I
-                x: Fraction = J[0] - I[0]
-                y: Fraction = J[1] - I[1]
-                L.append((I, J))
-                V.append((x, y))
+            p0: tuple = H[i]
+            p1: tuple = H[j]
+            if p1 < p0:
+                p0, p1 = p1, p0
+            L.append((p0, p1))
 
-    # print(len(L))
-    # print(L)
-    R: list = []
     L.sort(key=cmp_to_key(cmp_))
-    for i in range(len(L)):
-        I: tuple = L[i - 1]
-        J: tuple = L[i]
-        R.append(intersection(I[0], I[1], J[0], J[1]))
 
-    # print(R)
+    V: list = []
+    sz: int = len(L)
+    cnt: int = 1
+    M: int = (K - 1) * (K - 2) // 2
+    for i in range(sz):
+        l0: tuple = L[i]
+        l1: tuple = L[(i + 1) % sz]
+        if collinear(l0, l1):
+            cnt += 1
+        else:
+            if cnt == M:
+                r: int = 0
+                l: int = 0
+                p0: tuple = l0[0]
+                p1: tuple = l0[1]
+                for j in range(N):
+                    tq: Fraction = cross(p0, p1, H[j])
+                    if tq == 0:
+                        continue
+                    elif tq > Fraction(0, 1):
+                        l += 1
+                    else:
+                        r += 1
+                if l < r:
+                    p0, p1 = p1, p0
+                V.append((p0, p1))
+                cnt = 1
+            else:
+                cnt = 1
+                continue
+
+    R: list = []
+
+    V.sort(key=cmp_to_key(cmp_))
+    for i in range(len(V)):
+        l0: tuple = V[i - 1]
+        l1: tuple = V[i]
+        R.append(intersection(l0[0], l0[1], l1[0], l1[1]))
 
     print(len(R))
     for ret in R:
