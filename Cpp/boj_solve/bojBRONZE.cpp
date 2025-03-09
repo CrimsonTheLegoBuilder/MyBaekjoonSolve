76e5d3256120f8b1a746c2e599fad4336a674d41
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-03-09 22:27:50 +0900

    25

--- Cpp/boj_solve/bojBRONZE.cpp
+++ Cpp/boj_solve/bojBRONZE.cpp
@@ -4,19 +4,17 @@
 #include <cstring>
 typedef long long ll;
 typedef long double ld;
-int T, X, N, M, Ki, Ai;
-int A[1005];
+ll T, N, W;
 
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(9);
-	std::cin >> X >> N;
-	for (int i = 1; i < N; i++) {
-		if (i & 1) X = (X / 2) ^ 6;
-		else X = (X * 2) ^ 6;
-	}
-	std::cout << X << "\n";
+	std::cin >> N;
+	T = 1;
+	for (ll i = 1; i <= N; i++) T *= i;
+	W = 60ll * 60 * 24 * 7;
+	//std::cout << T / W << "\n";
 	return 0;
 }
