24ec3d03632355d5e9124de1b444441e0266c6e9
crimson <redcube231@gmail.com>
2025-03-18 07:57:09 +0900

    25

--- Cpp/boj_solve/___now_solving.cpp
+++ Cpp/boj_solve/___now_solving.cpp
@@ -7,12 +7,4 @@
 #include <vector>
 
 int a, b, x, y, T, A, B;
-int main() {
-	std::cin.tie(0)->sync_with_stdio(0);
-	std::cout.tie(0);
-	std::cin >> a >> x >>b >> y >> T;
-	A = a + 21 * x * std::max(0, T - 30);
-	B = b + 21 * y * std::max(0, T - 45);
-	std::cout << A << " " << B << "\n";
-	return 0;
-}
+int main() { return 0; }//boj5418 - 3Dcnvx 5774-dijk
