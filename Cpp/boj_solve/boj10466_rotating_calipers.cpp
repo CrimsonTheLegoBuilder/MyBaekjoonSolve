18113e7f8c7da0315712b26073b3cfbb7f675919
crimson <redcube231@gmail.com>
2025-03-20 15:14:05 +0900

    25

--- Cpp/boj_solve/boj10466_rotating_calipers.cpp
+++ Cpp/boj_solve/boj10466_rotating_calipers.cpp
@@ -21,39 +21,17 @@ struct Pos {
 	ll Euc() const { return x * x + y * y; }
 	ld mag() const { return hypot(x, y); }
 };
-std::vector<Pos> C, H;
+std::vector<Pos> H;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
-std::vector<Pos> monotone_chain(std::vector<Pos>& C) {
-	std::vector<Pos> H;
-	std::sort(C.begin(), C.end());
-	if (C.size() <= 2) {
-		for (const Pos& p : C) H.push_back(p);
-		return H;
-	}
-	for (int i = 0; i < C.size(); i++) {
-		while (H.size() > 1 && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
-			H.pop_back();
-		H.push_back(C[i]);
-	}
-	H.pop_back();
-	int sz = H.size() + 1;
-	for (int i = C.size() - 1; i >= 0; i--) {
-		while (H.size() > sz && cross(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
-			H.pop_back();
-		H.push_back(C[i]);
-	}
-	H.pop_back();
-	return H;
-}
 void init() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(6);
 	std::cin >> N;
-	C.resize(N);
+	H.resize(N);
 	for (int i = 0; i < N; i++) std::cin >> C[i].x >> C[i].y;
 	//H = monotone_chain(C);
 	//N = H.size();
@@ -61,19 +39,23 @@ void init() {
 }
 void rotating_calipers() {
 	init();
-	std::swap(C[0], *min_element(C.begin(), C.end()));
-	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) {
-		ll ret = cross(C[0], p, q);
+	std::swap(H[0], *min_element(H.begin(), H.end()));
+	std::sort(H.begin() + 1, H.end(), [&](const Pos& p, const Pos& q) {
+		ll ret = cross(H[0], p, q);
 		if (ret != 0) return ret > 0;
-		return (C[0] - p).Euc() < (C[0] - q).Euc();
+		return (H[0] - p).Euc() < (H[0] - q).Euc();
 	});
+	int S = -1;
 	for (int i = 0; i < N; i++) {
-		while (H.size() >= 2 && cross(H[H.size() - 2], H.back(), C[i]) <= 0) {
-			H.pop_back();
+		//while (H.size() >= 2 && cross(H[H.size() - 2], H.back(), C[i]) <= 0) {
+		while (S >= 1 && cross(H[S - 2], H[S - 1], H[i]) <= 0) {
+			//H.pop_back();
+			S--;
 		}
-		H.push_back(C[i]);
+		//H.push_back(C[i]);
+		H[++S] = H[i];
 	}
-	N = H.size();
+	N = S;
 	if (N == 2) { std::cout << "0\n"; return; }
 
 	int R{ 0 }, U{ 0 }, L{ 0 };
