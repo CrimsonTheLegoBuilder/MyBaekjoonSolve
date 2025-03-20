b3f433f6fe0cced212eb23e6a52167a907f977e9
crimson <redcube231@gmail.com>
2025-03-20 15:28:40 +0900

    25

--- Cpp/boj_solve/boj10466_rotating_calipers.cpp
+++ Cpp/boj_solve/boj10466_rotating_calipers.cpp
@@ -10,16 +10,15 @@ const ll INF = 1e17;
 
 int N;
 struct Pos {
-	ll x, y;
-	Pos(ll X = 0, ll Y = 0) : x(X), y(Y) {}
+	int x, y;
+	Pos(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	ll operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
-	ll operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
-	Pos& operator *= (const ll& scale) { x *= scale; y *= scale; return *this; }
-	ll Euc() const { return x * x + y * y; }
+	ll operator * (const Pos& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
 	ld mag() const { return hypot(x, y); }
-}; std::vector<Pos> H;
+} H[100000];
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
@@ -28,36 +27,29 @@ void init() {
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(6);
-	std::cin >> N; H.resize(N);
+	std::cin >> N;
 	for (int i = 0; i < N; i++) std::cin >> H[i].x >> H[i].y;
 	return;
 }
 void rotating_calipers() {
 	init();
-	std::swap(H[0], *min_element(H.begin(), H.end()));
-	std::sort(H.begin() + 1, H.end(), [&](const Pos& p, const Pos& q) {
+	std::swap(H[0], *std::min_element(H, H + N));
+	std::sort(H + 1, H + N, [&](const Pos& p, const Pos& q) {
 		ll ret = cross(H[0], p, q);
 		return ret == 0 ? (H[0] - p).Euc() < (H[0] - q).Euc() : ret > 0;
-	});
+		});
 	int S = -1;
 	for (int i = 0; i < N; i++) {
-		//while (H.size() >= 2 && cross(H[H.size() - 2], H.back(), C[i]) <= 0) {
-		while (S > 1 && cross(H[S - 1], H[S], H[i]) <= 0) {
-			//H.pop_back();
-			S--;
-		}
-		//H.push_back(C[i]);
+		while (S > 1 && cross(H[S - 1], H[S], H[i]) <= 0) S--;
 		H[++S] = H[i];
 	}
 	N = S + 1;
 	if (N == 2) { std::cout << "0\n"; return; }
-
 	int R{ 0 }, U{ 0 }, L{ 0 };
 	for (int j = 0; j < N; j++) if (dot(H[0], H[1], H[j], H[(j + 1) % N]) <= 0) { R = j; break; }
 	for (int k = R; k < N; k++) if (cross(H[0], H[1], H[k], H[(k + 1) % N]) <= 0) { U = k; break; }
 	for (int l = U; l <= N; l++) if (dot(H[0], H[1], H[l % N], H[(l + 1) % N]) >= 0) { L = l; break; }
-
-	ld MIN{ INF };
+	ld MIN = INF;
 	for (int i = 0; i < N; i++) {
 		while (dot(H[i], H[(i + 1) % N], H[R % N], H[(R + 1) % N]) > 0) R++;
 		while (cross(H[i], H[(i + 1) % N], H[U % N], H[(U + 1) % N]) > 0) U++;
@@ -71,39 +63,3 @@ void rotating_calipers() {
 	return;
 }
 int main() { rotating_calipers(); return 0; }//boj10466
-
-//void solve() {
-//	std::cin.tie(0)->sync_with_stdio(0);
-//	std::cout.tie(0);
-//	std::cout << std::fixed;
-//	std::cout.precision(6);
-//	//freopen("boj10466.in", "r", stdin);
-//	//freopen("boj10466.txt", "w", stdout);
-//	while (std::cin >> N) {
-//		C.resize(N);
-//		for (int i = 0; i < N; i++) std::cin >> C[i].x >> C[i].y;
-//		H = monotone_chain(C);
-//		N = H.size();
-//		if (N == 2) { std::cout << "0\n"; continue; }
-//
-//		int R{ 0 }, U{ 0 }, L{ 0 };
-//		for (int j = 0; j < N; j++) if (dot(H[0], H[1], H[j], H[(j + 1) % N]) <= 0) { R = j; break; }
-//		for (int k = R; k < N; k++) if (cross(H[0], H[1], H[k], H[(k + 1) % N]) <= 0) { U = k; break; }
-//		for (int l = U; l <= N; l++) if (dot(H[0], H[1], H[l % N], H[(l + 1) % N]) >= 0) { L = l; break; }
-//
-//		ld MIN{ INF };
-//		for (int i = 0; i < N; i++) {
-//			while (dot(H[i], H[(i + 1) % N], H[R % N], H[(R + 1) % N]) > 0) R++;
-//			while (cross(H[i], H[(i + 1) % N], H[U % N], H[(U + 1) % N]) > 0) U++;
-//			while (dot(H[i], H[(i + 1) % N], H[L % N], H[(L + 1) % N]) < 0) L++;
-//			ld h = cross(H[i], H[(i + 1) % N], H[U % N]) / (H[i] - H[(i + 1) % N]).mag();
-//			ld w = dot(H[i], H[(i + 1) % N], H[L % N], H[R % N]) / (H[i] - H[(i + 1) % N]).mag();
-//			MIN = std::min(MIN, h * w);
-//		}
-//		ll ans = MIN;
-//		if (MIN - ans > .5) ans++;
-//		std::cout << ans << "\n";
-//	}
-//	return;
-//}
-//int main() { solve(); return 0; }//boj10466
