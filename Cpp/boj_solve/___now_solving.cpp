50e856cdc1a0804f9e92ce7d4057ea29283c21c8
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-03-23 15:33:35 +0900

    25

--- Cpp/boj_solve/___now_solving.cpp
+++ Cpp/boj_solve/___now_solving.cpp
@@ -1,5 +1,38 @@
 //freopen("../../../input_data//", "r", stdin);
 //freopen("../../../input_data//", "w", stdout);
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <algorithm>
+//#include <vector>
+//#include <cmath>
+//#include <cstring>
+//#include <cassert>
+//typedef long long ll;
+////typedef long double ld;
+//typedef double ld;
+//typedef std::vector<int> Vint;
+//const ld INF = 1e21;
+//const ld TOL = 1e-9;
+//const ld PI = acos(-1);
+//const int LEN = 1e5 + 10;
+//inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
+//inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+//inline bool zero(const ld& x) { return !sign(x); }
+//
+//int N, K, Q;
+//void solve() {
+//	std::cin.tie(0)->sync_with_stdio(0);
+//	std::cout.tie(0);
+//	std::cout << std::fixed;
+//	std::cout.precision(13);
+//
+//	std::cin >> N;
+//
+//	return;
+//}
+//int main() { solve(); return 0; }//boj30839 29688
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
@@ -10,25 +43,17 @@
 typedef long long ll;
 //typedef long double ld;
 typedef double ld;
-typedef std::vector<int> Vint;
-const ld INF = 1e21;
-const ld TOL = 1e-9;
-const ld PI = acos(-1);
-const int LEN = 1e5 + 10;
-inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
-inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
-inline bool zero(const ld& x) { return !sign(x); }
 
-int N, K, Q;
-void solve() {
+int N, D, A[30], T;
+std::string s;
+int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(13);
-
-	std::cin >> N;
-
-	return;
+	std::cout.precision(10);
+	std::cin >> N >> D;
+	for (int i = 0; i < N; i++) std::cin >> A[i], T += A[i];
+	D /= T;
+	for (int i = 0; i < N; i++) std::cout << A[i] * D << "\n";
+	return 0;
 }
-int main() { solve(); return 0; }//boj30839 29688
-
