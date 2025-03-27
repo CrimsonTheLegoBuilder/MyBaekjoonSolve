f870ba34fa0278c8f57375cbbc6a9d103cc52096
CrimsonTheLegoBuildergit config --global user.name CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-03-27 19:55:25 +0900

    25

--- Cpp/boj_solve/boj3123.cpp
+++ Cpp/boj_solve/boj3123.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 bool L[1001][1001];
 double A;
-int main() {
+main() {
 	int X, Y, N, x, y, s, i, j;
 	scanf("%d%d%d", &X, &Y, &N);
 	for (i = 0; i < N; i++) {
@@ -18,6 +18,36 @@ int main() {
 			}
 			else if (s) A += 0.785398;//pi/4
 		}
+	}
+	printf("%.4lf", A);
+}
+
+#include <stdio.h>
+bool L[1001][1001];
+double A, PI = 3.141592653, a1 = 0.04338852250, a2 = 0.17121331409;
+void add(const int& x, const int& y, const int& dx, const int& dy) {
+	bool d, r, l;
+	d = L[y + dy][x + dx];
+	r = L[y + dy][x];
+	l = L[y][x + dx];
+	if (r && l) return;
+	else if (!d && !r && !l) A += PI * .25;
+	else if ((d && r) || (d && l)) A += a1;
+	else if (!d && (r || l)) A += a2;
+	else if (d && (!r && !l)) A += 1 - (PI * .25);
+	return;
+}
+main() {
+	int X, Y, N, x, y, i;
+	scanf("%d%d%d", &X, &Y, &N);
+	for (i = 0; i < N; i++) {
+		scanf("%d%d", &x, &y);
+		if (L[y][x]) continue;
+		L[y][x] = 1;
+		if (x < X && y < Y) add(x, y, 1, 1);
+		if (x < X && 0 < y) add(x, y, 1, -1);
+		if (0 < x && y < Y) add(x, y, -1, 1);
+		if (0 < x && 0 < y) add(x, y, -1, -1);
 	}
 	printf("%.4lf", A);
 }
